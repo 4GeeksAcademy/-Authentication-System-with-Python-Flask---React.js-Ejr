4832445ffb40288c524fd5077ec233c14e66ad98
Anmora96 <anmora.2396@gmail.com>
2021-03-09 07:37:02 +0000

    Se eliminan codigos innecesarios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,20 +9,6 @@ export const Home = () => {
 
 	return (
 		<Container>
-			{/* <div className="bg-color text-center">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImageUrl} />
-				</p>
-				<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-				<p>
-					This boilerplate comes with lots of documentation:{" "}
-					<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-						Read documentation
-					</a>
-				</p>
-			</div> */}
-
 			<h1>Hola</h1>
 		</Container>
 	);
