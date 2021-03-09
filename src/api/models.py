c76e8e419aa7e4ae8c157d9f7d8fe25a184d1b72
orv0670 <orv0670@gmail.com>
2021-03-09 18:29:19 +0000

    modelos terminados y rutas en construccion

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,10 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    respuesta_de_seguridad = db.Column(db.String(120), unique=False, nullable=False)
+    mi_pasaporte = db.relationship('Mi_pasaporte', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +17,64 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "nombre": self.nombre
             # do not serialize the password, its a security breach
+        }
+
+class Playa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), unique=True, nullable=False)
+    descripcion = db.Column(db.String(300), unique=False, nullable=False)
+    provincia = db.Column(db.String(300), unique=False, nullable=False)
+    contacto = db.Column(db.String(300), unique=False, nullable=False)
+    imagen = db.Column(db.String(1000), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Playa %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "provincia": self.provincia,
+            "contacto": self.contacto
+        }
+
+class Montana(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), unique=True, nullable=False)
+    descripcion = db.Column(db.String(300), unique=False, nullable=False)
+    provincia = db.Column(db.String(300), unique=False, nullable=False)
+    contacto = db.Column(db.String(300), unique=False, nullable=False)
+    imagen = db.Column(db.String(1000), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Montana %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "provincia": self.provincia,
+            "contacto": self.contacto
+        }
+
+class Mi_pasaporte(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nombre = db.Column(db.String(250), unique=False, nullable=False)
+    #tipo playa o montaña
+    tipo = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Mi_pasaporte %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "nombre": self.nombre,
+            "tipo": self.nombre
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,23 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+#import os
+from flask import Flask, request, jsonify, url_for, Blueprint, json
+from api.models import db, User, Playa, Montana, Mi_pasaporte
 from api.utils import generate_sitemap, APIException
+#from flask_swagger import swagger
+#from flask_cors import CORS
+#from admin import setup_admin
+import datetime
+#from werkzeug.security import generate_password_hash, check_password_hash
+#from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
+#CORS(api)
+#jwt = JWTManager(api)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    list_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(list_users), 200
