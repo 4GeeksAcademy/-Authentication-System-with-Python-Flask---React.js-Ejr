558c7afb8e3051d7f6a50d03ad4718489d3e3d2f
yun joung <yunjoungji@gmail.com>
2021-03-10 19:14:46 -0300

    agregar guia de diseno

---
+++ src/front/img/image.js
@@ -0,0 +1,13 @@
+import logoBlanco from "./cotecBlanco.png"
+import logoAzul from "./cotecAzul.png"
+import logoGris from "./cotecGris.png"
+import freeman from "./freemen.png"
+import backGround from "./backGround.png"
+
+export {
+    logoBlanco,
+    logoAzul,
+    logoGris,
+    backGround,
+    freeman
+}
