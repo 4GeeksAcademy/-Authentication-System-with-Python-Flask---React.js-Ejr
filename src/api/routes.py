9c960334b796f8fdac18e8f1ddf1bbc98a9747ca
orv0670 <orv0670@gmail.com>
2021-03-10 16:31:58 +0000

    problema del flask_jwt_extended solucionado

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,20 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-#import os
 from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User, Playa, Montana, Mi_pasaporte
 from api.utils import generate_sitemap, APIException
-#from flask_swagger import swagger
-#from flask_cors import CORS
-#from admin import setup_admin
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-#from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
-#CORS(api)
-#jwt = JWTManager(api)
+
 
 #bloque de GET's
 @api.route('/users', methods=['GET'])
@@ -130,7 +124,7 @@ def registro():
         return jsonify({"exito!": "gracias, su regristro fue exitoso", "status": "true"}), 200
 
 #bloque de metodo DELETE
-@api.route('/mi_pasaporte/<int:user_id>/<nombre_pymes>')
+#@api.route('/mi_pasaporte/<int:user_id>/<nombre_pymes>')
 #@jwt_required()
-def borrar_mi_pasaporte()
+#def borrar_mi_pasaporte():
 
