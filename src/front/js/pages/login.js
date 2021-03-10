54e7841fdbdef5000167b217bc9ef6b68c2689b3
Anmora96 <anmora.2396@gmail.com>
2021-03-10 07:28:31 +0000

    Se arregla parte del codigo

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -6,7 +6,7 @@ export const Login = () => {
 	return (
 		<Carousel className="Carousel">
 			<Carousel.Item interval={3000}>
-				<Link to="/character">
+				<Link to="##">
 					<img
 						className="d-block w-100"
 						src="https://i.pinimg.com/originals/f3/23/55/f32355d4790b1083ba03bfa535c497a7.jpg"
@@ -19,7 +19,7 @@ export const Login = () => {
 				</Link>
 			</Carousel.Item>
 			<Carousel.Item interval={3000}>
-				<Link to="/planets">
+				<Link to="##">
 					<img className="d-block w-100" src="https://i.imgur.com/fiKu58y.jpg" alt="Planets" />
 					<Carousel.Caption className="shadow">
 						<h3>Planetas</h3>
@@ -28,7 +28,7 @@ export const Login = () => {
 				</Link>
 			</Carousel.Item>
 			<Carousel.Item interval={3000}>
-				<Link to="/vehicle">
+				<Link to="##">
 					<img className="d-block w-100" src="http://i.imgur.com/rE70lnQ.jpg" alt="Vehicles" />
 					<Carousel.Caption className="shadow">
 						<h3>Vehicles</h3>
