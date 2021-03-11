5f6db1227b13a63ac01a6bdf81179ffcafbeaea6
yun joung <yunjoungji@gmail.com>
2021-03-11 13:36:39 -0300

    agregar landing page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,7 +7,7 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import Navbar from "./component/navbar";
 import { Footer } from "./component/footer";
 import Landingpage from "./pages/landingpage";
 
--- src/front/js/pages/landingPage.js
+++ src/front/js/pages/landingPage.js
@@ -3,7 +3,7 @@ import "../../styles/home.scss";
 import "../../styles/index.scss";
 import { logoBlanco, man } from "../../img/image";
 import { withRouter } from "react-router-dom";
-import { Container, Button, Form, FormControl } from "react-bootstrap";
+import { Container, Button, Form, FormControl, Row, Col } from "react-bootstrap";
 import { IconBox } from "../component/IconBox.jsx";
 import { CardBox } from "../component/cardBox.jsx";
 
@@ -15,43 +15,59 @@ const LandingPage = () => {
 				backgroundImage: `url(http://localhost:3000/backGround.png)`
 			}}>
 			<Container>
-				<div className="transBox mb-3" />
-				<img src={logoBlanco} width="85" height="25" className="d-inline-block align-top" alt="coteclogo" />
-				<div />
-				<div className="mt-5">
-					<h1 className="text-white">
-						Contrata en línea
-						<br />a los mejores freelancers
-					</h1>
-					<Form inline>
-						<FormControl
-							type="text"
-							placeholder="Search"
-							className="mr-sm-2 my-3"
-							style={{ borderRadius: "1.75rem", width: "526px" }}
-						/>
-					</Form>
-					<button type="button" className="btn btn-outline-light mr-3 mt-2 px-5">
-						Buscar una freelancer
-					</button>
-					<Button variant="secondary  mt-2 px-5">Soy un freelancer</Button>
-					<img src={man} width="280" height="250" className="d-inline-block align-top" alt="freelancer" />
-				</div>
-				<IconBox />
-				<CardBox />
-				<div className="transBox mb-3">
+				<div>
+					<Row>
+						<Col md={6}>
+							<img
+								src={logoBlanco}
+								width="110"
+								height="33"
+								className="d-inline-block align-top mt-5"
+								alt="coteclogo"
+							/>
+							<div className="transBox" />
+
+							<h1 className="text-white mt-3">
+								Contrata en línea
+								<br />a los mejores freelancers
+							</h1>
+							<Form inline>
+								<FormControl
+									type="text"
+									placeholder="Search"
+									className="mr-sm-2 my-3 "
+									style={{ borderRadius: "1.75rem", width: "526px" }}
+								/>
+								<Button variant="outline-light" className="mr-3 mt-2 px-5">
+									Buscar una freelancer
+								</Button>
+								<Button variant="secondary" className="mt-2 px-5">
+									Soy un freelancer
+								</Button>
+							</Form>
+						</Col>
+						<Col md={6} sm={12} className="d-flex align-items-end align-items-sm-center ">
+							<img
+								src={man}
+								width="700px"
+								height="500px"
+								className="d-inline-block align-top mt-3 "
+								alt="freelancer"
+							/>
+						</Col>
+					</Row>
+					<div className="transBox" />
+
+					<IconBox />
+					<CardBox />
 					<Button variant="primary">Primary</Button>
 					<Button variant="secondary">Secondary</Button>
 					<Button variant="success">Success</Button>
 					<Button variant="outline-primary">Primary</Button>
 					<Button variant="outline-secondary">Secondary</Button>
 					<Button variant="outline-success">Success</Button>
-					<button type="button" className="btn btn-light">
-						Light
-					</button>
-					<button type="button" className="btn btn-outline-light">
-						Light
-					</button>
+					<Button variant="light">Light</Button>
+					<Button variant="outline-light">Light</Button>
 				</div>
 			</Container>
 		</div>
