b4d7c9348be0976553a0c31278ea79da6344526c
Anmora96 <anmora.2396@gmail.com>
2021-03-11 18:45:58 +0000

    cambios

--- src/front/js/component/presentation.js
+++ src/front/js/component/presentation.js
@@ -8,14 +8,14 @@ export const Presentation = () => {
 			<Carousel.Item>
 				<img
 					className="d-block w-100"
-					src="https://img5.goodfon.com/wallpaper/nbig/8/2d/kokteil-miata-led-apnlsin-laim.jpg"
+					src="https://www.dazzlingwallpapers.com/wp-content/uploads/2014/08/Best-Green-Cocktail-wallpaper.jpg"
 					alt="Imagen ilustrativa"
 				/>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img
 					className="d-block w-100"
-					src="https://www.dazzlingwallpapers.com/wp-content/uploads/2014/08/Best-Green-Cocktail-wallpaper.jpg"
+					src="https://img5.goodfon.com/wallpaper/nbig/8/2d/kokteil-miata-led-apnlsin-laim.jpg"
 					alt="Imagen ilustrativa"
 				/>
 			</Carousel.Item>
