dc219fb7bc3fa952cd45f0813e73a582b9bf45ef
nivek29 <kevinmezamc@gmail.com>
2021-03-11 16:50:28 +0000

    interfaz de proveedor

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,6 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { Provider } from "./pages/provider";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -24,6 +25,9 @@ const Layout = () => {
 					<Switch>
 						<Route exact path="/">
 							<Home />
+						</Route>
+						<Route exact path="/provider">
+							<Provider />
 						</Route>
 						<Route exact path="/demo">
 							<Demo />
