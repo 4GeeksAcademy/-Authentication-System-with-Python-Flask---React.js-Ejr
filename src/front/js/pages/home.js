f9289d8ebde0b6faf754d5ab28b41ad72207f897
Fabizuni <fabiola.ucr@gmail.com>
2021-03-12 17:05:50 +0000

    updated

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Carrusel } from "/workspace/react-flask-hello-proyecto-final-grupo-6A/src/front/js/component/carrusel.js";
 import "../../styles/home.scss";
+import { ComponenteInf } from "../component/ComponentInf";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -9,6 +10,7 @@ export const Home = () => {
 	return (
 		<div className="container" style={{ width: "75%" }}>
 			<Carrusel />
+			<ComponenteInf />
 		</div>
 	);
 };
