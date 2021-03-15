065f4cdbd9221c0f6f430701719949b591b11c37
Mauriel <mauriel.azofeifa@uned.cr>
2021-03-15 22:28:55 +0000

    wgfwe

--- src/front/js/component/Cocktails.js
+++ src/front/js/component/Cocktails.js
@@ -31,13 +31,6 @@ export const Cocktails = () => {
 							</Col>
 						</Media.Body>
 					</Media>
-					{/* <ListGroup>
-						<ListGroup.Item>Cras justo odio</ListGroup.Item>
-						<ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-						<ListGroup.Item>Morbi leo risus</ListGroup.Item>
-						<ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-						<ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-					</ListGroup> */}
 				</Card>
 			</Row>
 		</Container>
