287cf83da3f1530c723d8f2c39f5f16292a42acc
Anmora96 <anmora.2396@gmail.com>
2021-03-16 23:31:09 +0000

    Solve the different cuestion about the page and conection

--- src/front/js/component/base_categories.js
+++ src/front/js/component/base_categories.js
@@ -17,6 +17,7 @@ export const Base_Categories = () => {
 				</article>
 				<div className="mt-1 mb-1 d-flex justify-content-end" id="padre">
 					<Image
+						id="WithoutBase"
 						className="contentimg col-12 col-md-8"
 						src="https://i.pinimg.com/originals/61/e7/aa/61e7aa2a06dff9d8a9b6800b8ebb1367.jpg"
 						fluid
@@ -44,6 +45,7 @@ export const Base_Categories = () => {
 				</div>
 				<div className="mt-1 mb-1 d-flex justify-content-start" id="padre">
 					<Image
+						id="RUM"
 						className="contentimg col-12 col-md-8"
 						src="https://anti-fake.ru/assets/img/articles/Bacardi/B_U_8Anos_Desktop.jpg"
 						fluid
@@ -71,6 +73,7 @@ export const Base_Categories = () => {
 				</div>
 				<div className="mt-1 mb-1 d-flex justify-content-end" id="padre">
 					<Image
+						id="GIN"
 						className="contentimg col-12 col-md-8"
 						src="https://wallpaperaccess.com/full/2476836.jpg"
 						fluid
@@ -98,6 +101,7 @@ export const Base_Categories = () => {
 				</div>
 				<div className="mt-1 mb-1 d-flex justify-content-start" id="padre">
 					<Image
+						id="VODKA"
 						className="contentimg col-12 col-md-8"
 						src="https://i.pinimg.com/originals/61/e7/aa/61e7aa2a06dff9d8a9b6800b8ebb1367.jpg"
 						fluid
@@ -125,6 +129,7 @@ export const Base_Categories = () => {
 				</div>
 				<div className="mt-1 mb-1 d-flex justify-content-end" id="padre">
 					<Image
+						id="TEQUILA"
 						className="contentimg col-12 col-md-8"
 						src="https://www.wallpaperup.com/uploads/wallpapers/2015/06/02/708845/5064001e98ce2a2cac2144f90644d0c9.jpg"
 						fluid
@@ -152,6 +157,7 @@ export const Base_Categories = () => {
 				</div>
 				<div className="mt-1 mb-1 d-flex justify-content-start" id="padre">
 					<Image
+						id="WHISKY"
 						className="contentimg col-12 col-md-8"
 						src="https://www.wallpaperup.com/uploads/wallpapers/2015/06/02/708845/5064001e98ce2a2cac2144f90644d0c9.jpg"
 						fluid
