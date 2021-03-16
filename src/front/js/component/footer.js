8928b0bf6fa62f20258f83177dd9205aacdecb16
yun joung <yunjoungji@gmail.com>
2021-03-16 12:44:38 -0300

    agregar landing page

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { logoAzul } from "../../img/image";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p />
+	<footer className="footer mt-auto py-3 ">
+		<img src={logoAzul} width="110" height="33" className="mt-5" alt="cotec" />
+		<p className="mt-2">© 2021 cotec® Global Inc.</p>
 	</footer>
 );
--- src/front/js/component/serviceBox.jsx
+++ src/front/js/component/serviceBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/home.scss";
 import "../../styles/index.scss";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Row, Col } from "react-bootstrap";
 
 export const ServiceBox = () => {
 	return (
@@ -9,27 +9,42 @@ export const ServiceBox = () => {
 			<Jumbotron className="whiteBox shadow-lg p-3 mb-5 p-5">
 				<h2>¿Qué es lo fabuloso de Cotec?</h2>
 				<br />
-				<h4>
-					<i className="fas fa-dollar-sign" />
-					&nbsp;&nbsp; Lo mejor para cada presupuesto
-				</h4>
-				<p>
-					Encuentre servicios de alta calidad a todos los precios. Sin tarifas por hora, solo precios basados
-					en proyectos
-				</p>
-				<h4>
-					<i className="fas fa-history" />
-					&nbsp;&nbsp; Trabajo de calidad hecho rápidamente
-				</h4>
-				<p>
-					Encuentre el profesional independiente adecuado para comenzar a trabajar en su proyecto en cuestión
-					de minutos.
-				</p>
-				<h4>
-					<i className="far fa-credit-card" />
-					&nbsp;&nbsp; Pagos protegidos, siempre
-				</h4>
-				<p>Siempre sepa lo que pagará por adelantado. Su pago no se libera hasta que aprueba el trabajo.</p>
+				<Row>
+					<Col md={1}>
+						<i className="fas fa-dollar-sign icon" />
+					</Col>
+					<Col md={11}>
+						<h4>Lo mejor para cada presupuesto</h4>
+						<p>
+							Encuentre servicios de alta calidad a todos los precios. Sin tarifas por hora, solo precios
+							basados en proyectos
+						</p>
+					</Col>
+				</Row>
+				<Row className="my-3">
+					<Col md={1}>
+						<i className="fas fa-history icon" />
+					</Col>
+					<Col md={11}>
+						<h4>Trabajo de calidad hecho rápidamente</h4>
+						<p>
+							Encuentre el profesional independiente adecuado para comenzar a trabajar en su proyecto en
+							cuestión de minutos.
+						</p>
+					</Col>
+				</Row>
+				<Row>
+					<Col md={1}>
+						<i className="far fa-credit-card icon" />
+					</Col>
+					<Col md={11}>
+						<h4>Pagos protegidos, siempre</h4>
+						<p>
+							Siempre sepa lo que pagará por adelantado. Su pago no se libera hasta que aprueba el
+							trabajo.
+						</p>
+					</Col>
+				</Row>
 			</Jumbotron>
 		</>
 	);
