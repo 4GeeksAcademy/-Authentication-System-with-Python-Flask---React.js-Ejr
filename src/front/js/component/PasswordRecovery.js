3bffcf99f99910c92c1c4b47879113de2f211e1e
pieroantonucci <piero.antonucci@gmail.com>
2021-03-17 01:37:51 +0000

    Actualizado

--- src/front/js/component/PasswordRecovery.js
+++ src/front/js/component/PasswordRecovery.js
@@ -8,26 +8,28 @@ export const PasswordRecovery = () => {
 			<div className="row">
 				<div className="col-2" />
 				<div className="col-8">
-                    <>
 					<div className="iconBox shadow-lg p-3" style={{ height: "100%" }}>
-                        <i className="fas fa-user-lock" /><h1 style={{ textAlign: "center", marginBottom: "20px" }}>Ingresa a tu cuenta</h1>
-                        
-                        <h5>
-                        Para recuperar tu contraseña favor ingresa tu correo electrónico registrado y te enviaremos
-                        la clave por email
-                        </h5>
-                        		{/* <Form.Label>Correo Electrónico</Form.Label> */}
-								<Form.Control type="email" placeholder="Ingresa tu correo electrónico" />
-								<Form.Text className="text-muted">Nunca compartiremos tu email con nadie.</Form.Text>
-							</Form.Group>
+						<>
+							<i className="fas fa-user-lock" />
+							<h1 style={{ textAlign: "center", marginBottom: "20px" }}>Ingresa a tu cuenta</h1>
 
-								Ingresar
-							</Button>
-						</Form>
+							<h5>
+								Para recuperar tu contraseña favor ingresa tu correo electrónico registrado y te
+								enviaremos la clave por email
+							</h5>
+							<Form>
+								<Form.Group controlId="formBasicEmail">
+									{/* <Form.Label>Correo Electrónico</Form.Label> */}
+									<Form.Control type="email" placeholder="Ingresa tu correo electrónico" />
+								</Form.Group>
+								<Button variant="primary" size="lg" type="submit" block style={{ marginTop: "70px" }}>
+									Ingresar
+								</Button>
+							</Form>
+						</>
 					</div>
 				</div>
-				<div cl
-                    </>assName="col-2" />
+				<div className="col-2"></div>
 			</div>
 		</div>
 	);
