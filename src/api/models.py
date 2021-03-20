ae179e80626619c2e8af42bcbc425cbd5a224af2
yun joung <yunjoungji@gmail.com>
2021-03-20 18:55:22 -0300

    agregar api

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,89 @@
+import os
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+#from sqlalchemy import create_engine
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    type_user = db.Column(db.String(45), nullable=False)
+    username = db.Column(db.String(45), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+class Tipo_Cobro(db.Model):
+    __tablename__ = 'tipo_cobro'
+    id = db.Column(db.Integer,primary_key=True, nullable=False)
+    name_tipocobro = db.Column(db.String(45), nullable=False)
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer,primary_key=True, nullable=False)
+    name_category = db.Column(db.String(50), nullable=False)
+
+class Subcategory(db.Model):
+    __tablename__ = 'subcategory'
+    id = db.Column(db.Integer,primary_key=True, nullable=False)
+    id_category = db.Column(db.Integer, ForeignKey('category.id'), nullable=False)
+    name_subcategory = db.Column(db.String(50), nullable=False)
+    category = relationship(Category)
+    
+class Servicio_registrados(db.Model):
+    __tablename__ = 'servicio_registrados'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id_tipo_cobro = db.Column(db.Integer, ForeignKey('tipo_cobro.id'), nullable=False)
+    valor = db.Column(db.Integer, nullable=False)
+    name_servicio = db.Column(db.String(50), nullable=False)
+    descrip_servicio = db.Column(db.String(250), nullable=False)
+    anos_experiencia = db.Column(db.Integer, nullable=False)
+    merit = db.Column(db.String(50), nullable=False)
+    tipo_membresia = db.Column(db.String(50), nullable=False)
+    tipo_cobro = relationship(Tipo_Cobro)
+
+class Relacion_registrados_subcategory(db.Model):
+    __tablename__ = 'relacion_registrados_subcategory'
+    id_servicios_registrados = db.Column(db.Integer, ForeignKey('servicio_registrados.id'),primary_key=True)
+    id_subcategory = db.Column(db.Integer, ForeignKey('subcategory.id'), primary_key=True)
+    subcategory = relationship(Subcategory)
+    servicio_registrados = relationship(Servicio_registrados)
+
+
+class User_Sericios_Registrados(db.Model):
+    __tablename__ = 'user_Sericios_Registrados'
+    id_users = db.Column(db.Integer, ForeignKey('user.id'),primary_key=True)
+    id_servicio_registrados = db.Column(db.Integer, ForeignKey('servicio_registrados.id'), primary_key=True)
+    user = relationship(User)
+    servicio_registrados = relationship(Servicio_registrados)
+
+    
+class Sericios_prestados(db.Model):
+    __tablename__ = 'sericios_prestados'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id_users_oferente = db.Column(db.Integer, ForeignKey('user.id'),primary_key=True)
+    id_users_compra = db.Column(db.Integer, ForeignKey('user.id'),primary_key=True)
+    id_subcategory = db.Column(db.Integer, ForeignKey('subcategory.id'),primary_key=True)
+    create_time = db.Column(db.Integer, nullable=False)
+    evaluacion = db.Column(db.Integer)
+    cantidad_servicio = db.Column(db.Integer,nullable=False)
+    total_valor_servicio = db.Column(db.Integer,nullable=False)
+    fecha_inicio = db.Column(db.Integer, nullable=False)
+    fecha_termino = db.Column(db.Integer, nullable=False)
+    user = relationship(User)
+    subcategory = relationship(Subcategory)
+
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id_users = db.Column(db.Integer, ForeignKey('user.id'))
+    id_servicio_registrados = db.Column(db.Integer, ForeignKey('servicio_registrados.id'))
+    user = relationship(User)
+    servicio_registrados = relationship(Servicio_registrados)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +92,33 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "type_user": self.type_user,
+            "username": self.username,
+
+            "name_tipocobro": self.name_tipocobro,
+
+            "name_category": self.name_category,
+
+            "name_subcategory": self.name_subcategory,
+
+            "id_tipo_cobro": self.id_tipo_cobro,
+            "valor": self.valor,
+            "name_servicio": self.name_servicio,
+            "descrip_servicio": self.descrip_servicio,
+            "anos_experiencia": self.anos_experiencia,
+            "merit": self.merit,
+            "tipo_membresia": self.tipo_membresia,
+
+            "id_Servicios_registrados": self.id_Servicios_registrados,
+            "id_subcategory": self.id_subcategory,
+
+            "id_users": self.id_users,
+            "create_time": self.create_time,
+            "evaluacion": self.evaluacion,
+            "cantidad_servicio": self.cantidad_servicio,
+            "total_valor_servicio": self.total_valor_servicio,
+            "fecha_inicio": self.fecha_inicio,
+            "fecha_termino": self.fecha_termino,
+
             # do not serialize the password, its a security breach
         }
--- src/app.py
+++ src/app.py
@@ -2,62 +2,109 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import re
 from flask import Flask, request, jsonify, url_for, send_from_directory
-from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
-#from models import Person
+from api.models import User
+#Tipo_Cobro, Category, Subcategory, Servicio_registrados, Relacion_registrados_subcategory, User_Sericios_Registrados, Sericios_prestados, Favoritos
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, "test.db")
+app.config["DEBUG"] = True
+app.config["ENV"] = "development"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secret-key"
+app.config['JWT_SECRET_KEY'] = 'encrypt'
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 # database condiguration
 if os.getenv("DATABASE_URL") is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
-
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 # Allow CORS requests to this API
 CORS(app)
 
-# add the admin
-setup_admin(app)
+@app.route('/signup', methods=['POST'])
+def signup():
+      ereg = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+      preg = '^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$'
+      user = User()
+      #Checking email 
+      if (re.search(ereg,request.json.get("email"))):
+           user.email = request.json.get("email")
+      else:
+          return "Invalid email format", 400
+      #Checking password
+      if (re.search(preg,request.json.get('password'))):
+          pw_hash = bcrypt.generate_password_hash(request.json.get("password"))
+          user.password = pw_hash
+      else:
+          return "Invalid password format", 400
 
-# Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api, url_prefix='/api')
+      user.username = request.json.get("username", None )
+      user.name = request.json.get("name")
+      user.age = request.json.get("age")
+      user.bio = request.json.get("bio")
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+      db.session.add(user)
+      db.session.commit()
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+      return jsonify({"success":True}), 201
 
-# any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
-    return response
+# # add the admin
+# setup_admin(app)
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+# # Add all endpoints form the API with a "api" prefix
+# app.register_blueprint(api, url_prefix='/api')
+
+# # Handle/serialize errors like a JSON object
+# @app.errorhandler(APIException)
+# def handle_invalid_usage(error):
+#     return jsonify(error.to_dict()), error.status_code
+
+# # generate sitemap with all your endpoints
+# @app.route('/')
+# def sitemap():
+#     if ENV == "development":
+#         return generate_sitemap(app)
+#     return send_from_directory(static_file_dir, 'index.html')
+
+# # any other endpoint will try to serve it like a static file
+# @app.route('/<path:path>', methods=['GET'])
+# def serve_any_other_file(path):
+#     if not os.path.isfile(os.path.join(static_file_dir, path)):
+#         path = 'index.html'
+#     response = send_from_directory(static_file_dir, path)
+#     response.cache_control.max_age = 0 # avoid cache memory
+#     return response
+
+# # this only runs if `$ python src/main.py` is executed
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3001))
+#     app.run(host='0.0.0.0', port=PORT, debug=True)
