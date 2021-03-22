fc97fb2019ceea5959e6db973bed3ef3f4c9f1bf
Mauriel <mauriel.azofeifa@uned.cr>
2021-03-22 16:35:47 +0000

    pull

--- src/front/js/component/content.js
+++ src/front/js/component/content.js
@@ -1,15 +1,12 @@
 import React from "react";
+import Content_jpg from "../../img/logo/conten.jpg";
 import { Container, Row, Image, Card } from "react-bootstrap";
 export const Content = () => {
 	return (
 		<Container className="mt-2 mb-2" id="AboutContent">
 			<Row>
 				<div className="d-flex justify-content-end" id="content1">
-					<Image
-						className="contentimg col-12 col-md-8"
-						src="https://foodandtravel.mx/wp-content/uploads/2019/02/coctelesclassic.jpg"
-						fluid
-					/>
+					<Image className="contentimg col-12 col-md-8" src={Content_jpg} fluid />
 					<div className="col-12 col-md-6" id="content2">
 						<Card className="Card text-light p-3">
 							<blockquote className="blockquote mb-0 card-body">
