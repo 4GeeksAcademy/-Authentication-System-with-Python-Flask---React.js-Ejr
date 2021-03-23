bbf4bff876e116a1c0fbc013f90981a1cc799d47
Mauriel <mauriel.azofeifa@uned.cr>
2021-03-23 00:04:29 +0000

    pull

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -37,7 +37,7 @@ import { Cocktail_Tequila } from "./component/cocktail_tequila";
 import { Cocktail_Whisky } from "./component/cocktail_whisky";
 import { Cocktail_Gin } from "./component/cocktail_gin";
 
-//create your first component
+//create your first component.
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
