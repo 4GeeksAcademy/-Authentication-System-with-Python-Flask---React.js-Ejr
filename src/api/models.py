3dbf148b61e676acf0fbd44a268b2896f40bd7ed
aparral <aparra001@gmail.com>
2021-03-24 01:17:05 +0000

    add models category

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=False, nullable=False)
     username = db.Column(db.String(200), unique=True, nullable=False)
+    typeuser = db.Column(db.String(200), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -20,18 +21,13 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-
-    # id = db.Column(db.Integer, primary_key=True)
-    # email = db.Column(db.String(120), unique=True, nullable=False)
-    # password = db.Column(db.String(80), unique=False, nullable=False)
-    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    # def __repr__(self):
-    #     return '<User %r>' % self.username
-
-    # def serialize(self):
-    #     return {
-    #         "id": self.id,
-    #         "email": self.email,
-    #         # do not serialize the password, its a security breach
-    #     }
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer,primary_key=True)
+    name_category = db.Column(db.String(50), nullable=False)
+    # subcategory = db.relationship('Subcategory', backref="category", lazy=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name_category": self.name_category
+        }
