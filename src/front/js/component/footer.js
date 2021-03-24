a13c77efdeaed33c247d9efcfc905491e723f61d
Cesar-Antonio23 <ces.acevedo.23@gmail.com>
2021-03-24 15:08:23 +0000

    Agregando el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,3 +1,39 @@
 import React, { Component } from "react";
+import "../styles/footer.css";
+export const Footer = () => {
+	return (
+		<footer>
+			<footer className="text-center text-white">
+				<div className="container pt-4">
+					<section className=" iconos mb-4">
+						<a
+							className="btn-face btn btn-link btn-floating btn-lg text-dark m-1"
+							href="https://www.facebook.com/"
+							role="button"
+							data-mdb-ripple-color="dark">
+							<i className="fab fa-facebook-f" />
+						</a>
 
-export const Footer = () => <footer />;
+						<a
+							className="btn-twitter btn btn-link btn-floating btn-lg text-dark m-1"
+							href="https://twitter.com/?lang=es"
+							role="button"
+							data-mdb-ripple-color="dark">
+							<i className="fab fa-twitter" />
+						</a>
+
+						<a
+							className="btn-insta btn btn-link btn-floating btn-lg text-dark m-1"
+							href="https://www.instagram.com/?hl=es"
+							role="button"
+							data-mdb-ripple-color="dark">
+							<i className="fab fa-instagram" />
+						</a>
+					</section>
+				</div>
+
+				<div className="text-center text-white p-3">© 2021 Copyright: Pichangapp</div>
+			</footer>
+		</footer>
+	);
+};
