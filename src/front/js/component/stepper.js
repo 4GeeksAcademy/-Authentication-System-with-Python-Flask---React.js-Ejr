8a5b792eacc113c6ce970a659c4fb43ae2a68d97
Cesar-Antonio23 <ces.acevedo.23@gmail.com>
2021-03-25 23:21:24 +0000

    Agregando stepper al home

---
+++ src/front/js/component/stepper.js
@@ -0,0 +1,42 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "../styles/home.css";
+import Picture from "/workspace/canchapp2/src/front/img/vectorstock_8229541.png";
+const Stepper = () => {
+	return (
+		<div className="card-group">
+			<div className="card">
+				<img src={Picture} className="stepper-1 card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Paso 1</h5>
+					<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+				{/* <div className="card-footer">
+					<small className="text-muted">Last updated 3 mins ago</small>
+				</div> */}
+			</div>
+			<div className="card">
+				<img src={Picture} className="stepper-2 card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Paso 2</h5>
+					<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+				{/* <div className="card-footer">
+					<small className="text-muted">Last updated 3 mins ago</small>
+				</div> */}
+			</div>
+			<div className="card">
+				<img src={Picture} className="stepper-3 card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Paso 3</h5>
+					<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+				{/* <div className="card-footer">
+					<small className="text-muted">Last updated 3 mins ago</small>
+				</div> */}
+			</div>
+		</div>
+	);
+};
+
+export default Stepper;
