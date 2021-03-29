80feb9ae10ad8e778a700bd740b104fc2ca4de40
codehsalas <hectorsalas7@gmail.com>
2021-03-29 16:06:05 +0000

    Add links navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,64 +1,42 @@
 import React from "react";
 import logo from "/workspace/canchapp2/src/front/img/logo_navbar.png";
 import "../styles/navbar.css";
-
-// import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt="" width="150" />
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    El complejo
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Reserva
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Faqs
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Contacto
-                                </a>
-                            </li>
-                            <button type="button" className="btn btn-info">
-                                Log in
-                            </button>
-                            <button type="button" className="btn btn-success">
-                                Registrate
-                            </button>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg navbar-light">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						<img src={logo} alt="" width="150" />
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon" />
+					</button>
+
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<button type="button" className="btn btn-primary">
+								<Link to="/home">Home</Link>
+							</button>
+							<button type="button" className="btn btn-info">
+								<Link to="/login">Login</Link>
+							</button>
+							<button type="button" className="btn btn-success">
+								<Link to="/registrate">Registrate</Link>
+							</button>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 };
