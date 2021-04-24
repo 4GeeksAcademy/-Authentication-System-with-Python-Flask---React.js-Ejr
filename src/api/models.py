2f9bba7bfcfa1dd91f5d9696b0a31deb0821900e
Carlos Espinoza <obandocarlos604@gmail.com>
2021-04-24 15:57:36 +0000

    models update

--- src/api/models.py
+++ src/api/models.py
@@ -10,8 +10,7 @@ class User(db.Model):
     location = db.Column(db.String(50), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __init__(self, id, name, email, password, location, is_active):
-        self.id = id
+    def __init__(self, name, email, password, location, is_active):
         self.name = name
         self.email = email 
         self.password = password
@@ -22,10 +21,25 @@ class SuperMarket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     market_name = db.Column(db.String(50), unique=True, nullable=False)
     location = db.Column(db.String(50), nullable=False)
+    information = db.Column(db.String(255))
+
+    def __init__(self, market_name, location, information):
+        self.market_name = market_name
+        self.location = location
+        self.information = information
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    Product_name = db.Column(db.Integer, unique=True ,nullable=False)
+    product_name = db.Column(db.Integer, unique=True ,nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(50), unique=True ,nullable=False)
+    category = db.Column(db.String(50), unique=True, nullable=False)
+    created_at = db.Column(db.Integer)
+    
+    def __init__(self, product_name, price, category, created_at):
+        self.product_name = product_name
+        self.price = price
+        self.category = category
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     
