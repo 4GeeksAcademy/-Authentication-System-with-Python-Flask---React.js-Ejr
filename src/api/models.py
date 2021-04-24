7be799ffdda48b04a18d25daf2405bd95c12942c
Carlos Espinoza <obandocarlos604@gmail.com>
2021-04-24 02:19:54 +0000

    endpoint model

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,28 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def __init__(self, id, name, email, password, location, is_active):
+        self.id = id
+        self.name = name
+        self.email = email 
+        self.password = password
+        self.location = location
+        self.is_active = is_active
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class SuperMarket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    market_name = db.Column(db.String(50), unique=True, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Product_name = db.Column(db.Integer, unique=True ,nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), unique=True ,nullable=False)
+    
