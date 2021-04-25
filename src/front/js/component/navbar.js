b81818fbb5a4c41fc51c6c42858f1e287fb2b2a2
jgarciaf106 <jgarciaf106@gmail.com>
2021-04-25 23:08:18 +0000

    Initial Components Created and heroku, configuration

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light colorNav mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Pura Vida Mart</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<Link to="/logUserIn">
+					<button className="btn btn-login">Log In</button>
 				</Link>
 			</div>
 		</nav>
