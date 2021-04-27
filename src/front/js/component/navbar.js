2f83bd4e1911902f8507baae65ad2ecf087b5803
Dilana777 <dylana7792@hotmail.com>
2021-04-27 01:51:06 +0000

    Subiendo home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import rigoImageUrl from "../../img/logo.jpeg";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-verde mb-3 verde">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src="logo.jpeg" style={{ width: "80px", height: "40px" }} />
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button style={{ color: "white", background: "#859240", fontFamily: "Roboto" }}>
+						Crear Cuenta
+					</button>
+				</Link>
+				<Link to="/demo">
+					<button style={{ color: "white", background: "#859240", fontFamily: "Roboto" }}>Ingresar</button>
+				</Link>
+				<Link to="/demo">
+					<button style={{ color: "white", background: "#859240", fontFamily: "Roboto" }}>Contacto</button>
 				</Link>
 			</div>
 		</nav>
