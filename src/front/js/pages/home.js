b7e47daafd0ecf9d266f6d4faa3a658df9eb8fad
socamacho <socamachoruiz@gmail.com>
2021-04-28 05:14:44 +0000

    pending structure

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,13 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect, useState } from "react";
 import "../../styles/home.scss";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
 	return (
-		<div className="text-center mt-5">
-			<h1>Global Market</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div className="container">
+			<div className="text-center mt-5">
+				<h2>Insertar lema y logotipo</h2>
+			</div>
 		</div>
 	);
 };
