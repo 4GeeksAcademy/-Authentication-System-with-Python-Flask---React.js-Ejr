4bc25dd95ce867622cf4d4d3f548f6b2cbbae9d0
socamacho <socamachoruiz@gmail.com>
2021-04-28 01:45:09 +0000

    texto rojo y navbar negro

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-black mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
