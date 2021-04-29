5810b26aebb169d44122493865cf534c52b79bbc
socamacho <socamachoruiz@gmail.com>
2021-04-29 06:29:18 +0000

    pending navbar and footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,10 +3,47 @@ import "../../styles/home.scss";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+	/*Pensar en lema*/
 	return (
 		<div className="container">
-			<div className="text-center mt-5">
-				<h2>Insertar lema y logotipo</h2>
+			<div className="row">
+				<div className="col-12">
+					<div className="text-white">
+						<h3>¡Con más opciones, ahorrás más colones!</h3>
+					</div>
+					<form>
+						<div className="form-group text-white">
+							<label htmlFor="exampleInputEmail1">Nombre del producto</label>
+							<div className="input-group mb-3">
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Qué estás buscando?"
+									aria-label="Recipient's username"
+									aria-describedby="basic-addon2"
+								/>
+								<div className="input-group-append">
+									<span className="input-group-text" id="basic-addon2">
+										<i className="fas fa-search" />
+									</span>
+								</div>
+							</div>
+						</div>
+						<div className="form-group text-white">
+							<label htmlFor="exampleInputPassword1">Ubicación</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Dónde estás ubicado?"
+								aria-label="Recipient's username"
+								aria-describedby="basic-addon2"
+							/>
+						</div>
+						<button type="submit" className="btn btn-outline-warning btn-lg btn-block">
+							Iniciar búsqueda
+						</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
