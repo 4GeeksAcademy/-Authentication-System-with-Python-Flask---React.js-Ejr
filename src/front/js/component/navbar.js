7036f5764065d33fcd20dbfefad042fe8d03d424
Gab20031995 <luizclzm@gmail.com>
2021-04-30 16:06:04 +0000

    login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,58 +21,66 @@ export const Navbar = () => {
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon" />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ float: "right" }}>
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<a className="btn btn-outline-success text-white" aria-current="page" href="#">
+							<a
+								className="btn btn-success text-white"
+								aria-current="page"
+								href="#"
+								style={{ margin: "10px" }}>
 								Inicio
 							</a>
 						</li>
 
 						<li className="nav-item">
-							<a className="btn btn-outline-success text-white" href="#">
+							<a className="btn btn-success text-white" href="#" style={{ margin: "10px" }}>
 								Categorias
 							</a>
 						</li>
 
 						<li className="nav-item">
 							<a
-								className="btn btn-outline-success text-white"
+								className="btn btn-success text-white"
 								href="#"
 								tabIndex="-1"
-								aria-disabled="true">
+								aria-disabled="true"
+								style={{ margin: "10px" }}>
 								Cupones
 							</a>
 						</li>
 
 						<li className="nav-item">
 							<a
-								className="btn btn-outline-success text-white"
+								className="btn btn-success text-white"
 								href="#"
 								tabIndex="-1"
-								aria-disabled="true">
+								aria-disabled="true"
+								style={{ margin: "10px" }}>
 								Registro
 							</a>
 						</li>
 
 						<li className="nav-item">
 							<a
-								className="btn btn-outline-success text-white"
+								className="btn btn-success text-white"
 								href="#"
 								tabIndex="-1"
-								aria-disabled="true">
+								aria-disabled="true"
+								style={{ margin: "10px" }}>
 								Login
 							</a>
 						</li>
 
 						<li className="nav-item dropdown">
 							<a
-								className="btn btn-outline-success text-white nav1"
+								className="btn btn-success text-white nav1"
 								href="#"
 								id="navbarDropdown"
 								role="button"
 								data-bs-toggle="dropdown"
-								aria-expanded="false">
+								aria-expanded="false"
+								style={{ margin: "10px" }}>
 								°°°
 							</a>
 							<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,82 +3,45 @@ import React from "react";
 
 export const Login = () => {
 	return (
-		<div className="container rounded shadow w-75 h-50 bg-primary mt-5">
-			<div className="row align-items-stretch">
-				<div className="col rounded bg d-none d-lg-block" />
-				<div className="col bg-white p-2 rounded-end position-relative">
-					<div className="text-end pt-4">
-						<img
-							src="https://cdn.pixabay.com/photo/2018/03/14/12/53/shopping-3225130__340.png"
-							width="150px"
-						/>
-					</div>
-					<h2 className="text-center py-5">Bienvenidos</h2>
-
-					<form action="#">
-						<div className="mb-4">
-							<label htmlFor="email" className="form-label">
-								Correo electronico
+		<div className="container col-xl-10 col-xxl-8 px-4 py-5">
+			<div className="row align-items-center g-5 py-5">
+				<div className="col-lg-7 text-center text-lg-start">
+					<h1 className="display-4 fw-bold lh-1 mb-3">Vertically centered hero sign-up form</h1>
+					<p className="col-lg-10 fs-4 text-white">
+						Below is an example form built entirely with Bootstraps form controls. Each required form group
+						has a validation state that can be triggered by attempting to submit the form without completing
+						it.
+					</p>
+				</div>
+				<div className="col-10 mx-auto col-lg-5">
+					<form className="p-5 border rounded-3 bg-light">
+						<div className="form-floating mb-3">
+							<input
+								type="email"
+								className="form-control"
+								id="floatingInput"
+								placeholder="Email address"
+							/>
+						</div>
+						<div className="form-floating mb-3">
+							<input
+								type="password"
+								className="form-control"
+								id="floatingPassword"
+								placeholder="Password"
+							/>
+						</div>
+						<div className="checkbox mb-3">
+							<label>
+								<input type="checkbox" value="remember-me" /> Remember me
 							</label>
-							<input type="email" name="email" className="form-control" required />
 						</div>
-						<div className="mb-4">
-							<label htmlFor="password" className="form-label">
-								Contraseña
-							</label>
-							<input type="password" name="password" className="form-control" required />
-						</div>
-						<div className="mb-4">
-							<input type="checkbox" name="conected" id="" className="form-check-input" />
-							<label htmlFor="conected" className="form-check-label">
-								Mantenerme conectado
-							</label>
-						</div>
-						<div className="d-grid ">
-							<button type="submit" className="btn btn-primary">
-								iniciar sesión
-							</button>
-						</div>
-						<div className="my-3">
-							<span>
-								¿No tienes una cuenta? <a href="#">Registrate</a>
-							</span>{" "}
-							<br />
-							<span>
-								<a href="#">Recuperar contraseña</a>
-							</span>
-						</div>
+						<button className="w-100 btn btn-lg btn-primary" type="submit">
+							Sign up
+						</button>
+						<hr className="my-4" />
+						<small className="text-muted">Iniciar secion</small>
 					</form>
-
-					<div className="container w-100 my-5">
-						<div className="row">
-							<div className="col-12 text-center">
-								<span>Iniciar sesión</span>
-							</div>
-						</div>
-						<div className="row my-2">
-							<div className="col">
-								<button className="btn btn-outline-primary w-100">
-									<div className="row">
-										<div className="col-2">
-											<i className="bi bi-facebook" />
-										</div>
-										<div className="col-10">Facebook</div>
-									</div>
-								</button>
-							</div>
-							<div className="col">
-								<button className="btn btn-outline-danger w-100">
-									<div className="row">
-										<div className="col-2">
-											<i className="bi bi-google" />
-										</div>
-										<div className="col-10">Google</div>
-									</div>
-								</button>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
