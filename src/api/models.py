dbb725968a0e3fb19e7abcbb3142e6b0bc2ada57
Ricardo-Patino <rickpatinor@gmail.com>
2021-05-03 01:30:20 +0000

    modelado de s=datos

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import ForeignKey
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -26,6 +26,41 @@ class Client(db.Model):
 
     def __repr__(self):
         return '<Client %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fullName": self.fullName,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Seller(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullName = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Seller %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fullName": self.fullName,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fk_id = db.Column(db.Integer, ForeignKey('Seller.id'))
+    fullName = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.username
 
     def serialize(self):
         return {
