82d7e4d16ec73bc13eee55902e50801d34f2fd9a
jgarciaf106 <jgarciaf106@gmail.com>
2021-05-03 02:52:17 +0000

    Fixed back-end migrations

--- src/api/models.py
+++ src/api/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKey
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_Admin = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -18,8 +19,10 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     fullName = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -35,8 +38,10 @@ class Client(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
 class Seller(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     fullName = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -52,20 +57,22 @@ class Seller(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fk_id = db.Column(db.Integer, ForeignKey('Seller.id'))
-    fullName = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    fk_id = db.Column(db.Integer, db.ForeignKey('seller.id'), unique=True, nullable=False)
+    productName = db.Column(db.String(255),  nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
-        return '<Product %r>' % self.username
+        return '<Product %r>' % self.productName
 
     def serialize(self):
         return {
             "id": self.id,
-            "fullName": self.fullName,
-            "email": self.email,
+            "productName": self.productName,
+            "price": self.price,
+            "price": self.image
             # do not serialize the password, its a security breach
         }
