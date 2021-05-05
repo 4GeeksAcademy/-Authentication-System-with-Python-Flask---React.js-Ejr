48d1839be52fd5910b48f8b7b1b0fd5ec18183cc
jgarciaf106 <jgarciaf106@gmail.com>
2021-05-05 02:15:15 +0000

    la teacher vera si esto funciona

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    fullName = db.Column(db.String(255), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
