7662c2c90901231c1b9dd74947a035fa85fc46e1
socamacho <socamachoruiz@gmail.com>
2021-05-08 22:44:53 +0000

    first sprint

--- src/front/js/pages/products.js
+++ src/front/js/pages/products.js
@@ -5,127 +5,129 @@ export const Productos = () => {
 	return (
 		<>
 			<p className="h2 mx-auto py-4 font-weight-light">Productos disponibles</p>
-			<div className="container1">
-				<div className="card" style={{ width: "18rem" }}>
-					<img
-						src="https://images.nidux.net/eyJidWNrZXQiOiJuaWR1eC1zdG9yZXMiLCJrZXkiOiIxMzQwN1wvMzYxNC1wcm9kdWN0LTVmZDE2MDg3NDlhN2QtMDQwNzM5LTEuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJwbmciLCJyZXNpemUiOnsid2lkdGgiOjcwMCwiaGVpZ2h0Ijo3MDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjI1NSwiZyI6MjU1LCJiIjoyNTUsImFscGhhIjowfX19fQ==--ff385acd487f324081366ce114f3f34c19cb2f22f3d533efca9a1f5fd64956c0"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
+			<div className="whole_content">
+				<div className="container1">
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							src="https://images.nidux.net/eyJidWNrZXQiOiJuaWR1eC1zdG9yZXMiLCJrZXkiOiIxMzQwN1wvMzYxNC1wcm9kdWN0LTVmZDE2MDg3NDlhN2QtMDQwNzM5LTEuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJwbmciLCJyZXNpemUiOnsid2lkdGgiOjcwMCwiaGVpZ2h0Ijo3MDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjI1NSwiZyI6MjU1LCJiIjoyNTUsImFscGhhIjowfX19fQ==--ff385acd487f324081366ce114f3f34c19cb2f22f3d533efca9a1f5fd64956c0"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<li className="Precio"> Precio: 680.00 colones</li>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
+					</div>
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							src="https://express.dospinos.com/media/catalog/product/1/5/15000194.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<li className="Precio"> Precio: 680.00 colones</li>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
+					</div>
+					<div className="card " style={{ width: "18rem" }}>
+						<img
+							src="https://express.dospinos.com/media/catalog/product/1/5/15000195-render-leche_entera_3.5-edge.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<li className="Precio"> Precio: 680.00 colones</li>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
 					</div>
 				</div>
-				<div className="card" style={{ width: "18rem" }}>
-					<img
-						src="https://express.dospinos.com/media/catalog/product/1/5/15000194.jpg"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
+				<div className="container2">
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							src="https://images.nidux.net/eyJidWNrZXQiOiJuaWR1eC1zdG9yZXMiLCJrZXkiOiIxMzAxOVwvNTQyMi1wcm9kdWN0LTVmOTcwMDg3MTdkMGQtNTQyMi0xLnBuZyIsImVkaXRzIjp7InRvRm9ybWF0IjoicG5nIiwicmVzaXplIjp7IndpZHRoIjozNTAsImhlaWdodCI6MzUwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MH19fX0=--6deec71977633da57b797a7cab0cc3a99abbb3cb67d395a97a85ca9d472a81d0"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<li className="Precio"> Precio: 680.00 colones</li>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
 					</div>
-				</div>
-				<div className="card " style={{ width: "18rem" }}>
-					<img
-						src="https://express.dospinos.com/media/catalog/product/1/5/15000195-render-leche_entera_3.5-edge.jpg"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							src="http://triquitraque.cr/images/thumbs/0003005_leche-pasteurizada-2-grasa-galon-dos-pinos-38l_510.jpeg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<li className="Precio"> Precio: 680.00 colones</li>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div className="container2">
-				<div className="card" style={{ width: "18rem" }}>
-					<img
-						src="https://images.nidux.net/eyJidWNrZXQiOiJuaWR1eC1zdG9yZXMiLCJrZXkiOiIxMzAxOVwvNTQyMi1wcm9kdWN0LTVmOTcwMDg3MTdkMGQtNTQyMi0xLnBuZyIsImVkaXRzIjp7InRvRm9ybWF0IjoicG5nIiwicmVzaXplIjp7IndpZHRoIjozNTAsImhlaWdodCI6MzUwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MH19fX0=--6deec71977633da57b797a7cab0cc3a99abbb3cb67d395a97a85ca9d472a81d0"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
-					</div>
-				</div>
-				<div className="card" style={{ width: "18rem" }}>
-					<img
-						src="http://triquitraque.cr/images/thumbs/0003005_leche-pasteurizada-2-grasa-galon-dos-pinos-38l_510.jpeg"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
-					</div>
-				</div>
-				<div className="card" style={{ width: "18rem" }}>
-					<img
-						src="https://s.cornershopapp.com/product-images/2396159.jpg?versionId=0dSB2gVycI.56L1TiAQMDGV7vXsQo54n"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Leche Dos Pinos</h5>
-						<ul>
-							<li className="Precio"> Precio: 680.00 colones</li>
-							<li className="Precio"> Supermercado: Palí</li>
-						</ul>
-						<a href="#" className="btn btn-outline-success float-right">
-							Ver detalles
-						</a>
-						<button type="button" className="btn btn-outline-success float-right">
-							<i className="fas fa-cart-arrow-down" />
-						</button>
+					<div className="card" style={{ width: "18rem" }}>
+						<img
+							src="https://s.cornershopapp.com/product-images/2396159.jpg?versionId=0dSB2gVycI.56L1TiAQMDGV7vXsQo54n"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Leche Dos Pinos</h5>
+							<ul>
+								<p className="Precio"> Precio: 680.00 colones</p>
+								<li className="Precio"> Supermercado: Palí</li>
+							</ul>
+							<a href="#" className="btn btn-outline-success float-right">
+								Ver detalles
+							</a>
+							<button type="button" className="btn btn-outline-success float-right">
+								<i className="fas fa-cart-arrow-down" />
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
