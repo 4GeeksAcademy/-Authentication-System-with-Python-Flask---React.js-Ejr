6f9c7a336f2b0ad28a973c5ed0394218bbe68c39
Gab20031995 <luizclzm@gmail.com>
2021-05-12 00:49:57 +0000

    Cambios en los archivos CSS

--- src/front/js/component/globalcard.js
+++ src/front/js/component/globalcard.js
@@ -2,20 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export const Granos = props => {
+export const Globalcard = props => {
 	return (
 		<div className="tarjeta">
 			<div className="card" style={{ width: "16rem" }}>
 				<img
-					src="https://www.peridomicilio.com/images/detailed/28/7441006002460.jpg"
+					src="https://www.cellshop.com/52067-large_default/desodorante-rexona-women-bamboo-48hs-150ml.jpg"
 					className="card-img-top"
 					alt="..."
 				/>
 				<div className="card-body">
-					<h5 className="card-title">Arroz 95% Tio Pelon</h5>
+					<h5 className="card-title">Desodorante Rexona</h5>
 					<ul>
-						<p className="Precio"> Precio: 1500.00 colones</p>
-						<p className="Precio"> Supermercado: Perimercado</p>
+						<p className="Precio"> Precio: 1900.00 colones</p>
+						<p className="Precio"> Supermercado: Magasuper</p>
 					</ul>
 					<a href="#" className="btn btn-outline-success float-right">
 						Ver detalles
