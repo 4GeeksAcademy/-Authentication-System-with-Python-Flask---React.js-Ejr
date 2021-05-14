82364c3ca37e2bbcc40143304066811889c45a55
Gab20031995 <luizclzm@gmail.com>
2021-05-14 04:36:31 +0000

    Cambios

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register1 = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [location, setLocation] = useState("");
+	const [password, setPassword] = useState("");
+
+	const getName = event => {
+		setName(event.target.value);
+	};
+	const getEmail = event => {
+		setEmail(event.target.value);
+	};
+	const getLocation = event => {
+		setLocation(event.target.value);
+	};
+	const getPassword = event => {
+		setPassword(event.target.value);
+	};
+
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	var raw = JSON.stringify({
+		name: name,
+		email: email,
+		password: password,
+		location: location
+	});
+	var requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: raw,
+		redirect: "follow"
+	};
+
+	const registrarUsuario = () => {
+		fetch("https://3001-moccasin-pigeon-4ixmcu8a.ws-us04.gitpod.io/api/register", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	};
+
 	return (
 		<div className="container">
 			<div className="login" />
@@ -12,14 +52,20 @@ export const Register1 = () => {
 						<h3>Registrarme</h3>
 					</div>
 					<div className="card-body">
-						<form>
+						<form onSubmit={registrarUsuario()}>
 							<div className="input-group form-group">
 								<div className="input-group-prepend">
 									<span className="input-group-text">
 										<i className="fas fa-user" />
 									</span>
 								</div>
-								<input type="text" className="form-control" placeholder="Nombre" required />
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Nombre"
+									required
+									onChange={getName}
+								/>
 							</div>
 							<div className="input-group form-group">
 								<div className="input-group-prepend">
@@ -27,15 +73,27 @@ export const Register1 = () => {
 										<i className="fa fa-envelope" />
 									</span>
 								</div>
-								<input type="text" className="form-control" placeholder="Email" required />
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Email"
+									required
+									onChange={getEmail}
+								/>
 							</div>
 							<div className="input-group form-group">
 								<div className="input-group-prepend">
 									<span className="input-group-text">
-										<i className="fas fa-location" />
+										<i className="fas fa-map-marker-alt" />
 									</span>
 								</div>
-								<input type="text" className="form-control" placeholder="Ubicación" required />
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Ubicación"
+									required
+									onChange={getLocation}
+								/>
 							</div>
 							<div className="input-group form-group">
 								<div className="input-group-prepend">
@@ -43,7 +101,13 @@ export const Register1 = () => {
 										<i className="fas fa-key" />
 									</span>
 								</div>
-								<input type="password" className="form-control" placeholder="Contraseña" required />
+								<input
+									type="password"
+									className="form-control"
+									placeholder="Contraseña"
+									required
+									onChange={getPassword}
+								/>
 							</div>
 							<div className="input-group form-group">
 								<div className="input-group-prepend">
