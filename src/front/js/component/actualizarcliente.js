cbca59923ae1560b32d4151da7873325606f2be5
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2021-05-14 20:54:01 -0600

    Create actualizarcliente.js
    vista actualizar cliente

---
+++ src/front/js/component/actualizarcliente.js
@@ -0,0 +1,77 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+export const Actualizarvendedor = () => {
+	return (
+		<div className="container">
+			<form>
+				<h2 className="text-center">Actualizar Cuenta</h2>
+				<img src={logo} alt="logo" className="img-thumbnail mx-auto d-block rounded my-3" />
+				<div className="form-group">
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							name="name"
+							placeholder="Nombre Completo"
+							required="required"
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							name="username"
+							placeholder="Usuario"
+							required="required"
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="input-group">
+						<input
+							type="email"
+							className="form-control"
+							name="email"
+							placeholder="Email"
+							required="required"
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="input-group">
+						<input
+							type="password"
+							className="form-control"
+							name="password"
+							placeholder="Contraseña"
+							required="required"
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<button
+						className="btn btn-login login-btn btn-block"
+						onClick={() => {
+							createNewUser;
+							regAlert();
+						}}>
+						Actualizar Cuenta
+					</button>
+					<button
+						className="btn btn-login login-btn btn-block"
+						onClick={() => {
+							createNewUser;
+							regAlert();
+						}}>
+						Registrar Producto
+					</button>
+				</div>
+
+				<div className="or-seperator" />
+			</form>
+		</div>
+	);
+};
