5e0f86446f3c0db7fe8d6255b82ea8eb278f9b09
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2021-05-14 20:58:50 -0600

    actualizarvendedorview

---
+++ src/front/js/pages/actualizarvendedorview.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Actualizarvendedor } from "../component/actualizarvendedor";
+
+export const Actualizarvendedorview = () => {
+	return (
+		<div>
+			<Actualizarvendedor />
+		</div>
+	);
+};
