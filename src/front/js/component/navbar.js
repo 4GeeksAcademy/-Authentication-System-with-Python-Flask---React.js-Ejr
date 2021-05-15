70b53290f6ca6688662a3a77019923cac03d3940
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2021-05-14 21:05:10 -0600

    agregado de editar vendedor y comprador

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,60 +1,76 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
-import { Link } from "react-router-dom";
-import logo from "../../img/logo.png";
+import { Link, useHistory } from "react-router-dom";
+import logo from "../../img/newAppLogo.jpeg";
 import { Search } from "./search";
 import "../../styles/navbar.scss";
+import "../pages/actualizarclienterview";
+import "../pages/actualizarvendedorview";
 
 export const Navbar = () => {
 	const { actions, store } = useContext(Context);
+	const history = useHistory();
 	return (
 		<section className="ftco-section">
 			<div className="container">
 				<div className="row justify-content-between">
 					<div className="col">
-						<a className="navbar-brand" href="index.html">
-							<img src={logo} />
-						</a>
+						<span>
+							<Link to="/" className="navbar-brand">
+								<img
+									className="rounded float-left"
+									src={logo}
+									style={{ height: "100px", width: "200px" }}
+								/>
+							</Link>
+						</span>
+						{/* <Link to="/" className="navbar-brand">
+							Pura Vida <span>Mart</span>
+						</Link> */}
 					</div>
 					<div className="col d-flex justify-content-end">
 						<div className="social-media">
 							<p className="mb-0 d-flex">
-								<Link
-									to="/registerUserIn"
-									className="d-flex align-items-center justify-content-center"
-									data-toggle="tooltip"
-									title="Crear Cuenta">
-									<span className="fas fa-user-plus" />
-								</Link>
-								<Link
-									to="/logUserIn"
-									className="d-flex align-items-center justify-content-center"
-									data-toggle="tooltip"
-									title="Ingresar">
-									<span className="fas fa-sign-in-alt" disabled={store.isLoggedIn ? true : false} />
-								</Link>
-								<Link
-									to="/"
-									className="d-flex align-items-center justify-content-center"
-									data-toggle="tooltip"
-									title="Salir"
-									onClick={() => {
-										actions.logUserOut();
-									}}
-									disabled={store.isLoggedIn ? false : true}>
-									<span className="fas fa-sign-out-alt" />
-								</Link>
-								<Link
-									to="/"
-									className="d-flex align-items-center justify-content-center"
-									data-toggle="tooltip"
-									title="Comprar"
-									onClick={() => {
-										//actions.logUserOut();
-									}}
-									disabled={store.isLoggedIn ? false : true}>
-									<span className="fa fa-shopping-cart" />
-								</Link>
+								{store.token && store.token !== "" && store.token !== undefined ? null : (
+									<Link
+										to="/registerUserIn"
+										className="d-flex align-items-center justify-content-center"
+										data-toggle="tooltip"
+										title="Crear Cuenta">
+										<span className="fas fa-user-plus" />
+									</Link>
+								)}
+								{store.token && store.token !== "" && store.token !== undefined ? null : (
+									<Link
+										to="/logUserIn"
+										className="d-flex align-items-center justify-content-center"
+										data-toggle="tooltip"
+										title="Ingresar">
+										<span className="fas fa-sign-in-alt" />
+									</Link>
+								)}
+								{store.token && store.token !== "" && store.token !== undefined ? (
+									<Link
+										to="/"
+										className="d-flex align-items-center justify-content-center"
+										data-toggle="tooltip"
+										title="Salir"
+										onClick={() => {
+											actions.logUserOut();
+											history.push("/");
+										}}>
+										<span className="fas fa-sign-out-alt" />
+									</Link>
+								) : null}
+								{store.token && store.token !== "" && store.token !== undefined ? (
+									<Link
+										to="/"
+										className="d-flex align-items-center justify-content-center"
+										data-toggle="tooltip"
+										title="Carrito de Compras">
+										<span className="fas fa-shopping-cart" />
+									</Link>
+								) : null}
 							</p>
 						</div>
 					</div>
@@ -98,17 +114,22 @@ export const Navbar = () => {
 									Vistas
 								</a>
 								<div className="dropdown-menu" aria-labelledby="dropdown04">
-									<a className="dropdown-item" href="#">
-										Nuevo Producto
-									</a>
+									<a className="dropdown-item">Nuevo Producto</a>
 									<a className="dropdown-item" href="/checkoutview.js">
 										Comprar
 									</a>
 									<a className="dropdown-item" href="/logueado.js">
 										logueado
 									</a>
-									<a className="dropdown-item" href="#">
-										Page 4
+									<a className="dropdown-item">
+										<Link to="/actualizarvendedorview" className="nav-link">
+											Atualizar Vendedor
+										</Link>
+									</a>
+									<a className="dropdown-item">
+										<Link to="/actualizarclienteview" className="nav-link">
+											Atualizar Comprador
+										</Link>
 									</a>
 								</div>
 							</li>
