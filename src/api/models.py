44c3bbcdfc920b36caf6cc170dd3f0a904256dee
mojonapower <maca95@gmail.com>
2021-12-23 22:54:28 +0000

    delete: se elimino en models.py funcion repr de clase User

--- src/api/models.py
+++ src/api/models.py
@@ -8,8 +8,6 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
     def serialize(self):
         return {
