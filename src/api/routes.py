a130a585933d2fa77262d825f54632cdebd89351
KrisEng123 <kristalleng@outlook.de>
2022-03-09 18:56:58 +0000

    login-structure

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,15 +12,18 @@ import os
 
 api = Blueprint('api', __name__)
 
-
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
 @api.route("/token", methods=["POST"])
 def create_token():
-    login = request.json.get("login", None)
-    password = request.json.get("password", None)
+    login = request.json.get('login')
+    password = request.json.get('password')
     if login != "test" or password != "test":
-        return jsonify({"msg": "Bad login or password"}), 401
+         return jsonify({"msg": "Bad login or password"}), 401
+
+    # To-do: 1. verify that the user exists in the database
+    # 2. compare the hash from the password of the password input (line20) with the hash in the database (password field in the database)
+    # 3. if above true, return token (line 28), if not, return error (line 22)
 
     access_token = create_access_token(identity=login)
     return jsonify(access_token=access_token)
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,49 +3,69 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 const Login = () => {
-	const { store, actions } = useContext(Context);
-	const {login, setLogin} = useState("");
-	const {password, setPassword} = useState("");
+  const { store, actions } = useContext(Context);
+  const [ user, setUser ] = useState("");
+  const [ password, setPassword ] = useState("");
 
-	const handleClick = () => {
-		const opts = {
-			method: 'POST',
-			headers: {
-				"Content-Type" : "application/json"
-			},
-			body: JSON.stringify({
-				"login" : login,
-				"password" : password
-			})
-		}
+  const handleClick = () => {
+	const opts = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        login: user,
+        password: password
+        }),
+    };
 
+	console.log(opts)
 
-		fetch('https://3000-nozigs-reactflaskhello-0v4i02inons.ws-eu34.gitpod.io/api/token', opts)
-			.then(resp => {
-				if(resp.status === 200) return resp.json();
-				else alert("There was an error!")
-			})
-			.then()
-			.catch(error => {
-				console.error("There was an error!", error);
-			} )
-	}
+    fetch(
+      "https://3001-nozigs-reactflaskhello-0v4i02inons.ws-eu34.gitpod.io/api/token",
+      opts
+    )
+      .then((resp) => {
+        if (resp.status === 200) return resp.json();
+        else alert("There was an error!");
+      })
+      .then()
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  };
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Login</h1>
-			<br></br>
-			<div>
-				<input type="text" placeholder="your login" value={login} onChange={(e) => setLogin(e.target.value)} />
-				<br></br>
-				<br></br>
-				<input type="password" placeholder="your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-				<br></br>
-				<br></br>
-				<button type="button" class="btn btn-outline-info" onClick={handleClick}>Login</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <h1>Login</h1>
+      <br></br>
+      <div>
+        <input
+          type="text"
+          placeholder="your login"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <input
+          type="password"
+          placeholder="your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br></br>
+        <br></br>
+        <button
+          type="button"
+          className="btn btn-outline-info"
+          onClick={handleClick}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
