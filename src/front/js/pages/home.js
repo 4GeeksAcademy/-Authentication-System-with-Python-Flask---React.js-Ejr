13f0b77b6e62aef4d18842f96a528cf1035cb64c
xetnal <santiago.deaguirre@gmail.com>
2022-05-11 16:02:37 +0000

    first commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,19 +8,8 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<h1>Invierte</h1>
+			
 		</div>
 	);
 };
