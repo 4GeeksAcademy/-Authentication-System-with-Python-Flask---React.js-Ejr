5c8e948ba8802b70904939af9d2294bdb8e7b0c6
Israel Munoz <imunoz.developer@gmail.com>
2022-05-12 17:27:50 -0400

    Se realizan los componenetes Proyect, UserHome, UserProfile para que retornen algo

---
+++ src/front/js/views/userProfile.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../styles/login.css";
+import { Link } from "react-router-dom";
+
+export const UserProfile = () => {
+  return (
+    <h1>Santiago te Amo</h1>
+  );
+};
