1a4121207fe402d126dc117551f67181913307e8
mariaherreramilla <maria.millaherrera@gmail.com>
2022-05-12 21:50:27 +0000

    companydashboard

--- src/front/js/views/companydashboard.js
+++ src/front/js/views/companydashboard.js
@@ -13,7 +13,7 @@ export const UserProfileSetup = () => {
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
-                This is a wider card with supporting text below as a natural lead-in
+                This  is a wider card with supporting text below as a natural lead-in
                 to additional content. This content is a little bit longer.
                 </p>
                 <p className="card-text">
