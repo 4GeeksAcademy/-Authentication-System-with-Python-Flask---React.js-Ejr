830a13e28e0962ce43a722b67b9b568cd0f15f7a
mariaherreramilla <maria.millaherrera@gmail.com>
2022-05-15 21:56:47 +0000

    comanyompanydashboard

--- src/front/js/views/companydashboard.js
+++ src/front/js/views/companydashboard.js
@@ -5,49 +5,167 @@ import { Link } from "react-router-dom";
 export const UserProfileSetup = () => {
   return (
     <>
-      
       <form>
         <div className="card-group">
-            <div className="card">
+          <div className="card">
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                This  is a wider card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.
-                </p>
-                <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+              <h5 className="card-title"> Nombre Proyecto </h5>
+              <p className="card-text">
+                Características del Proyecto.
+              </p>
+              <p className="card-text">
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic checkbox toggle button group"
+                >
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck1"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck1"
+                  >
+                    Checkbox 1
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck2"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck2"
+                  >
+                    Checkbox 2
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck3"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck3"
+                  >
+                    Checkbox 3
+                  </label>
+                </div>
+              </p>
             </div>
-            </div>
-            <div className="card">
+          </div>
+          <div className="card">
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                This card has supporting text below as a natural lead-in to additional
-                content.
-                </p>
-                <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+              <h5 className="card-title"> Nombre Proyecto </h5>
+              <p className="card-text">
+                Características del Proyecto.
+              </p>
+              <p className="card-text">
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic checkbox toggle button group"
+                >
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck1"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck1"
+                  >
+                    Checkbox 1
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck2"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck2"
+                  >
+                    Checkbox 2
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck3"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck3"
+                  >
+                    Checkbox 3
+                  </label>
+                </div>
+              </p>
             </div>
-            </div>
-            <div className="card">
+          </div>
+          <div className="card">
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                This is a wider card with supporting text below as a natural lead-in
-                to additional content. This card has even longer content than the
-                first to show that equal height action.
-                </p>
-                <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+              <h5 className="card-title">Nombre Proyecto</h5>
+              <p className="card-text">
+                Características del Proyecto
+              </p>
+              <p className="card-text">
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic checkbox toggle button group"
+                >
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck1"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck1"
+                  >
+                    Checkbox 1
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck2"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck2"
+                  >
+                    Checkbox 2
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="btncheck3"
+                    autoComplete="off"
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="btncheck3"
+                  >
+                    Checkbox 3
+                  </label>
+                </div>
+              </p>
             </div>
-            </div>
+          </div>
         </div>
       </form>
     </>
