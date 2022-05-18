ea659642587abf9b67c86c022c4663820854a090
Israel Munoz <imunoz.developer@gmail.com>
2022-05-18 11:49:34 -0400

    Se sube el layout de proyect.js

---
+++ src/front/js/views/userProyects.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../styles/login.css";
+import { Link } from "react-router-dom";
+
+export const UserProyects = () => {
+  return (
+      <div>User Proyects</div>
+  );
+};
