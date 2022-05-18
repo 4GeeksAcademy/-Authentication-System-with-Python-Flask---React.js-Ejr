18a225092d9955e374ebb1706199f89d9d07077b
joaquin <juacopinto@gmail.com>
2022-05-18 17:46:59 -0400

    upgrade del company dashboard y login empresa

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,24 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Investor</span>
-        </Link>
-        <Link to="/company_register">
-          <button className="btn btn-success">Empresas</button>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/register">
-            <button className="btn btn-success mx-5">Registrate</button>
-          </Link>
-          <Link to="/login">
-            <button className="btn btn-success">Ingresa</button>
-          </Link>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">Investor</span>
+				</Link>
+				<Link to="/company_register">
+					<button className="btn btn-success">Empresas</button>
+				</Link>
+				<div className="ml-auto">
+					<Link to="/register">
+						<button className="btn btn-success mx-5">Registrate</button>
+					</Link>
+					<Link to="/login">
+						<button className="btn btn-success">Ingresa</button>
+					</Link>
+					<Link to="/company_login">
+						<button className="btn btn-success">Ingreso Empresa</button>
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
 };
---
+++ src/front/js/views/loginEmpresa.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/login.css";
+
+export const LoginEmpresa = () => {
+	return (
+		<div id="login my-5">
+			<div className="container my-5">
+				<div
+					id="login-row"
+					className="row justify-content-center align-items-center"
+				>
+					<div id="login-column" className="col-md-6">
+						<div id="login-box" className="col-md-12">
+							<form id="login-form" className="form">
+								<h3 className="text-center text-info">Login Empresa</h3>
+								<div className="form-group">
+									<label className="text-info">Nombre de Empresa:</label>
+									<br />
+									<input
+										type="text"
+										name="companyName"
+										id="companyName"
+										className="form-control"
+									/>
+								</div>
+								<div className="form-group">
+									<label for="password" className="text-info">
+										Contraseña:
+									</label>
+									<br />
+									<input
+										type="text"
+										name="companyPassword"
+										id="companyPassword"
+										className="form-control"
+									/>
+								</div>
+								<br />
+								<div className="form-group">
+									<Link to="/company_dashboard">
+										<button
+											type="submit"
+											name="submit"
+											className="btn btn-info btn-md"
+											value="submit"
+										>
+											Entrar
+										</button>
+									</Link>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
