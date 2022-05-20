c26aa1c7e4ae3bba34013349dedd88168a832683
joaquin <juacopinto@gmail.com>
2022-05-20 16:52:35 -0400

    formulario ingreso de proyecto

--- src/front/js/views/companydashboard.js
+++ src/front/js/views/companydashboard.js
@@ -1,159 +1,110 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 /* import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import { Proyect } from "./proyect"; */
 import { CompanyProyects } from "./companyProyects";
-
-/* "company_id": self.company_id,
-            "title": self.title,
-            "address": self.address,
-            "comuna": self.comuna,
-            "ciudad": self.ciudad,
-            "size": self.size,
-            "sale_type": self.sale_type,
-            "bathrooms": self.bathrooms, 
-            'rooms': self.rooms,
-            'monto_reserva': self.monto_reserva,
-            'bono_pie': self.bono_pie,
-            'parking_spots': self.parking_spots,
-            'bodega': self.bodega,
-            'total_price': self.total_price,
-            'pictures': self.pictures,
-            'body': self.body,
-            'perks': self.perks */
+import { RegisterProyectForm } from "./registerProyectForm";
 
 export const CompanyDashboard = () => {
-	const handleSubmit = (e) => {
-		console.log("hola");
+	const [title, setTitle] = useState("");
+	const [address, setAdress] = useState("");
+	const [size, setSize] = useState(0);
+	const [sale_type, setSale_type] = useState("");
+	const [bathrooms, setBathrooms] = useState(0);
+	const [perks, setPerks] = useState("");
+	const [rooms, setRooms] = useState("");
+	const [monto_reserva, setMonto_reserva] = useState(0);
+	const [bono_pie, setBono_pie] = useState(0);
+	const [parking_spot, setParking_spot] = useState(0);
+	const [bodega, setBodega] = useState("");
+	const [typology, setTypology] = useState("");
+	const [total_price, setTotal_price] = useState(0);
+	const [pictures, setPictures] = useState("sin-foto.png");
+	//agregar las siguientes
+	const [comuna, setComuna] = useState("");
+	const [ciudad, setCiudad] = useState("");
+	const [body, setBody] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const formData = new FormData();
+		formData.append("tittle", title);
+		formData.append("comuna", comuna);
+		formData.append("ciudad", ciudad);
+		formData.append("body", body);
+		formData.append("address", address);
+		formData.append("size", size);
+		formData.append("sale_type", sale_type);
+		formData.append("bathrooms", bathrooms);
+		formData.append("perks", perks);
+		formData.append("rooms", rooms);
+		formData.append("monto_reserva", monto_reserva);
+		formData.append("bono_pie", bono_pie);
+		formData.append("parking_spot", parking_spot);
+		formData.append("bodega", bodega);
+		formData.append("typology", typology);
+		formData.append("total_price", total_price);
+		formData.append("pictures", pictures);
+
+		const info = await registerFetch(formData);
+		console.log(info);
+	};
+
+	const registerFetch = async (data) => {
+		const respuesta = await fetch(
+			"https://3001-xetnal-finalproject-rjwnejuo77t.ws-us45.gitpod.io/api/projects",
+			{ method: "POST", body: data }
+		);
+		const info = await respuesta.json();
+		return info;
 	};
 
 	return (
 		<>
 			<h1>Estos son tus proyectos ya publicados:</h1>
 			<br />
+
 			<CompanyProyects />
 
 			<h1>Formulario para subir un proyecto:</h1>
-			<div className="container">
-				<form className="row" onSubmit={handleSubmit}>
-					<div className="col-6">
-						<div className="form-group">
-							<label>Nombre del Proyecto</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputNombre"
-								aria-describedby="emailHelp"
-								placeholder="Ingresa tu nombre"
-							/>
-							<label>Direccion</label>
-							<input
-								type="text"
-								className="form-control"
-								id="Rut"
-								aria-describedby="emailHelp"
-								placeholder="Ingresa tu rut"
-							/>
-							<label>Mt2</label>
-							<input
-								type="telephone"
-								className="form-control"
-								id="InputTelefono"
-								aria-describedby="emailHelp"
-								placeholder="Ingresa tu telefono"
-							/>
-							<label>Tipo de Venta</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputTipoVenta"
-								placeholder="Ingresa el tipo de venta"
-							/>
-							<label>Numero de Baños:</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputBaños"
-								placeholder="Cantidad de Baños"
-							/>
-							<label>Tipo de Venta</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputTipoVenta"
-								placeholder="Ingresa el tipo de venta"
-							/>
-							<label>Numero de Habitaciones</label>
-							<input
-								type="number"
-								className="form-control"
-								id="InputHabitaciones"
-								placeholder="Ingrese cantidad de habitaciones"
-							/>
-							{/* <Link to="/user_profile_setup"> */}
-							<button type="submit" className="btn btn-primary mt-3">
-								Publicar
-							</button>
-							{/* </Link> */}
-						</div>
-					</div>
-					{/* de aqui para abajo, la columna derecha del fromulairo */}
-					<div className="col-6">
-						<div className="form-group">
-							<label>Monto de Reserva</label>
-							<input
-								type="number"
-								className="form-control"
-								id="InputMontoReserva"
-								placeholder="Ingresa el Monto"
-							/>
-							<label>Bono Pie</label>
-							<input
-								type="number"
-								className="form-control"
-								id="InputBonoPie"
-								placeholder="Ingrese si cuenta con un bono"
-							/>
-							<label>Numero de Estacionamientos</label>
-							<input
-								type="number"
-								className="form-control"
-								id="InputEstacionamiento"
-								placeholder="Ingrese el numero de estacionamientos"
-							/>
-							<label>Bodega</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputBodega"
-								placeholder="Cuenta con Bodega?"
-							/>
-							<label>Descripcion</label>
-							<input
-								type="text"
-								className="form-control"
-								id="InputDescripcion"
-								placeholder="Ingrese una descripcion"
-							/>
-							<label>Precio</label>
-							<input
-								type="email"
-								className="form-control"
-								id="InputEmail1"
-								aria-describedby="emailHelp"
-								placeholder="Ingresa tu email"
-							/>
-							<label>Foto</label>
-							<input
-								type="password"
-								className="form-control"
-								id="InputPassword1"
-								placeholder="Ingresa tu contraseña"
-							/>
-						</div>
-					</div>
-				</form>
-			</div>
+			<RegisterProyectForm
+				handleSubmit={handleSubmit}
+				nombre={title}
+				setNombre={setTitle}
+				direccion={address}
+				setDireccion={setAdress}
+				tamaño={size}
+				setTamaño={setSize}
+				tipoVenta={sale_type}
+				setTipoVenta={setSale_type}
+				numeroBaños={bathrooms}
+				setNumeroBaños={setBathrooms}
+				ventajas={perks}
+				setVentajas={setPerks}
+				habitaciones={rooms}
+				setHabitaciones={setRooms}
+				reserva={monto_reserva}
+				setReserva={setMonto_reserva}
+				pie={bono_pie}
+				setPie={setBono_pie}
+				estacionamiento={parking_spot}
+				setEstacionamiento={setParking_spot}
+				bodega={bodega}
+				setBodega={setBodega}
+				descripcion={typology}
+				setDescripcion={setTypology}
+				precio={total_price}
+				setPrecio={setTotal_price}
+				imagen={pictures}
+				setImagen={setPictures}
+				comuna={comuna}
+				setComuna={setComuna}
+				ciudad={ciudad}
+				setCiudad={setCiudad}
+				body={body}
+				setBody={setBody}
+			/>
 		</>
 	);
 };
