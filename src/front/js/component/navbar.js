6568e6482a83c42e1f97eea30acf7d876342567b
xetnal <santiago.deaguirre@gmail.com>
2022-05-24 21:48:19 +0000

    fixed navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,45 +1,51 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
+  const history = useHistory();
+  const onLogout = () => {
+    history.push("/home");
+  };
 
-	const { store, actions } = useContext(Context)
-	const history = useHistory();
-	const onLogout = () => {
-		history.push('/home')
-	}
-
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Investor</span>
-				</Link>
-				<Link to="/company_register">
-					<button className="btn btn-success">Empresas</button>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/register">
-						<button className="btn btn-success mx-5">Registrate</button>
-					</Link>
-					{!store.token ?
-					<Link to="/login">
-						<button className="btn btn-success">Ingresar</button>
-					</Link>
-					:
-					<Link to="/">
-					<button onClick={() => actions.logout()} className="btn btn-success">Salir</button>
-					</Link>
-								
-				}
-					<Link to="/company_login">
-						<button className="btn btn-success">Ingreso Empresa</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Investor</span>
+        </Link>
+        <Link to="/company_register">
+          <button className="btn btn-success">Empresas</button>
+        </Link>
+        <div className="ml-auto">
+          {!store.token ? (
+            <>
+              <Link to="/register">
+                <button className="btn btn-success mx-5">Registrate</button>
+              </Link>
+              <Link to="/login">
+                <button className="btn btn-success">Ingresar</button>
+              </Link>
+              <Link to="/company_login">
+                <button className="btn btn-success mx-5">
+                  Ingreso Empresa
+                </button>
+              </Link>
+            </>
+          ) : (
+            <Link to="/">
+              <button
+                onClick={() => actions.logout()}
+                className="btn btn-success mx-5"
+              >
+                Salir
+              </button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
