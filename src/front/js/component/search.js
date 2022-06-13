798d9858473d8f0f107fcd3c5a52ae8d0c195cf5
Daninzrth <dani.angulo1910@gmail.com>
2022-06-13 18:37:29 +0000

    Cambios a la Busqueda Avanzada

--- src/front/js/component/search.js
+++ src/front/js/component/search.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Search = () => {
   return (
-    <div className="mask d-flex align-items-center">
+    <div className="mask d-flex align-items-center my-5">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <div className="card mb-2">
+          <div className="col-md-10 col-lg-8 col-xl-12 mx-auto rounded">
+            <div className="card mb-2 mb-5">
               <div className="card-body p-2">
                 <div className="input-group input-group-lg">
                   <input
@@ -22,11 +22,11 @@ export const Search = () => {
                 </div>
               </div>
             </div>
-            <div className="card bg-black">
+            <div className="card bg-dark">
               <div className="card-body p-4">
                 <h6 className="text-white">Busqueda Avanzada</h6>
                 <div className="row">
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-2 mb-2 ms-5">
                     <div class="dropdown">
                       <button
                         class="btn btn-secondary dropdown-toggle"
@@ -64,7 +64,7 @@ export const Search = () => {
                       </ul>
                     </div>
                   </div>
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-2 mb-2">
                     <div className="dropdown">
                     <button
                         class="btn btn-secondary dropdown-toggle"
@@ -102,7 +102,45 @@ export const Search = () => {
                       </ul>
                     </div>
                   </div>
-                  <div className="col-md-4 mb-3">
+                  <div className="col-md-3 mb-2">
+                    <div className="dropdown">
+                    <button
+                        class="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Precio por Publicación
+                      </button>
+                      <ul
+                        className="dropdown-menu"
+                        aria-labelledby="dropdownMenuLink1"
+                      >
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            0€ - 100€
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            100€ - 300€
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            300€ - 500€
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            + 500€
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-md-2 mb-2">
                     <div className="dropdown">
                     <button
                         class="btn btn-secondary dropdown-toggle"
@@ -119,34 +157,112 @@ export const Search = () => {
                       >
                         <li>
                           <a className="dropdown-item" href="#">
-                            Madrid
+                            Andalucía
                           </a>
                         </li>
                         <li>
                           <a className="dropdown-item" href="#">
-                            Barcelona
+                            Aragón
                           </a>
                         </li>
                         <li>
                           <a className="dropdown-item" href="#">
-                            Islas Canarias
+                            Asturias
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Islas Baleares
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Canarias
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Cantabria
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Castilla y León
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Castilla-La Mancha
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Cataluña
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Comunidad Valenciana
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Extremadura
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Galicia
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Comunidad de Madrid
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Región de Murcia
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Comunidad de Navarra
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            País Vasco
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            La Rioja
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Ciudad Autónoma de Ceuta
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Ciudad Autónoma de Melilla
                           </a>
                         </li>
                       </ul>
                     </div>
                   </div>
-                </div>
-                <div className="d-flex justify-content-between align-items-center mt-4">
-                  <div>
-                    <button
+                  <div className="col-md-2 pe-1 ms-5">
+                  <button
                       type="button"
                       className="btn btn-link text-white"
                       data-mdb-ripple-color="dark"
                     >
-                      Reset
+                      Reiniciar
                     </button>
                     <button type="button" className="btn btn-warning">
-                      Search
+                      Buscar
                     </button>
                   </div>
                 </div>
