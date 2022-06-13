c4b83a52e51536194dec2675d10fdd1f15ba329b
jaygosling <javijimenezclemente@gmail.com>
2022-06-13 18:29:16 +0000

    Revert "footer"
    This reverts commit e3a78439b45799ee1c311942bd830b2e7c408a57.
    
    Deshacer commit

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-		<footer className="footer mt-auto py-3 text-center">
-		<div className="container">
-			<p>Logo Proyecto Final</p>
-		</div>
-		<div className="container d-flex justify-content-evenly">
-			<div>Política de Privacidad</div>
-			<div>Términos del Servicio</div>
-			<div>Contacto</div>
-		</div>
-		<hr class="text-primary border-4 opacity-50"></hr>
-		<div className="container">
-			<p className="text-secondary">
-				© influĕre 2022 Todos los derechos reservados
-			</p>
-		</div>
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
 	</footer>
 );
