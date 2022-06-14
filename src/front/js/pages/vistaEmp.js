b051f1ea40f611e43cf47096cdf672d28bf36343
natigalsan <natividadgalsan@gmail.com>
2022-06-14 09:07:14 +0000

    perfil emp

--- src/front/js/pages/vistaEmp.js
+++ src/front/js/pages/vistaEmp.js
@@ -3,73 +3,56 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const VistaEmp = () =>{
+export const VistaEmp = () => {
     return <>
-        <div className="container-fluid">
-            <div
-                className="container img-principal"
-                id="imgPerfil2"
-                style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: "10px 80px",
-                minHeight: "500px",
-                }}
-            >
-                <img
-                className="img2 rounded-circle"
-                style={{ maxWidth: "300px", height:"300px" }}
-                src={
-                    "https://images.pexels.com/photos/6476587/pexels-photo-6476587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                }
-               
-                />
-            </div>
-            <br></br>
-            <br></br>
+        <div className="container">
+            <div className="row" style={{ maxWidth: "1440px", border: "1px solid lightgrey", borderRadius: "10px 80px" }}>
 
-            <div className="headerVI" style={{ position: "relative" }}>
-                <h1
-                className="title1 text-center"
-                style={{ color: "rgba(59, 102, 197, 0.952)" }}
-                >
-                Nombre de Empresa
-                </h1>
-                <h3 className="title2 text-center">SECTOR AL QUE PERTENECE</h3>
-                <h5 className="textoBreve1 text-center" style={{ opacity: "40%" }}>
-                Eslogan
-                </h5>
+                <div className=" col-4 img-principal"
+                    id="imgPerfil"
+                    style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+                    <img
+                        className="img img-fluid"
+                        style={{ maxWidth: "400px", maxHeight: "350px" }}
+                        src={
+                            ""
+                        }
+                        alt={"imgPefil"}
+                    />
+                </div>
                 <br></br>
-                <h5 className="textobreve2 text-center " style={{ opacity: "40%" }}>
-                Breve descripción sobre el influencer, escrito por él mismo, puede
-                introducir actitudes, aptitudes y logros.
-                </h5>
-                <h5 className="textobreve2 text-center " style={{ opacity: "40%" }}>
-                Ubicación .... Provincia, ciudad             </h5>
-            </div>
-            <br></br>
-            <br></br>
-            <div
-                className="publicaciones"
-                style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                }}
-            >
-                <h2>INFLUENCERS FAVORITOS</h2>
-            </div>
-            <br></br>
-            <br></br>
+                <br></br>
+                <div className="col-8" >
+                    <div className="headerVI" style={{ position: "relative", marginTop: "100px" }}>
+                        <h1
+                            className="title1"
+                            style={{ color: "rgba(59, 102, 197, 0.952)" }}
+                        >
+                            Moderna de Pueblo
+                        </h1>
+                        <h3 className="title2 ">SECTOR AL QUE PERTENECE</h3>
+                        <h5 className="textoBreve1 " style={{ pacity: "40%" }}>
+                            Eslogan
+                        </h5>
+                        <br></br>
+                        <h5 className="textobreve2 " style={{ opacity: "40%", maxWidth: "80%" }}>
+                            Breve descripción sobre el influencer, escrito por él mismo, puede
+                            introducir actitudes, aptitudes y logros.
+                        </h5>
+                    </div>
+                    <br></br>
+                    <br></br>
 
 
+
+                </div>
+            </div>
 
         </div>
-    
-    
-    
-    
+
+
+
+
     </>
 
 }
