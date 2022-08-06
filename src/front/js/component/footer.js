f1f44324d4e9d984a438bf059cba8c882112c83d
marellanorero <marellanonorero@gmail.com>
2022-08-06 15:45:57 +0000

    ordenado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSeedling } from '@fortawesome/free-solid-svg-icons';
-import { AiFillFacebook } from 'react-icons/ai';
-import { FaHandPointDown } from 'react-icons/fa';
-import { AiOutlineInstagram } from 'react-icons/ai';
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { AiFillFacebook } from "react-icons/ai";
+import { FaHandPointDown } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 function Footer() {
-
   return (
-    <footer className=' bg-success text-light text-center pt-3'>
-      <h6>© Casino Corporativo <FontAwesomeIcon icon={faSeedling} /> 2022  </h6>
-      <p>¡Síguenos en nuestras redes sociales! <FaHandPointDown /></p>
-      <button className='btn btn-secondary me-3 text-dark border-warning bg-warning rounded-circle'><AiFillFacebook />  </button>
-      <button className='btn btn-secondary text-dark border-warning bg-warning rounded-circle'> <AiOutlineInstagram /> </button>
+    <footer className=" bg-success text-light text-center p-4">
+      <h6>
+        © Casino Corporativo <FontAwesomeIcon icon={faSeedling} /> 2022{" "}
+      </h6>
+      <p>
+        ¡Síguenos en nuestras redes sociales! <FaHandPointDown />
+      </p>
+      <button className="btn btn-secondary me-3 text-dark border-warning bg-warning rounded-circle mb-4">
+        <AiFillFacebook />{" "}
+      </button>
+      <button className="btn btn-secondary text-dark border-warning bg-warning rounded-circle mb-4">
+        {" "}
+        <AiOutlineInstagram />{" "}
+      </button>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import SlideCarousel from '../component/slideCarousel';
-import TextoHome from '../component/textoHome';
-
-
+import React from "react";
+import SlideCarousel from "../component/slideCarousel";
+import TextoHome from "../component/textoHome";
 
 function Home() {
-
-    return (
-        <>
-            <SlideCarousel />
-            <TextoHome />
-        </>
-    );
+  return (
+    <>
+      <SlideCarousel />
+      <TextoHome />
+    </>
+  );
 }
 
 export default Home;
