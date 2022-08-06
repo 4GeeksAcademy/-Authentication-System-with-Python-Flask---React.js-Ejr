1981995715da1baf00498609bf3433017c1a9621
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-08-06 19:50:39 +0000

    avance

--- src/api/models.py
+++ src/api/models.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from eralchemy import render_er
 from flask_sqlalchemy import SQLAlchemy
 
 #falta backref en relationships y lazy=true
@@ -12,7 +11,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Direccion(db.Model):
-    __tablename__ = 'direccion'
     id = db.Column(db.Integer, primary_key=True)
     nombre_calle = db.Column(db.String(120), unique=False, nullable=False)
     numero_calle = db.Column(db.Integer, unique=True, nullable=False)
@@ -28,7 +26,6 @@ class Direccion(db.Model):
         }
 
 class Empresa(db.Model):
-    __tablename__ = 'empresa'
     id = db.Column(db.Integer, primary_key=True)
     nombre_empresa = db.Column(db.String(80), unique=True, nullable=False)
     cantidad_trabajadores = db.Column(db.Integer, unique=False, nullable=False)
@@ -43,7 +40,6 @@ class Empresa(db.Model):
 
 
 class Usuario(db.Model):
-    __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True)
     nombre_usuario = db.Column(db.String(80), unique=False, nullable=False)
     apellido_usuario = db.Column(db.String(80), unique=False, nullable=False)
@@ -72,7 +68,6 @@ class Usuario(db.Model):
 
 
 class Reporte(db.Model):
-    __tablename__ = 'reporte'
     id = db.Column(db.Integer, primary_key=True)
     contenido = db.Column(db.String(120), unique=False, nullable=False)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), unique = False, nullable = False)
@@ -88,7 +83,6 @@ class Reporte(db.Model):
 
 
 class Pedidos(db.Model):
-    __tablename__ = 'pedidos'
     id = db.Column(db.Integer, primary_key=True)
     colacion_id = db.Column(db.Integer, db.ForeignKey('colacion.id'), unique = False, nullable = False)
     usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), unique = False, nullable = False)
@@ -107,30 +101,25 @@ class Pedidos(db.Model):
 
 
 class Principal(db.Model):
-    __tablename__ = 'principal'
     id = db.Column(db.Integer, primary_key=True)
     descripcion_principal = db.Column(db.String(150), unique = True, nullable = False)
 
 
 class Ensalada(db.Model):
-    __tablename__ = 'ensalada'
     id = db.Column(db.Integer, primary_key=True)
     descripcion_ensalada = db.Column(db.String(150), unique = True, nullable = False)
 
 
 class Postre(db.Model):
-    __tablename__ = 'postre'
     id = db.Column(db.Integer, primary_key=True)
     descripcion_postre = db.Column(db.String(150), unique = True, nullable = False)
 
 class Bebida(db.Model):
-    __tablename__ = 'bebida'
     id = db.Column(db.Integer, primary_key=True)
     descripcion_bebida = db.Column(db.String(150), unique = True, nullable = False)
 
 
 class Casino(db.Model):
-    __tablename__ = 'casino'
     id = db.Column(db.Integer, primary_key=True)
     principal_id = db.Column(db.Integer, db.ForeignKey('principal.id'), unique = False, nullable = False)
     ensalada_id = db.Column(db.Integer, db.ForeignKey('ensalada.id'), unique = False, nullable = False)
@@ -143,7 +132,6 @@ class Casino(db.Model):
 
 
 class Colacion(db.Model):
-    __tablename__ = 'colacion'
     id = db.Column(db.Integer, primary_key=True)
     casino_id = db.Column(db.Integer, db.ForeignKey('casino.id'), unique = False, nullable = False)
     casino = db.relationship('Casino')
@@ -157,13 +145,3 @@ class Colacion(db.Model):
             "pedidos_id": self.pedidos_id,
             # do not serialize the password, its a security breach
         }
-
-
-
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
-render_er(db.Model, 'diagram.png')
-
-    
