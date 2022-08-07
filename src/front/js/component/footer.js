02f57c8e6e90f2cb48bffaec2274d661963c24c4
Ramses Cevallos <cevallosramses@gmail.com>
2022-08-07 23:07:55 +0000

    fixed links color

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,38 +5,45 @@ import "../../styles/footer.css";
 export const Footer = () => {
   return (
     <div id="footer" className="container-fluid">
-      <div className="row">
-        <div className="col-4 mt-3">
-          <h5>Sobre nosotros</h5>
-          <hr className="light" />
-          <Link to={"/contacto"}>
-            <p>Contacto</p>
-          </Link>
-          <Link to={"/preguntas"}>
-            <p>Preguntas frecuentes</p>
-          </Link>
-        </div>
-        <div className="col-4 mt-3">
-          <h5>Países donde estamos</h5>
-          <hr className="light" />
-          <p>Costa Rica</p>
-        </div>
-        <div className="d-grid col-4 my-auto">
-          <div className="mx-auto">
-            <Link to={"/registrocaminador"}>
-              <button className="btn rounded-pill">
-                Convierte en caminador
-              </button>
+      <div className="container m-auto p-2">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-4 mt-3 align-items-center">
+            <h5>Sobre nosotros</h5>
+            <hr className="light" />
+            <Link style={{ textDecoration: "none" }} to={"/contacto"}>
+              <p>Contacto</p>
+            </Link>
+            <Link style={{ textDecoration: "none" }} to={"/preguntas"}>
+              <p>Preguntas frecuentes</p>
             </Link>
           </div>
+          <div className="col-sm-12 col-md-6 col-lg-4 mt-3 align-items-center">
+            <h5>Países donde estamos</h5>
+            <hr className="light" />
+            <p>Costa Rica</p>
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-4 my-auto align-items-center">
+            <div className="text-center">
+              <Link to={"/registrocaminador"}>
+                <button className="btn rounded-pill">
+                  Convierte en caminador
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <footer className="text-center mt-3">
+          <p>
+            Copyright &copy; 2022.{" "}
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to={"/terminosycondiciones"}
+            >
+              TERMINOS Y CONDICIONES
+            </Link>
+          </p>
+        </footer>
       </div>
-      <footer className="text-center">
-        <p>
-          Copyright &copy; 2022.{" "}
-          <Link to={"/terminosycondiciones"}>TERMINOS Y CONDICIONES</Link>
-        </p>
-      </footer>
     </div>
   );
 };
