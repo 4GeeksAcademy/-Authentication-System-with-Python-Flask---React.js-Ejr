f0a547d38224dd67ff9e52b83086c8b053b95b8c
Roberto Lopez J <roberto@lightsaint.com>
2022-08-07 23:54:02 +0000

    [Added styles to home] -Added the curves and bettered the styles

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,38 +5,42 @@ import "../../styles/footer.css";
 export const Footer = () => {
   return (
     <div id="footer" className="container-fluid">
-      <div className="row">
-        <div className="col-4 mt-3">
-          <h5>Sobre nosotros</h5>
-          <hr className="light" />
-          <Link to={"/contacto"}>
-            <p>Contacto</p>
-          </Link>
-          <Link to={"/preguntas"}>
-            <p>Preguntas frecuentes</p>
-          </Link>
-        </div>
-        <div className="col-4 mt-3">
-          <h5>Países donde estamos</h5>
-          <hr className="light" />
-          <p>Costa Rica</p>
-        </div>
-        <div className="d-grid col-4 my-auto">
-          <div className="mx-auto">
-            <Link to={"/registrocaminador"}>
-              <button className="btn rounded-pill">
-                Convierte en caminador
-              </button>
+      <div className="contrainer m-auto">
+        <div className="row">
+          <div className="col-4 mt-4 align-items-center">
+            <h5>Sobre nosotros</h5>
+            <hr className="light" />
+            <Link to={"/contacto"}>
+              <p>Contacto</p>
+            </Link>
+            <Link to={"/preguntas"}>
+              <p>Preguntas frecuentes</p>
             </Link>
           </div>
+
+          <div className="col-4 mt-4 align-items-center">
+            <h5>Países donde estamos</h5>
+            <hr className="light" />
+            <p>Costa Rica</p>
+          </div>
+
+          <div className="d-grid col-4 my-auto align-items-center">
+            <div className="mx-auto">
+              <Link to={"/registrocaminador"}>
+                <button className="btn rounded-pill">
+                  Convierte en caminador
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <footer className="text-center">
+          <p>
+            Copyright &copy; 2022.{" "}
+            <Link to={"/terminosycondiciones"}>TERMINOS Y CONDICIONES</Link>
+          </p>
+        </footer>
       </div>
-      <footer className="text-center">
-        <p>
-          Copyright &copy; 2022.{" "}
-          <Link to={"/terminosycondiciones"}>TERMINOS Y CONDICIONES</Link>
-        </p>
-      </footer>
     </div>
   );
 };
