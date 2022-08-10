eba9b12eb63c5e8e038a55ecb5a38b8fa5edfcb6
Roberto Lopez J <roberto@lightsaint.com>
2022-08-10 04:06:53 +0000

    [Added HomeCaminador view]
    - Added the first part of the Walker view
    - Modified the background style for it to be a component

---
+++ src/front/js/component/carousel.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "../component/card";
+
+const Carousel = () => {
+  let style1 = {
+    overflowX: "scroll",
+    overflowY: "white",
+    alignItems: "center",
+    backgroundColor: "rgba(224, 224, 224, 0.3)",
+    height: "30rem",
+    paddingLeft: "20px",
+    paddingRight: "5px",
+    borderRadius: "5px",
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="d-flex flex-row rounded" style={style1}>
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
