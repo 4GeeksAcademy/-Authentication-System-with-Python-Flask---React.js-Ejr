5bb77945892c969aa52c87cbf5f9797b405ee5df
Roberto Lopez J <roberto@lightsaint.com>
2022-08-19 05:07:16 +0000

    [Added imaga upload through cloudinary]

--- src/front/js/component/uploadimage.js
+++ src/front/js/component/uploadimage.js
@@ -1,39 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+cloud_name = "wbpza7rq";
+api_key = "366651217213899";
+api_secret = "cAhOpTg6lEjCcUE1rcs0vcLeiSI";
 
 const UploadImage = () => {
-  const { image, setImage } = useState("");
-  const { loading, setLoading } = useState(false);
+  const { images, setImages } = useState([]);
+  const { imageToRemove, setImageToRemove } = useState(null);
 
-  const uploadImage = async (e) => {
-    const files = e.target.files;
-    const data = new FormData();
-    data.append("file", files[0]);
-    data.append("upload_preset", "Doggerimages");
-    setLoading(true);
-    const res = await fetch("https://api/cloudinary.com/v1_1/etolopez/upload", {
-      method: "POST",
-      body: data,
-    });
-    const file = await res.json();
-    setImage(file.secure_url);
-    setLoading(false);
+  const handleRemoveImg = (imgObj) => {
+    setImageToRemove(imgObj.public_id);
+    axios
+      .delete(`https://dogger-web-app.herokuapp.com/${imgObj.public_id}`)
+      .then(() => {
+        setImageToRemove(null);
+        setImages((prev) =>
+          prev.filter((img) => img.public_id !== imgObj.public_id)
+        );
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleOpenWidget = () => {
+    var myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "etolopez",
+        uploadPreset: "wbpza7rq",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          setImages[
+            (prev) => [
+              ...prev,
+              { url: result.info.url, public_id: result.info.public_id },
+            ]
+          ];
+        }
+      }
+    );
+    myWidget.open();
   };
 
   return (
     <div className="row">
       <div className="col">
-        <input
-          type="file"
-          name="file"
-          id="media"
-          placeholder="Sube tu imagen aqui"
-          onChange={uploadImage}
-        />
-        {loading ? (
-          <h3>Cargando Imagenes...</h3>
-        ) : (
-          <img src={image} style={{ width: "300px" }} />
-        )}
+        <button className=" btn btn-primary cloudinary-button rounded-pill">
+          <input
+            type="file"
+            id="upload-widget"
+            onClick={() => handleOpenWidget()}
+          />
+        </button>
       </div>
     </div>
   );
