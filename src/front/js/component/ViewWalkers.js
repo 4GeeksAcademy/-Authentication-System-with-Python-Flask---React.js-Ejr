b67ba1aa5f87616bb98244a17417c6abc2295d41
Roberto Lopez J <roberto@lightsaint.com>
2022-09-02 20:06:11 +0000

    Changed a simple image view

--- src/front/js/component/ViewWalkers.js
+++ src/front/js/component/ViewWalkers.js
@@ -10,6 +10,10 @@ const ViewWalkers = () => {
   const walkerUrl = process.env.BACKEND_URL + "/api/walkers/";
   const reviewUrl = process.env.BACKEND_URL + "/api/reviews/";
 
+  const imageStyle = {
+    objectFit: "cover",
+  };
+
   return (
     <div className="container">
       <h4 className="separator text-muted">Caminadores disponibles</h4>
@@ -24,6 +28,7 @@ const ViewWalkers = () => {
               <img
                 src={`${process.env.BACKEND_URL}/walker/download/${walker.file}`}
                 className="card-img-top"
+                style={imageStyle}
               />
               <div className="card-body">
                 <h5 className="card-title">{walker.username}</h5>
