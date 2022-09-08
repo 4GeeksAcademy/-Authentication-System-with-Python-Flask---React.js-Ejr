f0193202cd5c813b6561987d5a7dc591ac7c4497
Ksavir <kevinsavir20@gmail.com>
2022-09-08 21:24:45 +0000

    primer modelo pagina casas de cambio

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,41 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="d-flex justify-content-around">
+			<div>
+				<div id="globo-info" className="d-flex">
+					<div id="logo-casa">
+						imagen
+					</div>
+					<div id="info-casa">
+					<p>texto, información</p>
+						<button id="boton-casa">Ir a casa</button>
+					</div>
+				</div>
+				<div id="globo-info" className="d-flex">
+					<div id="logo-casa">
+						imagen
+					</div>
+					<div  id="info-casa">
+						<p>texto, información</p>
+						<button id="boton-casa">Ir a casa</button>
+
+					</div>
+				</div>
+				<div id="globo-info" className="d-flex">
+					<div id="logo-casa">
+						imagen
+					</div>
+					<div id="info-casa">
+					<p>texto, información</p>
+						<button id="boton-casa">Ir a casa</button>
+					</div>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<div id="maps">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d75352.4489663738!2d-70.66174117923224!3d-33.419614353711644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1safex!5e0!3m2!1ses-419!2scl!4v1662669729294!5m2!1ses-419!2scl" width="500" height="600" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
+			</div>
 		</div>
+
 	);
 };
