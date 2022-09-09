654545a7f92b2269200195135f68ef06313dacfe
leoescobarh <ldescobarherrera@gmail.com>
2022-09-09 02:16:36 +0000

    incorporacion de grafico y carousel de graficos, falta incorporar mas grafico.

--- src/front/js/pages/ExchangeRate.js
+++ src/front/js/pages/ExchangeRate.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/ExchangeRate.css";
 import { Text } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
 
 export const ExchangeRate = ({cambioMoneda, primeraMoneda, segundaMoneda}) => {
     
@@ -9,9 +10,10 @@ export const ExchangeRate = ({cambioMoneda, primeraMoneda, segundaMoneda}) => {
       <div >
       <Text fontSize='3xl'>Cambio Monto:</Text>
           
-      <Text fontSize='4xl'>{cambioMoneda}</Text>
+      <Center><Text fontSize='4xl'>{cambioMoneda}</Text></Center>
           
-      <Text fontSize='xl' as='i'>{primeraMoneda} <Text as='b'>Hacia</Text> {segundaMoneda}</Text>
+      <Center><Text as='i' fontSize='4xl'>{primeraMoneda} <Text as='b' fontSize='xl'>Hacia</Text> {segundaMoneda}</Text></Center>
+      
         </div>
   
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,8 @@ import { Context } from "../store/appContext";
 import { CurrrencyConverter } from "./CurrencyConverter";
 import "../../styles/home.css";
 import { ExchangeRate } from "./ExchangeRate";
+import Grafico from "./Grafico.jsx";
+import ImageSlider from "./ImageSlider";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -10,6 +12,7 @@ export const Home = () => {
   return (
     <div className="home">
           <CurrrencyConverter />
+          <ImageSlider/>
     </div>
     
   )
