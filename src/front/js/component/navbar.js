1a994452fe17c8d32ea929c581057dfe255d8781
Ksavir <kevinsavir20@gmail.com>
2022-09-09 21:30:05 +0000

    se modifica color de body y se agregar burbuja de dialogo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-warning  d-flex justify-content-between p-2">
-			<div className="container">
+			<div className="container-fluid">
 				<Link to="/">
 				<a class="navbar-brand" href="#">
      			 <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
