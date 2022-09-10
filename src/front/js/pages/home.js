86bea9541487e3766898653dbb5b6950061a11fb
Tolcas <zavala.juanep@gmail.com>
2022-09-10 13:46:45 +0000

    navBar

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,53 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import Stark from "../../img/stark.jpg";
+import "../../styles/index.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div>
+		<form id="formulario" className="">
+			<h2 id="title" className="my-3" >Consigue la mejor tasa aqui</h2>
+			<div className="mb-3 d-flex mx-4" id="name">
+				<label id="ingresar-monto" for="exampleInputName" className="form-label">Imgresa un monto</label>
+				<input type="usuario" className="form-control" id="data"/>
+				<div class="dropdown">
+				<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="boton">
+					Selecciona divisa
+				</button>
+				<ul class="dropdown-menu">
+					<li><button class="dropdown-item" type="button">USD</button></li>
+					<li><button class="dropdown-item" type="button">CLP</button></li>
+					<li><button class="dropdown-item" type="button">EU</button></li>
+				</ul>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
+			<div className="mb-3 d-flex mx-4">
+				<label id="monto-recibir" for="exampleInputEmail1" className="form-label">Monto as recibir</label>
+				<input type="email" className="form-control" id="data"/>
+				<div class="dropdown">
+				<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="boton">
+					Selecciona divisa
+				</button>
+				<ul class="dropdown-menu">
+					<li><button class="dropdown-item" type="button">USD</button></li>
+					<li><button class="dropdown-item" type="button">CLP</button></li>
+					<li><button class="dropdown-item" type="button">EU</button></li>
+				</ul>
+				</div>
+			</div>
+			
+			<div className="mb-3 d-flex mx-4">
+				<label id="tipo-cambio" for="exampleInputPassword1" className="form-label">Tipo de cambio</label>
+				<input type="password" className="form-control" id="data"/>
+				<button type="submit" id="boton2">Cambiar</button>
+			</div>
+		</form>
+		</div>	
 	);
 };
+
+
+
