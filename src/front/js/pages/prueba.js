77b15ab9719b5a23e8c25c68537d0637edf7f8a4
edwardsfonseca <eduards_fonseca@hotmail.com>
2022-09-12 05:01:41 +0000

    cc

--- src/front/js/pages/prueba.js
+++ src/front/js/pages/prueba.js
@@ -7,19 +7,18 @@ export const Prueba= () => {
 
 	return (
 		
-		<div className="row">
-			<hr style={{"background":"#5e5e7c" ,"width": "-webkit-fill-available","height":"3px"}}></hr>
-			<h1 style={{ "display": "flex", "color": "rgb(188 191 40)", "padding": "20px" }}>Characters</h1>
-			{store.comidas.map((e, i) => {
-				console.log(e)
-				return <div className="container " style={{ "width": "18rem"}}>
+		<div className="">
+		<h1 style={{ "display": "flex", "color": "rgb(188 191 40)", "padding": "20px" }}>Characters</h1>
+			{store.comida.map((e, i) => {
+				
+				return <div className="" >
 					
-					<div className="card-body" key={i}>
-						<h5 className="card-title">{e.name}</h5>
+					<div className="card-body">
+						<h5 className="card-title">sss</h5>
 						<p>Nombre:&nbsp;{}</p>
 						
 						
-							<button onClick={()=> actions.getAddTask(e.name)} type="button" class="btn btn-outline-warning m-2"> <i class="far fa-heart"></i></button>
+							
 
 					</div>
 				</div>
