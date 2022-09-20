51439be02a2a5d820a6abdab3319a1f6c6cf483f
Benjamin Gutierrez <gutybv@gmail.com>
2022-09-20 21:55:30 +0000

    finish: what if

--- src/front/js/pages/whatIf.jsx
+++ src/front/js/pages/whatIf.jsx
@@ -19,9 +19,11 @@ import {
   Select,
   Spinner,
   Flex,
+  Center,
 } from "@chakra-ui/react";
 import SidebarWithHeader from "../component/sideBar.jsx";
 import { format } from "util";
+import CountUp from "react-countup";
 
 export default function WhatIf() {
   const [coins, setCoins] = useState([]);
@@ -34,6 +36,7 @@ export default function WhatIf() {
   const [loading, setLoading] = useState(false);
   const [precioAntiguo, setPrecioAntiguo] = useState(0);
   const [numeroFinal, setNumeroFinal] = useState(0);
+  const [counterOn, setCounterOn] = useState(false);
 
   const getData = async () => {
     const res = await axios.get(
@@ -92,63 +95,111 @@ export default function WhatIf() {
   return (
     <Box display="flex">
       <SidebarWithHeader />
-      <Flex p={10}>
-        <Box p={10}>
-          <Text></Text>
-          <FormControl>
-            <FormLabel>Elige la cripto que quieras simular </FormLabel>
-            <Select
-              placeholder="Elige una cripto"
-              value={token}
-              onChange={changeToken}
+      <Flex p={10} background={"black"}>
+        <Box pt={20}>
+          <Center>
+            <Text fontSize="5xl" color={"white"} fontWeight={800}>
+              Maquina del tiempo{" "}
+            </Text>
+          </Center>
+          <Center>
+            <Text as="i" fontSize="5xl" color={"white"} fontWeight={600}>
+              cripto
+            </Text>
+          </Center>
+          <Text fontSize="lg" color={"white"} py={10} px={14}>
+            {" "}
+            Que seria de tu dinero si hubieras invertido en cripto un par de
+            años antes, gracias a CriptoSchool lo podras saber.
+          </Text>
+          <Center pt={10}>
+            <Text fontSize="5xl" color={"white"} fontWeight={800}>
+              Dinero total ganado{" "}
+            </Text>
+          </Center>
+          <Center>
+            <Text
+              fontSize="5xl"
+              fontWeight={800}
+              style={{
+                color: numeroFinal > 0 ? "#13C783" : "#ff0000",
+              }}
             >
-              <option value="bitcoin">bitcoin</option>
-              <option value="ethereum">ethereum</option>
-              <option value="solana">solana</option>
-              <option value="bnb">bnb</option>
-              <option value="cardano">cardano</option>
-              <option value="polygon">polygon</option>
-              <option value="polkadot">polkadot</option>
-              <option value="avalanche">avalanche</option>
-              <option value="dogecoin">dogecoin</option>
-              <option value="uniswap">uniswap</option>
-            </Select>
-            <FormLabel>
-              Elige cuanto dinero te hubiera gustado invertir
-            </FormLabel>
-            <NumberInput
-              min={100}
-              max={1000000}
-              value={amount}
-              onChange={(valueString) => setAmount(valueString)}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <FormLabel>Elige la fecha en este formato dia-mes-año</FormLabel>
-            <Input
-              placeholder="22-07-2016"
-              value={date}
-              onChange={handleChange}
-            />
-            <Button
-              mt={4}
-              colorScheme="teal"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-
-            {loading && <Spinner />}
-          </FormControl>
+              $
+              <CountUp start={0} end={numeroFinal} />
+            </Text>
+          </Center>
         </Box>
-        <Box p={20} px={200}>
-          <Text fontSize="5xl">${Math.floor(numeroFinal)}</Text>
+        <Box w={700} h={500} borderRadius="lg" background={"#361A60"}>
+          <Box px={10} py={10}>
+            <Center pb={5}>
+              <Text color={"white"} fontSize={"3xl"} fontWeight={800}>
+                Calculadora
+              </Text>
+            </Center>
+            <FormControl>
+              <FormLabel color={"white"}>
+                Elige la cripto que quieras simular{" "}
+              </FormLabel>
+              <Select
+                pb={5}
+                placeholder="Elige una cripto"
+                value={token}
+                onChange={changeToken}
+                color={"white"}
+              >
+                <option value="bitcoin">bitcoin</option>
+                <option value="ethereum">ethereum</option>
+                <option value="solana">solana</option>
+                <option value="bnb">bnb</option>
+                <option value="cardano">cardano</option>
+                <option value="matic-network">polygon</option>
+                <option value="polkadot">polkadot</option>
+                <option value="avalanche">avalanche</option>
+                <option value="dogecoin">dogecoin</option>
+                <option value="uniswap">uniswap</option>
+              </Select>
+              <FormLabel color={"white"}>
+                Digita cuanto dinero te hubiera gustado invertir
+              </FormLabel>
+              <NumberInput
+                min={100}
+                max={1000000}
+                value={amount}
+                onChange={(valueString) => setAmount(valueString)}
+                color={"white"}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormLabel color={"white"} pt={5}>
+                Elige la fecha en este formato dia-mes-año
+              </FormLabel>
+              <Input
+                placeholder="22-07-2016"
+                value={date}
+                onChange={handleChange}
+                color={"white"}
+              />
+              <Center>
+                <Button
+                  mt={6}
+                  colorScheme="purple"
+                  type="submit"
+                  onClick={handleSubmit}
+                  borderRadius={50}
+                >
+                  Al pasado!
+                </Button>
+              </Center>
+              {loading && <Spinner />}
+            </FormControl>
+          </Box>
         </Box>
+        <Box p={20} px={200}></Box>
       </Flex>
     </Box>
   );
