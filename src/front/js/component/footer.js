4622e5a9d9883d0ce6eba15ccaa94c54eb3e732a
diegomatah <diegoa.matah@gmail.com>
2022-09-21 18:46:44 +0000

    footer dm

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center bg-light">
 		<p>
-			Made with <i class="fa-solid fa-map-pin"></i>{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		Copyright © 2022 CoinChange Powered by JKD
 		</p>
 	</footer>
 );
