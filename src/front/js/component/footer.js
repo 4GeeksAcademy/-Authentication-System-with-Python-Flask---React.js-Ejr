f09186b049a40bca5daa9f11f8ba529dd665717a
diegomatah <diegoa.matah@gmail.com>
2022-09-22 18:49:23 +0000

    prueba2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center bg-light align-items-end">
 		<p>
-		Copyright © 2022 CoinChange by JKD 
+		Copyright © 2022 Cacha el cambio by JKD 
 		</p>
 	</footer>
 );
