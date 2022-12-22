2c8fa07a9d26bbcf2f2a88e8d0edc6dfd3530ba8
Kochu26 <josuefallasm26@hotmail.com>
2022-12-22 06:18:46 +0000

    importacion de fontawesome

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,6 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//import fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheckSquare,
+  faCoffee,
+  faHandBackFist,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckSquare, faCoffee, faHandBackFist);
+
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
