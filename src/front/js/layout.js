2b082f5a17a563fd40787753e0290648588352d7
NanoProgram <danielj.perezd@gmail.com>
2023-01-08 18:52:28 +0000

    Calendar Ready

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Calendar } from "./pages/Calendar.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -17,20 +18,9 @@ const Layout = () => {
     const basename = process.env.BASENAME || "";
 
     return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
+        <>
+            <Calendar />
+        </>
     );
 };
 
---
+++ src/front/js/pages/Calendar.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/calendar.css";
+
+export const Calendar = () => {
+
+	return (
+		<div className="position-absolute top-50 start-50 translate-middle">
+            <div className="year">
+			    <div className="mes mes1"><div className="ene"><button>ENE</button></div></div>
+                <div className="mes mes2"><div className="feb"><button>FEB</button></div></div>
+                <div className="mes mes3"><div className="mar"><button>MAR</button></div></div>
+                <div className="mes mes4"><div className="abr"><button>ABR</button></div></div>
+                <div className="mes mes5"><div className="may"><button>MAY</button></div></div>
+                <div className="mes mes6"><div className="jun"><button>JUN</button></div></div>
+                <div className="mes mes7"><div className="jul"><button>JUL</button></div></div>
+                <div className="mes mes8"><div className="ago"><button>AGO</button></div></div>
+                <div className="mes mes9"><div className="sep"><button>SEP</button></div></div>
+                <div className="mes mes10"><div className="oct"><button>OCT</button></div></div>
+                <div className="mes mes11"><div className="nov"><button>NOV</button></div></div>
+                <div className="mes mes12"><div className="dic"><button>DIC</button></div></div>
+		    </div>
+        </div>
+	);
+};
