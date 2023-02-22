b66c3237e79c121e7057aaf366dd4fa46a2f8bfb
yonatancrespo <yonatancrespo4@gmail.com>
2023-02-22 03:59:41 +0000

    navbar-starter-home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,35 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+const MyNavbar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+         Game Portal 
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/games">
+              Games
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#store">Store</Nav.Link>
+            <Nav.Link href="#community">Community</Nav.Link>
+            <Nav.Link href="#support">Support</Nav.Link>
+            <Nav.Link href="#account">Account</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
+
+export { MyNavbar as Navbar };
