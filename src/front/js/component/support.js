e526ad3506c74069d6454a7919a0b6c2cd955a9b
LeserraJ <leserrabusiness@gmail.com>
2023-03-08 03:39:07 +0000

    Added full support page functionality on front-end

--- src/front/js/component/support.js
+++ src/front/js/component/support.js
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useForm } from "react-hook-form"
+import { Navbar } from "./navbar";
+import { Container, Button, Form, Row, Col } from "react-bootstrap";
 
 const MySupport = () =>{
-    return(
 
+    const initState = {
+        firstLast:"",
+        email:"",
+        contactNumber:"",
+    };
+
+    const [initialValues, setInitialValues] = useState(initState)
+    const onSubmit =(values) =>{
+        console.log("Values:::", values);
+        console.log("Values:::", JSON.stringify(values));
+    };
+
+    const onError= (error) =>{
+        console.log("ERROR:::", error)
+    };
+
+
+    const {
+        register,
+        handleSubmit,
+        getValues,
+        watch,
+        formState: {errors}
+    } = useForm({
+        mode:"onTouched",
+        reValidateMode:"onSubmit",
+        defaultValues:initialValues
+    });
+
+
+
+    return(
+        <Container>
+            <h1 className="footer mt-auto py-3 text-center" style={{ color: "white" }}>How can we help?</h1>
+            <Form onSubmit={handleSubmit(onSubmit,onError)} style={{ color: "white" }}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>First and Last Name</Form.Label>
+                    <Form.Control type="text"  placeholder="" {...register("firstLast",{required: "Please enter your first and last name."})}></Form.Control>
+                    {errors.firstLast && (
+                        <Form.Text className="text-danger">
+                            {errors.firstLast.message}
+                        </Form.Text>
+                    )}<br/>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="" {...register("email",{required:"Please enter a valid email."})}></Form.Control>
+                    {errors.email &&(
+                        <Form.Text className="text-danger">
+                            {errors.email.message}
+                        </Form.Text>
+                    )}
+                    <br/>
+                    <Form.Label>Contact Number</Form.Label>
+                    <Form.Control type="number" placeholder="" {...register("contactNumber", {required:"Please enter a valid phone number."})}></Form.Control>
+                    {errors.contactNumber &&(
+                        <Form.Text className="text-danger">
+                            {errors.contactNumber.messagasd}
+                        </Form.Text>
+                    )}
+                    <br/>
+                    <Form.Label>Please describe the issue you are having.</Form.Label>
+                    <Form.Control type="text" placeholder=""></Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+        </Container>
     );
 };
 
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,7 +9,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-
+import { Support } from "./component/support";
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -23,7 +23,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Support />} path="/support" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
