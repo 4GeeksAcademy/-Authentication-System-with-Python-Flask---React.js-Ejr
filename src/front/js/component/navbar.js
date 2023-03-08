1e8e031cdf65706123277bcb47cd7a79273e1dae
lexie70 <alexandracampos658@gmail.com>
2023-03-08 02:37:29 +0000

    bg-Pink class

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [isAuthenticate,setIsAuthenticate]= useState(true)
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
+		<>
+		<nav className="navbar bg-pink">
+			{/* left-side */}
+			
+				<Logo/>
+			
+			{/* right-side */}
+			
+			 {isAuthenticate?<AutnGadgeth/>: <SingupAndLogin/>}
+			
 		</nav>
+
+				
+		</>
 	);
 };
+ 
+const Logo = () => {
+	return (
+		<div>
+		<h1>eCommerce</h1>
+		</div>
+		
+		
+	);
+};
+ 
+const SingupAndLogin = () => {
+	return (
+		<>
+		<div>
+			
+			<button>Login</button>
+			<button>singup</button>
+		</div>
+
+		
+		</>
+		
+	);
+};
+
+const AutnGadgeth = () => {
+	return (
+		<>
+		<div>
+			
+			<h1>hello</h1><span>something</span>
+		</div>
+
+		
+		</>
+		
+	);
+};
+
