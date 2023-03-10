bf7bbda6a5328061a8ef251c71ea4cda1d4bc9f7
Wishmastering <charsdb@gmail.com>
2023-03-10 02:15:13 +0000

    Borrando prettier 2.0 attempt

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,62 +1,53 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	const [isAuthenticate,setIsAuthenticate]= useState(false)
-	return (
-		<>
-		<nav className="navbar p-2  bg-pink jost ">
-			{/* left-side */}
-			
-				<Logo/>
-			
-			{/* right-side */}
-			
-			 {isAuthenticate?<AutnGadgeth/>: <SingupAndLogin/>}
-			
-		</nav>
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+  return (
+    <>
+      <nav className="navbar p-2  bg-pink jost ">
+        {/* left-side */}
 
-				
-		</>
-	);
+        <Logo />
+
+        {/* right-side */}
+
+        {isAuthenticate ? <AutnGadgeth /> : <SingupAndLogin />}
+      </nav>
+    </>
+  );
 };
- 
+
 const Logo = () => {
-	return (
-		<div>
-		<h1 className="ms-5">eCommerce</h1>
-		</div>
-		
-
-	);
+  return (
+    <div>
+      <h1 className="ms-5">eCommerce</h1>
+    </div>
+  );
 };
- 
-const SingupAndLogin = () => {
-	return (
-		<>
-		<div>
-			
-			<button className="bg-white p-2 border border-0 text-color"><Link to="/login">Ingresa</Link></button>
-			<button className="bg-button p-2 ms-3 me-5 border border-0 text-color2 ">Registrate</button>
-		</div>
 
-		
-		</>
-		
-	);
+const SingupAndLogin = () => {
+  return (
+    <>
+      <div>
+        <button className="bg-white p-2 border border-0 text-color">
+          <Link to="/login">Ingresar</Link>
+        </button>
+        <button className="bg-button p-2 ms-3 me-5 border border-0 text-color2 ">
+          Registrate
+        </button>
+      </div>
+    </>
+  );
 };
 
 const AutnGadgeth = () => {
-	return (
-		<>
-		<div>
-			
-			<h1>hello</h1><span>something</span>
-		</div>
-
-		
-		</>
-		
-	);
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+        <span>something</span>
+      </div>
+    </>
+  );
 };
-
