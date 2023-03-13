7d48c01bfa6ad108b35aad87533a5b9571b1edc3
LeserraJ <leserrabusiness@gmail.com>
2023-03-13 23:24:17 +0000

    Maybe making progress

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,8 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Support } from "./component/support";
+
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
