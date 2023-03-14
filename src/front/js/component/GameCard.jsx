cd5ecb25cba5f20f435b0563e798c6077ca8af88
LeserraJ <leserrabusiness@gmail.com>
2023-03-14 01:26:49 +0000

    on same page

--- src/front/js/component/GameCard.jsx
+++ src/front/js/component/GameCard.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import {Card, Button} from "react-bootstrap"
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 const GameCard = ({ title, imageUrl, addToCart }) => {
   const handleAddToCart = () => {
