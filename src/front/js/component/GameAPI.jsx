1e9173300833c5517cd3863622ce6a21b6bb5c0b
yonatancrespo <yonatancrespo4@gmail.com>
2023-03-16 23:36:00 +0000

    put game api back to original

--- src/front/js/component/GameAPI.jsx
+++ src/front/js/component/GameAPI.jsx
@@ -2,6 +2,33 @@ import React, { useEffect, useState } from "react";
 
 
 import axios from "axios";
+
+function GameData() {
+  var myHeaders = new Headers();
+myHeaders.append("Client-ID", "l9z8jtrdbnyiypji85ggptiealo4em");
+myHeaders.append("Authorization", "Bearer 50ufriiai9th133z4mjy6uzrfpige2");
+myHeaders.append("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+myHeaders.append("Access-Control-Max-Age", "86400");
+myHeaders.append("Access-Control-Allow-Origin", "*");
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Cookie", "__cf_bm=Ppf.q5yFe9FpNgPweC2iHqGI2KOUMrXGT6rPrfhOJO4-1678753987-0-AacLoCfmyDGpFFLKbKy6GgDO8wmkEoqaHBKlwn/RDZujHw1Y5Gruc1bxcebNixXbYQsqToQ7pO+OziXdWFRjci0=");
+
+var raw = "fields name; limit 10;";
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://api.igdb.com/v4/games/", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+export default GameData;
+
 /*function API_Call() {
   const URL = `https://api.igdb.com/v4`;
   useEffect(() => {
