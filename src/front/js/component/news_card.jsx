d2fae7010496fb3eed0933462615af8e651d04cf
LeserraJ <leserrabusiness@gmail.com>
2023-03-21 00:35:41 +0000

    fixed mistake

--- src/front/js/component/news_card.jsx
+++ src/front/js/component/news_card.jsx
@@ -21,17 +21,16 @@ const News_Card = () => {
     .catch(function (error) {
       console.error(error);
     });
-
   return (
     <Stack className="mt-5" gap={4}>
       <Row className="border border-end-0 border border-start-0 border border-1">
         <Col md={3} className="d-inline-flex">
-          <a style={{ color: "white" }} href={Object.link}>
-            {Object.image}
+          <a style={{ color: "white" }} href="https://www.google.com/">
+            Image Goes Here
           </a>
         </Col>
         <Col className="d-table-row">
-          <h3 className="font-weight-bold">{Object.title}</h3>
+          <h3 className="font-weight-bold">Title of Article</h3>
           <p>Desciption of Article 1-2 Sentences</p>
           <a style={{ color: "white" }} href="https://www.google.com/">
             Read More
