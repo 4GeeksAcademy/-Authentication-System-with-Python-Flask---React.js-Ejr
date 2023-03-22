432d20b784165cd857e67782c8e0d13ccbc5cff5
lexie70 <alexandracampos658@gmail.com>
2023-03-22 00:49:57 +0000

    componente card y add plant

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Plants
 from api.utils import generate_sitemap, APIException
 
 from flask_jwt_extended import create_access_token
@@ -54,3 +54,29 @@ def login():
 
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token), 200
+
+
+@api.route("/add/plant", methods=["POST"])
+def add_plants():
+    name = request.json.get("name", None)
+    size34 = request.json.get("size34", None)
+    size35 = request.json.get("size35", None)
+    size36 = request.json.get("size36", None)
+    size37 = request.json.get("size37", None)
+    size38 = request.json.get("size38", None)
+    size39 = request.json.get("size39", None)
+    size40 = request.json.get("size40", None)
+    print(name,size34)
+   
+    existing_plant = Plants.query.filter_by(name=name).first()
+
+    if existing_plant:
+        return "The plant already registered", 400
+
+    new_plant = Plants(name=name, size34=size34,size35=size35, size36=size36, size37=size37, size38=size38, size39=size39, size40=size40, size41=0)
+    
+    db.session.add(new_plant)    
+    db.session.commit()
+
+    response_body = "You have registered a plant"
+    return jsonify(response_body), 200
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,27 +1,14 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Card } from "../component/card";
-
+import Card from "../component/card";
+import AddPlants from "../component/addPlants";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<>
-		<div className="d-flex justify-content-center">
-			<Card />
-			<Card />
-			<Card />
-		</div>
-
-		
-		<div className="d-flex justify-content-center">
-			<Card />
-			<Card />
-			<Card />
-		</div>
-
-	
+		<AddPlants/>
 		</>
 	);
 };
