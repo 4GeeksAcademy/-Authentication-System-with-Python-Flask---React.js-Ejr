4da81981ef3819e5330f893848924b4aa0320923
lexie70 <alexandracampos658@gmail.com>
2023-03-24 00:43:02 +0000

    se creo componente addmaster

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,18 +4,23 @@ import "../../styles/home.css";
 import Card from "../component/card";
 import AddPlants from "../component/addPlants";
 import GetPlants from "../component/getPlants";
+import AddMaster from "../component/addMaster";
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	// const { store, actions } = useContext(Context);
 
-	const [component,setComponent]= useState(<AddPlants/>)
-	return (
-		<>
-		{/* <AddPlants/> */}
-		{/* <GetPlants/> */}
-		<h1 onClick={()=>{
-			setComponent(<GetPlants/>)
-		}}>show getPlants component</h1>
-		{component}
-		</>
-	);
+	// const [component,setComponent]= useState(<AddPlants/>)
+	// return (
+	// 	<>
+	// 	{/* <AddPlants/> */}
+	// 	{/* <GetPlants/> */}
+	// 	<h1 onClick={()=>{
+	// 		setComponent(<GetPlants/>)
+	// 	}}>show getPlants component</h1>
+	// 	{component}
+	// 	</>
+	// );
+	return(
+
+		<AddMaster/>
+	)
 };
