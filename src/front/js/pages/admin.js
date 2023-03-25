ac23308b534c4ce4f2ba49285143e8a9a9af5a40
Charlytoc <charlyjchaconc@gmail.com>
2023-03-25 02:20:59 +0000

    mobile view for the admin component

--- src/front/js/pages/admin.js
+++ src/front/js/pages/admin.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
 import Options from "../component/options";
+import GetPlants from "../component/getPlants";
+import AddPlants from "../component/addPlants";
 export const Admin = () => {
-	
+	 const [showOptions, setShowOptions] = useState(false);
+	const [component, setComponent] = useState(<AddPlants />)
 
+	const setAndClose = (comp) => {
+		setComponent(comp);
+		setShowOptions(false)
+	}
 	return (
         <>
-		 <div className="d-flex">
-		 <Options />
+		 <button className="button-dark button-options" onClick={() => setShowOptions(!showOptions)}>
+        {showOptions ? "Ocultar opciones" : "Mostrar opciones"}
+      	</button>
+		 <div className="d-flex admin-container">
+		 {showOptions && <Options setComponent={setAndClose} />}
 		 <div className="admin-component"> 
-			asdasdasd
+			{component}
 		 </div>
 		 </div>
         </>
