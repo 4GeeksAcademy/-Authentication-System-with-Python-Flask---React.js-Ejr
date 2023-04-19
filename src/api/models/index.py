c1694644ceb06e40f904eef70972403a23c9802a
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-19 11:06:20 +0000

    feat: folder structure created, service and technician added

---
+++ src/api/models/index.py
@@ -0,0 +1,3 @@
+from api.models.db import db
+from api.models.technician import Technician
+from api.models.service import Service
---
+++ src/api/models/service.py
@@ -0,0 +1,21 @@
+from api.models.db import db
+
+class Service(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    id_technician = db.Column(db.Integer,db.ForeignKey('technician.id'), unique=True, nullable=False)
+    techinician = db.relationship('Technician')
+    
+    def __init__(self, id_technician, description, name):
+        self.name = name
+        self.id_technician =  id_technician
+        
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "id_technician": self.id_technician
+            
+        }
