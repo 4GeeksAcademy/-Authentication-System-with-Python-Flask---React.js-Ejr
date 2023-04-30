6f691b219206ad9054b8829d40f51d67be6c7dae
BilelMatallah <bilelmatallah@hotmail.com>
2023-04-30 14:06:02 +0000

    feat: nav y foot hechos (queda diseño)

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,42 +1,39 @@
 import React, { Component } from "react";
 import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGooglePlusG } from "react-icons/fa";
+
 
 
 export const Footer = () => (
 	
-	<footer className="mainfooter" role="contentinfo">
-	<div className="footer-middle">
+<footer>
+	
 		<div className="container">
-			<div className="row">
-				<div className="col-md-3">
-					<h4>Follow Us</h4>
-					<div className="social-network text-center d-flex">
+			<div className="d-flex pt-4 pb-4 footer_body">
+				<div className="social">
+					<div className="icon-circle text-center d-flex gap-3">
 						<a href="#"><FaFacebookF className="ico facebook"/></a>
-						<a href="#"><i className="fa fa-linkedin"></i></a>
+						<a href="#"><FaTwitter className="ico twitter"/></a>
+						<a href="#"><FaLinkedin className="ico linkedin"/></a>
+						<a href="#"><FaGooglePlusG className="ico google"/></a>
 					</div>				
 				</div>
 
-				<div className="col-md-3 col-sm-6">
-					<div className="footer-pad">
-						<h4>Política de privacidad</h4>
+				<div className="priv_contact">
+					<div className="privacy">
+						<a href="#"><h4>Política de privacidad</h4></a> 
+					</div>
+
+					<div className="contact">
+						<a href="#"><h4>Contacto</h4></a>  
 					</div>
 				</div>
 
-				<div className="col-md-3 col-sm-6">
-					<div className="footer-pad">
-						<h4>Contacto</h4>
-					</div>
-				</div>
-
-			</div>
-			
-			<div className="row">
-				<div className="col-md-12 copy">
-					<p className="text-center">&copy; Copyright 2023 - Jobs Hood.  All rights reserved.</p>
-				</div>
 			</div>
 		</div>
-	</div>
-  </footer>
+	
+</footer>
 	
 );
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home.jsx";
 import injectContext from "./store/appContext";
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Navbar } from "./component/navbar.js";
+import { Footer } from "./component/footer.js";
 import { RegistroWorker } from "./pages/form-register-worker.jsx";
 import { RegistroLawyer } from "./pages/form-register-lawyer.jsx"
 import { Login } from "./pages/login-users.jsx";
