4e59439dcd79743752e391d07b6d28dbccb4249b
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-05-26 00:08:45 +0000

    adding tailwind CSS working

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,7 +8,12 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			
+			<h1 className="text-3xl font-bold underline">
+				Hello world!
+			</h1>
+			<div class="alert alert-primary" role="alert">
+				A simple primary alert—check it out!
+			</div>
 		</div>
 	);
 };
