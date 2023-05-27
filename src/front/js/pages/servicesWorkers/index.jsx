0792b2048ee2062980cce434bad314c786f28701
Enricrypto <eibarraf@gmail.com>
2023-05-27 10:16:56 +0000

    fixed import issue

--- src/front/js/pages/servicesWorkers/index.jsx
+++ src/front/js/pages/servicesWorkers/index.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import "../../pages/servicesWorkers/styles.css";
 import BigContainer from "../../components/bigContainer/index.jsx";
 import { listWorkers } from "../../service/workers";
-import { listServices } from "../../service/service";
+import { listServiceByCompany } from "../../service/service.js";
 import { createServiceWorker } from "../../service/service_worker";
 import Toast from "../../components/toast/index.jsx";
 import { toast } from "react-toastify";
@@ -26,7 +26,7 @@ const ServicesWorkers = () => {
   };
 
   const getServices = async () => {
-    const services = await listServices(company_id);
+    const services = await listServiceByCompany(company_id);
     setServicesList(services);
   };
 
