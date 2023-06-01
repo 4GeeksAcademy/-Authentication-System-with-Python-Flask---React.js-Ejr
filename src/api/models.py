67219da18fa46944cc0e6fa035cee5cfd897c26b
Daniel Martinez <d.martinez74@gmail.com>
2023-06-01 00:57:04 +0000

    services db added

--- src/api/models.py
+++ src/api/models.py
@@ -38,3 +38,9 @@ class TokenBlockedList(db.Model):
     __tablename__ = "token_blocked_list"
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(50), nullable = False)
+
+class Services(db.Model):
+    __tablename__ = "services"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    price = db.Column(db.Integer)
