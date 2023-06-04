b29b21c004ba06756928d63751d46ab95fe48b63
Juan Penalver <122556197+JuanPenalver11@users.noreply.github.com>
2023-06-04 17:38:54 +0000

    signup2

--- src/front/js/component/registratevoluntarioform.js
+++ src/front/js/component/registratevoluntarioform.js
@@ -1,92 +1,55 @@
 import React from "react";
 
-export const RegistrateVoluntarioForm =()=> {
-    return (
-
-        <form className="container bg-warning mt-5">
+export const RegistrateVoluntarioForm = () => {
+  return (
+    <div className="container">
+      <div className="row">
         <div className="col-md-6">
-          <label htmlFor="inputName" className="form-label">
-            Nombre
-          </label>
-          <input type="email" className="form-control" id="inputName" />
+          <form className="m-5">
+            <div className="text-center mb-4">
+              <h1>Hazte voluntario</h1>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="inputName" className="form-label">
+                Nombre
+              </label>
+              <input type="text" className="form-control" id="inputName" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="inputSurname" className="form-label">
+                Apellido
+              </label>
+              <input type="text" className="form-control" id="inputSurname" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="inputPassword" className="form-label">
+                Contraseña
+              </label>
+              <input type="password" className="form-control" id="inputPassword" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="inputRepeatPassword" className="form-label">
+                Repita contraseña
+              </label>
+              <input type="password" className="form-control" id="inputRepeatPassword" />
+            </div>
+            <div className="mb-3 form-check">
+              <input className="form-check-input" type="checkbox" id="showPassword" />
+              <label className="form-check-label" htmlFor="showPassword">
+                Mostrar contraseña
+              </label>
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary">
+                Sign up
+              </button>
+            </div>
+          </form>
         </div>
         <div className="col-md-6">
-          <label htmlFor="inputSurname" className="form-label">
-            Apellido
-          </label>
-          <input type="email" className="form-control" id="inputSurname" />
+          <img className="img-fluid m-5" src="https://images.pexels.com/photos/3823497/pexels-photo-3823497.jpeg?auto=compress&cs=tinysrgb&w=800" alt="Volunteer" />
         </div>
-        <div className="col-12">
-          <label htmlFor="inputAddress" className="form-label">
-            Address
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputAddress"
-            placeholder="1234 Main St"
-          />
-        </div>
-        <div className="col-12">
-          <label htmlFor="inputAddress2" className="form-label">
-            Address 2
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputAddress2"
-            placeholder="Apartment, studio, or floor"
-          />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputCity" className="form-label">
-            City
-          </label>
-          <input type="text" className="form-control" id="inputCity" />
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="inputState" className="form-label">
-            State
-          </label>
-          <select id="inputState" className="form-select">
-            <option selected="">Choose...</option>
-            <option>...</option>
-          </select>
-        </div>
-        <div className="col-md-2">
-          <label htmlFor="inputZip" className="form-label">
-            Zip
-          </label>
-          <input type="text" className="form-control" id="inputZip" />
-        </div>
-        <div className="col-12">
-        <div className="col-md-6">
-          <label htmlFor="inputPassword4" className="form-label">
-            Contraseña
-          </label>
-          <input type="password" className="form-control" id="inputPassword4" />
-        </div><div className="col-md-6">
-          <label htmlFor="inputPassword4" className="form-label">
-            Repita contraseña
-          </label>
-          <input type="password" className="form-control" id="inputPassword4" />
-        </div>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="gridCheck" />
-            <label className="form-check-label" htmlFor="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">
-            Sign in
-          </button>
-        </div>
-      </form>
-   
-
-
-         
-    );
+      </div>
+    </div>
+  );
 }
--- src/front/js/pages/registratevoluntario.js
+++ src/front/js/pages/registratevoluntario.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { RegistrateVoluntarioForm } from "../component/registratevoluntarioform";
+import "../../styles/registratevoluntario.css";
 
 export const RegistrateVoluntario =()=>{
 
