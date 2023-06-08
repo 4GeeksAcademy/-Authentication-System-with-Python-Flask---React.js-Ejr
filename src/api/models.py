77d6f07816d2e67b708d489046cf5233f695749a
Mauro Borges <mauroborgesm@gmail.com>
2023-06-08 22:39:17 +0000

    Database almost done

--- src/api/models.py
+++ src/api/models.py
@@ -15,12 +15,10 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     first_name=db.Column(db.String(120), unique=True, nullable=False)
     last_name=db.Column(db.String(120), unique=True, nullable=False)
-    birth_day=db.Column(db.Integer, unique=True, nullable=False)
-    birth_month=db.Column(db.Integer, unique=True, nullable=False)
-    birth_year=db.Column(db.Integer, unique=True, nullable=False)
+    birthday=db.Column(db.String(120), unique=True, nullable=False)
     gender=db.Column(db.String(120), unique=True, nullable=False)
     phone=db.Column(db.Integer, unique=True, nullable=False)
-    suscription = db.Column(db.Boolean(), unique=False, nullable=False)
+    # suscription = db.Column(db.Boolean(), unique=False, nullable=False)
     pedidos=db.relationship("Pedidos")
     
     def __repr__(self):
@@ -33,12 +31,10 @@ class User(db.Model):
             "is_active": self.is_active,
             "first_name":self.first_name,
             "last_name":self.last_name,
-            "birth_day":self.birth_day,
-            "birth_month":self.birth_month,
-            "birth_year":self.birth_year,
+            "birthday":self.birthday,
             "gender": self.gender,
             "phone": self.phone,
-            "suscription": self.suscription
+            # "suscription": self.suscription
         }
 
 
@@ -47,10 +43,13 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # user_id=db.Column(db.Integer, db.Foreignkey = ("User.id"))
     name = db.Column(db.String(120), unique=True, nullable=False)
-    
+    url=db.Column(db.String(500), unique=True, nullable=False)
     ubicaciones = db.Column(db.String(80), unique=False, nullable=False)
+    restaurantplatos=db.relationship("Restaurantplatos")
+
+    #platos=db.Column(db.Integer, db.Foreignkey ("Platos.id"))
     pedido=db.relationship("Pedidos")
-    detalles_de_pedido=db.relationship("DetalleDePedidos")
+    #detalles_de_pedido=db.relationship("DetalleDePedidos")
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
@@ -59,7 +58,7 @@ class Restaurant(db.Model):
         return {
             "id":self.id,
             "name":self.name,
-            "platos":self.platos,
+            "url":self.url,
             "ubicaciones":self.ubicaciones 
         }
 
@@ -69,9 +68,10 @@ class Platos(db.Model):
     name = db.Column(db.String(120), unique=True, nullable=False)
     description=db.Column(db.String(500), unique=True, nullable=False)
     price=db.Column(db.Integer, unique=True, nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurant.id"))
-    restaurant=db.relationship(Restaurant)
-    detalles_de_pedido=db.relationship("DetalleDePedidos")
+    
+    # restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurant.id"))
+    restaurantplatos=db.relationship("Restaurantplatos")
+    # detalles_de_pedido=db.relationship("DetalleDePedidos")
 
     def __repr__(self):
         return f'<Platos {self.name}>'
@@ -82,8 +82,8 @@ class Platos(db.Model):
             "name":self.name,
             "price": self.price,
             "description": self.description,
-            "platos":self.description,
-            "restaurant":self.restaurant
+            # "platos":self.description,
+            # "restaurant_id":self.restaurant_id
             }
 
 
@@ -91,10 +91,10 @@ class Pedidos(db.Model):
     __tablename__="pedidos"
     id = db.Column(db.Integer, primary_key=True)
     restaurant_id= db.Column(db.Integer, ForeignKey("restaurant.id"))
-    restaurant=db.relationship(Restaurant)
+    # restaurant=db.relationship(Restaurant)
     usuario_id=db.Column(db.Integer, ForeignKey("user.id"))
     platos_id=db.Column(db.Integer, ForeignKey("platos.id"))
-    platos=db.relationship(Platos)
+    # platos=db.relationship(Platos)
     # usuario=relationship("User")
 
     def __repr__(self):
@@ -106,33 +106,52 @@ class Pedidos(db.Model):
             "restaurant_id":self.restaurant_id,
             "usuario_id": self.usuario_id,
             "platos_id": self.platos_id,
-            "platos": self.platos
+            # "platos": self.platos
         }
-
-
-
-
-class DetalleDePedidos(db.Model):
-    # __tablename__="detalleDePedidos"
+class Restaurantplatos(db.Model):
+    __tablename__="restaurantplatos"
     id = db.Column(db.Integer, primary_key=True)
+    restaurant_id= db.Column(db.Integer, ForeignKey("restaurant.id"))
+    # restaurant=db.relationship(Restaurant)
     platos_id=db.Column(db.Integer, ForeignKey("platos.id"))
-    platos=db.relationship(Platos)
-    # pedido=relationship("Pedidos")
-    restaurant_id=db.Column(db.Integer, ForeignKey("restaurant.id"))
-    restaurant=db.relationship("Restaurant")
-    # resturante=relationship("Restaurant")
+    # platos=db.relationship(Platos)
+    # usuario=relationship("User")
 
     def __repr__(self):
-        return f'< DetalleDePedidos {self.id}>'
+        return f'<Restaurantplatos {self.id}>'
     
     def serialize(self):
         return {
             "id":self.id,
             "restaurant_id":self.restaurant_id,
+            # "usuario_id": self.usuario_id,
             "platos_id": self.platos_id,
-            "restaurant":self.restaurant,
-            "platos": self.platos
+            # "platos": self.platos
         }
+
+
+
+# class DetalleDePedidos(db.Model):
+#     # __tablename__="detalleDePedidos"
+#     id = db.Column(db.Integer, primary_key=True)
+#     platos_id=db.Column(db.Integer, ForeignKey("platos.id"))
+#     platos=db.relationship(Platos)
+#     # pedido=relationship("Pedidos")
+#     restaurant_id=db.Column(db.Integer, ForeignKey("restaurant.id"))
+#     restaurant=db.relationship("Restaurant")
+#     # resturante=relationship("Restaurant")
+
+#     def __repr__(self):
+#         return f'< DetalleDePedidos {self.id}>'
+    
+#     def serialize(self):
+#         return {
+#             "id":self.id,
+#             "restaurant_id":self.restaurant_id,
+#             "platos_id": self.platos_id,
+#             "restaurant":self.restaurant,
+#             "platos": self.platos
+#         }
 
 
 
