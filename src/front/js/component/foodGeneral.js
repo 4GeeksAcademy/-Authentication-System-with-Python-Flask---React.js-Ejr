cbd2e4063442fbebabb05c43d75436e12dc76d34
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-06-08 02:27:51 +0000

    adding small skeleton for react functionallity on foodGeneral to create a reactive general food view

--- src/front/js/component/foodGeneral.js
+++ src/front/js/component/foodGeneral.js
@@ -5,10 +5,13 @@ import LoginModal from "../pages/loginModal";
 import "../../styles/register.css";
 
 export const FoodGeneral = () => {
+
+
 	return (
+            
         store[restaurantes]?.map(item=>{
             <h1>`${item.name}`</h1>
                        
-        }))
+}))};
 
 export default FoodGeneral;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,27 +6,37 @@ const getState = ({ getStore, getActions, setStore }) => {
 				{
 					name: "wok",
 					platos: ["ramen", "onigiri", "arroz", "sushi"],
-					ubicaciones: "white"
+					ubicaciones: "white",
+					url: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/65d1d1eb-9819-4610-9871-6d8e0ff7a206/DreamShaper_v6_wok_ramen_logo_on_top_right_elegant_marketing_0.jpg",
+					id: "0"
 				},
 				{
 					name: "McDonalds",
 					platos: ["hamburguesa", "hamburguesa con queso", "Doble libra", "Big Mac"],
-					ubicaciones: "white"
+					ubicaciones: "white",
+					url: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/f58af186-c80a-417f-af68-a5680f639561/DreamShaper_v6_McDonalds_food_hamburger_advertising_elegant_we_0.jpg",
+					id: "0"
 				},
 				{
 					name: "Pollo a la leña",
 					platos: ["Pollo asado", "Medio pollo asado", "Cuarto de pollo asado", "Tortillas"],
-					ubicaciones: "white"
+					ubicaciones: "white",
+					url: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/ea3c309b-a9b1-4e5a-aac4-35da60887158/DreamShaper_v6_chicken_roaster_smoked_advertising_elegant_webp_0.jpg",
+					id: "0"
 				},
 				{
 					name: "La Bella Italia",
 					platos: ["Pizza con hongos", "Pasta bolognesa", "Fetuccini Alfredo", "Gnocchi"],
-					ubicaciones: "white"
+					ubicaciones: "white",
+					url: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/6d29df4b-801b-4bb5-9b3d-32c71d153902/DreamShaper_v6_italian_food_sphaguetti_pizza_lasagna_advertisi_0.jpg",
+					id: "0"
 				},
 				{
-					name: "La Bella Italia",
-					platos: ["Pizza con hongos", "Pasta bolognesa", "Fetuccini Alfredo", "Gnocchi"],
-					ubicaciones: "white"
+					name: "Don Pacino",
+					platos: ["Pastel con Arandanos", "Pasta bolognesa", "Fetuccini Alfredo", "Gnocchi"],
+					ubicaciones: "white",
+					url: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/7772b5c6-fc14-4b94-bcdc-8ee42ca9564f/DreamShaper_v6_Mafi_food_advertising_elegant_webpage_blue_gree_1.jpg",
+					id: "0"
 				}
 			],
 			user: [
