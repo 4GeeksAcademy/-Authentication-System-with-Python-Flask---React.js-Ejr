3e0e7f34108af11bde12c200d507befd711ff50b
Danilo Mejias <daniloemejias@gmail.com>
2023-06-09 18:48:07 -0300

    updating local branch

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -5,38 +5,38 @@ const getState = ({ getStore, getActions, setStore }) => {
 			restaurantes: [
 				{
 					name: "wok",
-					plates: [{plateName: "Ramen", price: "40000", description: "x", image: ""}, {plateName: "Onigiri", price: "20000", description: "x", image: ""}, {plateName: "Arroz", price: "4000", description: "x", image: ""}, {plateName: "Sushi", price: "20000", description: "x", image: ""}],
+					plates: [{ plateName: "Ramen", price: "40000", price: "5000", description: "x", image: "" }, { plateName: "Onigiri", price: "20000", price: "5000", description: "x", image: "" }, { plateName: "Arroz", price: "4000", price: "5000", description: "x", image: "" }, { plateName: "Sushi", price: "20000", price: "5000", description: "x", image: "" }],
 					locations: ["Colombia", "Costa"],
 					image: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/65d1d1eb-9819-4610-9871-6d8e0ff7a206/DreamShaper_v6_wok_ramen_logo_on_top_right_elegant_marketing_0.jpg",
-					subscription: [{SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000"}]
+					subscription: [{ SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000" }]
 				},
 				{
 					name: "McDonalds",
-					plates: [{plateName: "Hamburguesa", price: "15000", description: "x", image: ""}, {plateName: "Hamburguesa con queso", price: "19000", description: "x", image: ""}, {plateName: "Doble libra", price: "20000", description: "x", image: ""}, {plateName: "Big Mac", price: "25000", description: "x", image: ""}],
+					plates: [{ plateName: "Hamburguesa", price: "15000", price: "5000", description: "x", image: "" }, { plateName: "Hamburguesa con queso", price: "19000", price: "5000", description: "x", image: "" }, { plateName: "Doble libra", price: "20000", price: "5000", description: "x", image: "" }, { plateName: "Big Mac", price: "25000", price: "5000", description: "x", image: "" }],
 					locations: ["Colombia", "Costa"],
 					image: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/f58af186-c80a-417f-af68-a5680f639561/DreamShaper_v6_McDonalds_food_hamburger_advertising_elegant_we_0.jpg",
-					subscription: [{SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000"}]
+					subscription: [{ SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000" }]
 				},
 				{
 					name: "Pollo a la leña",
-					plates: [{plateName: "Pollo asado", price: "15000",  description: "x", image: ""}, {plateName: "Medio pollo asado", price: "8000", description: "x", image: ""}, {plateName: "Cuarto de pollo asado", price: "6000", description: "x", image: ""}, {plateName: "Tortillas", price: "4000", description: "x", image: ""}],
+					plates: [{ plateName: "Pollo asado", price: "15000", price: "5000", description: "x", image: "" }, { plateName: "Medio pollo asado", price: "8000", price: "5000", description: "x", image: "" }, { plateName: "Cuarto de pollo asado", price: "6000", price: "5000", description: "x", image: "" }, { plateName: "Tortillas", price: "4000", price: "5000", description: "x", image: "" }],
 					locations: ["Colombia", "Costa"],
 					image: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/ea3c309b-a9b1-4e5a-aac4-35da60887158/DreamShaper_v6_chicken_roaster_smoked_advertising_elegant_webp_0.jpg",
-					subscription: [{SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000"}]
+					subscription: [{ SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000" }]
 				},
 				{
 					name: "La Bella Italia",
-					plates: [{Platename: "Pizza con hongos", price: "5000", description: "x", image: ""}, {plateName: "Pasta bolognesa", price: "14000", description: "x", image: ""}, {plateName: "Fetuccini Alfredo", price: "13000", description: "x", image: ""}, {plateName: "Gnocchi", price: "20000", description: "x", image: ""}],
+					plates: [{ Platename: "Pizza con hongos", price: "5000", description: "x", image: "" }, { plateName: "Pasta bolognesa", price: "14000", price: "5000", description: "x", image: "" }, { plateName: "Fetuccini Alfredo", price: "13000", price: "5000", description: "x", image: "" }, { plateName: "Gnocchi", price: "20000", price: "5000", description: "x", image: "" }],
 					locations: ["Colombia", "Costa"],
 					image: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/6d29df4b-801b-4bb5-9b3d-32c71d153902/DreamShaper_v6_italian_food_sphaguetti_pizza_lasagna_advertisi_0.jpg",
-					subscription: [{SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000"}]
+					subscription: [{ SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000" }]
 				},
 				{
 					name: "Don Pacino",
-					plates: [{plateName: "Tarta de peces", price: "5000", description: "x", image: ""}, {plateName: "Pasta destazada", price: "14000", description: "x", image: ""}, {plateName: "Puñalini Alfredo", price: "13000", description: "x", image: ""}, {plateName: "Pastel de arandanitos", price: "20000", description: "x", image: ""}],
+					plates: [{ plateName: "Tarta de peces", price: "5000", price: "5000", description: "x", image: "" }, { plateName: "Pasta destazada", price: "14000", price: "5000", description: "x", image: "" }, { plateName: "Puñalini Alfredo", price: "13000", price: "5000", description: "x", image: "" }, { plateName: "Pastel de arandanitos", price: "20000", price: "5000", description: "x", image: "" }],
 					locations: ["Colombia", "Costa"],
 					image: "https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/7772b5c6-fc14-4b94-bcdc-8ee42ca9564f/DreamShaper_v6_Mafi_food_advertising_elegant_webpage_blue_gree_1.jpg",
-					subscription: [{SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000"}]
+					subscription: [{ SubscriptionName: "Italianisimo", description: "Tendras una fabulosa comida variada todos los días!, Lunes Pizza, martes Pasta, miercoles fettucinni, jueves, gnocci, variaremos las combinaciones e ingredientes a lo lagro de la semana, asume lo delicioso y asegura tus sorpresas!", price: "20000" }]
 				}
 			],
 			user: [
@@ -54,31 +54,67 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			fetchUserData: async(email) =>{
+			fetchUserData: async (email) => {
 				const baseUrl = `${process.env.BACKEND_URL}/${email}`;
 
-				try{
+				try {
 					let response = await fetch(baseUrl)
-					if(!response.ok) return response.status
+					if (!response.ok) return response.status
 					setStore(email)
 				}
-				catch (error){
+				catch (error) {
 					console.error(error)
 				}
 			},
-			// NO USAR, SOLO COMO EJEMPLO
-			// deleteFavorite:(name)=>{
-			// 	let {favorites} = getStore()
-			// 	let newFavorites=[...favorites]
-			// 		let itemIndex=favorites.findIndex(item=>item.itemName==name)
-			// 		newFavorites.splice(itemIndex,1);
-			// 		setStore({favorites:newFavorites})
-			// 		console.log(itemIndex)
-			// 		console.log(favorites)
-			// }
-			}
+			user_create: async (user) => {
+				try {
+					const response = await fetch(apiUrl + "/register", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(user),
+					});
+
+					if (!response.ok) {
+						console.log(response.status + ": " + response.statusText);
+						return;
+					}
+
+					const data = await response.json();
+					// Realiza cualquier acción adicional que necesites con los datos del usuario registrado
+					console.log("Usuario registrado:", data);
+				} catch (error) {
+					console.log("Error al registrar el usuario:", error);
+				}
+			},
+
+			user_login: async (credentials) => {
+				try {
+					const response = await fetch(apiUrl + "/login", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(credentials),
+					});
+
+					if (!response.ok) {
+						console.log(response.status + ": " + response.statusText);
+						return;
+					}
+
+					const data = await response.json();
+					// Realiza cualquier acción adicional que necesites con los datos del usuario autenticado
+					console.log("Usuario ha iniciado sesión:", data);
+				} catch (error) {
+					console.log("Error al iniciar sesión:", error);
+				}
+			},
+
 		}
-	};
+	}
+};
 ;
 
 export default getState;
