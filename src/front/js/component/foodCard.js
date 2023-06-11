c3cba7bef155182edb8f51c728fb7b51d7a4f894
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-06-11 03:03:14 +0000

    adding finalized version of foodCard, found minor bug on restaurantOverview in the works of fixing

--- src/front/js/component/foodCard.js
+++ src/front/js/component/foodCard.js
@@ -1,37 +1,34 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import LoginModal from "../pages/loginModal";
 import "../../styles/register.css";
 import { Context } from "../store/appContext";
 
 export const FoodCard = () => {
     const {store, actions}= useContext(Context);
+    const index = store.restaurantes.find(item=>item.index==restaurantIndex)
+    const {restaurantIndex} = useParams()
+
     function imgError(e){
         e.target.src="https://cdn.leonardo.ai/users/25acf724-d0fb-44e7-8da5-a5932af5eac9/generations/f3918add-a5b5-437e-9bed-186b25ef5636/DreamShaper_v5_3_An_AIpowered_machine_surrounded_by_a_vibr_1.jpg"
     }
-    const restaurant = store.restaurantes
-    const plates = store.restaurantes.plates
 
     return(
         <>   
-        {store[plates] && store[plates].length > 0 && store[plates].map((element, index) =>{
+        {index.plates && index.plates.length > 0 && index.plates.map((element, index) =>{
                                 return(
-                <div className="col card gradient-custom-contrast"> 
-                    <div key={index} className="card-img-top mx-0 p-1 m-1" alt="{item.url}">
-                        <img  onError={imgError} className="card-img-top mx-0 p-1 pt-3" alt="restaurantImg"></img>
-                        <Link to={`/order-food/${index}`}>
+                <div key={index} className="col card bg-transparent border border-primary"> 
+                    <div key={index} onError={imgError} className="card-img-top mx-0 p-1 m-1 w-100" alt="{item.url}">
+                        <img  src={element.image} onError={imgError} className="card-img-top mx-0 p-1 pt-3 img-fluid figure-image" alt="restaurantImg" style={{ backgroundSize: "cover"}}></img>
                             <div className="card-body">
-                                <h5 className="card-body"><strong>{element.restaurantName}</strong></h5>
-                            </div>
-                        </Link>
-                        <div className="footer card-body ms-auto px-auto">
-                                <Link key={index} to={`/${element.restaurantName}/${index}`}>
-                                    <div className="row g-3 gap-3">
-                                        <button  className="btn btn-outline-primary col fw-bold rounded d-flex align-self-center justify-content-center">
-                                            {element.plateName}
-                                        </button>
-                                        <button className="btn btn-outline-primary col fw-bold rounded d-flex align-self-center justify-content-center">{element.price} Pesos</button>
-                                    </div>
+                                <h1 className="w-100 card-body text-light"><strong>{element.plateName}</strong></h1>
+                            </div>                        
+                        <div className="p-5 mt-5 w-100">
+                            <p className="text-light fs-3 description-text">{element.description}</p>
+                        </div>
+                        <div className="mx-auto d-flex justify-content-evenly w-100">
+                                <Link key={index} to={`/${element.plateName}/${index}`}>
+                                    <button className="btn btn-outline-primary fw-bold rounded d-flex align-self-center justify-content-center w-100 px-5 py-3">{element.price} Pesos</button>
                                 </Link>
                         </div>
                     </div>
