b93408bbbdd80b7fe062b7c66e6d2ee410d467c0
Manuel Perez <66376102+manu4970@users.noreply.github.com>
2023-06-14 01:17:27 +0000

    se crea vista login

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Login } from "./pages/login"
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,14 +24,13 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Login />} path="/login"/>
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
