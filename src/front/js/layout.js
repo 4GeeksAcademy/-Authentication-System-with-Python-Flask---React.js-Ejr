fc2d4d5efd5ccf1441156fef76947938a063ca55
Manuel Perez <66376102+manu4970@users.noreply.github.com>
2023-06-16 23:24:38 +0000

    se empieza creacion de signin

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,7 +6,8 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-import { Login } from "./pages/login"
+import { Login } from "./pages/login";
+import { Signin } from "./pages/signin";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Login />} path="/login"/>
+                        <Route element={<Signin/>} path="/signin"/>
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
