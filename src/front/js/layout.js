d2f31e90eb8b541d614765910b7b24e6ac245ddb
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-06-21 17:38:00 +0000

    navbar, layout and some css

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
@@ -10,30 +10,49 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Signup } from "./pages/signup";
+import ReactSwitch from "react-switch";
+
+
+import { createContext } from "react";
+export const ThemeContext = React.createContext(null)
 
 //create your first component
 const Layout = () => {
+    const [theme, setTheme] = useState('light')
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
+
+    const toggleTheme = () => {
+        setTheme((act) => (act === "light" ? "dark" : "light"))
+    }
     return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <div>
+                <div className="App" id={theme}>
+                    <BrowserRouter basename={basename}>
+                        <ScrollToTop>
+                            <Navbar />
+                            
+                            <Routes>
+                                <Route element={<Home />} path="/" />
+                                <Route element={<Demo />} path="/demo" />
+                                <Route element={<Signup />} path="/signup" />
+                                <Route element={<Single />} path="/single/:theid" />
+                                <Route element={<h1>Not found!</h1>} />
+                            </Routes>
+                            <Footer />
+                        </ScrollToTop>
+                    </BrowserRouter>
+                </div>
+                
+            </div>
+        </ThemeContext.Provider>
+        
     );
 };
 
