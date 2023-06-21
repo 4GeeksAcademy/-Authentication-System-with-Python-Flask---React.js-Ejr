c81bc7e5ac4cdf86ed8b15434cad20c462616188
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2023-06-21 10:04:21 +0200

    Front/back/createworker/delete working schedule (#75)
    * feat: validations, navigate to admin dashboard, toast corrections
    
    * fix: create service
    
    * fix: button previous step
    
    * fix: password input
    
    * fix: delete working schedule form back/front, fix: password input
    
    * fix: error message Input
    
    * fix: error message on worker form

--- src/front/js/components/workerForm/index.jsx
+++ src/front/js/components/workerForm/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import BigContainer from "../../components/bigContainer/index.jsx";
 import Input from "../input/index.jsx";
+import InputField from "../inputField/index.jsx";
 import styles from "./workerForm.module.css";
 import Button from "../button/index.jsx";
 import { createWorker } from "../../service/workers.js";
@@ -15,7 +16,6 @@ const initialState = {
   firstname: "",
   lastname: "",
   email: "",
-  working_schedule: "",
   password: "",
 };
 
@@ -96,20 +96,6 @@ const WorkerForm = ({ textBtn }) => {
             </small>
           )}
           <Input
-            icon={<i className="fa-solid fa-calendar-days"></i>}
-            type="text"
-            placeholder="Working schedule"
-            name="working_schedule"
-            value={newWorker.working_schedule}
-            register={register}
-          />
-          {errors?.working_schedule && (
-            <small className={styles._fail}>
-              <i className="fa-solid fa-circle-exclamation"></i>{" "}
-              {errors.working_schedule?.message}
-            </small>
-          )}
-          <Input
             icon={<i className="fa-solid fa-envelope"></i>}
             type="email"
             placeholder="email"
@@ -123,13 +109,13 @@ const WorkerForm = ({ textBtn }) => {
               {errors.email?.message}
             </small>
           )}
-          <Input
-            icon={<i className="fa-solid fa-lock"></i>}
+          <InputField
+            icon="fa-solid fa-lock"
             type="password"
-            placeholder="password"
+            placeholder="Password"
             name="password"
-            value={newWorker.password}
             register={register}
+            errors={errors}
           />
           {errors?.password && (
             <small className={styles._fail}>
