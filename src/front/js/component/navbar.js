d31df0f31ef7dbe5614189775e1525f2f8bc48fa
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-06-26 19:17:35 +0000

    roboto

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -13,9 +13,12 @@ export const Navbar = () => {
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid mx-3">
     <a className="navbar-brand tittle-nav" href="/">WhataCar</a>
+    
+    <div className="justify-content-end d-flex">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+
     <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
       <ul className="navbar-nav ml-auto align-items-end">
 
@@ -24,9 +27,9 @@ export const Navbar = () => {
             <i className="fa-regular fa-heart"></i>
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="#">Coche favorito 1</a></li>
+            <li><a className="dropdown-item" href="#">Coche favorito 2</a></li>
+            <li><a className="dropdown-item" href="#">Coche favorito 3</a></li>
           </ul>
         </li>
 
@@ -45,14 +48,21 @@ export const Navbar = () => {
             </button>
           </a>
         </li>
-
-        <li className="switch ms-4">
-          <label className="mode me-2 switch">{theme === "light" ? <i className="fa-regular fa-sun"></i> : <i className="fa-solid fa-moon"></i>}</label>
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} className="switch" />
-        </li>
-
       </ul>
+
     </div>
+        <div className="switch ms-4 mt-2">
+          <label className="mode me-2 switch">{theme === "light" ? <i class="fa-regular fa-lightbulb"></i> : <i class="fa-solid fa-lightbulb"></i>}</label>
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={theme === "dark"}
+            className="switch"
+            checkedIcon={null}
+            uncheckedIcon={null}
+            onColor="#200000"
+          />
+        </div>
+      </div>
   </div>
 </nav>
 
