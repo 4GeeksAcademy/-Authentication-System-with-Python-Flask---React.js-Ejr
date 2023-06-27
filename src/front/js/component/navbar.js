07cd2739945d662b175286bc73647225964c4a7c
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-06-27 11:02:50 +0000

    glasmorfismo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -46,47 +46,45 @@ export const Navbar = () => {
 
 
   const showModal = () => {
-
-    
-
     return (
       <div className="modalLogin">
-      
-      <ReactModal
-      style={{
-        overlay: {
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgb(170, 190, 214)'
-        },
-        content: {
-          position: 'absolute',
-          top: '40px',
-          left: '40px',
-          right: '40px',
-          bottom: '40px',
-          border: '1px solid #ccc',
-          background: '#fff',
-          overflow: 'auto',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
-          outline: 'none',
-          padding: '20px',
-        }
-      }}
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Login Modal"
-      >
-        <Login onSubmit={handleSubmit} />
-      </ReactModal>
-    </div>
+        <ReactModal
+          style={{
+            overlay: {
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: 'rgba(170, 190, 214, 0.75)', // Color de fondo del overlay
+              backdropFilter: 'blur(8px) saturate(180%)', // Efecto de desenfoque
+              WebkitBackdropFilter: 'blur(16px) saturate(180%)', // Prefijo para navegadores basados en WebKit
+            },
+            content: {
+              position: 'absolute',
+              top: '40px',
+              left: '40px',
+              right: '40px',
+              bottom: '40px',
+              border: '1px solid rgba(209, 213, 219, 0.3)', 
+              backgroundColor: 'rgba(255, 255, 255, 0.75)', 
+              borderRadius: '12px', 
+              outline: 'none',
+              padding: '20px',
+              overflow: 'auto',
+              WebkitOverflowScrolling: 'touch',
+            },
+          }}
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Login Modal"
+        >
+          <Login onSubmit={handleSubmit} />
+        </ReactModal>
+      </div>
     );
-  }
-
+  };
+  
 
 
 
