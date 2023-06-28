0b02658a4ff2122540f16b1a0559a5266814e628
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-06-28 17:21:27 +0000

    resuelto el login

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,7 +12,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 
