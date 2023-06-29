2cc5b676e5b03b17b32c2633f44589667856b64f
Rebillot <125141214+Rebillot@users.noreply.github.com>
2023-06-29 00:19:36 +0000

    UploadWidget: working on it

--- src/front/js/pages/Profile-Protected.js
+++ src/front/js/pages/Profile-Protected.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Context } from "../store/appContext";
 import RentButton from "../component/RentButton"
 import FindCanchaButton from "../component/FindCanchaButton"
+import UploadWidget from '../component/UploadWidget';
 import "../../styles/profile.css"
 
 
@@ -55,11 +56,15 @@ export const Profile = () => {
                         <div className="card mb-4">
                             <div className="card-body text-center">
                                 <img src={profilePicture} alt="avatar" className="rounded-circle img-fluid" style={{ width: '150px' }} />
-                                <div>
+                                <div> 
+                                    <button className='btn'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-camera-fill" viewBox="0 0 16 16" onClick={handleChangePicture}>
                                         <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
                                         <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
                                     </svg>
+
+                                </button>
+                                    
                                 </div>
                                 <h5 className="my-3">{lastName} {name}</h5>
                                 <p className="text-muted mb-1">Full Stack Prisoner</p>
