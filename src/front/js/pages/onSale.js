753a20af9579dc31611a035044952b625c7ee83e
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-07-01 17:11:16 +0000

    visual onsale (pc y móvil) hechas (sin funcionalidad

--- src/front/js/pages/onSale.js
+++ src/front/js/pages/onSale.js
@@ -8,19 +8,23 @@ export const On_sale = () => {
     return (
         <>
             <Profile_navbar />
-            <div className="on_sale_products">
-                <div className="row">
-                    <input className="col-1" type="checkbox" id="checkbox"></input>
-                    <label for="checkbox" className="col-1"></label>
-                        <img src="https://www.ruroc.com/media/catalog/product/j/o/joker_visor_4.jpg" alt="product" className="col-1"/>
-                        <h4 className="col-2">4750€</h4>
-                        <h4 className="col-4">Honda CB500F como nueva</h4>
-                        <div className="col-2">
-                            <button className="product_profile_button edit"><i class="fas fa-pencil"></i></button>
-                            <button className="product_profile_button sold">🤝🏼</button>
-                        </div>
+                <div className="row row_product_profile">
+                    <div className="product_img_profile_box col-2">
+                        <img src="https://www.motofichas.com/images/phocagallery/Honda/cb500f-2022/01-honda-cb500f-2022-estudio-rojo.jpg" alt="product" className="product_img_profile"/>
+                    </div>
+                    <div className="price_name col-6">
+                        <h4 className="price_product_profile">4750€</h4>
+                        <h5 className="name_product_profile">Honda CB500F como nueva</h5>
+                    </div>
+                    <div className="col-2 published">
+                        <h6 className="published_title_profile">Publicado</h6>
+                        <h4 className="published_date_profile">12/03/2021</h4>
+                    </div>
+                    <div className="col-2 product_profile_buttons">
+                        <button className="product_profile_button edit"><i class="fas fa-pencil"></i></button>
+                        <button className="product_profile_button sold">🤝🏼</button>
+                    </div>
                 </div>
-            </div>
         </>
     )
 }
