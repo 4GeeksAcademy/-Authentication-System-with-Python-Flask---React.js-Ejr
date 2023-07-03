a043a3bac990c02e97a359a066ace85e5e324453
Rebillot <jonathan.rebillot@gmail.com>
2023-07-02 23:17:25 -0400

    new footer, cancha y canchas working

--- src/front/js/component/CanchaCard.js
+++ src/front/js/component/CanchaCard.js
@@ -1,8 +1,22 @@
 import React, { useContext } from 'react';
 import { Context } from "../store/appContext";
 import "../../styles/CanchaCard.css";
+import { Link } from 'react-router-dom';
 
 const CanchaCard = ({ cancha }) => {
+  const canchaTypeIcon = {
+    Tenis: <i className="fa-solid fa-racquet"></i>,
+    Football: <i className="fa-solid fa-futbol"></i>,
+    Paddle: <i className="fa-solid fa-table-tennis-paddle-ball"></i>,
+    Basketball: <i className="fa-solid fa-basketball"></i>,
+    Babyfutbol: <i className="fa-duotone fa-futbol"></i>
+  };
+
+  const getSportTypeIcon = () => {
+    const sportType = cancha.sportType;
+    return canchaTypeIcon[sportType] || null;
+  };
+
   return (
     <section className="dark">
       <div className="container py-4">
@@ -22,17 +36,20 @@ const CanchaCard = ({ cancha }) => {
             </h1>
             <div className="postcard__subtitle small">
               <time dateTime="2020-05-25 12:00:00">
-                <i className="fas fa-map-marker-alt mr-2"></i> {cancha.location}
+                <i className="fas fa-map-marker-alt mr-2"> </i> {cancha.location}
               </time>
             </div>
             <div className="postcard__bar"></div>
             <ul className="postcard__tagbox">
               <li className="tag__item">
-                <i className="fas fa-tag mr-2"></i> {cancha.sportType}
+                {getSportTypeIcon() ? getSportTypeIcon() : <i className="fas fa-tag mr-2"></i>} {cancha.sportType}
               </li>
-              <li className="tag__item play blue">
+              <li className="tag__item">
+                <i className="fa -solid fa-person mr-2"></i> {cancha.user.name} {cancha.user.lastname}
+              </li>
+              <li className="tag__item play blue btn">
                 <a href="#">
-                  <i className="fas fa-basketball-ball mr-2"></i> Rent
+                  <i className="fas fa-basketball-ball mr-2"></i> Arrendar
                 </a>
               </li>
             </ul>
