6e60c1fff2436ebdc6dea30e2da7148dd0c55166
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-07-03 22:09:01 +0000

    visual de favorites terminada y comienzo de reviews

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -8,6 +8,28 @@ export const Favorites = () => {
     return (
         <>
             <Profile_navbar />
+            <h2 className="product_type_profile">
+                Motos
+            </h2>
+            <div className="row row_favorites_profile">
+                <div className="product_profile_favorites col-2">
+                    <div className="product_img_profile_favorites_box">
+                        <img src="https://www.motofichas.com/images/phocagallery/Honda/cb500f-2022/01-honda-cb500f-2022-estudio-rojo.jpg" alt="product" className="product_img_profile_favorites"/>
+                    </div>
+                    <div className="product_description_profile_favorites">
+                        <div className="row">
+                            <h6 className="col-10 price_product_profile">4500€</h6>
+                            <h6 className="col-2">❤️</h6>
+                        </div>
+                        <div className="row">
+                            <h6>En perfecto estado. Todas ...</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2 className="product_type_profile">
+                Coches
+            </h2>
             <div className="row row_favorites_profile">
                 <div className="product_profile_favorites col-2">
                     <div className="product_img_profile_favorites_box">
--- src/front/js/pages/profile_reviews.js
+++ src/front/js/pages/profile_reviews.js
@@ -6,6 +6,18 @@ import "/workspaces/Watacar_v2/src/front/styles/profile.css"
 
 export const Profile_reviews = () => {
     return (
-        <Profile_navbar />
+        <>
+            <Profile_navbar />
+            <div className="review_box_profile row">
+                <div className="product_img_profile_reviews_box col-3">
+                    <img src="https://www.motofichas.com/images/phocagallery/Honda/cb500f-2022/01-honda-cb500f-2022-estudio-rojo.jpg" alt="product" className="product_img_profile_favorites"/>
+                </div>
+                <div className="review_content_profile col-8">
+                    <h6>Nombre del producto</h6>
+                    <h6>⭐️⭐️⭐️⭐️⭐️</h6>
+                    <h6>Comentario del otro usuario</h6>
+                </div>
+            </div>
+        </>
     )
 }
