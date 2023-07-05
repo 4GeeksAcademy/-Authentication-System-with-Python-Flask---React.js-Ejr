6d2efc338d31a0b87ae2b3eaa87fd03573312178
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-07-05 15:55:49 +0000

    Modal de producto vendido a medias

--- src/front/js/pages/onSale.js
+++ src/front/js/pages/onSale.js
@@ -29,7 +29,41 @@ export const On_sale = () => {
                     </div>
                     <div className="col-2 product_profile_buttons">
                         <button className="product_profile_button edit"><i className="fas fa-pencil"></i></button>
-                        <button className="product_profile_button sold">🤝🏼</button>
+                        <button className="product_profile_button sold" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">🤝🏼</button>
+                    </div>
+                    <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content sold-product_profile">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">¿Has vendido este vehículo?</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="product_img_profile_box col-2">
+                                        <img src="https://www.motofichas.com/images/phocagallery/Honda/cb500f-2022/01-honda-cb500f-2022-estudio-rojo.jpg" alt="product" className="product_img_profile"/>
+                                    </div>
+                                    <h6 className="state_product_profile">{product.name}</h6>
+                                </div>
+                                <div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                                        <label className="form-check-label" for="flexRadioDefault1">
+                                            Usuario 1
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+                                        <label className="form-check-label" for="flexRadioDefault2">
+                                            Usuario 2
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                    <button type="button" className="btn btn-primary">marcar como vendido</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
