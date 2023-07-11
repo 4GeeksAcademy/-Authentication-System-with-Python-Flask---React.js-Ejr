a3df965a4ab563e494bd779f3b0da33bdc2dfbfc
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-07-11 08:47:04 +0000

    He incluido en el profile navbar la visual de block, aunque a lo mejor meto en venta y block en una misma

--- src/front/js/component/profile_navbar.js
+++ src/front/js/component/profile_navbar.js
@@ -8,10 +8,11 @@ export const Profile_navbar = () => {
             <h6 className="profile_navbar_text">Aquí podrás revisar todos los datos de tu cuenta de usuario</h6>
             <div className="row">
                 <NavLink exact to="/profile/configuration" className="navbar_profile col-1" activeClassName="active">Perfil</NavLink>
+                <NavLink to="/profile/onsale" className="navbar_profile col-1" activeClassName="active">En venta</NavLink>
+                <NavLink to="/profile/block" className="navbar_profile col-1" activeClassName="active">Reservas</NavLink>
                 <NavLink to="/profile/sales" className="navbar_profile col-1" activeClassName="active">Ventas</NavLink>
                 <NavLink to="/profile/buys" className="navbar_profile col-1" activeClassName="active">Compras</NavLink>
                 <NavLink to="/profile/reviews" className="navbar_profile col-1" activeClassName="active">Valoraciones</NavLink>
-                <NavLink to="/profile/onsale" className="navbar_profile col-1" activeClassName="active">En venta</NavLink>
                 <NavLink to="/profile/favorites" className="navbar_profile col-1" activeClassName="active">Favoritos</NavLink>
             </div>
         </div>
