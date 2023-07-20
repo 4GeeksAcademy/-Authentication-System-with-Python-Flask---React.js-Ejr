454d1bdbdd040550c18c2c539d7f6a137def8fdd
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-07-20 00:01:51 +0000

    cleaned up some files

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,12 +4,9 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import  Demo  from "./pages/demo";
-import { Single } from "./pages/single";
 import Login from "./pages/login";
 import CreateAccount from "./pages/create-account";
 import injectContext from "./store/appContext";
-import CreateAccount from "./pages/create-account"
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
@@ -28,10 +25,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/Login" />
                         <Route element={<CreateAccount />} path="/signup" />
-                        <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
