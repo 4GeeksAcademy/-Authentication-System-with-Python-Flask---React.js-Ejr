f8518d96b15c15e757c65ec88b47274cb69bf01b
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-07-21 20:10:27 +0000

    arreglado onsale

--- src/front/js/component/Sales_navbar.js
+++ src/front/js/component/Sales_navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 
-export const Sales_navbar = (onSale, blockedCount, soldCount) => {
+export const Sales_navbar = ({onsaleCount, blockedCount, soldCount}) => {
   const location = useLocation();
 
   const isOnSaleActive = () => {
@@ -12,13 +12,13 @@ export const Sales_navbar = (onSale, blockedCount, soldCount) => {
 
   return (
     <div className="container sales_navbar my-3 me-4 ">
-      <NavLink exact to="/profile/onsale" className="sales_navbar_buttons px-2" activeClassName="actived" isActive={isOnSaleActive}>
+      <NavLink exact to="/profile/onsale" className="sales_navbar_buttons px-2" activeClassName="active" isActive={isOnSaleActive}>
         A la venta {onsaleCount}
       </NavLink>
-      <NavLink to="/profile/block" className="sales_navbar_buttons" activeClassName="actived" isActive={isOnSaleActive}>
+      <NavLink to="/profile/block" className="sales_navbar_buttons" activeClassName="active" isActive={isOnSaleActive}>
         Reservadas {blockedCount} 
       </NavLink>
-      <NavLink to="/profile/sales" className="sales_navbar_buttons" activeClassName="actived">
+      <NavLink to="/profile/sales" className="sales_navbar_buttons" activeClassName="active" isActive={isOnSaleActive}>
             Vendidos {soldCount}
       </NavLink>
     </div>
