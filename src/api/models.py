2d18aecbeb17da42304ba2d1a0dd7a1a097e6017
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-07-22 19:02:10 +0000

    test log update and db users

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,77 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    Address = db.Column(db.String(200), nullable=False)
+    dni = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.Integer, nullable=False)
+    payment_method = db.Column(db.String(100), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False) 
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "Address": self.Address,
+            "dni": self.dni,
+            "location": self.location,
+            "payment_method": self.payment_method,
+            "is_admin": self.is_admin 
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+    comment_text = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<Review %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "trip_id": self.trip_id,
+            "comment_text": self.comment_text
+        }
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, nullable=False)
+    review_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "author_id": self.author_id,
+            "review_id": self.review_id
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "trip_id": self.trip_id
         }
