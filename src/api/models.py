55aeaeb8cf59363efe85c63d30c9d9523cbbc8f7
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-07-23 05:28:03 +0000

    log and signup dont work but server is OK

--- src/api/models.py
+++ src/api/models.py
@@ -33,8 +33,8 @@ class Offers(db.Model):
     medium_user_price = db.Column(db.Integer, nullable=False)
     high_user_price = db.Column(db.Integer, nullable=False)
     premium_user_price = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, db.ForeignKey('trip.id'), nullable=False)
 
-    trip = db.relationship("Trip", backref="offers")
 
     def __repr__(self):
         return '<Offers %r>' % self.id
@@ -55,8 +55,7 @@ class Trip(db.Model):
     country = db.Column(db.String(40), nullable=False)
     city = db.Column(db.String(40), nullable=False)
     activities = db.Column(db.String(100), nullable=False)
-
-    offers = db.relationship("Offers", backref="trip")
+    offers = db.relationship('Offers', backref='trip', lazy=True)
 
     def __repr__(self):
         return '<Trip %r>' % self.id
@@ -68,3 +67,96 @@ class Trip(db.Model):
             "city": self.city,
             "activities": self.activities
         }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    Address = db.Column(db.String(200), nullable=False)
+    dni = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.Integer, nullable=False)
+    payment_method = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "Address": self.Address,
+            "dni": self.dni,
+            "location": self.location,
+            "payment_method": self.payment_method,
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+    comment_text = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<Review %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "trip_id": self.trip_id,
+            "comment_text": self.comment_text
+        }
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, nullable=False)
+    review_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "author_id": self.author_id,
+            "review_id": self.review_id
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "trip_id": self.trip_id
+        }
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=True)  # Vous pouvez définir la valeur par défaut à True si nécessaire
+
+    def __repr__(self):
+        return '<Admin %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "is_admin": self.is_admin
+        }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,44 +1,21 @@
-import os
-from flask import Flask, request, jsonify, Blueprint
-from flask_migrate import Migrate
-from flask_cors import CORS
-from utils import APIException
-from models import db, User, Business_user, Offers, Trip
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+from flask import Flask, request, jsonify, url_for, Blueprint
+from api.models import db, User, Business_user, Post, Favorites, Review, Offers, Trip, Admin
+from api.utils import generate_sitemap, APIException
+from flask_bcrypt import bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from sqlalchemy.orm.exc import NoResultFound
 
-api = Blueprint('api', _name_)
 
-bcrypt = Bcrypt()
-db = SQLAlchemy()
+api = Blueprint('api', __name__)
+
 jwt = JWTManager()
 
-@api.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-@api.route('/')
-def sitemap():
-    return generate_sitemap(api)
-
-
-from flask import Flask, request, jsonify
-from flask_restful import Api
-from flask_jwt_extended import create_access_token, JWTManager
-from sqlalchemy.orm.exc import NoResultFound
-from models import User, Business_user
-import bcrypt
-
-app = Flask(__name__)
-api = Api(app)
-
-# Set the secret key to use with Flask-JWT-Extended.
-app.config['JWT_SECRET_KEY'] = 'your-secret-key'
-jwt = JWTManager(app)
-
-
+# Fonction d'initialisation de l'extension JWTManager avec l'application Flask
+def initialize_jwt(api):
+    jwt.init_app(api)
 
 @api.route('/token', methods=['POST'])
 def get_token():
@@ -48,7 +25,7 @@ def get_token():
 
         if not email or not password:
             return jsonify({'error': 'Email and password are required.'}), 400
-        
+
         try:
             # Check if the provided email exists in the User table
             login_user = User.query.filter_by(email=email).one()
@@ -78,26 +55,26 @@ def get_token():
 
         else:
             return jsonify({'error': 'Incorrect Password'}), 401
-    
+
     except Exception as e:
         return jsonify({'error': 'An error occurred: ' + str(e)}), 500
 
 
- # Admin route   
-@api.route('/admin', methods=['GET'])
-@jwt_required()
-def admin_dashboard():
-    is_admin = get_jwt_identity()['is_admin']
-    if is_admin:
-        users = User.query.all()
-        business = business.query.all()
-        return jsonify({
-            'users': [user.serialize() for user in users],
-            'business_users': [business.serialize() for business in business]
-        }), 200
-    else:
-        return jsonify({'error': 'Unauthorized'}), 401
-    
+# # Admin route   
+# @api.route('/admin', methods=['GET'])
+# @jwt_required()
+# def admin_dashboard():
+#     is_admin = get_jwt_identity()['is_admin']
+#     if is_admin:
+#         users = User.query.all()
+#         business = Business_user.query.all()
+#         return jsonify({
+#             'users': [user.serialize() for user in users],
+#             'business_users': [business.serialize() for business in business]
+#         }), 200
+#     else:
+#         return jsonify({'error': 'Unauthorized'}), 401
+
 
 # admin can delete users and companies
 @api.route('/admin/delete/<string:resource>/<int:resource_id>', methods=['DELETE'])
@@ -108,7 +85,7 @@ def delete_resource(resource, resource_id):
         if resource == 'users':
             resource_instance = User.query.get(resource_id)
         elif resource == 'companies':
-            resource_instance = business.query.get(resource_id)
+            resource_instance = Business_user.query.get(resource_id)
         else:
             return jsonify({'error': 'Invalid resource type'}), 400
 
@@ -120,38 +97,216 @@ def delete_resource(resource, resource_id):
             return jsonify({'error': f'{resource[:-1].capitalize()} not found'}), 404
     else:
         return jsonify({'error': 'Unauthorized'}), 401
-    
-    
 
-# Update companies perfil
-@api.route('/business/profile', methods=['PUT'])
+
+@api.route('/business/<int:business_id>', methods=['PUT'])
 @jwt_required()
-def update_business_profile():
-    try: 
-        current_business_id = get_jwt_identity()
-        business = business.query.get(current_business_id)
+def update_business_profile(business_id):
+    try:
+        business = Business_user.query.get(business_id)
 
         if not business:
-            return jsonify({'error': 'business not found'}), 400
-        
+            return jsonify({'error': 'Business not found'}), 404
+
         data = request.get_json()
 
         # Update business profile data
         business.name_business = data.get('name_business', business.name_business)
         business.email = data.get('email', business.email)
-        business.tin = data.get('tin', business.tin)
+        business.nif = data.get('nif', business.nif)
         business.address = data.get('address', business.address)
         business.payment_method = data.get('payment_method', business.payment_method)
 
         db.session.commit()
 
-        return jsonify({'message': 'business profile updated successfully', 'business': business.serialize()}), 200
-    
+        return jsonify({'message': 'Business profile updated successfully', 'business': business.serialize()}), 200
+
     except Exception as e:
-        return jsonify({'error': 'Error in updating business profile' + str(e)}), 500
+        return jsonify({'error': 'Error in updating business profile: ' + str(e)}), 500
 
 
-# this only runs if `$ python src/api.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    api.run(host='0.0.0.0', port=PORT, debug=False)
+
+@api.route('/login', methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+
+        if not data.get("email") or not data.get("password"):
+            return jsonify({'error': 'Email and password are required.'}), 400
+
+        user_or_business = None
+        is_admin = False
+
+        # Vérifier si c'est un utilisateur
+        user = User.query.filter_by(email=data['email']).first()
+        if user:
+            password_db = user.password
+            if bcrypt.check_password_hash(password_db, data["password"]):
+                user_or_business = user
+                is_admin = user.is_admin
+
+        # Vérifier si c'est une entreprise
+        business = Business_user.query.filter_by(email=data['email']).first()
+        if business:
+            password_db = business.password
+            if bcrypt.check_password_hash(password_db, data["password"]):
+                user_or_business = business
+
+        if not user_or_business:
+            return jsonify({'error': 'User or Business not found or Incorrect password'}), 401
+
+        access_token = create_access_token(identity=user_or_business.id, is_admin=is_admin)
+        return jsonify({'access_token': access_token, 'user_or_business': user_or_business.serialize()}), 200
+
+    except Exception as e:
+        return jsonify({'error': 'Error in login: ' + str(e)}), 500
+
+# Route de login spéciale pour l'administrateur
+@api.route('/admin/login', methods=['POST'])
+def admin_login():
+    try:
+        data = request.get_json()
+
+        if not data.get("email") or not data.get("password"):
+            return jsonify({'error': 'Email and password are required.'}), 400
+
+        # Vérifier si l'administrateur existe
+        admin = Admin.query.filter_by(email=data['email']).first()
+        if admin:
+            password_db = admin.password
+            if bcrypt.check_password_hash(password_db, data["password"]):
+                access_token = create_access_token(identity=admin.id, is_admin=True)
+                return jsonify({'access_token': access_token, 'admin': admin.serialize()}), 200
+
+        return jsonify({'error': 'Admin not found or Incorrect password'}), 401
+
+    except Exception as e:
+        return jsonify({'error': 'Error in admin login: ' + str(e)}), 500
+
+@api.route('/signup', methods=['POST'])
+def create_user_or_business():
+    try:
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        # Vérifier si l'email et le mot de passe sont fournis
+        if not email or not password:
+            return jsonify({'error': 'Email and password are required.'}), 400
+
+        # Vérifier si l'email existe déjà pour un utilisateur ou une entreprise
+        existing_user = User.query.filter_by(email=email).first()
+        existing_business = Business_user.query.filter_by(email=email).first()
+
+        if existing_user:
+            return jsonify({'error': 'Email already exists for a user.'}), 409
+
+        if existing_business:
+            return jsonify({'error': 'Email already exists for a business.'}), 409
+
+        # Si le champ 'name_business' est présent, c'est une inscription d'entreprise
+        if 'name_business' in data:
+            name_business = data.get('name_business')
+            nif = data.get('nif')
+            address = data.get('address')
+            payment_method = data.get('payment_method')
+
+            # Hacher le mot de passe et créer l'entreprise
+            password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_business = Business_user(business_name=name_business, email=email, password=password_hash, nif=nif, address=address, payment_method=payment_method)
+            db.session.add(new_business)
+            db.session.commit()
+
+            return jsonify({'message': 'Business created successfully', 'business': new_business.serialize()}), 201
+
+        # Sinon, c'est une inscription d'utilisateur
+        else:
+            firstname = data.get('firstname')
+            lastname = data.get('lastname')
+            username = data.get('username')
+            Address = data.get('Address')
+            dni = data.get('dni')
+            location = data.get('location')
+            payment_method = data.get('payment_method')
+
+            # Hacher le mot de passe et créer l'utilisateur
+            password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = User(email=email, password=password_hash, firstname=firstname, lastname=lastname, username=username, Address=Address, dni=dni, location=location, payment_method=payment_method)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return jsonify({'message': 'User created successfully', 'user': new_user.serialize()}), 201
+
+    except Exception as e:
+        return jsonify({'error': 'Error in user/business creation: ' + str(e)}), 500
+
+
+# Private route
+@api.route('/private')
+@jwt_required()
+def private():
+    current_user = get_jwt_identity()
+    login_user = User.query.get(current_user)
+
+    if login_user:
+        return jsonify({'message': 'Welcome to the private area!', 'user': login_user.serialize()})
+    else:
+        login_business = Business_user.query.get(current_user)
+        if login_business:
+            return jsonify({'message': 'Welcome to the private area!', 'business': login_business.serialize()})
+        else:
+            return jsonify({'error': 'Unauthorized'}), 401
+        
+        # Update user profil
+@api.route('/user/<int:user_id>', methods=['PUT'])
+@jwt_required()
+def update_user_profile(user_id):
+    try:
+        user = User.query.get(user_id)
+
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+
+        data = request.get_json()
+
+        # Update user profile data
+        user.username = data.get('username', user.username)
+        user.firstname = data.get('firstname', user.firstname)
+        user.lastname = data.get('lastname', user.lastname)
+        user.Address = data.get('Address', user.Address)
+        user.dni = data.get('dni', user.dni)
+        user.location = data.get('location', user.location)
+        user.payment_method = data.get('payment_method', user.payment_method)
+
+        db.session.commit()
+
+        return jsonify({'message': 'User profile updated successfully', 'user': user.serialize()}), 200
+
+    except Exception as e:
+        return jsonify({'error': 'Error in updating user profile: ' + str(e)}), 500
+
+
+@api.route('/user/profile', methods=['GET'])
+@jwt_required()
+def get_user_profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    return jsonify({'user': user.serialize()}), 200
+
+
+
+@api.route('/business/profile', methods=['GET'])
+@jwt_required()
+def get_business_profile():
+    business_id = get_jwt_identity()
+    business = Business_user.query.get(business_id)
+
+    if not business:
+        return jsonify({'error': 'Business not found'}), 404
+
+    return jsonify({'business': business.serialize()}), 200
+
