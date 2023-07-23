79bd53bb181785223789bcfd1c5d0ac797f3cd73
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-23 03:32:38 +0000

    Authorization

--- src/api/routes.py
+++ src/api/routes.py
@@ -32,7 +32,7 @@ def create_user():
     return jsonify({"Message": "User successfully created"})
 
 
-
+ 
 # LOGIN ENDPOINT FOR USERS
 @api.route('/login', methods=['POST'])
 def login_user():
@@ -63,3 +63,25 @@ def show_email():
     return jsonify ({"email": user.email, "id": user.id, "response": "That is your data up there!"}), 200
 
 
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
+
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Query your database for email and password
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
