323444cd8ef62aa5cbe6b81c1bc39e10d6ef5061
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-07-24 01:20:19 +0000

    Cambios para la estructura inicial

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
-//import your own components
 import Layout from "./layout";
 
-//render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+const root = createRoot(document.querySelector('#app'))
+root.render(<Layout />)
+
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,13 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+
 
 //create your first component
 const Layout = () => {
@@ -23,14 +19,10 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
