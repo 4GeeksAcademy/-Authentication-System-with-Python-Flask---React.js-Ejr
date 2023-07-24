badf3795c9fc0c28e83109a861f982c8bbf47fb0
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-07-24 23:21:52 +0000

    arreglados los resultados de filtros

--- src/front/js/component/searchResults.js
+++ src/front/js/component/searchResults.js
@@ -8,7 +8,7 @@ import { Placeholder_carousel } from "../pages/placeholder_carousel";
 
 export const SearchResults = (props) => {
   const { store, actions } = useContext(Context);
-  const carImage = "https://images.coches.com/_vn_/kia/Sportage/c399cf1d98a95d24f8e8715dd0b13fb2.jpg?p=cc_vn_high"
+  const carImage = "https://images.coches.com/_vn_/kia/Sportage/c399cf1d98a95d24f8e8715dd0b13fb2.jpg?p=cc_vn_high";
 
   const [hasFiltered, setHasFiltered] = useState(false); // Estado local para indicar si se ha realizado un filtrado
 
@@ -31,72 +31,77 @@ export const SearchResults = (props) => {
   }, []);
 
   useEffect(() => {
-    actions.getFilteredProducts()
+    actions.getFilteredProducts();
 
-  },[])
-  console.log(store.filterProducts)
+  }, []);
+  console.log(store.filterProducts);
   return (
     <div className="container">
-    <div className="d-flex overflow-auto my-5">
-      {store.filterProducts.length > 0 ? (
-        <div className="d-flex overflow-auto my-5">
-          {store.filterProducts.map((vehicle, index) => (
-            <div className="col-12 col-md-4 card-item" key={index}> 
-              <div className="card" style={{ width: "18rem" }}>
-                <div className="flip-card">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front">
+    <div className="d-flex overflow-auto my-5 ">
+      {store.products.length > 0 ? (
+        store.products.map((vehicle, index) => (
+          <div className="mx-3 mb-5" key={index}>
+            <div className="card card-blur" style={{ width: "18rem" }}>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    {/* Update the rendering to show the first image of the vehicle */}
+                    {vehicle.images.length > 0 ? (
+                      <img src={vehicle.images[0].image} className="card-img-top imgCarousel" alt="..." />
+                    ) : (
                       <img src={carImage} className="card-img-top imgCarousel" alt="..." />
-                    </div>
-                    <div className="flip-card-back">
-                      <Link to={`product/${vehicle.id}`} style={{ color: "white", textDecoration: "none" }} className="link-hover">
-                        <h3 className="pt-2">{vehicle.brand.name}</h3>
-                      </Link>
+                    )}
+                  </div>
+                  <div className="flip-card-back">
+                    <Link to={`product/${vehicle.id}`} style={{ color: 'white', textDecoration: 'none' }} className="link-hover">
+                      <h3 className="pt-2">{vehicle.brand.name}</h3>
                       <p>Matriculación: {vehicle.year}</p>
                       <p>Estado: {vehicle.state}</p>
                       <p>{vehicle.km} km</p>
                       <p>{vehicle.fuel}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="card-body d-flex justify-content-between">
-                  <div>
-                    <Link to={`product/${vehicle.id}`} style={{ color: "black", textDecoration: "none" }} className="link-hover">
-                      <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
-                        <strong>
-                          {vehicle.name.length >= 25 ? vehicle.name.slice(0, 19) + "..." : vehicle.name}</strong>
-                      </h5>
-                    </Link>
-                    <h5 className="card-title justify-content-start d-flex">20.000€</h5>
-                    <p>
-                      Vendido por{" "}
-                      <Link
-                        to={!store.token ? "/login" : "/profile"}
-                        style={{ color: "black", textDecoration: "none" }}
-                        className="link-hover"
-                      >
-                        {vehicle.user_full_name}
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="d-flex justify-content-end">
-                    <Link id="heartCard" to="/profile/favorites" onClick={() => selectFavoriteVehicle(vehicle.id)}>
-                      <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
-                      <lord-icon
-                        src="https://cdn.lordicon.com/rjzlnunf.json"
-                        trigger="hover"
-                        colors="primary:#1663c7,secondary:#16a9c7"
-                        stroke="80"
-                        style={{ width: "50px", height: "30px" }}
-                      ></lord-icon>
                     </Link>
                   </div>
                 </div>
               </div>
+              <div className="card-body d-flex justify-content-between">
+                <div>
+                  <Link to={`/product/${vehicle.id}`} style={{ color: 'black', textDecoration: 'none' }} className="link-hover">
+                    <h5 className="card-title justify-content-start d-flex" id="vehicleCardTittle">
+                      {vehicle.name.length >= 25 ? vehicle.name.slice(0, 19) + "..." : vehicle.name}
+                    </h5>
+                  </Link>
+                  <h5 className="card-title justify-content-start d-flex">{vehicle.price} €</h5>
+                  <p>
+                    Vendido por{" "}
+                    <span
+
+                      style={{ color: 'black', textDecoration: 'none' }}
+                      className="link-hover"
+                    >
+                      {vehicle.user_full_name}
+                    </span>
+                  </p>
+                </div>
+                <div className="d-flex justify-content-end">
+                  <Link
+                    id="heartCard"
+                    to=""
+                    onClick={() => selectFavoriteVehicle(store.user.id, vehicle.id)}
+                  >
+                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                    <lord-icon
+                      src="https://cdn.lordicon.com/rjzlnunf.json"
+                      trigger="hover"
+                      colors="primary:#1663c7,secondary:#16a9c7"
+                      stroke="80"
+                      style={{ width: "50px", height: "30px" }}
+                    ></lord-icon>
+                  </Link>
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-        
+          </div>
+        ))
       ) : (
         <div className="d-flex justify-content-center m-auto" >
           <h3 className="my-5">
