991896675c69f19f2359466ca8ad50a4ba886df5
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-07-25 07:55:18 +0000

    arreglado placeholder de profile y profile, además de visual de talleres

--- src/front/js/pages/placeholder_profile.js
+++ src/front/js/pages/placeholder_profile.js
@@ -10,51 +10,52 @@ export const Placeholder_profile = () => {
 return (
   <>
   <div className="container_profile">
-        <div className="d-lg-flex justify-content-end">
+      <div className="mx-5 px-5 box w-100"> 
+      <div className="d-lg-flex justify-content-start my-5">
         <p aria-hidden="true" className="placeholder-glow ">
           <span className="placeholder col-4 avatar_image"></span>
         </p>
       </div>
 
-      <div className="profile_info">
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
-          <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+      <div className="profile_info  m-auto pb-5">
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10  col-sm-10 col-md-10 col-lg-10"></a>
+          <p aria-hidden="true" className="placeholder-glow   ">
+            <span className="placeholder col-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></a>
           <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+            <span className="placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></a>
           <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+            <span className="placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></a>
           <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+            <span className="placeholder col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10 col-sm-10 col-md-10 col-lg-10"></a>
           <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+            <span className="placeholder  col-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
-        <div className="row_profile_configuration">
-          <a className="btn btn-primary disabled placeholder col-4"></a>
+        <div className="row_profile_configuration mx-1 justify-content-around text-center">
+          <a className="btn btn-primary disabled placeholder col-10 col-sm-10 col-md-10 col-lg-10"></a>
           <p aria-hidden="true" className="placeholder-glow">
-            <span className="placeholder col-4"></span>
+            <span className="placeholder col-10 col-sm-10 col-md-10 col-lg-10"></span>
           </p>
         </div>
       </div>
-    </div>
+    </div></div>
   </>
         )
    }
