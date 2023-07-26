8d1d6f2e442a717b9825bca46525797fcf7baae5
Albert González Escribano <126629570+AlbertgEscribano@users.noreply.github.com>
2023-07-26 21:38:11 +0000

    feat all missing fetchs in flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,7 +6,7 @@ const getState = ({ getStore, getActions, setStore }) => {
       user: {},
       business_user: [],
       trip: [],
-      post: [],
+      review: [],
       offers: [],
     },
     actions: {
@@ -118,6 +118,445 @@ const getState = ({ getStore, getActions, setStore }) => {
         let token = localStorage.getItem("myToken");
         return token != null ? true : false;
       },
+
+      // get fetch  for all users
+      getAllUsers : async () => {
+        try {
+          const response = await fetch(API_URL + "/users", {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData.users;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching users");
+            return [];
+          }
+        } catch (err) {
+          console.log(err);
+          return []; // Handle other errors, return an empty array by default
+        }
+      },
+
+      // get fetch for individual users
+
+      getUserById : async (userId) => {
+        try {
+          const response = await fetch(API_URL + `/users/${userId}`, {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle user not found error
+            console.log("User not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching user");
+            return null;
+          }
+        } catch (err) {
+          console.log(err);
+          return null; // Handle other errors, return null by default
+        }
+      },
+
+      // get fetch for all business users
+
+      getAllBusinessUsers : async () => {
+        try {
+          const response = await fetch(API_URL + "/business_users", {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData.business_users;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching business users");
+            return [];
+          }
+        } catch (err) {
+          console.log(err);
+          return []; // Handle other errors, return an empty array by default
+        }
+      },
+
+      // get fetch for individuals business users
+
+      getBusinessUserById :async (businessUserId) => {
+        try {
+          const response = await fetch(API_URL + `/business_users/${businessUserId}`, {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle business user not found error
+            console.log("Business user not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching business user");
+            return null;
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      
+      // get fetch for all offers that are created
+
+      getAllOffers : async () => {
+        try {
+          const response = await fetch(API_URL + "/offer", {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData.offers;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching offers");
+            return [];
+          }
+        } catch (err) {
+          console.log(err);
+          return []; // Handle other errors, return an empty array by default
+        }
+      },
+
+      // get fetch to get individual offer by id
+
+        getOfferById : async (offerId) => {
+        try {
+          const response = await fetch(API_URL + `/offers/${offerId}`, {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle offer not found error
+            console.log("Offer not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching offer");
+            return null;
+          }
+        } catch (err) {
+          console.log(err);
+          return null; // Handle other errors, return null by default
+        }
+      },
+
+      // create an offer
+
+      createOffer : async (data) => {
+        try {
+          const response = await fetch(API_URL + "/offer", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+            body: JSON.stringify(data),
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else {
+            // Handle other errors
+            console.log("Error in creating offer");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // update an offer by id
+
+      updateOfferById : async (offerId, data) => {
+        try {
+          const response = await fetch(API_URL + `/offer/${offerId}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+            body: JSON.stringify(data),
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle offer not found error
+            console.log("Offer not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in updating offer");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // delete an offer by id
+
+      deleteOfferById : async (offerId) => {
+        try {
+          const response = await fetch(API_URL + `/offer/${offerId}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+          });
+      
+          if (response.ok) {
+            console.log("Offer deleted successfully");
+            return true;
+          } else if (response.status === 404) {
+            // Handle offer not found error
+            console.log("Offer not found");
+            return false;
+          } else {
+            // Handle other errors
+            console.log("Error in deleting offer");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // get fetch to get all trips 
+
+      getAllTrips : async () => {
+        try {
+          const response = await fetch(API_URL + "/trips", {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData.trips;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching trips");
+            return [];
+          }
+        } catch (err) {
+          console.log(err);
+          return []; // Handle other errors, return an empty array by default
+        }
+      },
+
+      // get individual trip by id
+
+      getTripById : async (tripId) => {
+        try {
+          const response = await fetch(API_URL + `/trips/${tripId}`, {
+            method: "GET",
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle trip not found error
+            console.log("Trip not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in fetching trip");
+            return null;
+          }
+        } catch (err) {
+          console.log(err);
+          return null; // Handle other errors, return null by default
+        }
+      },
+
+      // create a trip
+
+      createTrip : async (data) => {
+        try {
+          const response = await fetch(API_URL + "/trip", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+            body: JSON.stringify(data),
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else {
+            // Handle other errors
+            console.log("Error in creating trip");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // update trip by id
+
+      updateTripById : async (tripId, data) => {
+        try {
+          const response = await fetch(API_URL + `/trip/${tripId}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+            body: JSON.stringify(data),
+          });
+      
+          if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            return responseData;
+          } else if (response.status === 404) {
+            // Handle trip not found error
+            console.log("Trip not found");
+            return null;
+          } else {
+            // Handle other errors
+            console.log("Error in updating trip");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // delete trip by id
+
+      deleteTripById : async (tripId) => {
+        try {
+          const response = await fetch(API_URL + `/trip/${tripId}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+          });
+      
+          if (response.ok) {
+            console.log("Trip deleted successfully");
+            return true;
+          } else if (response.status === 404) {
+            // Handle trip not found error
+            console.log("Trip not found");
+            return false;
+          } else {
+            // Handle other errors
+            console.log("Error in deleting trip");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // delete method for user 
+
+      deleteUserById : async (userId) => {
+        try {
+          const response = await fetch(API_URL + `/business_user/delete/users/${userId}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+          });
+      
+          if (response.ok) {
+            console.log("User deleted successfully");
+            return true;
+          } else if (response.status === 404) {
+            // Handle user not found error
+            console.log("User not found");
+            return false;
+          } else {
+            // Handle other errors
+            console.log("Error in deleting user");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+
+      // delete method for business_user 
+
+      deleteBusinessUserById : async (businessUserId) => {
+        try {
+          const response = await fetch(API_URL + `/business_user/delete/business_users/${businessUserId}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("myToken"),
+            },
+          });
+      
+          if (response.ok) {
+            console.log("Business user deleted successfully");
+            return true;
+          } else if (response.status === 404) {
+            // Handle business user not found error
+            console.log("Business user not found");
+            return false;
+          } else {
+            // Handle other errors
+            console.log("Error in deleting business user");
+            return false;
+          }
+        } catch (err) {
+          console.log(err);
+          return false; // Handle other errors, return false by default
+        }
+      },
+      
     },
   };
 };
