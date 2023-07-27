3910ab832c8aeabba1a81c65723cd334f55dbc2c
Luis Vela <luismvl114@gmail.com>
2023-07-26 22:49:45 -0500

    separo los fetch en un archivo aparte

--- src/front/js/views/Login.jsx
+++ src/front/js/views/Login.jsx
@@ -1,73 +1,72 @@
-import React, { useState, useContext } from "react";
-import { Context } from "../store/appContext";
-import { useNavigate } from "react-router";
-// import styles from "./styles/Login.module.css"
+import React, { useState, useContext } from 'react'
+import { Context } from '../store/appContext'
+import { useNavigate } from 'react-router'
 
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { actions } = useContext(Context)
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const { actions } = useContext(Context);
+  let navigate = useNavigate()
 
-let navigate = useNavigate();
-
-async function handleSubmit(e) {
-    e.preventDefault();
-    let isLogged = await actions.login(email, password);
-    
-    if (isLogged) {
-      //true
-      navigate("/Home");
-      
-    }else {
-      return alert("Incorrect user/password")
-    }
+  async function handleSubmit(e) {
+    e.preventDefault()
+    actions
+      .login(email, password)
+      .then((res) => navigate('/home'))
+      .catch((err) => {
+        alert('Incorrect user/password')
+        console.log(err.message)
+        console.log(err.httpStatus)
+      })
   }
 
   const handleRedirect = () => {
-    navigate('/signup');
-  };
+    navigate('/signup')
+  }
 
+  return (
+    <div>
+      <h1 className='mx-auto text-center' style={{ fontSize: '3rem' }}>
+        Welcome Back!
+      </h1>
+      <form className='card w-75 mx-auto mb-5' onSubmit={handleSubmit}>
+        <div className='m-3'>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Email address</h5>
+          </label>
+          <input
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-control'
+            id='exampleInputEmail1'
+            aria-describedby='emailHelp'
+          />
+        </div>
+        <div className='m-3'>
+          <label htmlFor='exampleInputPassword1' className='form-label'>
+            <h5>Password </h5>
+          </label>
+          <input
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            className='form-control'
+            id='exampleInputPassword1'
+          />
+        </div>
+        <button type='submit' className='btn btn-success  mx-auto m-3'>
+          Enter
+        </button>
+      </form>
+      <h5
+        className='mt-3 card w-75 mx-auto'
+        style={{ cursor: 'pointer' }}
+        onClick={handleRedirect}
+      >
+        You haven't account? Click Here to Register!
+      </h5>
+    </div>
+  )
+}
 
-return (
-  <div >
-     <h1 className="mx-auto text-center" style={{ fontSize: '3rem' }}>Welcome Back!</h1>
-    <form
-      className="card w-75 mx-auto mb-5"
-      onSubmit={handleSubmit}
-    >
-      <div className="m-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Email address</h5>
-        </label>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div className="m-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-        <h5>Password </h5>
-        </label>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <button type="submit" className="btn btn-success  mx-auto m-3">
-        Enter
-      </button>
-    </form>
-    <h5 className="mt-3 card w-75 mx-auto" style={{ cursor: 'pointer' }}  onClick={handleRedirect}>You haven't account? Click Here to Register!</h5>
-    
-  </div>
-  );
-};
-
-export default Login;
-
+export default Login
--- src/front/js/views/SignUp.jsx
+++ src/front/js/views/SignUp.jsx
@@ -1,174 +1,179 @@
-import React, { useState, useContext } from "react";
-import { Context } from "../store/appContext";
-import { useNavigate } from "react-router-dom";
-
-
+import React, { useState, useContext } from 'react'
+import { Context } from '../store/appContext'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [location, setLocation] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
+  const [paymentMethod, setPaymentMethod] = useState('')
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [location, setLocation] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [paymentMethod, setPaymentMethod] = useState("");
+  const { actions } = useContext(Context)
 
-
-  const { actions } = useContext(Context);
-
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
   async function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    let signup = await actions.signup(email, password , firstName, lastName ,phone , location , address, paymentMethod);
-    console.log(signup);
-    if (signup && !signup.error) {
-      navigate("/login");
-    } else if (signup && signup.error) {
-      alert("Error: " + signup.error);
-    } else {
-      alert("Error 409: Usuario ya existe");
-    }
+    actions
+      .signup(
+        email,
+        password,
+        firstName,
+        lastName,
+        phone,
+        location,
+        address,
+        paymentMethod
+      )
+      .then((res) => navigate('/login'))
+      .catch((error) => {
+        if (error.httpStatus === 409) alert(error.message)
+        else alert(error.message)
+      })
   }
 
-  // function redirectToLogin() {
-  //   setTimeout(() => {
-  //     window.location.href = '/login';
-  //   }, 1000); 
-  // }
-
   const handlePaymentSelection = (event) => {
-    setPaymentMethod(event.target.value);
-  };
+    setPaymentMethod(event.target.value)
+  }
 
   const handleLogin = () => {
-    navigate('/login');
-  };
+    navigate('/login')
+  }
 
   return (
-    <div className="">
-    
-    <h1 className="mx-auto text-center" style={{ fontSize: '3rem' }}>Welcome to Vanguard Vesture!</h1>
-    <form
-      className="card w-50 mx-auto mb-5"
-      onSubmit={handleSubmit}
-    >
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Email address</h5>
-        </label>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-          id="email"
-        />
-      </div>
-      <div className="m-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          <h5>Password</h5>
-        </label>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="form-control"
-          id="password"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>First Name</h5>
-        </label>
-        <input
-          type="text"
-          onChange={(e) => setFirstName(e.target.value)}
-          className="form-control"
-          id="firstName"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Last Name</h5>
-        </label>
-        <input
-          type="text"
-          onChange={(e) => setLastName(e.target.value)}
-          className="form-control"
-          id="lastName"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Phone</h5>
-        </label>
-        <input
-          type="text"
-          onChange={(e) => setPhone(e.target.value)}
-          className="form-control"
-          id="Phone"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Location</h5>
-        </label>
-        <input
-          type="text"
-          onChange={(e) => setLocation(e.target.value)}
-          className="form-control"
-          id="location"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Address</h5>
-        </label>
-        <input
-          type="text"
-          onChange={(e) => setAddress(e.target.value)}
-          className="form-control"
-          id="address"
-        />
-      </div>
-      <div className="m-3 ">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          <h5>Your Favorite Payment Methond</h5>
-        </label>
-        <label>
+    <div className=''>
+      <h1 className='mx-auto text-center' style={{ fontSize: '3rem' }}>
+        Welcome to Vanguard Vesture!
+      </h1>
+      <form className='card w-50 mx-auto mb-5' onSubmit={handleSubmit}>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Email address</h5>
+          </label>
           <input
-            className="m-3"
-            type="radio"
-            name="metodo_pago"
-            value="VISA"
-            checked={paymentMethod === 'VISA'}
-            onChange={handlePaymentSelection}
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-control'
+            id='email'
           />
-          VISA
-        </label>
-        <label>
+        </div>
+        <div className='m-3'>
+          <label htmlFor='exampleInputPassword1' className='form-label'>
+            <h5>Password</h5>
+          </label>
           <input
-            className="m-3"
-            type="radio"
-            name="metodo_pago"
-            value="MASTERCARD"
-            checked={paymentMethod === 'MASTERCARD'}
-            onChange={handlePaymentSelection}
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            className='form-control'
+            id='password'
           />
-          MASTERCARD
-        </label>
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>First Name</h5>
+          </label>
+          <input
+            type='text'
+            onChange={(e) => setFirstName(e.target.value)}
+            className='form-control'
+            id='firstName'
+          />
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Last Name</h5>
+          </label>
+          <input
+            type='text'
+            onChange={(e) => setLastName(e.target.value)}
+            className='form-control'
+            id='lastName'
+          />
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Phone</h5>
+          </label>
+          <input
+            type='text'
+            onChange={(e) => setPhone(e.target.value)}
+            className='form-control'
+            id='Phone'
+          />
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Location</h5>
+          </label>
+          <input
+            type='text'
+            onChange={(e) => setLocation(e.target.value)}
+            className='form-control'
+            id='location'
+          />
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Address</h5>
+          </label>
+          <input
+            type='text'
+            onChange={(e) => setAddress(e.target.value)}
+            className='form-control'
+            id='address'
+          />
+        </div>
+        <div className='m-3 '>
+          <label htmlFor='exampleInputEmail1' className='form-label'>
+            <h5>Your Favorite Payment Methond</h5>
+          </label>
+          <label>
+            <input
+              className='m-3'
+              type='radio'
+              name='metodo_pago'
+              value='VISA'
+              checked={paymentMethod === 'VISA'}
+              onChange={handlePaymentSelection}
+            />
+            VISA
+          </label>
+          <label>
+            <input
+              className='m-3'
+              type='radio'
+              name='metodo_pago'
+              value='MASTERCARD'
+              checked={paymentMethod === 'MASTERCARD'}
+              onChange={handlePaymentSelection}
+            />
+            MASTERCARD
+          </label>
+        </div>
+        <button
+          style={{ cursor: 'pointer' }}
+          type='submit'
+          className='btn btn-success  mx-auto m-3'
+        >
+          Create account
+        </button>
+      </form>
+      <div className=' mb-5 card w-50 mx-auto'>
+        <h5
+          className='text-center'
+          style={{ cursor: 'pointer' }}
+          onClick={handleLogin}
+        >
+          if you already have an account go to Login
+        </h5>
       </div>
-      <button  style={{ cursor: 'pointer' }} type="submit" className="btn btn-success  mx-auto m-3">
-        Create account
-      </button>
-    </form>
-    <div className=" mb-5 card w-50 mx-auto">
-    <h5 className="text-center" style={{ cursor: 'pointer' }}  onClick={handleLogin}>if you already have an account go to Login</h5>
     </div>
-    </div>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
