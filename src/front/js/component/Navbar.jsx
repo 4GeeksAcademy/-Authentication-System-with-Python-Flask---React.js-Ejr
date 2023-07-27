5168b22a2c7bb05b2395350f70a8a63b961f777d
Julio Vargas <117667674+JulioV10@users.noreply.github.com>
2023-07-27 10:55:47 +0000

    navbar&footer

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "../views/styles/navbar.css"
 // import { useNavigate } from "react-router-dom";
 
 // import { Context } from '../store/appContext.js';
@@ -7,12 +9,54 @@ import React from "react";
 
 const Navbar = () => {
 
-	
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light header">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Kwl4gT7z8mc8Ug0BaVPrzvedTvuLAZ8VFQ&usqp=CAU</img" alt="logo" />
+				</Link>
+				<div className="botonesNavbar">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0 botonLogin">
+							<li className="nav-item">
+								<Link className="nav-link" to="/login">LOGIN</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="/signup">REGISTER</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="/">SETTINGS</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link" to="/"> <i class="fa-solid fa-cart-shopping" /></Link>
+							</li>
+							<li className="nav-item dropdown">
+								<a className="nav-link dropdown-toggle" href="#" id="favoritesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+									<i class="fa-sharp fa-solid fa-heart"></i>
+								</a>
+								<ul className="dropdown-menu" aria-labelledby="favoritesDropdown">
+									<li><a className="dropdown-item" href="#">Action</a></li>
+									<li><a className="dropdown-item" href="#">Another action</a></li>
+									<li><hr className="dropdown-divider" /></li>
+									<li><a className="dropdown-item" href="#">Something else here</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<br />
+					<br />
 
-	
-	return(
-		<h1> Soy Navbar</h1>
-    );
-	}
+					<form className="d-flex">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success" type="submit">Search</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+	);
+}
 
 export default Navbar;
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,8 @@ import Prueba from "./views/PruebaComponentes.jsx";
 
 
 import injectContext from "./store/appContext";
+import Navbar from "./component/Navbar.jsx";
+import Footer from "./component/Footer.jsx";
 
 
 
@@ -25,6 +27,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
+                    <Navbar />
                     <Routes>
                         <Route path="/" element={<Home />} />
 						<Route path="/home" element={<Home />} />
@@ -33,6 +36,7 @@ const Layout = () => {
                         <Route path="/prueba" element={<Prueba />} />
                         <Route path="*" element={<h1>Not found!</h1>} />
                     </Routes>
+                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
