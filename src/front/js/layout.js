ac5cba85f51e4bde01334b2aae7cb00c645bfee1
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-07-28 03:52:51 +0000

    frontendLogin

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -29,6 +30,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Login />} path="/login" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
