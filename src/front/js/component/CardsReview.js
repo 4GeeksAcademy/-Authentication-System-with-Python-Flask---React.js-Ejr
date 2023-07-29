b5b18985f42b08aa9f78d14465a5bcfef0983ba5
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-07-29 23:15:36 +0000

    filtermenu testing

--- src/front/js/component/CardsReview.js
+++ src/front/js/component/CardsReview.js
@@ -1,16 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import FilterMenu from "./FilterMenu";
 
 const CardsReview = () => {
   const { store, actions } = useContext(Context);
   const [editContentId, setEditContentId] = useState(null);
   const [editContent, setEditContent] = useState("");
   const [formData, setFormData] = useState({ title: "", comment_text: "" });
-  const [filteredReviews, setFilteredReviews] = useState([]);
-  const [searchCountry, setSearchCountry] = useState("");
-  const [searchCity, setSearchCity] = useState("");
-  const [searchTripType, setSearchTripType] = useState("");
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -44,37 +39,13 @@ const CardsReview = () => {
     window.location.reload();
   };
 
-  const filterReviews = (reviews) => {
-    return reviews.filter((review) => {
-      const containsCountry =
-        !searchCountry || !review.country || review.country.includes(searchCountry);
-      const containsCity = !searchCity || !review.city || review.city.includes(searchCity);
-      const containsTripType =
-        !searchTripType || !review.tripType || review.tripType.includes(searchTripType);
-      return containsCountry && containsCity && containsTripType;
-    });
-  };
-
   useEffect(() => {
     actions.getReviews();
     console.log("test");
   }, []);
 
-  useEffect(() => {
-    setFilteredReviews(filterReviews(store.reviews));
-  }, [searchCountry, searchCity, searchTripType, store.reviews]);
-
   return (
     <div>
-      {/* Composant FilterMenu pour effectuer les recherches */}
-      <FilterMenu
-        handleSearch={(country, city, tripType) => {
-          setSearchCountry(country);
-          setSearchCity(city);
-          setSearchTripType(tripType);
-        }}
-      />
-
       {store.auth ? (
         <div className="form-review-content">
           <form onSubmit={handleSubmit}>
@@ -104,56 +75,41 @@ const CardsReview = () => {
 
       {/* Publicar las cartas que ya existen */}
       <div className="cards-review">
-        {filteredReviews.length > 0 ? (
-          filteredReviews
-            .sort((a, b) => b.id - a.id)
-            .map((review) => (
-              <div
-                key={review.id}
-                className="card card-review text-white mt-4 container"
-                style={{ height: "16rem", width: "20rem" }}
-              >
-                <div className="card-img-overlay">
-                  {store.user.id === review.user.id ? (
-                    <div className="btn-options d-flex justify-content-end">
-                      <button onClick={() => handleUpdate(review.id)}>
-                        &#9998;
-                      </button>
-                      <button onClick={() => handleDelete(review.id)}>
-                        &#10008;
-                      </button>
-                    </div>
-                  ) : null}
-                  <h5 className="card-title">{review.title}</h5>
-                  {editContentId === review.id ? (
-                    <>
-                      <textarea
-                        autoFocus={true}
-                        value={editContent}
-                        onChange={(e) => setEditContent(e.target.value)}
-                        className="card-text"
-                        rows="4"
-                        cols="30"
-                        maxLength="150"
-                        style={{ resize: "none" }}
-                      ></textarea>
-                      <button onClick={() => handleSave(review.id)}>
-                        Validar
-                      </button>
-                    </>
-                  ) : (
-                    <p className="card-text">{review.comment_text}</p>
-                  )}
-                  <span>Mensaje escrito por: {review.user.username}</span>
-                  {/* <p className="card-text">Likes: {review.likes}</p>
-                  <button onClick={() => handleLike(review.id)}>Like</button> */}
+      {store.reviews
+      .sort((a, b) => b.id - a.id)
+      .map((review) => (
+          <div key={review.id} className="card card-review text-white mt-4 container" style={{ height: "16rem", width: "20rem" }}>
+            <div className="card-img-overlay">
+              {store.user.id === review.user.id ? (
+                <div className="btn-options d-flex justify-content-end">
+                  <button onClick={() => handleUpdate(review.id)}>&#9998;</button>
+                  <button onClick={() => handleDelete(review.id)}>&#10008;</button>
                 </div>
-              </div>
-            ))
-        ) : (
-          // Si les avis sont filtrés, mais aucun résultat ne correspond, affiche un message
-          <p>Ninguna reseña coincide con los criterios de búsqueda.</p>
-        )}
+              ) : null}
+              <h5 className="card-title">{review.title}</h5>
+              {editContentId === review.id ? (
+                <>
+                  <textarea
+                    autoFocus={true}
+                    value={editContent}
+                    onChange={(e) => setEditContent(e.target.value)}
+                    className="card-text"
+                    rows="4"
+                    cols="30"
+                    maxLength="150"
+                    style={{ resize: "none" }}
+                  ></textarea>
+                  <button onClick={() => handleSave(review.id)}>Validar</button>
+                </>
+              ) : (
+                <p className="card-text">{review.comment_text}</p>
+              )}
+              <span>Mensaje escrito por: {review.user.username}</span>
+              {/* <p className="card-text">Likes: {review.likes}</p>
+              <button onClick={() => handleLike(review.id)}>Like</button> */}
+            </div>
+          </div>
+      ))}
       </div>
     </div>
   );
