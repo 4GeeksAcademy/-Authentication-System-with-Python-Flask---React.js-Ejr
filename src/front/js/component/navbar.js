8acb2d981d889b707515149e7701ae270275a03b
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-31 21:37:37 +0000

    Landing

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,17 +7,16 @@ export const MyNavbar = () => {
 	return (
 		<Navbar bg="light" expand="lg" className="my-navbar">
 			<Container>
-				<Navbar.Brand>
+				<Navbar.Brand >
 					<Link to="/">
 						<span className="navbar-brand mb-0 h1">Travel Website</span>
 					</Link>
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav>
-						<Nav.Link to="/">Action</Nav.Link>
-						<Nav.Link to="/">Another action</Nav.Link>
-						<Nav.Link to="/">Something else</Nav.Link>
+					<Nav >
+						<Nav.Link to="/">Login</Nav.Link>
+						<Nav.Link to="/">Register</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
