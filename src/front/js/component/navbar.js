3d69b1fd02830d43d9a5d166657843f253fa3033
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-31 20:54:45 +0000

    Login&Register

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,43 +1,24 @@
 import React from "react";
-import { Nav, Button, Form, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
 
 export const MyNavbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<Link to="/">
-				<div className="d-flex align-items-center"> 
-					<span className="text-primary" style={{marginLeft: '10px'}}>Group2 Travel Site</span>
-					<div className="divider-custom">
-						<hr className="bg-dark" />
-					</div>
-				</div>
-			</Link>
-
-			<Navbar bg="light" expand="lg">
-				<Navbar.Toggle aria-controls="navbarSupportedContent" />
-				<Navbar.Collapse id="navbarSupportedContent">
-					<Nav className="mr-auto">
-						<Nav.Link href="/destinations">Destinations</Nav.Link>
-						<Nav.Link href="/activities">Activities</Nav.Link>
-						<Nav.Link href="/deals">Deals</Nav.Link>
-						<Nav.Dropdown data-toggle="dropdown">
-							<Nav.Dropdown.Toggle id="navbarDropdown" title="Dropdown">
-								<span>Dropdown</span>
-							</Nav.Dropdown.Toggle>
-							<Nav.Dropdown.Menu>
-								<Nav.Dropdown.Item href="#">Action</Nav.Dropdown.Item>
-								<Nav.Dropdown.Item href="#">Another action</Nav.Dropdown.Item>
-								<Nav.Dropdown.Item href="#">Something else here</Nav.Dropdown.Item>
-							</Nav.Dropdown.Menu>
-						</Nav.Dropdown>
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand>
+					<Link to="/">
+						<span className="navbar-brand mb-0 h1">Travel Website</span>
+					</Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Link to="/"><Nav.Item href="#/action-1">Login</Nav.Item></Link>
+						<Link to="/"><Nav.Item href="#/action-1">Register</Nav.Item></Link>
 					</Nav>
-					<Form inline>
-						<Button variant="primary" href="/login">Login</Button>
-						<Button variant="primary" href="/register">Register</Button>
-					</Form>
 				</Navbar.Collapse>
-			</Navbar>
-		</nav>
+			</Container>
+		</Navbar>
 	);
 };
