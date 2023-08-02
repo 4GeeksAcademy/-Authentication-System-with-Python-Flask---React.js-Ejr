df254a8e5141e8570171f41da74a2bfad1febae3
William35er <125413709+William35er@users.noreply.github.com>
2023-08-02 00:55:21 +0000

    added error handling

--- src/front/js/pages/aboutcar.js
+++ src/front/js/pages/aboutcar.js
@@ -1,21 +1,22 @@
 import React, { useContext } from 'react'
 import { Context } from '../store/appContext'
 import "../../styles/cardata.css"
-
+import { useParams } from 'react-router-dom'
 const AboutCar = () => {
+  let{theid}= useParams()
     const {store} = useContext(Context)
-    const actualCar = store.singleCar
-
+    const actualCar = store.cars[parseInt(theid)-1]
+    console.log(actualCar)
   return (
     <div>
         <div className="container">
         <div className="row mb-4">
           <div className="col-5 carFormatted">
-          <h1>{actualCar.car_name}</h1>
-          <h1>{actualCar.car_type}</h1>
-          <h1>{actualCar.brand}</h1>
-          <h1>{actualCar.transmission}</h1>
-          {actualCar.images && actualCar.images.length > 0 ? (
+          <h1>{actualCar && actualCar.car_name}</h1>
+          <h1>{actualCar && actualCar.car_type}</h1>
+          <h1>{actualCar && actualCar.brand}</h1>
+          <h1>{actualCar && actualCar.transmission}</h1>
+          {actualCar && actualCar.images && actualCar.images.length > 0 ? (
               <img
                 className="border border-warning border-4 border-opacity-50 w-50"
                 src={actualCar.images[0].image_url}
