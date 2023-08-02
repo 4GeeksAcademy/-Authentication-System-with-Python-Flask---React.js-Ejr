5ea0d8090a1618f9d24b4670b7436e8c95df5f46
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-08-02 20:06:41 +0000

    DestinationSearch

--- src/front/js/pages/destinationsearch.js
+++ src/front/js/pages/destinationsearch.js
@@ -1,33 +1,70 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import "../../styles/DestinationSearch.css";
 
 const MySearch = () => {
-  const [search, setSearch] = useState([]);
-  const [result, setResult] = useState([]);
+  const [search, setSearch] = useState("");
+  const [destinationResult, setDestinationResult] = useState([]);
+  const [weatherResult, setWeatherResult] = useState([]);
+  const [flightResult, setFlightResult] = useState([]);
   const [input, setInput] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     setError(null);
     setLoading(true);
 
     if (search !== "") {
-      fetch("https://api.teleport.org/api/cities/?search=" + search)
+      fetch(`https://api.teleport.org/api/cities/?search=${search}`)
         .then(response => response.json())
         .then(data => {
-          setResult(data._embedded?.["city:search-results"]);
+          setDestinationResult(data._embedded?.["city:search-results"]);
           setLoading(false);
         })
         .catch(error => {
-          setError("Error fetching data. Please try again later.");
+          setError("Error fetching destination data. Please try again later.");
           setLoading(false);
         });
     } else {
-      setLoading(false); 
-      setResult([]); 
+      setLoading(false);
+      setDestinationResult([]);
     }
   }, [search]);
+
+  useEffect(() => {
+    if (destinationResult.length > 0) {
+      const city = destinationResult[0].matching_full_name;
+
+      fetch(`https://api.example.com/api/weather?city=${city}`)
+        .then(response => response.json())
+        .then(data => {
+          setWeatherResult(data);
+        })
+        .catch(error => {
+          console.error("Error fetching weather data:", error);
+        });
+    } else {
+      setWeatherResult([]);
+    }
+  }, [destinationResult]);
+
+  useEffect(() => {
+    if (destinationResult.length > 0) {
+      const city = destinationResult[0].matching_full_name;
+
+      fetch(`https://api.example.com/api/flights?city=${city}`)
+        .then(response => response.json())
+        .then(data => {
+          setFlightResult(data);
+        })
+        .catch(error => {
+          console.error("Error fetching flight data:", error);
+        });
+    } else {
+      setFlightResult([]);
+    }
+  }, [destinationResult]);
 
   const startSearch = (onKeyDownEvent) => {
     if (onKeyDownEvent.key === "Enter") {
@@ -36,12 +73,32 @@ const MySearch = () => {
     }
   };
 
+  const addToFavorites = (destination) => {
+    setFavorites([...favorites, destination]);
+  };
+
+  const removeFromFavorites = (destination) => {
+    setFavorites(favorites.filter(fav => fav !== destination));
+  };
+
+  const isFavorite = (destination) => {
+    return favorites.includes(destination);
+  };
+
   const clearResults = () => {
-    setResult([]);
+    setDestinationResult([]);
+    setWeatherResult([]);
+    setFlightResult([]);
   };
 
   return (
     <div className="my-search-container">
+      <div className="jumbotron">
+        <h1>Vacation Destination Search</h1>
+        <p>Your next vacation is just a search away!</p>
+      </div>
+
+      
       <input
         className="my-search-input"
         value={input}
@@ -50,25 +107,65 @@ const MySearch = () => {
         id="searchInput"
         name="fname"
         onKeyDown={startSearch}
-        placeholder="Search for your vacation destination..."
+        placeholder="Where to?"
       />
+
+      
       {loading ? <div className="loader">Loading...</div> : null}
       {error ? <div className="error-message">{error}</div> : null}
-      {result.length > 0 ? (
+
+      
+      {destinationResult.length > 0 ? (
         <div className="my-search-results">
-          {result.map(city => (
+          {destinationResult.map(city => (
             <div key={city._links["city:item"].href} className="my-search-result-item">
-              <img src="vacation-icon.png" alt="Vacation Destination Icon" className="vacation-icon" />
+              <img src="https://png.pngtree.com/element_our/png/20180807/pngtree-pinmark-vacation-icon-and-concept-png-image_54653.jpg" alt="Vacation Destination Icon" className="vacation-icon" />
+              style={{ width: "10px", height: "10px" }}
               <div className="city-info">
                 <h3>{city.matching_full_name}</h3>
-                
+                <p>Country: {city.matching_alternate_names[0].name}</p>
+                <button onClick={() => isFavorite(city.matching_full_name) ? removeFromFavorites(city.matching_full_name) : addToFavorites(city.matching_full_name)}>
+                  {isFavorite(city.matching_full_name) ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}
+                </button>
               </div>
             </div>
           ))}
         </div>
       ) : (
-        <div className="no-results">No results found.</div>
+        <div className="no-results"></div>
       )}
+
+      
+      {weatherResult.length > 0 ? (
+        <div className="my-weather-results">
+          <h2>Weather Information</h2>
+          {weatherResult.map((weather, index) => (
+            <div key={index} className="weather-item">
+              <p>Date: {weather.date}</p>
+              <p>Temperature: {weather.temperature}</p>
+              <p>Weather Condition: {weather.condition}</p>
+              
+            </div>
+          ))}
+        </div>
+      ) : null}
+
+      
+      {flightResult.length > 0 ? (
+        <div className="my-flight-results">
+          <h2>Flight Prices</h2>
+          {flightResult.map((flight, index) => (
+            <div key={index} className="flight-item">
+              <p>Flight Provider: {flight.provider}</p>
+              <p>Price: {flight.price}</p>
+              <p>Departure Date: {flight.departureDate}</p>
+              
+            </div>
+          ))}
+        </div>
+      ) : null}
+
+      
       <button className="clear-results-button" onClick={clearResults}>Clear Results</button>
     </div>
   );
