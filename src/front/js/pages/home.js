44c3b7ddd90c88c9468f6c0aef1e5edc4e04baff
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-08-03 01:01:51 +0000

    some css update unfinished

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,29 +7,13 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="mt-5 row-row-2">
+		<div className="main">
 		  <div className="input-group align-content-start">
-		  <div className="register-body">
-            <span className="input-group-text">E-Mail Address</span>
-			    <input type="text" aria-label="E-Mail Address" className="form-control"></input>
-			<span className="input-group-text">Password</span>
-			    <input type="text" aria-label="E-Mail Address" className="form-control"></input>
- 			<span className="input-group-text">Recovery Question</span>
-  				<input type="text" aria-label="Last name" className="form-control"></input>
-				  <span className="input-group-text">Recovery Answer</span>
-			    <input type="text" aria-label="E-Mail Address" className="form-control"></input>
+		  <h1 className="Destinations">Destinations</h1>
 		</div>
 		
-		<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		
 		</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-		</div>
+		
 	);
 };
