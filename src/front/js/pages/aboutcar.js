23ec91586174db4fe506716d1f36d5bb20ae592d
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2023-08-09 22:10:25 +0000

    finished reviews

--- src/front/js/pages/aboutcar.js
+++ src/front/js/pages/aboutcar.js
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../store/appContext'
 import "../../styles/cardata.css"
-import "slick-carousel/slick/slick.css";
-import 'slick-carousel/slick/slick-theme.css'
 
 
 const AboutCar = () => {
