fb5acd3279ff2b90f2887a61824e1f97fd2a4b1c
jonio298 <122931363+jonio298@users.noreply.github.com>
2023-08-09 22:34:29 +0000

    892023

--- src/front/js/pages/aboutcar.js
+++ src/front/js/pages/aboutcar.js
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react'
 import { Context } from '../store/appContext'
 import "../../styles/cardata.css"
-import "slick-carousel/slick/slick.css";
-import 'slick-carousel/slick/slick-theme.css'
+
 
 
 const AboutCar = () => {
