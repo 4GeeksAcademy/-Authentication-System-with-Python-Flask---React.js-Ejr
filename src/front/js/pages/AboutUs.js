3e8f4836ae880924c712d5a5b9547580d3db3400
Valentin <valentin.rob30@outlook.com>
2023-08-18 20:27:00 -0300

    FINISHED

--- src/front/js/pages/AboutUs.js
+++ src/front/js/pages/AboutUs.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className='cards-about d-flex justify-content-evenly mt-5'>
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="https://res.cloudinary.com/albertge/image/upload/v1692394312/Imagen_de_WhatsApp_2023-08-18_a_las_16.29.50_gcm0vw.jpg" className="card-img-top" alt="foto de Valentin" />
-        <div className="card-body">
-          <h5 className="card-title">Valentin Robert</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className='about-content'>
+      <div class="card-about">
+        <img src="https://res.cloudinary.com/albertge/image/upload/v1692398693/Imagen_de_WhatsApp_2023-08-18_a_las_17.43.41_l1grwh.jpg" alt="Valentin" />
+        <h2>Valentin</h2>
+        <p>¡Hola a todos! Soy Valentin, un apasionado desarrollador de software y un amante de los viajes.</p>
+        <p>Junto con mi compañero Albert, hemos combinado nuestras habilidades para crear una emocionante plataforma en línea para todos los entusiastas de los viajes.</p>
+        <p>¿Estás buscando nuevas aventuras y experiencias emocionantes? ¡Has llegado al lugar adecuado! Nuestra página web es el fruto de nuestra pasión por la tecnología y nuestro amor por explorar el mundo.</p>
+        <p>Con una interfaz intuitiva y atractiva, nuestra página web te llevará a un mundo de posibilidades. Desde rutas de senderismo en parajes exóticos hasta consejos para viajar con bajo presupuesto, hemos reunido todo lo que necesitas para planificar tu próxima aventura.</p>
+        <p>¿Te emociona la idea de descubrir culturas diferentes y vivir experiencias únicas? ¡Nosotros también! Únete a nosotros en esta emocionante travesía mientras exploramos juntos lo que el mundo tiene para ofrecer. ¡Bienvenidos a nuestro rincón digital de descubrimientos y sueños!</p>
+        <div className='about-icons d-flex justify-content-end'>
           <a href="https://www.linkedin.com/in/valentin-m-robert/"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012272" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
@@ -22,11 +26,17 @@ const AboutUs = () => {
           </svg></a>
         </div>
       </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="https://res.cloudinary.com/albertge/image/upload/v1692394732/Imagen_de_WhatsApp_2023-06-19_a_las_11.08.54-removebg-preview_2_sv2y2t.png" className="card-img-top" alt="foto de Albert" />
-        <div className="card-body">
-          <h5 className="card-title">Albert González</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+      <div class="card-about">
+        <img src="https://res.cloudinary.com/albertge/image/upload/v1692394732/Imagen_de_WhatsApp_2023-06-19_a_las_11.08.54-removebg-preview_2_sv2y2t.png" alt="Albert" />
+        <h2>Albert</h2>
+        <p>Saludos a todos, soy Albert, un entusiasta del desarrollo de software con una pasión desbordante por los viajes.</p>
+        <p>Junto con mi compañero Valentin, hemos puesto en marcha una emocionante plataforma en línea que fusiona nuestra experiencia en tecnología con nuestro amor por explorar nuevos horizontes.</p>
+        <p>Siempre he creído que los viajes son la mejor manera de crecer como individuo y de crear recuerdos inolvidables.</p>
+        <p>Nuestra página web es el resultado de muchas horas de trabajo y dedicación, con el objetivo de ofrecerte la mejor experiencia posible al planificar tus próximas aventuras.</p>
+        <p>Desde recomendaciones de destinos exóticos hasta consejos para maximizar tu presupuesto de viaje, hemos creado un espacio donde cada viajero puede encontrar algo que le inspire.</p>
+        <p>Únete a nosotros mientras nos adentramos en un mundo lleno de posibilidades y descubrimientos. ¡Esperamos que encuentres nuestra página web tan emocionante y útil como nosotros encontramos cada nueva travesía! ¡Bienvenidos a un viaje de aventuras digitales!</p>
+        <div className='about-icons justify-content-end d-flex'>
           <a href="https://www.linkedin.com/in/albertgon/"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012272" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
@@ -44,5 +54,4 @@ const AboutUs = () => {
     </div>
   );
 };
-
 export default AboutUs;
