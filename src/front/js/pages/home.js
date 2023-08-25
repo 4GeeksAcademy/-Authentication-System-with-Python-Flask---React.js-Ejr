2bdfad71691c41a7ef976f51ed86efad4cef85e8
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-25 19:10:04 +0000

    added list

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [todo, setTodo] = useState('')
+	const [list, setList] = useState([])
+
+
+	const createTodo = () => {
+		e.preventDefault()
+		let newTodo = setTodo('e.target.todoInput.value')
+		setList(...todo, newTodo)
+		}
+
+
+	const removeTodo = (index) => {
+		e.preventDefault()
+		let filtered = todo.filter((i) => i !== index)
+		setList(filtered)
+		}
+
+
+	fetch('https://www.boredapi.com/api/activity')
+
+
+
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<h1>Bored? Need Something todo?</h1>
+			<div className="container bg-light">
+				
+			<form onSubmit={createTodo}>
+            <input 
+            className="input"
+            name="todoInput" 
+            type="text"
+            placeholder="add new task" 
+            >
+            </input>
+            </form>
+
+			
+			<ul>
+                {list.map((list,index)=> {
+
+                    return(
+                        <li className="theList d-flex justify-content-between"
+                            key={index}>
+                            {list}
+                            <button className='button mr-1 float-right end-0' type="button" onClick={(e) => removeTodo(e,index)}>
+                            	<i className="fas fa-times"></i>
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
