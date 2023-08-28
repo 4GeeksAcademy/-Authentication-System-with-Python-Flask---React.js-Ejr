023d6a2ae59b1a270d2111c917d5a083c63d6f57
VeronicaCastroMurillo <119750643+VeronicaCastroMurillo@users.noreply.github.com>
2023-08-28 03:44:28 +0000

    [adding a new signUp endpoint]

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			hiddenLogout: null,
+			hiddenLogin: false,
+			message: null,
+			token_user: null,
+			signup: false,
+			isloged: false,
+			passwordIsCorrect: false,
+			go: true,
+			user_loged: {},
+			user_data: {
+				username: null,
+				first_name: null,
+				last_name: null,
+				email: null,
+				password: null
+			},
+
+			login_user: {
+				email: null,
+				password: null
+			},
 			message: null,
 			demo: [
 				{
@@ -46,7 +67,57 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+
+			isPropertyEmpty: (obj) => {
+				for (const key in obj) {
+					if (obj[key] === "" || obj[key] == null || obj[key] === undefined) {
+						return true;
+					}
+				}
+				return false;
+			},
+
+			signUpUser: async () => {
+				const store = getStore()
+				const actions = getActions()
+				try {
+
+					if (actions.isPropertyEmpty(store.user_data)) {
+						alert("Le falta llenar algunos datos");
+						return;
+					}
+
+					const response = await fetch(process.env.BACKEND_URL + "/signup", {
+						method: 'POST',
+						body: JSON.stringify(store.user_data),
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+
+					const result = await response.json()
+					if (response.status == 400) {
+						setStore({ signup: false })
+						alert(result.message)
+
+					}
+
+
+					if (response.ok) {
+						setStore({ signup: true })
+						alert("User add success")
+						setStore({ hiddenLogin: true })
+					}
+
+
+				} catch (error) {
+					console.error(error + " Error loading message from backend");
+					setStore({ signup: false })
+				}
+			},
+
+
 		}
 	};
 };
