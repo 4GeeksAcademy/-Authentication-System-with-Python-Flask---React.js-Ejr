34c4c4784e8611e98f0b6ffb522476e68ec9b397
Diana Chacón <diianachm1802@gmail.com>
2023-08-29 01:37:35 +0000

    Added signup endpoint and user model 	modified:   Pipfile - added bcrypt and flask-jwt-extended 	modified:   Pipfile.lock - added bcrypt and flask-jwt-extended 	modified:   src/api/models.py - user model 	modified:   src/app.py - signup endpoint

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,29 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
+            "password": self.password
             # do not serialize the password, its a security breach
         }
