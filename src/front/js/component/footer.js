e94dc4f12148eb286584c40ccd58da24e98b39d1
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-08-30 06:32:45 +0000

    Footer Completed

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <nav
+    className="navbar fixed-bottom navbar"
+    style={{ backgroundColor: "#161C28" }}
+  >
+    <div className="col-7 ms-5 my-4">
+      <h1 style={{ color: "#FD862C" }}>Bexplora</h1>
+      <h6 style={{ color: "#A6A6A6", marginBottom: "2rem" }}>
+        Mejorando tu futuro
+      </h6>
+      <h6 style={{ color: "#FFFFFF" }}>
+        © 2023 Bexplora. Nos reservamos los derechos de autor.
+      </h6>
+    </div>
+    <div className="col-3 d-flex justify-content-end me-5">
+      <img
+        src="https://i.imgur.com/J6XQNp5.png"
+        alt="Logo Bexplora"
+        style={{ width: "100px", height: "auto" }}
+      />
+    </div>
+  </nav>
 );
