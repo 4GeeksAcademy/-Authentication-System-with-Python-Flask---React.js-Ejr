49754cc7e70206cf88670918cf16f76e2833849b
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-08-30 07:11:20 +0000

    Margin and color corrections

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,16 +5,19 @@ export const Footer = () => (
     className="navbar fixed-bottom navbar"
     style={{ backgroundColor: "#161C28" }}
   >
-    <div className="col-7 ms-5 my-4">
+    <div className="col-7  mt-4 mb-4" style={{ marginLeft: "5rem" }}>
       <h1 style={{ color: "#FD862C" }}>Bexplora</h1>
-      <h6 style={{ color: "#A6A6A6", marginBottom: "2rem" }}>
+      <h6 style={{ color: "#E8D2D2", marginBottom: "2rem" }}>
         Mejorando tu futuro
       </h6>
-      <h6 style={{ color: "#FFFFFF" }}>
+      <h6 style={{ color: "#A6A6A6" }}>
         © 2023 Bexplora. Nos reservamos los derechos de autor.
       </h6>
     </div>
-    <div className="col-3 d-flex justify-content-end me-5">
+    <div
+      className="col-3 d-flex justify-content-end"
+      style={{ marginRight: "5rem" }}
+    >
       <img
         src="https://i.imgur.com/J6XQNp5.png"
         alt="Logo Bexplora"
