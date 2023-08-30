1b585091cd0851084bf8afc1443bb13ddd3531d9
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-08-30 06:39:28 +0000

    Add file RegistroUsuario.js

---
+++ src/front/js/pages/RegistroUsuario.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RegistroUsuario = () => {
+  return (
+    <button type="button" className="btn btn-primary">
+      Primary
+    </button>
+  );
+};
+
+export default RegistroUsuario;
