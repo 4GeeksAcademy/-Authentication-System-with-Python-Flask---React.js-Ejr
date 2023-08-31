0f557f398225074f22b27ff964802646fa2acabb
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-08-31 19:34:22 +0000

    Reaparado bug de background en body a landing

--- src/front/js/pages/landing.js
+++ src/front/js/pages/landing.js
@@ -1,31 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../../styles/landing.css";
 
 import { Context } from "../store/appContext";
 
 export const Landing = () => {
   const { store, actions } = useContext(Context);
+  const [background, setBackground] = useState("background0");
+  // const [text, setText] = useState("text0"); hacer lo mismo con el texto y poner un efecto al cambiar de imagen
 
   return (
-    <>
-      <div id="landing">
-        <div className="landing-content">
-          <div className="text-container">
-            <h1 className="landing-title">
-              Expert dog <br /> walking service
-            </h1>
-            <h4 className="landing-description">
-              Exploring with a dog <br />
-              Discovering beauty in every step.
-            </h4>
-          </div>
-          <div className="buttons-container">
-            <button className="btn-carrusel"></button>
-            <button className="btn-carrusel"></button>
-            <button className="btn-carrusel"></button>
-          </div>
+    <div className={background} id="landing">
+      <div className="landing-content">
+        <div className="text-container">
+          <h1 className="landing-title">
+            Expert dog <br /> walking service
+          </h1>
+          <h4 className="landing-description">
+            {" "}
+            lo mismo para el settexto Exploring with a dog <br />
+            Discovering beauty in every step.
+          </h4>
+        </div>
+        <div className="buttons-container">
+          <button
+            className="btn-carrusel"
+            onClick={() => setBackground("background1")}></button>
+          <button
+            className="btn-carrusel"
+            onClick={() => setBackground("background2")}></button>
+          <button
+            className="btn-carrusel"
+            onClick={() => setBackground("background3")}></button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
