551def4b33284daa05a63f289cb503c71a5963c9
Amanda Esquivel <128640315+xmanda@users.noreply.github.com>
2023-08-31 05:43:34 +0000

    becas block progress

--- src/front/js/component/CardBeca.js
+++ src/front/js/component/CardBeca.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const CardBeca = () => {
   return (
-    <div>CardBeca</div>
+    <div>
+<div className="card" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+    </div>
   )
 }
 
