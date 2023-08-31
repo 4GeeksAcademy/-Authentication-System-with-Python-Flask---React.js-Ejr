69b67e72894fdcb487813a3d478627b7572a7577
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-08-31 03:03:43 +0000

    first commit

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,12 +1,8 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import ContactForm from "./component/ContactForm.jsx";
 
-//include your index.scss file into the bundle
-import "../styles/index.css";
-
-//import your own components
-import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+ReactDOM.render(<ContactForm/>, document.querySelector("#app"));
