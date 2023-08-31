6c52f8582a4b1567636885f73cc2492a191965a1
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-08-31 01:19:21 +0000

    Register Progress

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,40 +3,42 @@ import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar">
-			<div className="container my-4">
-				<div className="d-flex align-items-center">
-					<Link to="/">
-						<img className="logo" src="https://i.imgur.com/J6XQNp5.png" />
-					</Link>
-					<Link to="/">
-						<h1 className="title">Bexplora</h1>
-					</Link>
-				</div>
-				<div className="d-inline-flex ml-auto align-items-center">
-					<Link to="/nosotros">
-						<button className="button-regular mx-1">Nosotros</button>
-					</Link>
-					<Link to="/faq">
-						<button className="button-regular mx-1">Preguntas Frecuentes</button>
-					</Link>
-					<Link to="/aplicaciones">
-						<button className="button-regular mx-1">Mis Aplicaciones</button>
-					</Link>
-					<Link to="/perfil">
-						<button className="button-regular mx-1">Mi Perfil</button>
-					</Link>
-					<div className="buttons-right mx-2">
-						<Link to="/signup">
-							<button className="button-login">Iniciar sesión</button>
-						</Link>
-						<Link to="/demo">
-							<button className="button-signup">Registrarse</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="container my-4">
+        <div className="d-flex align-items-center">
+          <Link to="/">
+            <img className="logo" src="https://i.imgur.com/J6XQNp5.png" />
+          </Link>
+          <Link to="/">
+            <h1 className="title">Bexplora</h1>
+          </Link>
+        </div>
+        <div className="d-inline-flex ml-auto align-items-center">
+          <Link to="/nosotros">
+            <button className="button-regular mx-1">Nosotros</button>
+          </Link>
+          <Link to="/faq">
+            <button className="button-regular mx-1">
+              Preguntas Frecuentes
+            </button>
+          </Link>
+          <Link to="/aplicaciones">
+            <button className="button-regular mx-1">Mis Aplicaciones</button>
+          </Link>
+          <Link to="/perfil">
+            <button className="button-regular mx-1">Mi Perfil</button>
+          </Link>
+          <div className="buttons-right mx-2">
+            <Link to="/signup">
+              <button className="button-login">Iniciar sesión</button>
+            </Link>
+            <Link to="/registroEleccion">
+              <button className="button-signup">Registrarse</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,8 +5,7 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import RegistroUsuario from "./pages/RegistroUsuario";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import RegistroEleccion from "./pages/RegistroEleccion";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -28,10 +27,9 @@ const Layout = () => {
           <Navbar />
           <Routes>
             <Route element={<Home />} path="/" />
-            <Route element={<Demo />} path="/demo" />
-            <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
             <Route element={<RegistroUsuario />} path="/registroUsuario" />
+            <Route element={<RegistroEleccion />} path="/registroEleccion" />
           </Routes>
           <Footer />
         </ScrollToTop>
