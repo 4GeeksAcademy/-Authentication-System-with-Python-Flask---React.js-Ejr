931ecd58dcb2d68dd562ec9c3ea39b542f68c987
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-08-31 08:48:03 +0000

    ajustes del texto del landing

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,9 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// export const Footer = () => (
-//   <footer className="footer mt-auto py-3 text-center">
-//     <i className="fa-solid fa-dog"></i>
-//   </footer>
-// );
+export const Footer = () => (
+  <footer className="footer mt-auto py-3 text-center">
+    <i className="fa-brands fa-instagram"></i>
+    <i className="fa-brands fa-facebook"></i>
+    <i className="fa-brands fa-twitter"></i>
+  </footer>
+);
--- src/front/js/pages/landing.js
+++ src/front/js/pages/landing.js
@@ -3,6 +3,7 @@ import { Navbar } from "../component/navbar";
 import "../../styles/landing.css";
 
 import { Context } from "../store/appContext";
+import { Footer } from "../component/footer";
 
 export const Landing = () => {
   const { store, actions } = useContext(Context);
@@ -10,17 +11,25 @@ export const Landing = () => {
   return (
     <>
       <Navbar />
-      <div className="container">
-        <div id="landing-wrap">
-          <h1>
-            Expert dog <br /> walking service
-          </h1>
-          <h4>
-            Exploring with a dog: <br />
-            Discovering beauty in every step.
-          </h4>
+      <div id="landing">
+        <div className="landing-content">
+          <div className="text-container">
+            <h1 className="landing-title">
+              Expert dog <br /> walking service
+            </h1>
+            <h4 className="landin-description">
+              Exploring with a dog <br />
+              Discovering beauty in every step.
+            </h4>
+          </div>
+          <div className="buttons-container">
+            <button className="btn-carrusel"></button>
+            <button className="btn-carrusel"></button>
+            <button className="btn-carrusel"></button>
+          </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
