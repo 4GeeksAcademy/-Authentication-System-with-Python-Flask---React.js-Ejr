7b09134fa27867d14975ffe77788a702436e1c0b
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-09-06 07:17:05 +0000

    Agregando actions al landing

--- src/front/js/pages/landing.js
+++ src/front/js/pages/landing.js
@@ -3,33 +3,55 @@ import "../../styles/landing.css";
 
 import { Context } from "../store/appContext";
 
+let titles = [
+  "Hazlo por el y por ti",
+  "Lavamos su pelo por ti",
+  "Tu amigo incondicional",
+  "Love and pets",
+  "El amor es eterno",
+];
+let descriptions = [
+  "Discovering beauty in every step",
+  "Grooming keeps pets looking great",
+  "Regular exercise ensures happy pets",
+  "Love and play daily together",
+];
+
 export const Landing = () => {
   const { store, actions } = useContext(Context);
-  const [background, setBackground] = useState("background0");
-  // const [text, setText] = useState("text0"); hacer lo mismo con el texto y poner un efecto al cambiar de imagen
+  const [background, setBackground] = useState("background");
+  const [title, setTitle] = useState("title");
+  const [description, setDescription] = useState("description");
+
+  const handleButtonClick = (index) => {
+    setBackground(`background${index}`);
+    setTitle(`title${index}`);
+    setDescription(`description${index}`);
+  };
 
   return (
     <div className={background} id="landing">
       <div className="landing-content">
         <div className="text-container">
-          <h1 className="landing-title">
-            Expert dog <br /> walking service
+          <h1 className={title} id="landing-title">
+            Siempre sera un cuidado mutuo, somos un todo
+            {titles[parseInt(title.charAt(title.length - 1))]}
           </h1>
-          <h4 className="landing-description">
-            Exploring with a dog <br />
-            Discovering beauty in every step.
+          <h4 className={description} id="landing-description">
+            Love pray and eat
+            {descriptions[parseInt(description.charAt(description.length - 1))]}
           </h4>
         </div>
         <div className="buttons-container">
           <button
             className="btn-carrusel"
-            onClick={() => setBackground("background1")}></button>
+            onClick={() => handleButtonClick(1)}></button>
           <button
             className="btn-carrusel"
-            onClick={() => setBackground("background2")}></button>
+            onClick={() => handleButtonClick(2)}></button>
           <button
             className="btn-carrusel"
-            onClick={() => setBackground("background3")}></button>
+            onClick={() => handleButtonClick(3)}></button>
         </div>
       </div>
     </div>
