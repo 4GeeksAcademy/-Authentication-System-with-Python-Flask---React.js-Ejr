e71a170efc837f3aefb32974c1ac5f4dea3bc3e9
VeronicaCastroMurillo <119750643+VeronicaCastroMurillo@users.noreply.github.com>
2023-09-07 00:45:38 +0000

    [adding signup function to the store]

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,51 +1,59 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			signup: false,
+			user_data: {
+				name: null,
+				last_name: null,
+				email: null,
+				password: null
+			}
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			isPropertyEmpty: (obj) => {
+				for (const key in obj) {
+					if (obj[key] === "" || obj[key] == null || obj[key] === undefined) {
+						return true;
+					}
 				}
+				return false;
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			signUpUser: async () => {
+				const store = getStore()
+				const actions = getActions()
+				try {
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+					if (actions.isPropertyEmpty(store.user_data)) {
+						alert("Le falta llenar algunos datos");
+						return;
+					}
 
-				//reset the global store
-				setStore({ demo: demo });
+					const response = await fetch(process.env.BACKEND_URL + "/signup", {
+						method: 'POST',
+						body: JSON.stringify(store.user_data),
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+
+					const result = await response.json()
+					if (response.status == 400) {
+						setStore({ signup: false })
+						alert(result.message)
+
+					}
+
+
+					if (response.ok) {
+						setStore({ signup: true })
+						alert("User add success")
+					}
+
+
+				} catch (error) {
+					console.error(error + " Error loading message from backend");
+					setStore({ signup: false })
+				}
 			}
 		}
 	};
