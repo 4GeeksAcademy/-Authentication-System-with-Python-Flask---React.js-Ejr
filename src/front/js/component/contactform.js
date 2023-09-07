aece0c5cfb2d29f313a46b9c67d13df35912f286
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-09-07 02:28:49 +0000

    formularios login, register y contact actualizados para usar bootstrap

--- src/front/js/component/contactform.js
+++ src/front/js/component/contactform.js
@@ -1,28 +1,104 @@
-import React from "react";
-import '../../styles/contacto.css'
+import React, { useState } from "react";
+import axios from "axios"; // Importa Axios para realizar solicitudes HTTP
+import '../../styles/contacto.css';
 
-function ContactForm(){ 
-    return(
-        <div className="contact-form-container">
-    <form className="contact-form">
-      <div className="form-field">
-        <input type="text" id="name" name="name" placeholder="Nombre" required />
-        <input type="tel" id="phone" name="phone" placeholder="Telefono" required />
-      </div>
-      <div className="form-field">
-        
-      </div>
-      <div className="form-field">
-        <input type="email" id="email" name="email" placeholder="Email" required />
-      </div>
-      <div className="form-field">
-        <textarea id="message" name="message" rows="4" placeholder="Mensaje" required></textarea>
-      </div>
-      <button className="btn btn-primary btn-lg m-2" id="submit-button" type="submit">Contactanos</button>
-    </form>
-  </div>
-    )
-    
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    sendCopy: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("URL_DEL_BACKEND", formData);
+
+      if (response.status === 200) {
+        // El envío fue exitoso, puedes mostrar un mensaje de éxito al usuario
+        console.log("Mensaje enviado con éxito");
+      } else {
+        // Puedes manejar errores aquí
+        console.error("Error al enviar el mensaje");
+      }
+    } catch (error) {
+      // Puedes manejar errores aquí
+      console.error("Error al enviar el mensaje", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-control white-background-input"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="email">Dirección de correo eléctronico</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control white-background-input"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="message">Mensaje</label>
+          <textarea
+            className="form-control white-background-input"
+            id="message"
+            name="message"
+            rows="4"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+
+        <div className="form-check d-flex justify-content-center mb-4">
+          <input
+            className="form-check-input me-2"
+            type="checkbox"
+            id="sendCopy"
+            name="sendCopy"
+            checked={formData.sendCopy}
+            onChange={handleChange}
+          />
+          <label className="form-check-label" htmlFor="sendCopy">
+            Envíame una copia de este mensaje
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block mb-4">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
