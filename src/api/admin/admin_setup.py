c5c06b260c42d868a1734920481c4f8e037c1f20
devAbreu <100288811+devAbreu@users.noreply.github.com>
2023-09-07 08:17:09 +0000

    Setup Flask-Admin for application administration
    - Set the Flask secret key for security.
    - Configure the Flask-Admin interface theme to 'cerulean'.
    - Create a Flask-Admin instance named '4Geeks Admin' with the
    'bootstrap3' template mode.
    - Add the User model to Flask-Admin for easy administration.
    
    [Filename: admin_setup.py]

---
+++ src/api/admin/admin_setup.py
@@ -0,0 +1,21 @@
+import os
+from flask_admin import Admin
+from api.models import User
+from api.extensions import db
+from flask_admin.contrib.sqla import ModelView
+
+def setup_admin(app):
+    """
+    Setup and configure Flask-Admin for the application.
+
+    :param app: The Flask app instance.
+    """
+    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')  # Set the secret key for the app
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'  # Set the Flask-Admin interface theme
+    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')  # Create a Flask-Admin instance
+
+    # Add your models here, for example, this is how we add the User model to the admin
+    admin.add_view(ModelView(User, db.session))
+
+    # You can duplicate that line to add new models
+    # admin.add_view(ModelView(YourModelName, db.session))
