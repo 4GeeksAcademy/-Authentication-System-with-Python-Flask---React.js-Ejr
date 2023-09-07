e438b2fc604a41d9edff7161deb75436a4c84286
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-09-07 20:14:11 +0000

    Metodo Post success 201

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Owner
 from api.utils import generate_sitemap, APIException
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token
@@ -22,32 +22,55 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/signup', methods=['POST'])
-def createUser():
-    data = request.get_json(force=True)
-    email = data["email"].lower()
-    if User.query.filter_by(email=email).first() is not None:
-        return jsonify({"msg":"Email already registered"}), 400
-    new_user=User()
-    new_user.email = email
-    secure_password = bcrypt.generate_password_hash((data["password"]), 10).decode("utf-8")
-    new_user.password = secure_password
-    new_user.dni = data["dni"]
-    new_user.name = data["name"]
-    new_user.lastname = data["lastname"]
-    new_user.is_active = True
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"msg": "User created successfully"}), 201
+# @api.route('/signup', methods=['POST'])
+# def createUser():
+#     data = request.get_json(force=True)
+#     email = data["email"].lower()
+#     if User.query.filter_by(email=email).first() is not None:
+#         return jsonify({"msg":"Email already registered"}), 400
+#     new_user=User()
+#     new_user.email = email
+#     secure_password = bcrypt.generate_password_hash((data["password"]), 10).decode("utf-8")
+#     new_user.password = secure_password
+#     new_user.dni = data["dni"]
+#     new_user.name = data["name"]
+#     new_user.lastname = data["lastname"]
+#     new_user.is_active = True
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return jsonify({"msg": "User created successfully"}), 201
 
-@api.route('/login', methods=["POST"])
-def login_user():
+# @api.route('/login', methods=["POST"])
+# def login_user():
+#     data = request.get_json(force=True)
+#     user = User.query.filter_by(email=data["email"]).first()
+#     if user is None:
+#         return jsonify({"msg": "Incorrect user or password"}), 401
+#     passwordCheck = bcrypt.check_password_hash(user.password, data["password"])
+#     if passwordCheck == False:
+#         return jsonify({"msg":"Wrong password"}), 401
+#     token = create_access_token(identity = data["dni"], additional_claims={"role":"admin"}) #cambiar por tipo de usuario de los modelos pendientes (Enum)
+#     return jsonify({"msg": "Login successful!", "token":token}),200
+
+
+@api.route('/owner', methods=["POST"])
+def create_owner():
     data = request.get_json(force=True)
-    user = User.query.filter_by(email=data["email"]).first()
-    if user is None:
-        return jsonify({"msg": "Incorrect user or password"}), 401
-    passwordCheck = bcrypt.check_password_hash(user.password, data["password"])
-    if passwordCheck == False:
-        return jsonify({"msg":"Wrong password"}), 401
-    token = create_access_token(identity = data["dni"], additional_claims={"role":"admin"}) #cambiar por tipo de usuario de los modelos pendientes (Enum)
-    return jsonify({"msg": "Login successful!", "token":token}),200
+    new_owner= Owner()
+    new_owner.first_name = data["first_name"]
+    new_owner.last_name =  data["last_name"]
+    new_owner.email = data["email"]
+    new_owner.password = data["password"]
+    new_owner.is_active = True
+    db.session.add(new_owner)
+    db.session.commit()
+    return jsonify({"msg": "Owner created successfully"}), 201
+
+
+@api.route('/owner', methods=["GET"])
+def get_owners():
+    owner = Owner.query.all()
+    owners_list = list(map(lambda x: x.to_dict(), owner))
+    return jsonify(owners_list)
+
+    
