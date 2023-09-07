198804dad4c4fa38f066a21e12535b3e9ce88499
devAbreu <100288811+devAbreu@users.noreply.github.com>
2023-09-07 08:15:56 +0000

    Run the application using Gunicorn
    - Import the `create_app` function from the `app` module instead of the
    app instance.
    - Use the `create_app` function to create an instance of the application.
    - Start the application if this script is executed as the main program.
    
    [Filename: wsgi.py]

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,9 @@
-# This file was created to run the application on heroku using gunicorn.
+# This file was created to run the application on Heroku using Gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
 
-from app import app as application
+from app import create_app  # Import the create_app function instead of the app instance
+
+application = create_app()  # Use the function to create an instance of the application
 
 if __name__ == "__main__":
-    application.run()
+    application.run()  # Start the application if this script is executed as the main program
