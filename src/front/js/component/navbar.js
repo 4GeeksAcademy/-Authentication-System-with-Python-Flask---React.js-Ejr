0a69af14b2b3f18cc7c63ec2fd1415daf03dc283
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2023-09-08 03:00:33 +0000

    info para primer PR

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -16,35 +16,7 @@ export const Navbar = () => {
 					</div>
 				</div>
 			</nav>
-			<nav className="navbar navbar-light bg-light">
-				<div className="container">
-					<Link to="/">
-						<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-					</Link>
-					<div className="ml-auto">
-						<Link to="/demo">
-							<button className="btn btn-primary">Check the Context in action</button>
-						</Link>
-					</div>
-				</div>
-			</nav>
-			<nav class="navbar navbar-expand-lg navbar-dark fw-bold text-white bg-dark">
-				<div class="container-fluid">
-					<div class="collapse navbar-collapse justify-content-center " id="navbarSupportedContent">
-						<ul class="navbar-nav">
-							<li class="nav-item p-1 border border-light rounded-start bg-success">
-								<Link to="/bookSales" className="nav-link active " aria-current="page">Book sales</Link>
-							</li>
-							<li class="nav-item p-1 border border-light bg-success ">
-								<Link to="/bookExchange" className="nav-link active">Book exchange</Link>
-							</li>
-							<li class="nav-item p-1 border border-light rounded-end bg-success">
-								<Link to="/bestSeller" className="nav-link active">Best seller</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+			
 		</>
 	);
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,9 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import { BookSale } from "./pages/bookSale";
+import { BestSellers } from "./pages/bestSellers";
+import { BookExchange } from "./pages/bookExchange";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -17,7 +20,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -27,6 +30,9 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<BookSale />} path="/bookSale" />
+                        <Route element={<BestSellers />} path="/bestSellers" />
+                        <Route element={<BookExchange />} path="/bookExchange" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
