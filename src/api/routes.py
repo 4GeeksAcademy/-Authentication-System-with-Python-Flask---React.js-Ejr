d8d5c065cbcf52de40013354b3a722645f80017b
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-09-08 04:30:16 +0000

    Usuario creado Owner y un test de keeper

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Owner
+from api.models import db, User, Owner, Keeper
 from api.utils import generate_sitemap, APIException
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token
@@ -22,23 +22,6 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# @api.route('/signup', methods=['POST'])
-# def createUser():
-#     data = request.get_json(force=True)
-#     email = data["email"].lower()
-#     if User.query.filter_by(email=email).first() is not None:
-#         return jsonify({"msg":"Email already registered"}), 400
-#     new_user=User()
-#     new_user.email = email
-#     secure_password = bcrypt.generate_password_hash((data["password"]), 10).decode("utf-8")
-#     new_user.password = secure_password
-#     new_user.dni = data["dni"]
-#     new_user.name = data["name"]
-#     new_user.lastname = data["lastname"]
-#     new_user.is_active = True
-#     db.session.add(new_user)
-#     db.session.commit()
-#     return jsonify({"msg": "User created successfully"}), 201
 
 # @api.route('/login', methods=["POST"])
 # def login_user():
@@ -53,18 +36,43 @@ def handle_hello():
 #     return jsonify({"msg": "Login successful!", "token":token}),200
 
 
-@api.route('/owner', methods=["POST"])
+@api.route('/signup/owner', methods=['POST'])
 def create_owner():
     data = request.get_json(force=True)
+    email = data["email"].lower()
+    if Owner.query.filter_by(email=email).first() is not None:
+        return jsonify({"msg":"Email already registered"}), 400
     new_owner= Owner()
     new_owner.first_name = data["first_name"]
     new_owner.last_name =  data["last_name"]
     new_owner.email = data["email"]
     new_owner.password = data["password"]
     new_owner.is_active = True
-    db.session.add(new_owner)
-    db.session.commit()
+    user_type = data.get("user_type")
+    if user_type == "owner":
+        db.session.add(new_owner)
+        db.session.commit()
     return jsonify({"msg": "Owner created successfully"}), 201
+
+
+@api.route('/signup/keeper', methods=['POST'])
+def create_keeper():
+    data = request.get_json(force=True)
+    email = data["email"].lower()
+    if Keeper.query.filter_by(email=email).first() is not None:
+        return jsonify({"msg":"Email already registered"}), 400
+    new_keeper= Keeper()
+    new_keeper.first_name = data["first_name"]
+    new_keeper.last_name =  data["last_name"]
+    new_keeper.email = data["email"]
+    new_keeper.password = data["password"]
+    new_keeper.hourly_pay = data["hourly_pay"]
+    new_keeper.is_active = True
+    user_type = data.get("user_type")
+    if user_type == "keeper":
+        db.session.add(new_keeper)
+        db.session.commit()
+    return jsonify({"msg": "Keeper created successfully"}), 201
 
 
 @api.route('/owner', methods=["GET"])
@@ -94,3 +102,33 @@ def delete_owner(owner_id):
     db.session.delete(owner)
     db.session.commit()
     return jsonify({"msg": "Owner deleted successfully"}), 200
+
+
+
+@api.route('/keeper', methods=["GET"])
+def keepers_list():
+    keepers = Keeper.query.all()
+    keepers_data = [{"id": keeper.id, "first_name": keeper.first_name, "last_name": keeper.last_name, "email": keeper.email, "hourly_pay": keeper.hourly_pay}
+                   for keeper in keepers]
+
+    return jsonify(keepers_data), 200
+
+
+@api.route('/keeper/<int:keeper_id>', methods=['GET'])
+def get_keeper(keeper_id):
+    keeper = Keeper.query.get(keeper_id)
+    keeper_data = {
+        "id": keeper.id,
+        "first_name": keeper.first_name,
+        "last_name": keeper.last_name,
+        "email": keeper.email,
+    }
+    return jsonify(keeper_data), 200
+
+
+@api.route('/keeper/<int:keeper_id>', methods=['DELETE'])
+def delete_keeper(keeper_id):
+    keeper = Keeper.query.get(keeper_id)
+    db.session.delete(keeper)
+    db.session.commit()
+    return jsonify({"msg": "keeper deleted successfully"}), 200
