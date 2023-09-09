649565851b35af1bb03eb14c8c73b1042f914137
Juan Solar <juansolarsolar@yahoo.com>
2023-09-09 18:12:52 -0300

    conectado a db

--- src/api/models.py
+++ src/api/models.py
@@ -1,17 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+from sqlalchemy import ForeignKey
+
+
+
+# roles_users = db.Table(
+#     'roles_users',
+#     db.Column('roles_id', db.Integer, db.ForeignKey('roles.id'), nullable=False, primary_key=True),
+#     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), nullable=False, primary_key=True)
+# )
 
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    rol_id = db.Column(db.String, foreign_key=True)
     name = db.Column(db.String(120), nullable=False)
     lastname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(250), nullable=False)
     region = db.Column(db.String(120), nullable=False)
     photo = db.Column(db.String(120), default="no-photo.png")
+    # roles = db.relationship('Role', secondary=roles_users)
 
     def serialize(self):
         return {
