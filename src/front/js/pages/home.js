b9e11a9409fbeaac5777d4a7959aebf402bae2cc
ecmapps <72906286+ecmapps@users.noreply.github.com>
2023-09-09 03:17:25 +0000

    Fixed profile bugs

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,10 +5,6 @@ import { Context } from "../store/appContext";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  useEffect(()=>{
-    console.log("Running");
-    actions.getPets();
-  },[])
   return (
     <div className="container">
       <h1>Aqui va el home</h1>
