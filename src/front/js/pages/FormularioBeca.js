8b6ac1756e13514d7d9fafebf40c7290356eb608
Diana Chacón <diianachm1802@gmail.com>
2023-09-09 20:56:23 +0000

    Added post scholarships fetch function 	modified:   src/app.py 	modified:   src/front/js/pages/FormularioBeca.js 	modified:   src/front/js/pages/IniciarSesionInst.js 	modified:   src/front/js/store/flux.js

--- src/front/js/pages/FormularioBeca.js
+++ src/front/js/pages/FormularioBeca.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Context } from "../store/appContext";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/Formulario.css";
 
 const FormularioBeca = () => {
+  const navigate = useNavigate()
+  const { store, actions } = useContext(Context);
+  const handleChangeScholarshipPost =(e)=>{
+    actions.handleChange(e, "createScholarship")
+  }
   return (
     <div className='container'>
       <h2 className='form-title'>Formulario Sobre Beca</h2>
@@ -10,25 +17,25 @@ const FormularioBeca = () => {
         <div className='row m-3'>
           <div className='col-9'>
             <label htmlFor="nombreInstitucion" className="form-label">Institución</label>
-            <input type="email" className="form-control" id="nombreInstitucion" placeholder='Nombre de la institución' />
+            <input name='institution' type="email" className="form-control" id="nombreInstitucion" placeholder='Nombre de la institución' onChange={handleChangeScholarshipPost}/>
           </div>
           <div className='col-3'>
             <label htmlFor="fechaLimite" className="form-label">Fecha Límite de Inscripción</label>
-            <input type="email" className="form-control" id="fechaLimite" placeholder='DD/MM/AA' />
+            <input name='deadline' type="date" className="form-control" id="fechaLimite" placeholder='DD/MM/AA' onChange={handleChangeScholarshipPost}/>
           </div>
         </div>
         {/* second row starts here */}
         <div className='row m-3 pt-3'>
           <div className='col-9'>
             <label htmlFor="nombreBeca" className="form-label">Nombre de la Beca por Ofrecer</label>
-            <input type="email" className="form-control" id="nombreBeca" placeholder='Nombre de la Beca' />
+            <input name='scholarship_name' type="email" className="form-control" id="nombreBeca" placeholder='Nombre de la Beca' onChange={handleChangeScholarshipPost}/>
           </div>
           <div className='col-3'>
             <label htmlFor="modalidadBeca" className="form-label">Presencial o Virtual?</label>
-            <select class="form-select" id="modalidadBeca">
-              <option selected disabled muted>Escoja una opción</option>
-              <option value="1">Presencial</option>
-              <option value="2">Virtual</option>
+            <select name='modality' class="form-select" id="modalidadBeca" onChange={handleChangeScholarshipPost}>
+              <option selected disabled muted >Escoja una opción</option>
+              <option value="Presencial">Presencial</option>
+              <option value="Virtual">Virtual</option>
             </select>
           </div>
         </div>
@@ -40,10 +47,10 @@ const FormularioBeca = () => {
           </div>
           <div className='col-4'>
             <label htmlFor="coberturaBeca" className="form-label">Cobertura Financiera de la Beca</label>
-            <select class="form-select" id="coberturaBeca">
-              <option selected disabled>Escoja una opción</option>
-              <option value="1">Completa</option>
-              <option value="2">Parcial</option>
+            <select name='coverage' class="form-select" id="coberturaBeca" onChange={handleChangeScholarshipPost}>
+              <option name='coverage' selected disabled>Escoja una opción</option>
+              <option value="Completa">Completa</option>
+              <option value="Parcial">Parcial</option>
             </select>
           </div>
         </div>
@@ -51,12 +58,12 @@ const FormularioBeca = () => {
         <div className='row mt-3 mx-3 mb-4 pt-3'>
           <div className='col-6'>
             <label htmlFor="urlBeca" className="form-label">URL para la Página de Inscripción</label>
-            <input type="email" className="form-control" id="urlBeca" placeholder='https://' />
-            <button className='mt-4 button-post'>Publicar</button>
+            <input name='url_to' type="email" className="form-control" id="urlBeca" placeholder='https://' onChange={handleChangeScholarshipPost}/>
+            <button className='mt-4 button-post' onClick={()=>actions.postScholarship()} >Publicar</button>
           </div>
           <div className='col-6'>
-            <label for="descripcionBeca" class="form-label">Descripción de los Beneficios y Requerimientos para Participar</label>
-            <textarea class="form-control" id="descripcionBeca" rows="4"></textarea>
+            <label for="descripcionBeca" class="form-label" >Descripción de los Beneficios y Requerimientos para Participar</label>
+            <textarea name='description' class="form-control" id="descripcionBeca" rows="4" onChange={handleChangeScholarshipPost}></textarea>
           </div>
         </div>
 
