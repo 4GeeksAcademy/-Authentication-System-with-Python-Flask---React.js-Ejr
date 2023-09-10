e16f3f239ac258b0d61acc8851e37198c4e5ce5b
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-09-10 21:36:57 +0000

    añadidos páginas de perfil, edición de eventos. Modificada página de inicio y registro de sesión. Sin en lazar al Back-End

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -6,8 +6,7 @@ import "../styles/index.css";
 
 //import your own components
 import Layout from "./layout";
-import UserProfile from "./component/perfil";
-import Userpage from "./pages/perfil";
+
 
 //render your react application
-ReactDOM.render(<Userpage/>, document.querySelector("#app"));
+ReactDOM.render(<Layout/>, document.querySelector("#app"));
