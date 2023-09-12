5d646fc0584665d74faebe4e575404bc6a8b9a12
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-09-12 18:32:04 +0000

    delete a house

--- migrations/versions/52f8aafdbb0f_.py
+++ migrations/versions/52f8aafdbb0f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6c5d316fe52a
+Revision ID: 52f8aafdbb0f
 Revises: 
-Create Date: 2023-09-12 14:18:05.916083
+Create Date: 2023-09-12 18:12:23.174302
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6c5d316fe52a'
+revision = '52f8aafdbb0f'
 down_revision = None
 branch_labels = None
 depends_on = None
