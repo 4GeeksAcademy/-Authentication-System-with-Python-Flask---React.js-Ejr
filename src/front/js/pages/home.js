bdfc47b4734650781862706a2061cbce3357b043
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-09-12 18:44:02 +0000

    ta hecho

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { CardFeedVen } from "../component/card_feed_ven.jsx"
+import { CardFeedAlq } from "../component/card_feed_alq.jsx"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -11,19 +12,15 @@ export const Home = () => {
 	const [register, setRegister] = useState("")
     const [registerST, setRegisterST] = useState("")
 
+    useEffect(() => {
+		actions.getAlquileres()
+		actions.getVentas()
+        console.log(store.ventas);
+        console.log(store.alquileres);
+	}, [])
 
-    async function submitLogin(e){
-        e.preventDefault();
-        if (logged === true) {
-            navigate("/")
-        }
-    }
-
-    async function submitSignUp(e){
-        e.preventDefault();
-    }
     
-    function logines() {
+    function alquileres() {
         if (login == "") {
             setLogin("show active")
             setLoginST("active")
@@ -32,7 +29,7 @@ export const Home = () => {
         }
     }
 
-    function registeres() {
+    function ventas() {
         if (register == "") {
             setRegister("show active")
             setRegisterST("active")
@@ -42,27 +39,33 @@ export const Home = () => {
     }
 
     return (
-        <div className="bg-dark bg-opacity-75 text-white p-5" style={{margin: "70px 70px 100px 70px"}}>
-            <ul className="nav nav-pills bg-black nav-justified mb-5" id="ex1" role="tablist">
+        <div className="text-white p-5">
+            <ul className="nav nav-pills nav-justified mb-5" id="ex1" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <a className={"nav-link " + loginST} id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
-                        aria-controls="pills-login" aria-selected="true" onClick={() => logines()}>Alquileres</a>
+                    <h2 className={"nav-link " + loginST} id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
+                        aria-controls="pills-login" aria-selected="true" onClick={() => alquileres()}>Alquileres</h2>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <a className={"nav-link " + registerST} id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
-                        aria-controls="pills-register" aria-selected="false" onClick={() => registeres()}>Ventas</a>
+                    <h2 className={"nav-link " + registerST} id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
+                        aria-controls="pills-register" aria-selected="false" onClick={() => ventas()}>Ventas</h2>
                 </li>
             </ul>
             <div className="tab-content">
                 <div className={"tab-pane fade " + login} id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-                    <form onSubmit={(e) => {submitLogin(e)}}>
-                        <h1>Alquileres</h1>
-                    </form>
+                {store.alquileres.map((item, index) => {
+					return (
+                    <div key={index}>
+                        <CardFeedAlq ubicacion={item.location} precio={item.price}/>
+                    </div>
+                    )})}
                 </div>
                 <div className={"tab-pane fade " + register} id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-                    <form onSubmit={(e) => {submitSignUp(e)}}>
-                        <h1>Ventas</h1>
-                    </form>
+                {store.ventas.map((item, index) => {
+					return (
+                    <div key={index}>
+                        <CardFeedVen ubicacion={item.location} precio={item.price}/>
+                    </div>
+                    )})}
                 </div>
             </div>
 
