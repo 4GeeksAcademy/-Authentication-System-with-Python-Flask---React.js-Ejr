cb8dad7200a926960680e56934920365e8557fcf
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-09-12 05:48:14 +0000

    components crud

--- src/api/routes.py
+++ src/api/routes.py
@@ -25,6 +25,7 @@ def handle_hello():
     return jsonify(response_body), 200
 
 
+# # # # # USER 👨👨👨👨👨👨
 
 @api.route('/signup', methods=['POST'])
 def signup_user():
@@ -63,5 +64,94 @@ def login_user():
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token), 200
 
+
+# # # # # COMPONENT 🔳🔳🔳🟧🟨🟩🟦
+
+@api.route('/components', methods=['GET'])
+def get_components():
+    #/components?page=1&per_page=10 #get first page, 10 components.
+    #/components?page=2&per_page=10 #get second page, 10 components.
+
+    page = request.args.get('page', default=1, type=int)
+    per_page = request.args.get('per_page', default=10, type=int)
+
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+
+    component_query = Component.query.slice(start_index, end_index).all() # Pagination
+    #component_query = Component.query.all() # No pagination 
+
+    results = list(map(lambda item: item.serialize(), component_query))
+    response_body = { "msg": "All components:",
+                     "results": results}
     
+    return jsonify(response_body), 200
+
+
+@api.route('/component/<int:component_id>', methods=['GET'])
+def get_one_component(component_id):
+
+    component = Component.query.filter_by(id=component_id).first()
+    if not component:
+        return jsonify({"msg": "Component not found"}), 404
+
+    response_body = { "msg": "Component:",
+                     "results": component.serialize()}
     
+    return jsonify(response_body), 200
+
+
+@api.route('/component/add', methods=['POST']) # TODO >> only admin jwt
+def add_component():
+    request_body = request.get_json(force=True)
+     
+    new_component = Component(
+                                name=request_body["name"], 
+                                type = request_body["type"], 
+                                html_code = request_body["html_code"],
+                                css_code = request_body["css_code"],
+                                js_code = request_body["js_code"],
+                                react_code = request_body["react_code"],
+                             )
+
+    db.session.add(new_component)
+    db.session.commit()
+   
+    return { "msg": "Component created", 
+            "response": new_component.serialize()
+            }
+
+
+@api.route('/component/delete/<int:component_id>', methods=['DELETE']) # TODO >> only admin jwt
+def delete_component(component_id):
+    # component exist ?
+    component = Component.query.get(component_id)
+    if not component:
+        return jsonify({"msg": "Component not found"}), 404
+    else:
+        db.session.delete(component)
+        db.session.commit()
+        return jsonify({"msg": f"Component {component_id} has been removed from database"}), 200
+
+
+@api.route('/component/update/<int:component_id>', methods=['PUT'])  # TODO >> only admin jwt
+def update_component(component_id):
+    # component exist ?
+    component = Component.query.get(component_id)
+    if not component:
+        return jsonify({"msg": "Component not found"}), 404
+
+    request_body = request.get_json(force=True)
+
+    # Update the component attributes
+    component.name = request_body.get("name", component.name)
+    component.type = request_body.get("type", component.type)
+    component.html_code = request_body.get("html_code", component.html_code)
+    component.css_code = request_body.get("css_code", component.css_code)
+    component.js_code = request_body.get("js_code", component.js_code)
+    component.react_code = request_body.get("react_code", component.react_code)
+
+    db.session.commit()
+
+    return jsonify({"msg": f"Component {component_id} has been updated",
+                    "response": component.serialize() }), 200
