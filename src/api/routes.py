6d102680c9d55b3e934b9b33e426ba0fa9422b0f
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-09-13 13:50:31 +0000

    models, crud payment

--- src/api/routes.py
+++ src/api/routes.py
@@ -214,8 +214,11 @@ def add_payment():
     body = json.loads(request.data)
     new_payment = Payment(
         id_user=body["id_user"],
-        id_component=body["id_component"]
+        plan_start_date =body["plan_start_date"],
+        plan_end_date =body["plan_end_date"],
+        id_plan =body["id_plan"]
     )
+
 
     db.session.add(new_payment)
     db.session.commit()
@@ -224,40 +227,46 @@ def add_payment():
 
 
 # # Modifica  el Payment por id
-# @api.route('/payment/<int:payment_id>', methods=['PUT'])
-# # @jwt_required()
-# def update_Payment(payment_id):
-#     body = json.loads(request.data)
-#     # busca que el favorite exista
-#     payment= Payment.query.filter_by(id=payment_id).first()
+@api.route('/payment/<int:payment_id>', methods=['PUT'])
+# @jwt_required()
+def update_Payment(payment_id):
+    body = json.loads(request.data)
+    # busca que el payment exista
+    payment= Payment.query.filter_by(id=payment_id).first()
 
-#     if payment is None:
-#         return jsonify({"msg": "no existe el Payment"}), 404
+    if payment is None:
+        return jsonify({"msg": "no existe el Payment"}), 404
 
-#     if  "id_user"  in body:
-#         payment.id_user  = body["id_user"]
+    if "id_user" in body:
+        payment.id_user = body["id_user"]
 
-#     if "id_component" in body:
-#         payment.id_component = body["id_component"]
+    if "id_plan" in body:
+        payment.id_plan = body["id_plan"]
+
+    if "plan_start_date" in body:
+        payment.plan_start_date = body["plan_start_date"]
+        
+    if "plan_end_date" in body:
+        payment.plan_end_date = body["plan_end_date"]
 
    
-#     db.session.commit()
-#     return jsonify({"msg": "Payment Modificado"}), 200
+    db.session.commit()
+    return jsonify({"msg":"Payment Modificado"}), 200
 
 
 
 # # Borra un  payment por id
-# @api.route('/ payment/<int: payment_id>', methods=['DELETE'])
-# # @jwt_required()
-# def delete_payment( payment_id):
+@api.route('/payment/<int:payment_id>', methods=['DELETE'])
+# @jwt_required()
+def delete_payment(payment_id):
 
-#     payment = Payment.query.get( payment_id)
-#     if not  payment:
-#         return jsonify({"msg": " payment not found"}), 404
-#     else:
-#         db.session.delete( payment)
-#         db.session.commit()
-#         return jsonify({"msg": " payment  deleted"}), 200
+    payment = Payment.query.get(payment_id)
+    if not payment:
+        return jsonify({"msg": "payment not found"}), 404
+    else:
+        db.session.delete(payment)
+        db.session.commit()
+        return jsonify({"msg": "payment  deleted"}), 200
 
 
 
