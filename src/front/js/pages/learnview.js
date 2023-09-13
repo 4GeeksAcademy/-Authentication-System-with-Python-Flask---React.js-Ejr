863acf0f63f0ceb4d05a2c559393455756ce2c9b
Juan Coronel <juanocoronell@gmail.com>
2023-09-13 01:48:56 +0000

    lesto

--- src/front/js/pages/learnview.js
+++ src/front/js/pages/learnview.js
@@ -10,7 +10,7 @@ const LearnView = () => {
                 <div className="col-md-3">
                     <div className="card shadow" style={{ backgroundColor: '#FFDAB9' }}>
                         <div className="card-body">
-                            <h5 className="text-left"> <strong> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-1-square-fill" viewBox="0 0 16 16">
+                            <h5 className="text-left" > <strong> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-1-square-fill" viewBox="0 0 16 16">
   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm7.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
 </svg> Download the Installation File</strong></h5>
                             <p className="text-left">To start the installation of [Application Name], you must first download the installation file from the official website.</p>
