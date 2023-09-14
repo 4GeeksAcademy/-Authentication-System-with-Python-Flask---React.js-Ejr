2a58b39f29bb72e4d3c31f6b1dbf3016e248b96a
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-09-14 12:34:56 +0000

    mejora en la navegacion

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Context } from "../store/appContext";
 
 export const Navbar = () => {
 
-    const { store, actions } = useContext(Context);
+	const { store, actions } = useContext(Context);
 
 	return (
 		<nav className="navbar navbar-light bg-light">
@@ -12,7 +12,13 @@ export const Navbar = () => {
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-				{store.auth ? null :
+				{store.auth ?
+					<div className="ml-auto">
+						<Link to="/login">
+							<button onClick={() => actions.logout()} className="btn btn-primary">Cerrar sesión</button>
+						</Link>
+					</div>
+					:
 					<div className="ml-auto">
 						<Link to="/signup">
 							<button className="btn btn-primary">Registrate</button>
