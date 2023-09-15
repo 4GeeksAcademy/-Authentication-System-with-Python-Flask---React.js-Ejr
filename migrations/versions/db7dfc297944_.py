a06d2afa9bc271a9b2ce0962eee7860bcafce238
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-09-15 03:47:06 +0000

    bcrypt passwords done

--- migrations/versions/db7dfc297944_.py
+++ migrations/versions/db7dfc297944_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 06474e79dac1
+Revision ID: db7dfc297944
 Revises: 
-Create Date: 2023-09-12 12:43:41.295853
+Create Date: 2023-09-15 02:52:25.159935
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '06474e79dac1'
+revision = 'db7dfc297944'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 #from models import Person
 
@@ -42,6 +43,9 @@ setup_admin(app)
 
 # add the admin
 setup_commands(app)
+
+# add bcrypt to app
+bcrypt = Bcrypt(app)
 
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
