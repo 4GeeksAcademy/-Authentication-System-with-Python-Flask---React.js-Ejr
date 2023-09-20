0ebdeff1f6c5d201a5f2462085dd0c9b814247a9
María Ripoll Vives <mariaripoll95@icloud.com>
2023-09-20 17:30:47 +0000

    Checked the Reviews Structure

--- src/front/js/component/review.js
+++ src/front/js/component/review.js
@@ -2,26 +2,29 @@ import React, { Component } from "react";
 import "../../styles/review.css";
 
 export const Review = () => (
-	<div className="row no-gutters">
-			<div className="container d-flex justify-content-center">
-                <div className="col-2">
-                    <img className="rounded-circle thumbnail mt-2 mx-2"src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp"/>
-                </div>
+    <div className="d-flex flex-row bd-highlight mb-3 col-10 align-items-center">
+        <div className="p-2 col-md-2">
+            <img className="rounded-circle mt-2 mx-2"src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" height="100"/>
+        </div>
 
-                <div className="col-10">
-                    <div className="reviewbody">
-                        <div className="row g-3 d-flex">
-                            <p className="username">@username rated this book</p>
-                            <p>02/02/2022</p>
-                        </div>
-                        <div className="row g-3 d-flex">
-                            <p>★★★★★</p>
-                            <p className="reviewdescription">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
-                        </div>
-                    </div>
+        <div className="p-2 col-md-10">
+            <div className="row">
+                <div className="d-flex flex-row bd-highlight mb-3">
+                    <div className="px-2 bd-highlight">@username rated this book</div>
+                    <div className="px-2 bd-highlight text-end">DATE</div>
                 </div>
-			</div>
-		</div>
+            </div>
+            <div className="row">
+                <div className="d-flex flex-row bd-highlight mb-3">
+                    <div className="px-2 bd-highlight">⭑⭑⭑⭑⭑</div>
+                    <div className="px-2 bd-highlight">5</div>
+                </div>
+            </div>
+            <div className="row">
+                <p>"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500."</p>
+            </div>
+        </div>
+    </div>
 );
 
 export default Review;
--- src/front/js/pages/book_details.js
+++ src/front/js/pages/book_details.js
@@ -30,56 +30,11 @@ export const BookDetails = () => {
 				</div>
 			</div>
 
-			<div className="container my-5 col-12">
+			<div className="container my-5 col-9">
 				<h2 className="text-center">Ratings & Reviews</h2>
-				<div className="multi-carousel vertical" style={{width:"20rem"}}>
-					<div className="multi-carousel-inner">
-						<div className="multi-carousel-item">
-						<img
-							src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/1.webp"
-							alt="Table Full of Spices"
-							className="w-100"
-						/>
-						</div>
-						<div className="multi-carousel-item">
-						<img
-							src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/2.webp"
-							alt="Winter Landscape"
-							className="w-100"
-						/>
-						</div>
-						<div className="multi-carousel-item">
-						<img
-							src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/3.webp"
-							alt="View of the City in the Mountains"
-							className="w-100"
-						/>
-						</div>
-						<div className="multi-carousel-item">
-						<img
-							src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/4.webp"
-							alt="Place Royale Bruxelles"
-							className="w-100"
-						/>
-						</div>
-					</div>
-					<button
-						className="carousel-control-prev"
-						type="button"
-						tabindex="0"
-						data-mdb-slide="prev"
-					>
-						<span className="carousel-control-prev-icon"></span>
-					</button>
-					<button
-						className="carousel-control-next"
-						type="button"
-						tabindex="0"
-						data-mdb-slide="next"
-					>
-						<span className="carousel-control-next-icon"></span>
-					</button>
-				</div>
+				<Review />
+				<Review />
+				<Review />
 			</div>
 
 			<div className="container my-5 col-12">
