64b64ada80ed4fc4b77b786963415d3e4b909bf0
devAbreu <100288811+devAbreu@users.noreply.github.com>
2023-10-09 14:06:42 -0500

    Removed commented code

--- src/api/routes/root_routes.py
+++ src/api/routes/root_routes.py
@@ -20,21 +20,6 @@ def sitemap():
         return generate_sitemap(current_app._get_current_object())
     return send_from_directory(static_file_dir, 'index.html')
 
-# Any other endpoint will be served as a general message for an invalid endpoint
-# @root.route('/<path:path>', methods=['GET'])
-# def invalid_endpoint(path):
-#     """
-#     Handle requests to invalid endpoints by returning a JSON message with a 404 status code.
-# 
-#     :param path: The path that was accessed.
-#     :return: A JSON response with a 404 status code and an error message.
-#     """
-#     response_body = {
-#         "message": "Invalid endpoint",
-#         "path": path  # Optionally, you can include the path that was attempted to be accessed
-#     }
-#     return jsonify(response_body), 404  # 404 is the HTTP status code for "Not Found"
-
 # This route can be used to serve any other file if needed
 @root.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
