3a7d6b05f3d956b489e689b2b5119c585a570b0b
Catalina Macrobio <catamacro@gmail.com>
2023-11-23 21:17:34 -0300

    modificacion nombre

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">				
-					<span className="navbar-brand mb-0 h1">EasyJob</span>
-					<span className="navbar-brand mb-0 h1">Trabajos</span>
-					<span className="navbar-brand mb-0 h1">Nosotros</span>
-					<span className="navbar-brand mb-0 h1">Experiencias</span>		
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Entrar</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1">EasyJob</span>
+        <span className="navbar-brand mb-0 h1">Trabajos</span>
+        <span className="navbar-brand mb-0 h1">Nosotros</span>
+        <span className="navbar-brand mb-0 h1">Experiencias</span>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">Entrar</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
