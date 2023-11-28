8b1c8196df72736a5e2afc430112aa7fb7a2d513
Catalina Macrobio <catamacro@gmail.com>
2023-11-27 21:19:07 -0300

    imagenes oficios

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/home.css";
+import easyJobUrl1 from "../../img/electricista.jpg";
+import easyJobUrl2 from "../../img/gasfiteria.jpg";
+import easyJobUrl3 from "../../img/carpintero.jpg";
+import easyJobUrl4 from "../../img/aseo.jpg";
+import easyJobUrl5 from "../../img/maestro-pintor.jpg";
 
 const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < 2 ? prevIndex + 1 : 2));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+  };
+
   return (
     <div className="carousel-oficios">
       <h1>
-        <strong>...Nuestros Oficios...</strong>
+        <strong>Prestadores & Oficios</strong>
       </h1>
 
       <div className="carousel-container">
-        <div className="carousel-box">Slide 1</div>
-        <div className="carousel-box">Slide 2</div>
-        <div className="carousel-box">Slide 3</div>
+        <div
+          className="carousel-wrapper"
+          style={{ transform: `translateX(-${currentIndex * 33.33}%)` }}
+        >
+          <div className="carousel-box">
+            <h2>Electricista</h2>
+            <img className="img-fluid" src={easyJobUrl1} />
+          </div>
+          <div className="carousel-box">
+            <h2>Carpintero</h2>
+            <img className="img-fluid" src={easyJobUrl3} />
+          </div>
+          <div className="carousel-box">
+            <h2>Aseo</h2>
+            <img className="img-fluid" src={easyJobUrl4} />
+          </div>
+          <div className="carousel-box">
+            <h2>Gasfiteria</h2>
+            <img className="img-fluid" src={easyJobUrl2} />
+          </div>
+          <div className="carousel-box">
+            <h2>Maestro Pintor</h2>
+            <img className="img-fluid" src={easyJobUrl5} />
+          </div>
+        </div>
+      </div>
+
+      <div className="carousel-controls">
+        <div className="arrow" onClick={handlePrev}>
+          &#9664;
+        </div>
+        <div className="arrow" onClick={handleNext}>
+          &#9654;
+        </div>
       </div>
     </div>
   );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,7 +4,6 @@ import easyJobUrl from "../../img/Easy-Job.jpg";
 import "../../styles/home.css";
 import Carousel from "../component/carousel";
 
-
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
@@ -18,35 +17,32 @@ export const Home = () => {
     console.log("Botón 2 clickeado");
   };
 
-  return ( 
-  <> 
+  return (
+    <>
+      <div className="container">
+        <div className="text-container d-flex flex-column">
+          <h1>Unete a Easy Job</h1>
+          <p className="mb-5">
+            Busca de forma sencilla y conecta con Prestadores para encontrar una
+            solución a tu medida, siempre tendrás un buen contacto en EasyJob.
+          </p>
 
-    <div className="container">
-      <div className="text-container d-flex flex-column">
-        <h1>Unete a Easy Job</h1>
-        <p className="mb-5">
-          Busca de forma sencilla y conecta con Prestadores para encontrar una
-          solución a tu medida, siempre tendrás un buen contacto en EasyJob.
-        </p>
-
-        <div className="button-container my-5">
-          <button onClick={handleButtonClick1}>
-            Registrate acá como cliente
-          </button>
-          <button className="blue" onClick={handleButtonClick2}>
-            Conviérte en Prestador
-          </button>
+          <div className="button-container my-5">
+            <button onClick={handleButtonClick1}>
+              Registrate acá como cliente
+            </button>
+            <button className="blue" onClick={handleButtonClick2}>
+              Conviérte en Prestador
+            </button>
+          </div>
+        </div>
+        <div className="image-container">
+          <img src={easyJobUrl} />
         </div>
       </div>
-      <div className="image-container">
-        <img src={easyJobUrl} />
+      <div className="container">
+        <Carousel />
       </div>
-    </div>
-    <div className="container">
-
-      <Carousel/>
-    </div>
-    
-  </>
+    </>
   );
 };
