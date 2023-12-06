1dbb9534ae61f6afcc5ba98bbd0aabad1f1ace3f
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2023-12-06 11:13:03 -0300

    cambiosbuscador.

--- src/front/js/component/Buscador.jsx
+++ src/front/js/component/Buscador.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Card } from "react-bootstrap";
 import "../../styles/elbuscador.css";
 
 export const Buscador = () => {
@@ -48,12 +48,69 @@ export const Buscador = () => {
         <br />
 
         <Link to="/">
-          <button type="button" className="submit btn btn-success">
+          <button type="button" className="submit btn btn-">
             REGRESAR
           </button>
         </Link>
 
-       
+        <div
+          style={{ backgroundColor: "aliceblue", padding: "30px" }}
+          class="card"
+        >
+          {" "}
+          {/*EN ESTA LINEA COLOR DE BACKGROUND*/}
+          <div className="card-body" style={{ padding: "10px 30px" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "120px",
+                alignItems: "center",
+                marginBottom: "30px",
+              }}
+            >
+              <p style={{ width: "40%", margin: "0" }}>
+                CONSIGUE NUEVAS OPORTUNIDADES
+              </p>
+
+              <div
+                style={{
+                  width: "30%",
+                  height: "70%",
+                  padding: "10px",
+                  background: "#88888E33",
+                  borderRadius: "6px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "10px",
+                  display: "inline-flex",
+                }}
+              >
+                <div
+                  style={{
+                    width: "100px",
+                    height: "96px",
+                    background: "#88888E33",
+                  }}
+                >
+                  {" "}
+                  IMAGEN
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="button"
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Link to="/Formulario">
+                <button type="button" className="submitbutton btn btn">
+                  Conviertete en Prestador
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
