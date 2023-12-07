a59b0092313e0787e7cc1f8bd95cbcbe4b7bca70
Felipe-LasHeras <FE.LASHERAS@DUOCUC.CL>
2023-12-07 19:44:59 -0300

    db publicaciones

--- src/front/js/component/Buscador.jsx
+++ src/front/js/component/Buscador.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import "../../styles/elbuscador.css";
+
 
 export const Buscador = () => {
   return (
