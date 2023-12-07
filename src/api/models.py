ad2fd3a1600024b79d881cb25244f35b73c48c1f
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-12-07 03:10:44 +0000

    Added models-US Db Modeling 1.0

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,53 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    date_of_birth= db.Column(db.String(120), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    routine = db.Column(db.String(120), unique=False, nullable=False)
+    diet = db.Column(db.String(120), unique=False, nullable=False)
+    pathologies = db.Column(db.String(120), unique=False, nullable=False)
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+
+class Trainer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    
+
+class Routines(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Chest = db.Column(db.String(120), unique=False, nullable=False)
+    shoulders = db.Column(db.String(120), unique=False, nullable=False)
+    arms = db.Column(db.String(120), unique=False, nullable=False)
+    legs = db.Column(db.String(120), unique=False, nullable=False)
+    id_user = db.Column(db.String(120), unique=False, nullable=True)
+    id_trainer = db.Column(db.String(120), unique=False, nullable=True)
+
+
+class Diets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    breakfast = db.Column(db.String(120), unique=False, nullable=False)
+    brunch = db.Column(db.String(120), unique=False, nullable=False)
+    lunch = db.Column(db.String(120), unique=False, nullable=False)
+    dinner = db.Column(db.String(120), unique=False, nullable=False)
+    supper = db.Column(db.String(120), unique=False, nullable=False)
+    id_trainer = db.Column(db.String(120), unique=False, nullable=True)
+    id_user = db.Column(db.String(120), unique=False, nullable=True)
+
+
+
+
 
     def __repr__(self):
         return f'<User {self.email}>'
