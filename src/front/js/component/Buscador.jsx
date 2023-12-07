4e5b34b2d431ee7adc6e640e9e3d6108a6ef6202
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2023-12-06 21:19:37 -0300

    ordenarbuscador

--- src/front/js/component/Buscador.jsx
+++ src/front/js/component/Buscador.jsx
@@ -1,87 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import '../../styles/elbuscador.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "../../styles/elbuscador.css";
 
 export const Buscador = () => {
-   return (
-    <div className='container'>
-      <Container className="buttons-container">
+  return (
+    <div className="container">
+      <div className="buttons-container">
         <h1> SELECCIONA TU BUSQUEDA </h1>
-        <br/>
+        <br />
         <Row className="justify-content-center">
           <Col md="auto">
             <Button variant="dark">
-            <h2>Carpintería</h2>
+              <h2>Carpintería</h2>
             </Button>
           </Col>
         </Row>
         <Row className="justify-content-center mt-3">
           <Col md="auto">
             <Button variant="dark">
-            <h2>Electricista</h2>
+              <h2>Electricista</h2>
             </Button>
           </Col>
         </Row>
         <Row className="justify-content-center mt-3">
           <Col md="auto">
             <Button variant="dark">
-            <h2>Gasfistería</h2>
+              <h2>Gasfistería</h2>
             </Button>
           </Col>
         </Row>
         <Row className="justify-content-center mt-3">
           <Col md="auto">
             <Button variant="dark">
-            <h2>Pintor</h2>
+              <h2>Pintor</h2>
             </Button>
           </Col>
         </Row>
         <Row className="justify-content-center mt-3">
           <Col md="auto">
             <Button variant="dark">
-            <h2>Aseo</h2>
+              <h2>Aseo</h2>
             </Button>
           </Col>
         </Row>
-        <br/>
-        <br/>
-        
+        <br />
+        <br />
 
-       
         <Link to="/">
-          <button type="button" className="submit btn btn-">
+          <button type="button" className="submitbutton1 btn btn-">
             REGRESAR
           </button>
         </Link>
 
-        <div
-          style={{ backgroundColor: "aliceblue", padding: "30px" }}
-          class="card"
-        >
+        <div className="card">
           {" "}
           {/*EN ESTA LINEA COLOR DE BACKGROUND*/}
-          <div className="card-body" style={{ padding: "10px 30px" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                gap: "120px",
-                alignItems: "center",
-                marginBottom: "30px",
-              }}
-            >
+          <div className="card-body">
+            <div className="card-body2">
               <p style={{ width: "40%", margin: "0" }}>
                 CONSIGUE NUEVAS OPORTUNIDADES
               </p>
 
               <div
                 style={{
-                  width: "30%",
+                  width: "40%",
                   height: "70%",
                   padding: "10px",
-                  background: "#88888E33",
+                  background: "#99fffd",
                   borderRadius: "6px",
                   justifyContent: "center",
                   alignItems: "center",
@@ -93,10 +79,9 @@ export const Buscador = () => {
                   style={{
                     width: "100px",
                     height: "96px",
-                    background: "#88888E33",
+                    background: "#99fffd",
                   }}
                 >
-                  {" "}
                   IMAGEN
                 </div>
               </div>
@@ -107,14 +92,14 @@ export const Buscador = () => {
               style={{ display: "flex", justifyContent: "flex-end" }}
             >
               <Link to="/Formulario">
-                <button type="button" className="submitbutton btn btn">
+                <button type="button" className="submitbutton2 btn btn success">
                   Conviertete en Prestador
                 </button>
               </Link>
             </div>
           </div>
         </div>
-      </Container>
+      </div>
     </div>
   );
 };
