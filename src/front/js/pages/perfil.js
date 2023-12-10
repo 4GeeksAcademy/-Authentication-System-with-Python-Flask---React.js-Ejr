1007cbf6e3e1b5f492175e7051e739cf8512c3f6
Simon <simontorbett@gmail.com>
2023-12-10 16:39:04 -0300

    cambio perfil

--- src/front/js/pages/perfil.js
+++ src/front/js/pages/perfil.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import "./../component/Buscador.jsx"
 import { Context } from "../store/appContext.js";
 
+
 export const Perfil = () => {
   const [formData, setFormData] = useState({
     firstName: "",
@@ -12,28 +13,13 @@ export const Perfil = () => {
     email: "",
     region: "",
     comuna: "",
-    tipoUsuario: "" ,
-    birthDate: ""
+    birthDate: "",
   });
-
-  const { store, actions } = useContext(Context);
-
-  useEffect(() => {
-    const fetchPerfil = async () => {
-      console.log("cualquiera");
-      let perfil = await actions.cargarPerfil(store.id);
-      if (perfil?.firstName) {
-        setFormData(perfil);
-      }
-    };
-
-    fetchPerfil();
-  }, [store.id, actions]);
 
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -41,86 +27,30 @@ export const Perfil = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3001/api/perfil", {
-        method: "POST",
+      const response = await fetch('http://localhost:3001/api/perfil', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
       if (response.ok) {
+        // Manejar la respuesta exitosa del servidor
         const data = await response.json();
-        console.log("Registro exitoso:", data);
+        console.log('Registro exitoso:', data);
       } else {
-        console.error("Error al registrar usuario");
+        // Manejar errores
+        console.error('Error al registrar usuario');
       }
     } catch (error) {
-      console.error("Error de red:", error);
+      console.error('Error de red:', error);
     }
   };
-  const [image, setImage] = useState(null);
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        setImage(event.target.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  // Calificaciones del SegundoPerfil
-  const ratings = [
-    {
-      comment: "¡Gran trabajo! Muy profesional.",
-      rating: 5,
-    },
-    {
-      comment: "Buen servicio, lo recomiendo.",
-      rating: 4,
-    },
-    // Puedes agregar más comentarios y calificaciones según sea necesario
-  ];
   return (
     <div className="profile-container">
-     
       <div className="header">
-        <h1 className="profile-name"  onSubmit={handleSubmit}> 
-          {formData.firstName}
-            {handleInputChange}</h1>
-            </div>
-            
-      <div className="photo text-center">
-        <label htmlFor="upload-photo" className="photo-text">
-          {image ? (
-            <img src={image} alt="uploaded" className="uploaded-image" />
-          ) : (
-            "FOTO"
-          )}
-        </label>
-        <input
-          type="file"
-          id="upload-photo"
-          accept="image/*"
-          onChange={handleImageChange}
-          style={{ display: "none" }}
-        />
-      </div>
-
-      {/* Mostrar calificaciones */}
-      <div className="ratings-section">
-        <div className="comments-ratings text-center">COMENTARIOS Y CALIFICACIONES</div>
-        <div className="ratings-list">
-          {ratings.map((item, index) => (
-            <div className="rating-item" key={index}>
-              <div className="comment">{item.comment}</div>
-              <div className="rating">Calificación: {item.rating}</div>
-            </div>
-          ))}
-        </div>
+        <h1 className="profile-name">Nombre Cliente</h1>
       </div>
 
       {/* Formulario de datos */}
@@ -155,7 +85,16 @@ export const Perfil = () => {
             onChange={handleInputChange}
           />
         </div>
-        
+        <div className="form-group">
+          <label htmlFor="region">Región:</label>
+          <input
+            type="text"
+            id="region"
+            name="region"
+            value={formData.region}
+            onChange={handleInputChange}
+          />
+        </div>
         <div className="form-group">
           <label htmlFor="comuna">Comuna:</label>
           <input
@@ -169,25 +108,13 @@ export const Perfil = () => {
         <div className="form-group">
           <label htmlFor="birthDate">Fecha de Nacimiento:</label>
           <input
-            type= "text"
+            type="date"
             id="birthDate"
             name="birthDate"
             value={formData.birthDate}
             onChange={handleInputChange}
           />
         </div>
-        {formData.tipoUsuario === "prestador" && (
-        <div className="form-group">
-          <label htmlFor="rubro">Rubro:</label>
-          <input
-            type="text"
-            id="rubro"
-            name="rubro"
-            value={formData.rubro}
-            onChange={handleInputChange}
-          />
-        </div>
-      )}
         <button className="action-button" type="submit">
           Guardar
         </button>
