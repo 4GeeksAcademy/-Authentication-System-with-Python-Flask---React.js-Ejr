cbd5c9079fd2dcacb3dc93828f98ee2816c107bf
marianospi123 <141750385+marianospi123@users.noreply.github.com>
2023-12-18 21:16:46 +0000

    buttons and home specialist

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,31 +3,57 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
-  return (
-    <div className="bubbleContainer">
-      <div className="navLinks">
-        <button className="navLink">
-          <div className="navLabel">Inicio</div>
-        </button>
-        <button className="navLink">
-          <div className="navLabel">Servicios</div>
-        </button>
-        <button className="navLink">
-          <div className="navLabel">Profesionales</div>
-        </button>
-      </div>
-      <div className="navLinks1">
-        <button className="navLink">
-          <div className="navLabel">Iniciar Sesión</div>
-        </button>
-        <button className="navLink">
-          <div className="navLabel">Registrarse</div>
-        </button>
-      </div>
-      <div className="brand">
-        <FontAwesomeIcon icon={faHeartbeat} />
-        <div className="brandname">PhysioCareSync</div>
-      </div>
-    </div>
-  );
+
+    const handleInicioClick = () => {
+        console.log("Botón de Inicio clicado");
+
+    };
+
+    const handleServiciosClick = () => {
+        console.log("Botón de Servicios clicado");
+
+    };
+
+    const handleProfesionalesClick = () => {
+        console.log("Botón de Profesionales clicado");
+
+    };
+
+    const handleLoginClick = () => {
+        console.log("Botón de Iniciar Sesión clicado");
+    
+    };
+
+    const handleRegisterClick = () => {
+        console.log("Botón de Registrarse clicado");
+  
+    };
+
+    return (
+        <div className="bubbleContainer">
+            <div className="navLinks">
+                <button className="navLink" onClick={handleInicioClick}>
+                    <div className="navLabel">Inicio</div>
+                </button>
+                <button className="navLink" onClick={handleServiciosClick}>
+                    <div className="navLabel">Servicios</div>
+                </button>
+                <button className="navLink" onClick={handleProfesionalesClick}>
+                    <div className="navLabel">Profesionales</div>
+                </button>
+            </div>
+            <div className="navLinks1">
+                <button className="navLink" onClick={handleLoginClick}>
+                    <div className="navLabel">Iniciar Sesión</div>
+                </button>
+                <button className="navLink" onClick={handleRegisterClick}>
+                    <div className="navLabel">Registrarse</div>
+                </button>
+            </div>
+            <div className="brand">
+                <FontAwesomeIcon icon={faHeartbeat} />
+                <div className="brandname">PhysioCareSync</div>
+            </div>
+        </div>
+    );
 };
