4dd643286982de97f57c6fe938c63a02a164753c
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-12-18 14:43:54 +0000

    Routes in Layout - SignUp and Login - Import from pages - Creating files js the Login and SignUp

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,8 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import SignUp from "./pages/SignUp.js";
+import Login from "./pages/Login.js";
 
 //create your first component
 const Layout = () => {
@@ -26,6 +28,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<SignUp />} path="/signup" />
+                        <Route element={<Login />} path="/login" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
