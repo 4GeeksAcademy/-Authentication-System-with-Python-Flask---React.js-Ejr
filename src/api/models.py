d61ff20f409f545d660eb90740e12f93490f49b5
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-06 19:17:30 +0000

    primer paso con models

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,59 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(250))
+    email = Column(String(250))
+    user_name = Column(String(250))
+    city = Column(String(250))
+    country = Column(String(250))
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Games(Base):
+    __tablename__ = 'games'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    pegi = Column(Integer)
+    argument = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+     
+class Platform(Base):
+    __tablename__ = 'platform'
+    id = Column(Integer, primary_key=True)
+    company = Column(String(250))
+    console = Column(String(250))
+    country = Column(String(250))
+    games_id = Column(Integer, ForeignKey('games.id'))
+    games = relationship(Games)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Gender(Base):
+    __tablename__ = 'gender'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    games_id = Column(Integer, ForeignKey('games.id'))
+    games = relationship(Games)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
