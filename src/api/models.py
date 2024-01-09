aea075500ed35f60769181fb2666551528c8be90
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-01-09 17:10:39 +0000

    first models, no migrated

--- src/api/models.py
+++ src/api/models.py
@@ -3,9 +3,36 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from flask_sqlalchemy import SQLAlchemy
+
 
 Base = declarative_base()
+
+db = SQLAlchemy()
+
+
+class Admin(Base):
+    __tablename__ = 'admin'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(250))
+    email = Column(String(250))
+    # is_active = db.Column(db.Boolean(), unique=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+        }
+    
+class Videogame(Base):
+    __tablename__ = 'videogame'
+    videogame_id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    pegi = Column(String(250))
+    year = Column(String(50))
+    # admin_id = Column(Integer, ForeignKey('admin.id'))
+    # admin = relationship(Admin)
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -15,45 +42,29 @@ class User(Base):
     user_name = Column(String(250))
     city = Column(String(250))
     country = Column(String(250))
-
-class Games(Base):
-    __tablename__ = 'games'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(250))
-    pegi = Column(Integer)
-    argument = Column(String(250))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
      
-class Platform(Base):
-    __tablename__ = 'platform'
-    id = Column(Integer, primary_key=True)
-    company = Column(String(250))
-    console = Column(String(250))
-    country = Column(String(250))
-    games_id = Column(Integer, ForeignKey('games.id'))
-    games = relationship(Games)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-class Gender(Base):
-    __tablename__ = 'gender'
-    id = Column(Integer, primary_key=True)
-    type = Column(String(250))
-    games_id = Column(Integer, ForeignKey('games.id'))
-    games = relationship(Games)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
 
 
+# class Platform(Base):
+#     __tablename__ = 'platform'
+#     id = Column(Integer, primary_key=True)
+#     company = Column(String(250))
+#     console = Column(String(250))
+#     country = Column(String(250))
+#     games_id = Column(Integer, ForeignKey('games.id'))
+#     games = relationship(Games)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     user = relationship(User)
 
-    def to_dict(self):
-        return {}
+# class Gender(Base):
+#     __tablename__ = 'gender'
+#     id = Column(Integer, primary_key=True)
+#     type = Column(String(250))
+#     games_id = Column(Integer, ForeignKey('games.id'))
+#     games = relationship(Games)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     user = relationship(User)
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+    # def to_dict(self):
+    #     return {}
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,16 +2,17 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Videogame
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
-
+# TEST ROUTE
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
@@ -20,3 +21,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# GET ALL Videogames
+@api.route('/videogames', methods=['GET'])
+def get_all_Videogame():
+    videogame = Videogame.query.all()
+    if len(videogame) < 1:
+        return jsonify({"msg": "not found"}), 404
+    
+    response_body = list(map(lambda item: item.serialize(), get_all_Videogame))
+    
+    return jsonify(response_body), 200
+
+# CREATE VIDEOJUEGO
+@api.route('/Videogame', methods=['POST'])
+def save_videogame(videogame_id):
+    name = request.json.get("name" ,None)
+    pegi = request.json.get("pegi" ,None)
+    year = request.json.get("year" ,None)
+
+    new_videogame = Videogame(videogame_id = videogame_id)
+    db.session.add(new_videogame)
+    db.session.commit()
+
+    return jsonify("Videojuego created"), 200
