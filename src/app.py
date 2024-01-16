3b71586374d5630aa345f786bdbf33f89ce3dc32
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2024-01-16 18:14:55 -0300

    Gestion basica de usuarios
    Se agregan las acciones necesarias para poder realizar el registro, login y logout de un usuario, con contraseña cifrada y token de acceso

--- src/app.py
+++ src/app.py
@@ -6,13 +6,17 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 # from models import Person
-
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
@@ -41,22 +45,78 @@ setup_commands(app)
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-
-
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+# User management
+@app.route('/signup', methods=['POST'])
+def signup():
+    body=request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Se debe enviar informacion en el body'}),400
+    if 'email' not in body:
+        return jsonify({'msg':'Se debe ingresar email'}),400
+    if 'password' not in body:
+        return jsonify({'msg':'Se debe ingresar password'}),400
+    if 'username' not in body:
+        return jsonify({'msg': 'No se ha proporcionado username'}), 400
+    if 'name' not in body:
+        return jsonify({'msg':'Se debe ingresar password'}),400
+    if 'age' not in body:
+        return jsonify({'msg': 'No se ha proporcionado age'}), 400
+    
+    pw_hash = Bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    
+    new_user = User()
+    new_user.username = body["username"]
+    new_user.name = body["name"]
+    new_user.age = body["age"]
+    new_user.email = body["email"]
+    new_user.password = pw_hash
+    new_user.is_active = True
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'msg':'Registrado correctamente'}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Debes enviar informacion en el body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg': 'No se ha proporcionado email'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'No se ha proporcionado password'}), 400
+    user = User.query.filter_by(email=body['email']).first()
+    if user is None or not Bcrypt.check_password_hash(user.password, body['password']):
+        return jsonify({'msg': 'Usuario o contraseña incorrectos'}), 400
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'msg':'ok','token':access_token})
+
+@app.route("/private", methods=["GET"])
+@jwt_required()
+def private():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@app.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    
+    # Perform any additional logout actions here, if needed    
+    return jsonify({'msg': 'Logout successful'}), 200
 
 
 @app.route('/<path:path>', methods=['GET'])
