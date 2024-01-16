54c11d1359c4a6d628186c1f8b6fe2aad81fc963
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-01-16 19:02:23 +0000

    image background and jumbotrons

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import road66 from "../../img/road66.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="home-container">
+			<img src={road66} alt="Road 66" className="home-background" />
+			<div className="jumbotron">
+				<h1>Ever wondered what to pack? How many days to spend? What museum to visit?</h1>
+				<p>Because life is too short to cross check each travel blog..</p>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+			<div className="jumbotron" style={{ top: '100%' }}>
+				<h1>DioDio's got you covered!</h1>
+			</div>
 		</div>
 	);
 };
