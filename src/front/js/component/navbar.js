86b60b235f6ca295edbd1e68a84b74d5debd1b7f
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-01-16 16:59:21 +0000

    Added our brand new logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
+import logoOrange from "../../img/logoOrange.png";
+
 export const Navbar = () => {
   return (
     <>
-          <nav className="navbar navbar fixed-top">
-        <div className="container-fluid">
+          <nav className="navbar navbar fixed-top p-0">
+        <div className="container-fluid p-1">
         <a class="navbar-brand" href="#">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap" viewBox="0 0 16 16">
-  <path d="M5.062 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23zm0 3.762V8.162h1.822c1.236 0 1.887.463 1.887 1.348 0 .896-.627 1.377-1.811 1.377z"/>
-  <path d="M0 4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4zm4-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V4a3 3 0 0 0-3-3z"/>
-</svg>
+        <img src={logoOrange} />
     </a>
-          <a className="navbar-brand" href="#">
-            DIODIO
-          </a>
-          
           
           <button
             className="navbar-toggler "
@@ -59,7 +54,7 @@ export const Navbar = () => {
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="#">
-                    Sign In
+                    Sign Up
                   </Link>
                 </li>
               </ul>
