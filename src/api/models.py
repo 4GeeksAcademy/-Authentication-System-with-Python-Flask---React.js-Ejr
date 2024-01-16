baaf60b662fb4a32b37c96dd5676de6cd788f869
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2024-01-16 18:11:27 -0300

    User Table MOD
    Se agregan campos para una mejor seguridad en la aplicacion. Como son username, name y age

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +17,9 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "name": self.name,
+            "age": self.age,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
