1c4b8b9eae73fceed4866459945989c5d3629624
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-01-19 19:46:16 +0000

    added link to signup

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -28,6 +28,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Signup />} path="/signup" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
