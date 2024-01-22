1b68a33768e94664e9df4b8d8b8605e000c2a814
Stefi <144570099+stefi1211@users.noreply.github.com>
2024-01-22 21:42:53 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,43 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context)
+
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-dark mb-3 px-5">
+			<Link className="navbar-brand" to="/">
+
+				<p className=" m-1">
+					<img src={logo} style={{ height: "40px" }} />
+				</p>
+
+			</Link>
+			<div className="ml-auto">
+
+
+				
+				<div className="dropdown">
+				<a class="nav-link dropdown-toggle align-items-center" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Mi Lista <div className="bg-danger px-1 mx-1 rounded d-flex justify-content-center">{store.contador}</div>
+				</a>
+
+					<ul className="dropdown-menu">
+
+							<li>
+								Agrega películas!
+
+							</li>
+
+
+					</ul>
+
 				</div>
+
 			</div>
 		</nav>
 	);
