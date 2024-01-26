8a751376000961c729697acd2b0325b0c77de299
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2024-01-26 18:14:15 +0000

    Creation of the jwt, need to create secret key aswell as API

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,76 +1,81 @@
-from flask import Flask, render_template, request, jsonify
-
-#Need to create a website that takes all the important information for an event
-#Do I need to secure it, because only promoters should be able to access to it
-
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-#create 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
-class Event(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(100), nullable=False)
-    event_address = db.Column(db.String(200), nullable=False)
-    ticket_price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    event_image_path = db.Column(db.String(200), nullable=False)
-    #Nice to have a follow buttom for this promoter
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    is_promoter = db.Column(db.Boolean, default=False)
 
-#Create the table where the information will be save
 db.create_all()
 
-# API endpoint for event creation 
-#
-@app.route('/api/create_event', methods=['POST'])
-def create_event():
-    title = request.form.get('event_name')
-    address = request.form.get('event_address')
-    ticket_price = float(request.form.get('ticket_price'))
-    description = request.form.get('description')
-    image = request.files.get('event_image')
+# Registration endpoint
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.json
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    password = data.get('password')
+    is_promoter = data.get('is_promoter', False)
 
-    # The image path is being save so it can be display in the front
-    image_path = f'static/images/{image.filename}'
-    image.save(image_path)
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'Email is already registered'}), 400
 
-    # Create a new event in the database
-    new_event = Event(title=title, address=address, ticket_price=ticket_price,
-                      description=description, image_path=image_path)
-    db.session.add(new_event)
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(first_name=first_name, last_name=last_name, email=email,
+                    password=hashed_password, is_promoter=is_promoter)
+    db.session.add(new_user)
     db.session.commit()
 
-#message for the user that the event was created succesfully, maybe we need a modal for this 
-    return jsonify({'message': 'Event created successfully'}), 201
+    return jsonify({'message': 'User registered successfully'}), 201
 
-#Delete function for the promoter to delete their 
-def delete_event(event_id):
-    event = Event.query.get(event_id)
-    if event:
-        # Remove the event from the database
-        db.session.delete(event)
-        db.session.commit()
-        return jsonify({'message': 'Event deleted successfully'}), 200
-    else:
-        return jsonify({'message': 'Event not found'}), 404
-        @app.route('/api/edit_event/<int:event_id>', methods=['PUT'])
-def edit_event(event_id):
-    event = Event.query.get(event_id)
-    if event:
-        # Update event details
-        updated_event_data = request.json
-        event.event_name = updated_event_data.get('event_name', event.event_name)
-        event.event_address = updated_event_data.get('event_address', event.event_address)
-        event.ticket_price = updated_event_data.get('ticket_price', event.ticket_price)
-        event.description = updated_event_data.get('description', event.description)
+# Login endpoint
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
 
-        db.session.commit()
-        return jsonify({'message': 'Event updated successfully', 'event': event}), 200
+    user = User.query.filter_by(email=email).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity=user.id, expires_delta=False)
+        return jsonify({'access_token': access_token, 'is_promoter': user.is_promoter}), 200
     else:
-        return jsonify({'message': 'Event not found'}), 404
+        return jsonify({'message': 'Invalid email or password'}), 401
+
+# Protected endpoint for promoters
+@app.route('/api/create_event', methods=['POST'])
+@jwt_required()
+def create_event():
+    current_user_id = get_jwt_identity()
+    current_user = User.query.get(current_user_id)
+
+    if current_user and current_user.is_promoter:
+        return jsonify({'message': 'Event created successfully'}), 201
+    else:
+        return jsonify({'message': 'Permission denied'}), 403
+
+# Protected endpoint for regular users
+@app.route('/api/view_events', methods=['GET'])
+@jwt_required()
+def view_events():
+    return jsonify({'message': 'Viewing events'}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
