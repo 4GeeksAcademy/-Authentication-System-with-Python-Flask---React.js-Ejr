067d9e522c40025e00ec56f03f66ff09e64a33f2
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2024-01-27 21:44:52 +0000

    fiz is_admin migration

--- src/api/models.py
+++ src/api/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     is_superuser = db.Column(db.Boolean(), unique=False, default=False)
-    is_admin = db.Columm(db.Boolean(), unique=False, default=False) 
+    is_admin = db.Column(db.Boolean(), unique=False, default=False) 
 
     def __repr__(self):
         return f'<User {self.email}>'
