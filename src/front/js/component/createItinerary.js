e77e36520877ffb3cb8152fc8ffede1772edd845
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-01-29 17:10:32 +0000

    generated itinerary

--- src/front/js/component/createItinerary.js
+++ src/front/js/component/createItinerary.js
@@ -5,26 +5,50 @@ import avatar1 from "../../img/avatar1.png";
 
 
 const CreateItinerary = () => {
-
   const [questions, setQuestions] = useState([
     'Where do you want to go?',
     'How many people are there in your group?',
     'How many days do you plan to stay?',
-    'What time of the year would you like to go?'
-
+    'What time of the year would you like to go?',
+    'What are your interests?',
+    'What is your level of fitness?',
+    'Please indicate your dietary preferences?',
+    'And finally.. your daily budget?',
   ]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [userAnswer, setUserAnswer] = useState([]);
-
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [generatedItinerary, setGeneratedItinerary] = useState(null);
 
   const handleAnswerInput = (e) => {
-    setUserAnswer(e.target.value);
+    setUserAnswers(e.target.value);
   };
 
-  const askNextQuestion = () => {
+  const askNextQuestion = async () => {
+    setUserAnswers([...userAnswers, userAnswers]);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setUserAnswers('');
 
+    if (currentQuestionIndex === questions.length - 1) {
 
-  }
+      const response = await fetch('/createItinerary', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({userAnswers}),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+
+        setGeneratedItinerary(result);
+      } else {
+
+        console.error('Error generating itinerary:', result.error);
+      }
+    }
+  };
 
 
 
@@ -45,13 +69,22 @@ const CreateItinerary = () => {
                 type='text'
                 id='answerInput'
                 placeholder='Your answer'
-                value={userAnswer}
+                value={userAnswers}
                 onChange={handleAnswerInput}></input>
               <button onClick={askNextQuestion}>Next Question</button>
             </div>
           </div>
           <div className='answer-box'>
-            <div className='answer-item'> AI Answer </div>
+            <div className='answer-item'>
+            {generatedItinerary ? (
+              <div>
+                <h3>Generated Itinerary</h3>
+                <p>{generatedItinerary}</p>
+              </div>
+            ) : (
+              'AI Answer'
+            )}
+            </div>
           </div>
         </div>
       </div>
