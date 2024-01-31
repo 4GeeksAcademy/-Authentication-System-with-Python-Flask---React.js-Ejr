7781739a09b47994f38baaab03b22c3bf73b5af2
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-01-31 19:07:38 +0000

    Added some new functionality and styling

--- src/front/js/component/privatePageComponent.js
+++ src/front/js/component/privatePageComponent.js
@@ -4,6 +4,8 @@ import BasicInfo from "./PrivatePage/basicInfo.js";
 import { Link } from 'react-router-dom';
 import Password from './PrivatePage/password.js';
 
+
+
 const PrivatePage = ({ user }) => {
     const [activeTab, setActiveTab] = useState('basic-info');
 
@@ -77,21 +79,19 @@ Basic Info
                                 Achievements
                             </a>
                         </li>
-                        <li className="nav-item" id="createIt">
-                            <a  
-                                id='pixel'
-                                className={`nav-link ${activeTab === 'itinerary' ? 'active' : ''}`}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    handleTabChange('itinerary');
-                            }}
-                                href="#itinerary"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-airplane" viewBox="0 0 16 16">
-  <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849m.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1s-.458.158-.678.599"/>
-</svg>
-                                Create Itinerary
-                            </a>
+                        <li className="nav-item" id="createIt" >
+                           <Link className="" to="/createItinerary">
+                                <button  
+                                    id='pixel'
+                                    className="nav-link"
+                                    
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-airplane" viewBox="0 0 16 16">
+    <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849m.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1s-.458.158-.678.599"/>
+    </svg>
+                                    Create Itinerary
+                                </button >
+                            </Link>
                         </li>
                     </ul>
                 </div>
