2f9ceaccea9102f4deaf9cd01fd94c7c67652dac
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-02-01 14:59:30 +0000

    nothing works

--- src/front/js/component/signUpForm.js
+++ src/front/js/component/signUpForm.js
@@ -9,15 +9,15 @@ const SignUpForm = () => {
     const [password, setPassword] = useState('');
     //confirm password logic
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState ('')
+    const [errorMessage, setErrorMessage] = useState('')
     //show password
     const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         if (password !== confirmPassword) {
-            setErrorMessage ('The passwords do not match')
+            setErrorMessage('The passwords do not match')
             return;
         }
 
@@ -40,7 +40,7 @@ const SignUpForm = () => {
 
         <form onSubmit={handleSubmit} className="form-container">
             <div className="page-title">Sign Up</div>
-            
+
             <div className="form-group">
                 <label className="label">First Name:</label>
                 <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="input" />
@@ -62,20 +62,20 @@ const SignUpForm = () => {
                 <input type={showPassword ? 'text' : 'password'} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="input" />
             </div>
             {errorMessage && <div className="error-message">{errorMessage}</div>}
-            <div className="eye-icon-container" > 
+            <div className="eye-icon-container" >
                 <div className="text pe-2"> Show Passwords  </div>
-                    <div className="eye" id="eye1">
-                        <button onClick={(e) => { e.preventDefault(); setShowPassword(!showPassword); }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
-                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-                            </svg>
-                        </button>
-                        
+                <div className="eye" id="eye1">
+                    <button onClick={(e) => { e.preventDefault(); setShowPassword(!showPassword); }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                        </svg>
+                    </button>
+
                 </div>
             </div>
             <button type="submit" className="submit-button">Submit</button>
-            
+
         </form>
     );
 };
