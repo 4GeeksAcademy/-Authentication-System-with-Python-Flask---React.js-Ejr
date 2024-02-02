f863f2fa688a3e3d727f20be88f026e9048ce6e9
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2024-02-02 19:11:55 -0300

    Actualizacion
    Se conecta con el flux y se agrega la posibilidad de mostrar u ocultar la pass

--- src/front/js/pages/signUp.js
+++ src/front/js/pages/signUp.js
@@ -1,48 +1,99 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 
-const SignUp = () => {
-  const { actions, store } = useContext(Context); // Agrega store al contexto
+export const SignUp = () => {
+  const { actions, store } = useContext(Context);
 
-  const [email, setEmail] = useState('');
-  const [nombre, setNombre] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State to track password visibility
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false); // State to track repeatPassword visibility
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // Llama a la acción del contexto para crear una cuenta
-      await actions.createAccount(email, nombre, password);
+      if (password === repeatPassword) {
+        await actions.signup(email, username, name, age, password);
 
-      // Limpiar inputs
-      setEmail('');
-      setNombre('');
-      setPassword('');
-      setRepeatPassword('');
+        // Limpiar inputs
+        setEmail("");
+        setUsername("");
+        setName("");
+        setAge("");
+        setPassword("");
+        setRepeatPassword("");
+      } else {
+        alert("Password doesn't match");
+      }
 
       // Accede al mensaje del estado global y muestra el mensaje si existe
       if (store.message) {
         console.log("Mensaje del estado global:", store.message);
       }
     } catch (error) {
-      console.error('Error al enviar los datos:', error);
+      console.error("Error al enviar los datos:", error);
     }
   };
 
   return (
-    <div className='CrearCuenta m-5'>
-      <h1 className='mb-3'>Crear Cuenta</h1>
-      <a>Bienvenido! Ingresa tus datos y comienza a disfrutar de increíbles películas y series </a>
+    <div className="CrearCuenta container m-5">
+      <h1 className="mb-3">Crear Cuenta</h1>
+      <a>
+        Bienvenido! Ingresa tus datos y comienza a disfrutar de increíbles
+        películas y series{" "}
+      </a>
       <form onSubmit={handleSubmit}>
+        <div id="is-relative">
+          <label>
+            <span id="icon">
+              <i className="fa-regular fa-user"></i>
+            </span>
+            <input
+              className="form-control inputCuenta"
+              placeholder="Nombre"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <br />
 
         <div id="is-relative">
           <label>
             <span id="icon">
               <i className="fa-regular fa-user"></i>
             </span>
-            <input className="form-control inputCuenta" placeholder="Nombre" type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <input
+              className="form-control inputCuenta"
+              placeholder="Nombre de usuario"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <br />
+
+        <div id="is-relative">
+          <label>
+            <span id="icon">
+              <i className="fa-regular fa-id-card"></i>
+            </span>
+            <input
+              className="form-control inputCuenta"
+              placeholder="Edad"
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
           </label>
         </div>
 
@@ -53,7 +104,13 @@ const SignUp = () => {
             <span id="icon">
               <i className="fa-regular fa-envelope"></i>
             </span>
-            <input className="form-control inputCuenta" placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input
+              className="form-control inputCuenta"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
         </div>
 
@@ -62,9 +119,20 @@ const SignUp = () => {
         <div id="is-relative">
           <label>
             <span id="icon">
-              <i className="fa-regular fa-eye"></i>
+              <i
+                className={
+                  showPassword ? "fa-regular fa-eye-slash" : "fa-regular fa-eye"
+                }
+                onClick={() => setShowPassword(!showPassword)}
+              ></i>
             </span>
-            <input className="form-control inputCuenta" placeholder="Contraseña" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input
+              className="form-control inputCuenta"
+              placeholder="Contraseña"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
         </div>
 
@@ -73,25 +141,43 @@ const SignUp = () => {
         <div id="is-relative">
           <label>
             <span id="icon">
-              <i className="fa-regular fa-eye"></i>
+              <i
+                className={
+                  showRepeatPassword
+                    ? "fa-regular fa-eye-slash"
+                    : "fa-regular fa-eye"
+                }
+                onClick={() => setShowRepeatPassword(!showRepeatPassword)}
+              ></i>
             </span>
-            <input className="form-control inputCuenta" placeholder="Repetir contraseña" type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
+            <input
+              className="form-control inputCuenta"
+              placeholder="Repetir contraseña"
+              type={showRepeatPassword ? "text" : "password"}
+              value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+            />
           </label>
         </div>
 
         <br />
 
-        <button type="submit" className="btn btnCrearCuenta"> Crear Cuenta </button>
+        <button type="submit" className="btn btnCrearCuenta">
+          {" "}
+          Crear Cuenta{" "}
+        </button>
       </form>
 
-      {store.message && <p className='error'>{store.message}</p>}
+      {store.message && <p className="error">{store.message}</p>}
 
-      <div className='link'>
-        <a>Ya tienes una cuenta? <a className='linkIngresar' href="/login">Ingresa Aquí</a> </a>
+      <div className="link">
+        <a>
+          Ya tienes una cuenta?{" "}
+          <a className="linkIngresar" href="/login">
+            Ingresa Aquí
+          </a>{" "}
+        </a>
       </div>
     </div>
   );
 };
-
-export default SignUp;
-
