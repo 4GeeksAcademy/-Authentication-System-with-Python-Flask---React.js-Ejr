7e4c43df1eab43cc5fbfb6a6480acc4302002b3f
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2024-02-02 20:26:47 +0000

    sets checkbox to only be acessed by logeed in users

--- src/front/js/component/eventInfoBox.js
+++ src/front/js/component/eventInfoBox.js
@@ -5,20 +5,28 @@ import { useState, useEffect } from "react";
 
 export const EventInfoBox = () => {
     const [eventDetails, setEventDetails] = useState({});
+    const [clicksCount, setClicksCount] = useState(0);
     const adminRouteRequirement = "/api/events";
     const url = `${process.env.BACKEND_URL}${adminRouteRequirement}`;
 
     useEffect(() => {
-        fetch(url)
+        const clicksApiUrl = `${process.env.BACKEND_URL}/api/clicks`;
+        fetch(clicksApiUrl)
             .then(response => response.json())
-            .then(data => setEventDetails(data));
+            .then(newData => setClicksCount(newData))
+            .catch(error => console.error('Error:', error));
     }, []);
 
     const handleCheckboxChange = () => {
+        const token = localStorage.getItem('userToken');
+        if (!token) {
+        alert("Please log in to use the checkbox");
+        return;
+    }
         const adminRouteRequirement = "/api/clickupdate";
         const url = `${process.env.BACKEND_URL}${adminRouteRequirement}`;
         const checkboxState = document.querySelector('#myCheckbox').checked ? 'on' : 'off';
-
+    
         fetch(url, {
             method: 'PUT',
             headers: {
@@ -27,9 +35,15 @@ export const EventInfoBox = () => {
             body: JSON.stringify({ checkbox_state: checkboxState })
         })
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(() => {
+            const clicksApiUrl = `${process.env.BACKEND_URL}/api/clicks`;
+            fetch(clicksApiUrl)
+                .then(response => response.json())
+                .then(newData => setClicksCount(newData))
+                .catch(error => console.error('Error:', error));
+        })
+        .catch(error => console.error('Error:', error));
     }
-
     return (
         <div className="EventCard">
             <h3>Upcoming events:</h3>
@@ -39,8 +53,8 @@ export const EventInfoBox = () => {
             <p>Meeting Point: {eventDetails.meeting_point}</p>
             <label>
                 <input type="checkbox" id="myCheckbox" name="myCheckbox" onChange={handleCheckboxChange} />
-                Count me in
-                </label>
+                Count me in<p>Smilers in: {clicksCount}</p>
+            </label>
         </div>
     );
 };
