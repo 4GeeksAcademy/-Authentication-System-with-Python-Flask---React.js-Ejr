9015581902a02fe28b78dc91f83cc97ad9c44d01
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2024-02-05 19:14:01 +0000

    backend image and message

--- src/api/utils.py
+++ src/api/utils.py
@@ -45,8 +45,8 @@ def generate_sitemap(app):
     return (
         """
         <div style="text-align: center;">
-        <img style="max-height: 80px" src='https://storage.googleapis.com/breathecode/boilerplates/rigo-baby.jpeg' />
-        <h1>Rigo welcomes you to your API!!</h1>
+        <img style="max-height: 80px" src='https://i.pinimg.com/originals/94/b6/84/94b6843382aad570a497bb5884b216ca.jpg' />
+        <h1>Welcome to the DioDio Travel Assistant!</h1>
         <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
         <p>Start working on your project by following the <a href="https://start.4geeksacademy.com/starters/full-stack" target="_blank">Quick Start</a></p>
         <p>Remember to specify a real endpoint path like: </p>
