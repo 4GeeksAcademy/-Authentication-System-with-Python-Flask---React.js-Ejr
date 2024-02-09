31d61bf789bd9c8886d547de14106f08ea5a4bae
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2024-02-09 18:19:49 +0000

    changed the logo and added a link to it. Missing the live counting and the donations link

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../img/logo.jpg";
+import logo from "../../img/logo.png";
 
 export const NavBar = () => {
 	return (
 			<nav className="nav">
 				<div className="container nav__menu">
-					<img src={logo} className="nav__logo"/>
+					<a href="https://improved-robot-g4q4pvg7xr9xcwjvx-3000.app.github.dev/">
+						<img src={logo} className="nav__logo"/>
+					</a>
 					<Link to="/signuplogin" className="nav__btn round">
 						<span>CLEANERS</span>
 					</Link>
