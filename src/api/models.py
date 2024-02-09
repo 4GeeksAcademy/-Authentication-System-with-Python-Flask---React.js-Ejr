fe42c61ce93bdb5d3abaa887cd30c7a48a612758
Erika Scott <143569907+EScott237@users.noreply.github.com>
2024-02-09 21:31:44 +0000

    Model Updates

--- src/api/models.py
+++ src/api/models.py
@@ -14,6 +14,63 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
+            # Note: Password is intentionally omitted for security reasons.
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('favorites', lazy=True))
+
+    def __repr__(self):
+        return f'<Favorite {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id
+        }
+
+class Exhibits(db.Model):
+    __tablename__ = 'exhibits'
+    id = db.Column(db.Integer, primary_key=True)
+    exhibit_name = db.Column(db.String(250), nullable=False)
+    favorite_id = db.Column(db.Integer, db.ForeignKey('favorites.id'), nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=True)
+    favorite = db.relationship('Favorite', backref=db.backref('exhibits', lazy=True))
+    artist = db.relationship('Artist', backref=db.backref('exhibits', lazy=True))
+
+    def __repr__(self):
+        return f'<Exhibit {self.exhibit_name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "exhibit_name": self.exhibit_name,
+            "favorite_id": self.favorite_id,
+            "artist_id": self.artist_id
+        }
+    
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.Integer, nullable=True)
+    death_year = db.Column(db.Integer, nullable=True)
+    nationality = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return f'<Artist {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "death_year": self.death_year,
+            "nationality": self.nationality
         }
