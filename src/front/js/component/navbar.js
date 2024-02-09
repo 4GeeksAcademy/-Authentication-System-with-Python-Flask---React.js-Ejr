c7e6249875fa2fe16be5af82a0e933292e6361fb
Erika Scott <143569907+EScott237@users.noreply.github.com>
2024-02-09 23:29:34 +0000

    Added navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
-};
+class Navbar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/#">
+              eCommerce
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/dashboard" className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/customers" className="nav-link">
+                    Customers
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/cart" className="nav-link">
+                    Shopping Cart
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Navbar;
