ca458f18be89f2091bf6cc1e39f477e68a504deb
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2024-02-10 01:53:41 +0000

    Moved Home Page to Exhibits

--- src/front/js/pages/exhibits.js
+++ src/front/js/pages/exhibits.js
@@ -8,7 +8,21 @@ export const Exhibits = () => {
 
 	return (
 		<div className="text-center mt-5">
-			This is the Exhibits Page
+			<div className="row1"> 
+				<div className="art-poster-1">1</div>
+				<div className="art-poster-2">2</div>
+				<div className="art-poster-3">3</div>
+			</div>
+			<div className="row2">
+				<div className="art-poster-4">4</div>
+				<div className="art-poster-5">5</div>
+				<div className="art-poster-6">6</div>
+			</div>
+			<div className="row3">
+				<div className="art-poster-7">7</div>
+				<div className="art-poster-8">8</div>
+				<div className="art-poster-9">9</div>
+			</div>
 		</div>
 	);
 };
