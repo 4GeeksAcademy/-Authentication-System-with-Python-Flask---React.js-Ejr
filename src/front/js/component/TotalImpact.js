21a580a4f6a8ddb3448591b9dc23d6aa193e409a
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2024-02-11 19:58:53 +0000

    sets the total impact to be showed on homepage

--- src/front/js/component/TotalImpact.js
+++ src/front/js/component/TotalImpact.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
+import image1 from "../../img/image1.jpg";
+import image2 from "../../img/image2.jpg";
+import image3 from "../../img/image3.jpg";
 
 export const TotalImpact = () => {
     const { store, actions } = useContext(Context);
@@ -24,10 +27,28 @@ export const TotalImpact = () => {
     return isLoading ? (
         <p>Smile.. total impact is uploading</p>
     ) : (
-        <div>
-            <p>Total Users: {store.total_users}</p>
-            <p>Total Time Spent: {store.total_impact_time}</p>
-            <p>Total Liters: {store.total_impact_liters}</p>
-        </div>
+        <div className="info container mt-5 mb-5 overflow-hidden">
+				<h2><strong>OUR IMPACT IN THE WORLD</strong></h2>
+				<div className="info__bubbles">
+					<div className="info__bubble">
+						<div className="info__blob">
+							<strong>{store.total_users}</strong>cleaners registered on our platform
+						</div>
+						<img className="info__img" src={image1} />
+					</div>
+					<div className="info__bubble">
+						<div className="info__blob">
+							<strong>{store.total_impact_liters}</strong>liters collected from our shores
+						</div>
+						<img className="info__img" src={image2} />
+					</div>
+					<div className="info__bubble">
+						<div className="info__blob">
+							<strong>{store.total_impact_time}</strong>time dedicated to the cause
+						</div>
+						<img className="info__img" src={image3} />
+					</div>
+				</div>
+			</div>
     );
 };
