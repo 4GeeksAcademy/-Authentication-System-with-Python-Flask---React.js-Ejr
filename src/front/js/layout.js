6383271cc78aaa2ca3ea919c424945dfa50462e2
cdavis2223 <cdavis2223@gmail.com>
2024-02-13 15:59:39 +0000

    First login change

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -30,6 +30,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Login />} path="/login" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
