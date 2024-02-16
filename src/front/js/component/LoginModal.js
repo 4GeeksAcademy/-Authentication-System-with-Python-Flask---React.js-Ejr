6e9ca42254c0688e151defef12b21e7aff55479d
SergioMontoro <84936651+SergioMontoro@users.noreply.github.com>
2024-02-16 20:09:25 +0000

    commit para pull

---
+++ src/front/js/component/LoginModal.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
+
+export const LoginModal = props => {
+
+	const {store, actions} = useContext(Context);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function sentFormInfo() {
+		actions.logIn(email, password);
+	}
+
+	return (
+		<div className="modal" tabIndex="-1" role="dialog" style={{ display: "inline-block"}}>
+			<div className="modal-dialog" role="document">
+
+				<div className="modal-content">
+
+					<div className="modal-header d-flex flex-column">
+						<div className="d-flex flex-row justify-content-end w-100">
+							<i className="fa-solid fa-xmark"></i>
+						</div>
+						<h4 className="modal-title">Inicia sesión</h4>
+						<h6>¿Aún no eres miembro? Regístrate.</h6>
+						{/* {props.onClose ? (
+							<button
+								onClick={() => props.onClose()}
+								type="button"
+								className="close"
+								data-dismiss="modal"
+								aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						) : (
+							""
+						)} */}
+					</div>
+
+					<div className="modal-body w-100">
+						<form className="d-flex flex-column justify-content-center align-items-center w-100">
+							<div className="form-group d-flex flex-column justify-content-center align-items-center w-100">
+								<label className="d-flex justify-content-start w-100">Correo electrónico</label>
+								<input
+									className="w-100 rounded-3"
+									type="text"
+									onChange={e => {setEmail(e.target.value)}}
+								/>
+							</div>
+
+							<div className="form-group d-flex flex-column justify-content-center align-items-center w-100">
+								<label className="d-flex justify-content-start w-100">Contraseña</label>
+								<input
+									className="w-100 rounded-3"
+									type="password"
+									onChange={e => {setPassword(e.target.value)}}
+								/>
+							</div>
+	
+							<div className="form-check d-flex flex-row justify-content-start w-100">
+								<input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+								<label className="form-check-label" for="flexCheckDefault">
+								Mantener sesión iniciada
+								</label>
+							</div>
+							<button type="submit" className="btn btn-primary form-control" onClick={() => sentFormInfo()}>
+								Inicia sesión
+							</button>
+						</form>
+					</div>
+
+					{/* <div className="modal-footer">
+						<button
+							onClick={() => {
+								props.onClose();
+							}}
+							type="button"
+							className="btn btn-primary">
+							Oh no!
+						</button>
+						<button
+							onClick={() => {
+								handleDelete();
+								props.onClose();
+							}}
+							type="button"
+							className="btn btn-secondary"
+							data-dismiss="modal">
+							Do it!
+						</button>
+					</div> */}
+
+				</div>
+			</div>
+		</div>
+	);
+};
+/**
+ * Define the data-types for
+ * your component's properties
+ **/
+LoginModal.propTypes = {
+	history: PropTypes.object,
+	onClose: PropTypes.func,
+	show: PropTypes.bool,
+	id: PropTypes.number
+};
+
+/**
+ * Define the default values for
+ * your component's properties
+ **/
+LoginModal.defaultProps = {
+	show: false,
+	onClose: null
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,24 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			user: {}
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			logIn: (email, password) => {
+				console.log(email, password);
+				fetch(BACKEND_URL + "/api/login", {
+					method: "POST",
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					}),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => {
+						console.log(response);
+						return response.json();
+					})
+					// .then(data => console.log(data)) guardar en localStore
+					.catch(error => console.log(error));
 			},
 
 			getMessage: async () => {
@@ -33,20 +36,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
