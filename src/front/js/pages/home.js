a6afbb5cacc637d1234a5c25ea320daa4c5216bd
Aleska134 <143849441+Aleska134@users.noreply.github.com>
2024-02-17 02:12:51 +0000

    add  jumbotron in home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,7 +9,7 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<div>
+			<div className="jumbotron">
 				<img src="../../img/jumbotron.png"></img>
 			</div>
 			<div className="history-title"><h3>HISTORY OF THE MET</h3></div>
