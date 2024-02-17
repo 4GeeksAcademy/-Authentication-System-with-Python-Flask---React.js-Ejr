4c0418fffa9d3c790987c0e91a2972366268b01c
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-02-17 16:38:30 +0000

    FEAT models with relationships

--- src/api/models.py
+++ src/api/models.py
@@ -10,9 +10,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     role = db.Column(db.String(120), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    # id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'))
-    # subscription = db.relationship('Subscription', backref='user', lazy=True)
+
+    id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'))
+    subscription = db.relationship('Subscription', backref='user', lazy=True)
+    subscription_start_date = db.Column(db.String(120), unique=False, nullable=False)
+    subscription_end_date = db.Column(db.String(120), unique=False, nullable=True)
+
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.id}>'
@@ -25,3 +29,118 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Subscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plan = db.Column(db.String(120), unique=True, nullable=False)
+    price = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Subscription {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "plan": self.plan,
+            "price": self.price,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Testimony(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.String(120), unique=True, nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user =  db.relationship('User', backref='testimony', lazy=True)
+    
+    # id_session = db.Column(db.Integer, db.ForeignKey('session.id'))
+
+
+
+    def __repr__(self):
+        return f'<Testimony {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
+
+class Types_of_session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Types_of_session {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    subtitle = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    link = db.Column(db.String(120), unique=True, nullable=False)
+    asana_focus = db.Column(db.String(120), unique=False, nullable=False)
+    level = db.Column(db.String(120), unique=False, nullable=False)
+    duration = db.Column(db.String(120), unique=False, nullable=False)
+    # type = db.Column(db.String(120), unique=False, nullable=False) #hacer enum?
+    id_type_of_session = db.Column(db.Integer, db.ForeignKey('types_of_session.id'))
+    type = db.relationship('Types_of_session', backref='sessions', lazy=True)
+
+    id_instructor = db.Column(db.Integer, db.ForeignKey('instructor.id'))
+    instructor_name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Session {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "subtitle": self.subtitle,
+            "description": self.description,
+            "link": self.link,
+            "asana_focus": self.asana_focus,
+            "level": self.level,
+            "duration": self.duration,
+            "type": self.type,
+            "instructor": self.instructor
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Instructor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    biografy = db.Column(db.String(300), unique=False, nullable=False)
+    sessions =  db.relationship('Session', backref='instructor', lazy=True)
+
+
+    def __repr__(self):
+        return f'<Instructor {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "biografy": self.biografy,
+            # do not serialize the password, its a security breach
+        }
+    
+
