c40f6524ce920d9182807d2655f8a3c29f97e0bf
omar saadane el meski <o_omar_96@hotmail.com>
2024-02-17 20:53:57 +0000

    he añadido los componentes falta estilizar

--- src/front/js/component/formulario.js
+++ src/front/js/component/formulario.js
@@ -6,31 +6,33 @@ export const Formulario = () => {
     const { store, actions } = useContext(Context)
     return (
 
-        <form className="bg-warning bg-gradient">
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Suscriptions</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-            </ul>
-            <div className="card-body text-center col-sm-6">
-                <h1>Login Om</h1>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">.</div>
+        <form className="d-flex flex-column align-items-center" style={{ backgroundColor: "#FFF6E9" }}>
+            <div className="text-center">
+                <div className="card-body">
+                    <h1>Login Om</h1>
+                    <div className="mb-1 text-start">
+                        <label for="exampleInputEmail1" className="form-label">Email/Username</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text"></div>
+                    </div>
+                    <div className="mb-1 text-start">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-50 mt-3">Login</button>
                 </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                <div className="m-auto">
+                <p><a href="#" class="link-primary">Don't have an acount?</a></p>
                 </div>
-                <button type="submit" className="btn btn-primary">Login</button>
+            </div>
+            <div>
+                <p>Follow us</p>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-facebook"></i>
             </div>
         </form>
     );
 };
+{/* style={{ fontFamily: "'Poiret One', sans-serif" }} (esta es la fuente de login om) */ }
+{/* <div src="https://www.sattology.com/wp-content/uploads/2020/06/simbolo-do-om-ornamental_1058-101.jpg"></div>  ((((esta es la foto de omkara que va en la derecha)))*/ }
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,10 +8,22 @@ export const Navbar = () => {
 			<div className="container">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">Ocean Of Om</span>
+					<p>Yoga Hub</p>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
+						<ul class="nav">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Suscriptions</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Contact Us</a>
+							</li>
+						</ul>
 					</Link>
 				</div>
 			</div>
