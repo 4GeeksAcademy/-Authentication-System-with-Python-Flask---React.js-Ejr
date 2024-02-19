f62f7b6162287d2601306522066cabee8cc0f898
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-02-19 00:03:50 +0000

    WIP adding modeling with endpoints (done: login, signup, session, user)

--- src/api/models.py
+++ src/api/models.py
@@ -10,11 +10,11 @@ class User(db.Model):
     date_of_birth = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    role = db.Column(db.Enum('Student', 'Instructor', name='role'), unique=False, nullable=False)
+    role = db.Column(db.Enum('Student', 'Instructor', name='role'), unique=False, nullable=True)
 
     id_subscription = db.Column(db.Integer, db.ForeignKey('subscription.id'))
     subscription = db.relationship('Subscription', backref='user', lazy=True)
-    subscription_start_date = db.Column(db.String(120), unique=False, nullable=False)
+    subscription_start_date = db.Column(db.String(120), unique=False, nullable=True)
     subscription_end_date = db.Column(db.String(120), unique=False, nullable=True) #Hacer funcion para si el usuario se da de baja, retorne en description_end_date la fecha?
 
     # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -110,6 +110,7 @@ class Instructor(db.Model):
     
 
 class Session(db.Model):
+    # __tablename__ = 'session'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     subtitle = db.Column(db.String(120), unique=False, nullable=False)
@@ -119,8 +120,8 @@ class Session(db.Model):
     level = db.Column(db.String(120), unique=False, nullable=False)
     duration = db.Column(db.String(120), unique=False, nullable=False)
     id_type_of_session = db.Column(db.Integer, db.ForeignKey('types_of_session.id'))
-    type = db.relationship('Types_of_session', backref='sessions', lazy=True)
-    instructor = db.relationship('Instructor', backref='sessions', lazy=True)
+    type = db.relationship('Types_of_session', backref='session', lazy=True)
+    instructor = db.relationship('Instructor', backref='session', lazy=True)
 
     id_instructor = db.Column(db.Integer, db.ForeignKey('instructor.id'))
 
@@ -137,11 +138,9 @@ class Session(db.Model):
             "asana_focus": self.asana_focus,
             "level": self.level,
             "duration": self.duration,
-            "type": self.type,
-            "instructor": self.instructor
+            "type": self.type.serialize(), #Para serializar el modelo types_of_session. Si no da error al ser otro modelo
+            "instructor": self.instructor.serialize()
+            #Para serializar el modelo instructor. Si no da error al ser otro modelo.
             # do not serialize the password, its a security breach
         }
     
-
-
-
