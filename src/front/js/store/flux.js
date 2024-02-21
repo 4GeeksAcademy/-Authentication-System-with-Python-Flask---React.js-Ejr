7c6fde7ed0e261764840e1ef8519c90d577bd80f
Erika Scott <143569907+EScott237@users.noreply.github.com>
2024-02-21 23:27:24 +0000

    latest updates

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,20 +6,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 			artDepartments: [],
 		},
 		actions: {
-		// getObjects: () => {
-		// 	for (let i = 1; i < 25; i++){
-		// 		fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/"+i)
-		// 		.then(response => response.json())
-		// 		.then(data => {
-		// 			let store = getStore()
-		// 			if (data.objectName){
-		// 				store.artPieces.push(data)
-		// 				setStore(store)
-		// 			}
-		// 		})
+		getObjects: () => {
+			for (let i = 1; i < 25; i++){
+				fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/"+i)
+				.then(response => response.json())
+				.then(data => {
+					let store = getStore()
+					if (data.objectName){
+						store.artPieces.push(data)
+						setStore(store)
+					}
+				})
 
-		// 	}
-		// },
+			}
+		},
 		getDepartments: () => {
 				fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
 				.then(response => response.json())
