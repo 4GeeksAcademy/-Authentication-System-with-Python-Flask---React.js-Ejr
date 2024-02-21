3ad0ab4e6aad4a8352e6038f795ca7823998a1c7
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2024-02-21 20:45:54 +0000

    modal registro

---
+++ src/front/js/component/SignUpModal.js
@@ -0,0 +1,82 @@
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
+export const SignUpModal = props => {
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isOpen, setOpen] = useState(true);
+
+    function handleSignUp(e) {
+        e.preventDefault();
+        let registered = actions.register(name, email, password);
+
+        if (registered) {
+            navigate("/demo");
+        } else {
+            // Aquí puedes agregar manejo de errores o retroalimentación para el usuario en caso de fallo en el registro
+        }
+    }
+
+    function closeModal() {
+        setOpen(false);
+    }
+
+    return (
+        <div className="modal" tabIndex="-1" role="dialog" style={{ display: isOpen ? "inline-block" : "none", cursor: "pointer" }}>
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header bg-100 d-flex flex-column px-5">
+                        <div className="d-flex flex-row justify-content-end w-100">
+                            <i className="fa-solid fa-xmark" onClick={closeModal}></i>
+                        </div>
+                        <h4 className="modal-title">Regístrate</h4>
+                    </div>
+                    <div className="modal-body w-100 px-5">
+                        <form className="d-flex flex-column justify-content-center align-items-center w-100 row gy-3" onSubmit={handleSignUp}>
+                            <div className="form-group d-flex flex-column justify-content-center align-items-center w-100">
+                                <label className="d-flex justify-content-start w-100">Nombre</label>
+                                <input
+                                    className="w-100 rounded-3"
+                                    type="text"
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group d-flex flex-column justify-content-center align-items-center w-100">
+                                <label className="d-flex justify-content-start w-100">Correo electrónico</label>
+                                <input
+                                    className="w-100 rounded-3"
+                                    type="text"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group d-flex flex-column justify-content-center align-items-center w-100">
+                                <label className="d-flex justify-content-start w-100">Contraseña</label>
+                                <input
+                                    className="w-100 rounded-3"
+                                    type="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <button type="submit" className="btn-300 to-be-hoved form-control">
+                                ¡REGÍSTRATE!
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+SignUpModal.propTypes = {
+    // Propiedades del componente
+};
+
+SignUpModal.defaultProps = {
+    // Valores predeterminados de las propiedades del componente
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -37,8 +37,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log(error);
 						return false;
 				}
+            },
+             register: async (name, email, password) => {
+        try {
+            let response = await fetch("https://shiny-dollop-qgq7xr79pxg24747-3001.app.github.dev/api/signup", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    "name": name,
+                    "email": email,
+                    "password": password
+                })
+            });
+
+            let data = await response.json();
+            if (response.ok) {
+                return true; // Registro exitoso
+            } else {
+                console.error(data.msg); // Manejo de errores
+                return false;
             }
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+}
         }
     };
-};
+
 export default getState;
