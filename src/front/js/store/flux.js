765945ada024b67682cf2c15f6c2e65727a8a1ac
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2024-02-22 16:16:32 +0000

    registro

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -39,32 +39,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						return false;
 				}
             },
-             register: async (name, email, password) => {
-        try {
-            let response = await fetch("https://shiny-dollop-qgq7xr79pxg24747-3001.app.github.dev/api/signup", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify({
-                    "name": name,
-                    "email": email,
-                    "password": password
-                })
-            });
-
-            let data = await response.json();
-            if (response.ok) {
-                return true; // Registro exitoso
-            } else {
-                console.error(data.msg); // Manejo de errores
-                return false;
-            } catch (error) {
-                console.error(error);
-                return false;
-            }
-        }
-    },
+       
 
             validate_token: async () => {
                 let token = localStorage.getItem("token")
@@ -88,9 +63,12 @@ const getState = ({ getStore, getActions, setStore }) => {
 				    }
                 }
 				
-            }
+            },
+
+            
+           
   
         }
-    };
+}};
 
 export default getState;
