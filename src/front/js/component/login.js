3725843f14dd1be7979f63e5964e85cc4aa8f0b5
Aleska134 <143849441+Aleska134@users.noreply.github.com>
2024-02-24 01:30:08 +0000

    delete link from login.js

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import { Context } from '../store/appContext';
-import { Link } from "react-router-dom";
 const Login = ({setAuthAttempt}) => {
   const {store,actions} = useContext(Context);
   const [email, setEmail] = useState('');
