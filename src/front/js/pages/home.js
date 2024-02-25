c15db6777c299d2cada697cb4e4d02237e758daa
jackievivianv <145908828+jackievivianv@users.noreply.github.com>
2024-02-25 17:52:18 +0000

    home diseño arreglado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,46 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import logo from "../../img/logoOCEANOM.png"
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+	const [state, setState] = useState({
+		//initialize state here
+	});
+
 	const { store, actions } = useContext(Context);
 
+	const [styleStartNow, setStyleStartNow] = useState("btn-light");
+
+	// boton Learn more que cuando haya hover se ponga el texto blanco
+    const handleHoverInButton = () => {
+        setStyleStartNow("btn-outline-secondary text-light");
+    }
+    // al quitar el hover vuelva al texto en azul
+    const handleHoverOutButton = () => {
+        setStyleStartNow("btn-light");
+    };
+
+
 	return (
-		<div className="container-fluid p-0 overflow-y-hidden h-100 landingfixed">
-			<img src="https://res.cloudinary.com/dx23woi99/image/upload/v1708541359/IMG_5841_tkuzrc.jpg" className="card-img-top p-0" alt="..." style={{width: "100%"}}/>
+		<div className="container-fluid d-flex p-0 col-lg-12 col-md-12 col-sm-12">
+			<img id="imagenFija" src="https://res.cloudinary.com/dx23woi99/image/upload/v1708541359/IMG_5841_tkuzrc.jpg" className="card-img-top p-0" alt="Imagen Fija" style={{width: "100%"}}/>
 			<div className="card-img-overlay">
 				
 					
 				
 					<div className="d-flex justify-content-end">
-						<button type="button" className="btn btn-outline-light p-2 mx-2">Free Trial</button>
+						<button type="button" className="btn btn-outline-light p-2 mx-2">Contact Us</button>
 						<Link to="/login">
-						<button type="button" className="btn btn-outline-light">Login</button>
+						<button type="button" className="btn btn-outline-light btn-lg">Login</button>
 						</Link>
 					</div>
-
-					<div className="mt-5 ms-4">
+		
+					<div className="mt-4 ms-2 col-lg-12 col-md-12 col-sm-12">
 						<img src={logo} className="card-img-top p-0" alt="..." style={{width: "50%"}}/>
 						
 					</div>
-					<div>
-						
-						<button type="button" className="btn btn-outline-light btn-lg ms-5">Dive In</button>
-						
-						
-					</div>
-					
-					
-
-					<div className="d-flex justify-content-end align-items-end">
-						<span className="text-light text-end fs-5">
-						An ocean of online yoga classes. <br></br>
-						An ocean of different styles & practices. <br></br>
-						Teachers from all over the globe. <br></br>
-						Here, all as one.
+				
+					<div className="d-block ms-4 text-start col-lg-12 col-md-6 col-sm-6">
+						<span className="text-light fs-5 text-center mt-3">
+						Come and practice in this ocean of online yoga <br></br> classes. 
+						With teachers from all over the globe! <br></br>
+						Any style, at any time.
 						</span>
+					<div className="d-flex align-items-center justify-content-start mt-5">
+						<button type="button" className={`btn ${styleStartNow} btn-lg text-secondary`} onMouseEnter={handleHoverInButton} onMouseLeave={handleHoverOutButton}>
+							{/* <Link className={`btn ${styleStartNow}`}> */}
+                            	Start Now
+                       	 	{/* </Link> */}
+						</button>
+					</div>	
 					</div>
 			</div>
 		
