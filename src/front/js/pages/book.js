f1287d1d24336cf96c17ffce5c71c3d66a779500
cdavis2223 <cdavis2223@gmail.com>
2024-02-29 02:31:26 +0000

    vista del libro solo título

--- src/front/js/pages/book.js
+++ src/front/js/pages/book.js
@@ -1,18 +1,49 @@
-import React from "react";
-import { useParams } from "react-router-dom";
 
-const BookDetails = () => {
-  const { id, title, author, publishDate } = useParams();
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+
+export const BookDetails = () => {
+  const { id } = useParams(); 
+  const [bookDetails, setBookDetails] = useState(null);
+console.log(bookDetails)
+  useEffect(() => {
+    const fetchBookDetails = async () => {
+      try {
+        const response = await fetch(`https://openlibrary.org/works/${id}.json`);
+        if (!response.ok) {
+          throw new Error('meee');
+
+          //la url tiene que estar en una variable de entorno en el archivo .env
+        }
+        const data = await response.json();
+        setBookDetails(data);
+      } catch (error) {
+        console.error('meee', error);
+      }
+    };
+
+    fetchBookDetails();
+  }, [id]);
+
+  if (!bookDetails) {
+    return <div>Cargando cargando librito</div>;
+  }
 
   return (
-    <div>
-      <h2>Detalles del Libro</h2>
-      <img src={`https://covers.openlibrary.org/b/id/${id}-L.jpg`} alt="Portada del libro" />
-      <h3>Título: {title}</h3>
-      <p>Autor: {author}</p>
-      <p>Año de Publicación: {publishDate}</p>
-      {/* Aquí puedes mostrar más detalles del libro si es necesario */}
-    </div>
+    <Container>
+      <Row>
+        <Col md={4}>
+          <img src={`https://covers.openlibrary.org/b/id/${bookDetails.cover_id}-L.jpg`} alt="Portada del libro" />
+        </Col>
+        <Col md={8}>
+          <h2>{bookDetails.title}</h2>
+          <p>Autor: {bookDetails.author_name}</p>
+          <p>Año de publicación: {bookDetails.first_publish_year}</p>
+          <p>ISBN: {bookDetails.isbn}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
