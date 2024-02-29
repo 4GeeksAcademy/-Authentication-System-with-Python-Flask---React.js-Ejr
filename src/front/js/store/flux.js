c36eb2ae48cc6da9a4c2bf70a07378f9262355f2
cdavis2223 <cdavis2223@gmail.com>
2024-02-29 01:00:21 +0000

    actualizacion de estilos signup

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,80 +1,4 @@
 const getState = ({ getStore, getActions, setStore }) => {
-<<<<<<< HEAD
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial:  ""
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: ""
-				}
-			]
-		},
-		actions: {
-			login: async (email, password) => {
-			  const actions = getActions()
-			  const data = await api.login(email, password)
-			  setStore({ user: data.user, token: data.token })
-			  actions.getFavorites()
-			  if (!data.user.is_admin) localStorage.setItem('myToken', data.token)
-			  return data
-			},
- 
-			signup: async (
-			  email,
-			  password,
-			  first_name,
-			  last_name,
-			  phone,
-			  location
-			) => {
-			  const response = await api.signup(
-				email,
-				password,
-				first_name,
-				last_name,
-				phone,
-				location
-			  )
-			  return response
-			},
-	},
-			getMessage: async () => {
-				let actions=getActions()
-				try{
-					// fetching data from the backend
-
-					const data = actions.APIfetch("/hello")
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			APIfetch: async (endpoint,method="GET",body=null)=>{
-=======
     return {
         store: {
             message: null,
@@ -124,7 +48,6 @@ const getState = ({ getStore, getActions, setStore }) => {
                 setStore({ demo: demo });
             },
             APIfetch: async (endpoint,method="GET",body=null)=>{
->>>>>>> ce74c73d9e06fa891b0680e171097358d24f2b92
 				let params={method}
 				if (body!=null){
 					params.headers={
