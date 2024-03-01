04f4afe72494e46a5f50a6d46cfd2fd55c188db2
Erika Scott <143569907+EScott237@users.noreply.github.com>
2024-03-01 04:43:19 +0000

    Updated museum page

--- src/front/js/pages/museums.js
+++ src/front/js/pages/museums.js
@@ -5,26 +5,31 @@ import "../../styles/museums.css";
 
 export const Museums = () => {
     const { store, actions } = useContext(Context);
-    const [showPopup, setShowPopup] = useState(false); // State to manage popup visibility
+    const [showPopup, setShowPopup] = useState(false);
 
-    const togglePopup = () => {
-        setShowPopup(!showPopup); // Toggle the state to show/hide the popup
-    };
+    const togglePopup = () => setShowPopup(!showPopup);
 
     return (
-        <div className="text-center mt-5">
-            <div className="">
-                <img src={Jumbotron} alt="Museum Jumbotron" onClick={togglePopup} style={{ cursor: 'pointer' }} />
-            </div>
-            {showPopup && (
-                <div className="popup">
-                    <div className="popup-inner">
-                        <button onClick={togglePopup}>Close</button>
-                        <h3>HISTORY OF THE MET</h3>
-                        <p>The Metropolitan Museum of Art's earliest roots date back to 1866 in Paris, France, when a group of Americans agreed to create a "national institution and gallery of art" to bring art and art education to the American people...</p>
+        <div className="content-container"> {/* This container will wrap your content and push the footer down */}
+            <div className="text-center mt-5">
+                <div className="image-frame-container">
+                    <div className="image-frame">
+                        <img src={Jumbotron} alt="Museum Jumbotron" onClick={togglePopup} />
                     </div>
                 </div>
-            )}
+                {showPopup && (
+                    <div className="popup">
+                        <div className="popup-inner">
+                            <h3>The Metropolitan Museum of Art</h3>
+                            <h5>New York, New York</h5>
+                            <h4>History of the MET</h4>
+                            <p>The Metropolitan Museum of Art's earliest roots date back to 1866 in Paris, France...</p>
+                            <button onClick={togglePopup}>Close</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
+
