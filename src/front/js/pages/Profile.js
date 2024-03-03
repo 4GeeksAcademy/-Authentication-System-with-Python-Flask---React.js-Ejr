7e7614b83fe35da7733edcaaf25905e6f7f5d53f
DANNIMENNA89 <146239929+DANNIMENNA89@users.noreply.github.com>
2024-03-03 15:43:08 +0000

    Informacion de usuario con eventos relacionados

--- src/front/js/pages/Profile.js
+++ src/front/js/pages/Profile.js
@@ -68,7 +68,7 @@ export const Profile = () => {
         <div className="container-fluid d-flex flex-column justify-content-between align-items-center">
             <div className="d-flex flex-row justify-content-center align-items-center my-5">
                 <div className="col-6 d-flex flex-row justify-content-center align-items-center ">
-                    <img className="col-4 img-fluid" src="https://img.freepik.com/fotos-premium/feliz-dibujo-dibujos-animados-boceto-imagen-fondo-blanco-arte-generado-ai_848903-6756.jpg" />
+                    <img className="col-4 img-fluid" src="https://img.freepik.com/fotos-premium/feliz-dibujo-dibujos-animclass=abindex`ados-boceto-imagen-fondo-blanco-arte-generado-ai_848903-6756.jpg" />
                     <div>
                         <ul className="col-8 list-group">
                             <h2>{store.user.name}</h2>
@@ -90,46 +90,90 @@ export const Profile = () => {
                     <h3>HAS ASISTIDO A {store.user.num_eventos_asistido ?  store.user.num_eventos_asistido : "0"} EVENTOS</h3>
                 </div>
             </div>
-            
-            <p className="d-inline-flex gap-1">
-                <a className="btn btn-400 active p-3" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Próximos eventos a los que asistir</a>
-                <button className="btn btn-400 active p-3" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Tus eventos creados</button>
-            </p>
-            <div className="row d-flex- flex-row flex-nowrap">
-                <div className="col col-12">
-                    <div className="collapse multi-collapse" id="multiCollapseExample1">
-                    <div className="card card-body border-0">
-                        <ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
-                            {store.user?.eventos_asistido ? (
-                            store.user?.eventos_asistido?.map((item, id) => (
-                                <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100" key={item.id}>
-                                    < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
+
+            <ul className="nav nav-tabs" id="myTab" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Próximos eventos a los que asistir</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Tus eventos creados</button>
+                </li>
+            </ul>
+                <div className="tab-content mt-2" id="myTabContent">
+                    <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
+                    <   ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
+                            {store.user?.eventos_asistido? (store.user?.eventos_asistido?.map((item, id) => (
+                                <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100 text-center" key={item.id}>
+                                < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
                                 </li>
                             ))
                             ) : (
-                            "Sin eventos asistidos"
+                            "Sin eventos creados"
+                            )}
+                        </ul>
+                    </div>
+                    <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
+                        <ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
+                            {store.user?.eventos_creados ? (store.user?.eventos_creados?.map((item, id) => (
+                                <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100" key={item.id}>
+                                < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
+                                </li>
+                            ))
+                            ) : (
+                            "Sin eventos creados"
+                            )}
+                        </ul>              
+                    </div>
+                </div>
+
+
+
+
+            
+            {/* <div className="accordion mb-5" id="accordionExample">
+                <div className="accordion-item mb-3 border-0">
+                    <h2 className="accordion-header">
+                    <button className="btn-400 accordion-button m-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Próximos eventos a los que asistir
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                        <ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
+                            {store.user?.eventos_asistido? (store.user?.eventos_asistido?.map((item, id) => (
+                                <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100 text-center" key={item.id}>
+                                < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
+                                </li>
+                            ))
+                            ) : (
+                            "Sin eventos creados"
                             )}
                         </ul>
                     </div>
                     </div>
                 </div>
-                <div className="col col-12">
-                    <div className="collapse multi-collapse" id="multiCollapseExample2">
-                        <div className="card card-body border-0">
-                            <ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
-                                {store.user?.eventos_creados ? (store.user?.eventos_creados?.map((item, id) => (
-                                    <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100" key={item.id}>
-                                    < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
-                                    </li>
-                                ))
-                                ) : (
-                                "Sin eventos creados"
-                                )}
-                            </ul>
-                        </div>
+                <div className="accordion-item border-0 text-center">
+                    <h2 className="accordion-header">
+                    <button className="btn-400 accordion-button collapsed m-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Tus eventos creados
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                        <ul className="list-group d-flex flex-column mb-5 w-100" id="contact-list">
+                            {store.user?.eventos_creados ? (store.user?.eventos_creados?.map((item, id) => (
+                                <li className="list-group col-xl-3 col-lg-4 col-md-6 col-12 mb-2 pe-2 w-100" key={item.id}>
+                                < SimpleCard id={item.id} evento={item.evento} descripcion={item.descripcion} ciudad={item.ciudad} fecha={item.fecha} />
+                                </li>
+                            ))
+                            ) : (
+                            "Sin eventos creados"
+                            )}
+                        </ul>
+                    </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
