e74aa3b80824ede9b4e50dc2e092e1565c73c481
Bruno Forti <139017911+Bforti@users.noreply.github.com>
2024-03-09 02:30:26 +0000

    commit

--- src/front/js/component/SearchBar.js
+++ src/front/js/component/SearchBar.js
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Resultados } from "./resultados";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import '../../styles/searchbar.css'; 
+import '../../styles/searchbar.css';
 
 const SearchBar = () => {
     const [search, setSearch] = useState("");
@@ -14,66 +14,43 @@ const SearchBar = () => {
         const value = event.target.value;
         setSearch(value);
         if (value.trim()) {
-           
-            actions.setBooks(value); 
+
+            actions.setBooks(value);
         }
     };
 
-<<<<<<< HEAD
-    }
-    useEffect(() => {
-        console.log('Input:', search)
-        
-    }, [search])
-    const [pag, setPag]=useState(false)
-    return (
-        <>
-            <div className="text-center">
-                <form onSubmit={handelOnSubmit} className="d-flex me-0 " >
 
-                         <input 
-                        className="form-control m-1 col-6"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                        value={search}
-                        onChange={(event) => setSearch(event.target.value)} />
+useEffect(() => {
+    console.log('Input:', search)
 
-                        <button className="btn btn-outline-success" type="submit" onClick={()=>setPag(!pag)}>Search</button>
-                </form>
-                <Resultados valor={search} />
-            </div>
+}, [search])
+const [pag, setPag] = useState(false)
+return (
+    <>
+        <div className="text-center">
+            <form onSubmit={handleSearchChange} className="d-flex" role="search" >
 
+                <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Busca libros, autores, editoriales..."
+                    aria-label="Search"
+                    value={search}
+                    onChange={handleSearchChange} />
 
-
-
-
-        </>
-    )
-=======
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-       
-    };
-
-    return (
-        <form onSubmit={handleOnSubmit} className="d-flex" role="search">
-            <input 
-                className="form-control me-2"
-                type="search"
-                placeholder="Busca libros, autores, editoriales..."
-                aria-label="Search"
-                value={search}
-                onChange={handleSearchChange}
-            />
-          <button className="btn btn-outline-success search-btn" type="submit">
-    <FontAwesomeIcon icon={faMagnifyingGlass} /> 
-</button>
-            
+                <button className="btn btn-outline-success search-btn" type="submit">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button></form>
             <Resultados valor={search} />
-        </form>
-    );
->>>>>>> 91777cf6763e47cd7625af0af05a9b97efa7eab7
+        </div>
+
+
+
+
+
+    </>
+)
+
 }
 
 export default SearchBar;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -19,35 +19,6 @@ const getState = ({ getStore, getActions, setStore }) => {
         },
         actions: {
             login: async (email, password) => {
-<<<<<<< HEAD
-                let actions = getActions()
-                const dat = await actions.APIfetch("/login", "POST", { email, password })
-                if (dat.error) {
-                    return false
-                }
-                setStore({ token: dat.token })
-                localStorage.setItem("accessToken", dat.token)
-                return true
-
-            },
-            signup: async (email, password, first_name, last_name, phone, location) => {
-
-                let actions = getActions()
-                const res = await actions.APIfetch("/signup", "POST", { email, password, email, password, first_name, last_name, phone, location })
-                if (res.ok) {
-                    console.log("Usuario registrado")
-                    return true
-
-                }
-                else {
-                    console.log("error")
-                    return false
-
-                }
-
-
-
-=======
                 let actions = getActions();
                 const data = await actions.APIfetch("/login", "POST", { email, password });
                 if (data.error) {
@@ -67,7 +38,6 @@ const getState = ({ getStore, getActions, setStore }) => {
                     console.log("Usuario registrado exitosamente");
                     return true;
                 }
->>>>>>> 91777cf6763e47cd7625af0af05a9b97efa7eab7
             },
             getMessage: async () => {
                 let actions = getActions();
@@ -88,7 +58,6 @@ const getState = ({ getStore, getActions, setStore }) => {
                 setStore({ demo: demo });
             },
             APIfetch: async (endpoint, method = "GET", body = null) => {
-<<<<<<< HEAD
                 let params = { method }
                 if (body != null) {
                     params.headers = {
@@ -164,53 +133,5 @@ const getState = ({ getStore, getActions, setStore }) => {
         
     }
 }
-=======
-                let params = { method, headers:{
-                    "Access-Control-Allow-Origin": "*"
-                } };
-                if (body != null) {
-                    params.headers = {
-                        "Content-Type": "application/json",
-                       
-                    };
-                    params.body = JSON.stringify(body);
-                }
-                try {
-                    let res = await fetch(process.env.BACKEND_URL + "api" + endpoint, params);
-                    if (!res.ok) {
-                        console.error(res.statusText);
-                        return { error: res.statusText };
-                    }
-                    let json = await res.json();
-                    return json;
-                } catch (error) {
-                    console.error("Error fetching data:", error);
-                    return { error: "Error fetching data" };
-                }
-            },
-            setBooks: async (endpoint, method = "GET", body = null) => {
-                let store = getStore();
-                try {
-                    const response = await fetch(store.urlBase + `?q=${endpoint}`);
-                    const data = await response.json();
-                    if (!response.ok) {
-                        console.error(data);
-                    }
-                    return data;
-                } catch (error) {
-                    console.error(error);
-                }
-            },
-            showNotification: async (message, type) => {
-                setStore({ response: { message, type } });
-            },
-            loadSession: () => {
-                let token = localStorage.getItem("accessToken");
-                setStore({ token });
-            }
-        }
-    };
-};
->>>>>>> 91777cf6763e47cd7625af0af05a9b97efa7eab7
 
 export default getState;
