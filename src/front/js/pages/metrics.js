28645f221798898aa923f1c48d301b470ffcffdc
NForlini <ninaforlini1@gmail.com>
2024-03-14 00:10:56 +0000

    fixed css

---
+++ src/front/js/pages/metrics.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Home = () => {
+  const [waterDrank, setWaterDrank] = useState("blue");
+  return (
+    <div id="whole-wheat" className="text-center mx-auto">
+      <div className="stick"></div>
+      <div className="lights">
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+        <div
+          className={activeLight === "blue" ? "blue light" : "blue light"}
+          onClick={() => setActiveLight("blue")}
+        ></div>
+      </div>
+    </div>
+  );
+};
