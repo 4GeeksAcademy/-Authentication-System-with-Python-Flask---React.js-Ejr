e6db31cc6b12f54fde843eb5972f3e37b2f015be
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-03-18 02:42:39 +0000

    added the breakfast and lunch column to component

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./recipe.css"; // Assuming you have custom styles
 import foodImage2 from "../../assets/foodImage2.jpg";
+import buddhaBowl from "../../assets/buddhaBowl.jpg";
+import breakfast1 from "../../assets/breakfast1.jpg";
+import breakfast2 from "../../assets/breakfast2.jpg";
+import breakfast3 from "../../assets/breakfast3.jpg";
+import veggieWrap from "../../assets/veggieWrap.jpeg";
+import lunch3 from "../../assets/lunch3.jpeg";
 
 const Recipe = () => {
   return (
@@ -56,7 +62,7 @@ const Recipe = () => {
                   placeholder="Your Email Address"
                 />
               </div>
-              <button type="submit" className="btn btn-primary">
+              <button type="submit" className="btn btn-success">
                 Subscribe
               </button>
             </form>
@@ -65,9 +71,9 @@ const Recipe = () => {
       </div>
       <div className="container text-center mt-5">
         <h4 className="upgrade-heading mb-4">
-          <strong>Upgrade to Premium for More Meals</strong>
+          <strong>Find your plan</strong>
         </h4>
-        <button type="button" className="btn btn-primary btn-lg">
+        <button type="button" className="btn btn-success btn-lg">
           Upgrade Now
         </button>
       </div>
@@ -75,63 +81,147 @@ const Recipe = () => {
         <h3 className="recipes-heading">
           Try one of our Delicious and Balanced Recipes
         </h3>
-        <h5 className="whats-hot-heading">What's Hot</h5>
+        <h5 className="whats-hot-heading">Breakfast</h5>
+        <div className="row">
+          {/* Row 1 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={breakfast2}
+                className="card-img-top"
+                alt="Breakfast 1"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Buddha Bowl</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Delicious and Nutritious
+                </h6>
+                <p className="card-text">
+                  Discover the perfect balance of flavors and nutrients in our
+                  Buddha Bowl. Packed with protein, fiber, and essential
+                  vitamins.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Row 2 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={breakfast1}
+                className="card-img-top"
+                alt="Breakfast 2"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Mediterranean Salad</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Fresh and Flavorful
+                </h6>
+                <p className="card-text">
+                  Indulge in the vibrant tastes of the Mediterranean with our
+                  refreshing salad. Bursting with fresh veggies, olives, feta
+                  cheese, and a tangy vinaigrette.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Row 3 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={breakfast3}
+                className="card-img-top"
+                alt="Breakfast 3"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Green Smoothie</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Energy Booster
+                </h6>
+                <p className="card-text">
+                  Revitalize your day with our Green Smoothie, packed with
+                  leafy greens, fruits, and superfoods. It's the perfect way to
+                  fuel your body and mind!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h5 className="whats-hot-heading">Lunch</h5>
+        <div className="row">
+          {/* Row 1 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={buddhaBowl}
+                className="card-img-top"
+                alt="Lunch 1"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Buddha Bowl</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Delicious and Nutritious
+                </h6>
+                <p className="card-text">
+                  Discover the perfect balance of flavors and nutrients in our
+                  Buddha Bowl. Packed with protein, fiber, and essential
+                  vitamins.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Row 2 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={veggieWrap}
+                className="card-img-top"
+                alt="Lunch 2"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Mediterranean Salad</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Fresh and Flavorful
+                </h6>
+                <p className="card-text">
+                  Indulge in the vibrant tastes of the Mediterranean with our
+                  refreshing salad. Bursting with fresh veggies, olives, feta
+                  cheese, and a tangy vinaigrette.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Row 3 */}
+          <div className="col">
+            <div className="card">
+              <img
+                src={lunch3}
+                className="card-img-top"
+                alt="Lunch 3"
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Green Smoothie</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Energy Booster
+                </h6>
+                <p className="card-text">
+                  Revitalize your day with our Green Smoothie, packed with
+                  leafy greens, fruits, and superfoods. It's the perfect way to
+                  fuel your body and mind!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Recipe;
-
-// import React from "react";
-// import "./recipe.css";
-// // import Hero from "../hero/Hero";
-
-// const Recipe = () => {
-//   return (
-//     <div className="gpt3__possibility section__padding" id="possibility">
-//         {/* <Hero /> */}
-//       <div className="gpt3__possibility-content">
-//         <div className="container">
-//           <div className="container text-center">
-//             <h1 className="gradient__text">
-//             Nutrition Made Easy.
-//             </h1>
-//             <div className="row">
-//               <div className="col text-start">
-//                 <h4>
-//                   "Welcome to Nourish, where culinary adventures await! Dive
-//                   into a world of tantalizing flavors, aromatic spices, and
-//                   mouthwatering dishes that will tantalize your taste buds and
-//                   ignite your passion for cooking. From sizzling stir-fries to
-//                   decadent desserts, our diverse collection of recipes caters to
-//                   every craving and skill level. Whether you're a seasoned chef
-//                   or a kitchen newbie, our easy-to-follow recipes, vibrant
-//                   photos, and helpful tips will empower you to create delicious
-//                   meals that will impress family and friends alike. Get ready to
-//                   embark on a delicious journey and discover the joy of cooking
-//                   with NourishNav!"
-//                 </h4>
-//               </div>
-//               <div className="col">Column</div>
-//               <div className="col">image</div>
-//             </div>
-//           </div>
-//           <h4>
-//             <strong>Upgrade to Premium for More Meals</strong>
-//           </h4>
-//           <button type="button" className="btn btn-primary btn-sm">
-//             Small button
-//           </button>
-//         </div>
-
-//         <h3>
-//           <strong>Try one of our Delicious and Balanced Recipes</strong>
-//         </h3>
-//         <h5>What's Hot</h5>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Recipe;
