ad38b0f861572fc8772c718ffa38f754105dca59
Luccadz75 <149024268+Luccadz75@users.noreply.github.com>
2024-03-19 22:02:34 +0000

    commit

---
+++ src/front/js/pages/privacy-policy.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="container mt-5">
+     <h1>Privacy Policy for Recommend Me a Book</h1>
+      <p>Last Updated: [Insert Date]</p>
+      
+      <p>Welcome to Recommend Me a Book, accessible via www.rmb.com or www.recommendmeabook.com. Our website is dedicated to providing entertainment services, including book recommendations, allowing users to leave reviews and share them with others. This Privacy Policy outlines how we collect, use, process, and share your personal information.</p>
+      
+      <h2>1. Information We Collect</h2>
+      <p>We collect personal information that you voluntarily provide to us, including your name, email address, phone number, date of birth, and the country from which you are accessing our website. This information is collected through forms and cookies to enhance your experience on our site.</p>
+      
+      <h2>2. How We Use Your Information</h2>
+      <p>Your personal information is used for data analysis, book recommendations, and marketing purposes. These activities help us to tailor our services to your interests and improve the overall quality of your experience on our website.</p>
+      
+      <h2>3. Sharing Information with Third Parties</h2>
+      <p>We may share your personal information with third-party book-selling platforms and data analysis services to further enhance our services. Any sharing of data will be conducted in compliance with applicable laws and with respect for your privacy.</p>
+      
+      <h2>4. International Data Transfers</h2>
+      <p>Your information may be transferred to, and processed in, countries outside of the European Economic Area (EEA) and the United States, under privacy protections that allow such transfers according to United States and European Union policies.</p>
+      
+      <h2>5. Your Privacy Rights</h2>
+      <p>You have the right to access, rectify, or erase your personal data, as well as the right to restrict processing, the right to data portability, and the right to object. These rights can be exercised by sending an email to support@rmb.com, in accordance with the laws applicable to your jurisdiction.</p>
+      
+      <h2>6. Data Security</h2>
+      <p>We adopt widely used web security measures to protect your personal information from unauthorized access, alteration, or destruction.</p>
+      
+      <h2>7. Data Retention</h2>
+      <p>We retain your personal information for the maximum period allowed by law, ensuring that we comply with our legal obligations and resolve disputes as needed.</p>
+      
+      <h2>8. Links to Third-Party Websites</h2>
+      <p>Our website may contain links to third-party websites. Please be aware that we are not responsible for the privacy practices of such other sites. We encourage you to be aware when you leave our site and to read the privacy statements of each website that collects personal information.</p>
+      
+      <h2>9. Changes to Our Privacy Policy</h2>
+      <p>We may update our Privacy Policy from time to time. All updates will be notified to you via email.</p>
+      
+      <h2>10. Contact Us</h2>
+      <p>If you have any questions or concerns regarding our Privacy Policy or the processing of your personal data, please contact us at support@rmb.com.</p>
+    </div>
+  );
+};
+
+
+export default PrivacyPolicy;
