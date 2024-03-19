9859763033d878566d58e2b29f7990b3abefb6d9
Luccadz75 <149024268+Luccadz75@users.noreply.github.com>
2024-03-19 09:05:56 +0000

    commite

--- src/front/js/component/Logo.js
+++ src/front/js/component/Logo.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
     return (
         <Link to="/" className="logo-wrap col-3">
-            <img src="/front/img/x.png" alt="Recomiéndame un Libro" />
+            <img src="/front/img/logo.png" alt="Recommend me a Book" />
         </Link>
     );
 }
