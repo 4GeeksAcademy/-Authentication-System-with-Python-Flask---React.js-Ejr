272329a1af7b9d241e956f334e389b55141d865f
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-03-22 18:26:48 +0000

    updated the store with global component

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,5 +1,5 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+This module takes care of starting the API Server, Loading the DB, and Adding the endpoints.
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
@@ -9,77 +9,80 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS 
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import timedelta
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt  # Add this import for Bcrypt
 
 api = Blueprint('api', __name__)
 
+# Initialize Bcrypt here
+bcrypt = Bcrypt()
+
 # Allow CORS requests to this API
-# CORS(api)
-CORS(api, resources={r"/api/*": {"origins": "https://supreme-robot-wr7w9jrjxjr729jvv-3000.app.github.dev"}})
+CORS(api)
 
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
 
+    return jsonify(response_body), 200
 
-def add_cors_headers(response):
-			
-			response.headers['Access-Control-Allow-Origin'] = '*'
-			response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
-			response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-			return response
-		        
-if __name__ == '__main__':
-    api.run(debug=True)
-			
-		
-
-# start of user related routes
 @api.route('/signup', methods=['POST'])
-def handle_signup():
-    # Extract data from request
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    name = request.json.get("name", None)
+def createUser():
+    password = request.json.get("password")
+    email = request.json.get("email")
 
-    # Check if user already exists
     user = User.query.filter_by(email=email).first()
-    if user:
-        return jsonify({"msg": "User account already exists"}), 409
-
-    # Hash password and create new user
-    hashed_password = generate_password_hash(password)
-    new_user = User(email=email, password=hashed_password, name=name )
-    db.session.add(new_user)
+    if user != None:
+        return jsonify({"msg": "email exists"}), 401
+    
+    user = User(password=password, email = email)
+    db.session.add(user)
     db.session.commit()
-    return jsonify({"msg": "User added successfully"}), 201
+    
+    response_body = {
+        "msg": "User successfully added "
+    }
+
+    return jsonify(response_body), 200
 
 @api.route('/login', methods=['POST'])
-def login():
-    # Extract data from request
-    email = request.json.get('email', None)
-    password = request.json.get('password', None)
-
-    # Find user by email
-    user = User.query.filter_by(email=email).first()
-    if user and check_password_hash(user.password, password):
-        # Correct password
-        # access_token = create_access_token(identity=email)
-        access_token = create_access_token(identity=email, expires_delta=timedelta(days=1))
-        return jsonify(access_token=access_token, user=user.serialize()), 200
+def handle_login():
+    body = request.get_json()
+    user = User.query.filter_by(email=body['email']).first()
+    if user and bcrypt.check_password_hash(user.password, body['password']):
+        access_token = create_access_token(identity=body['email'])
+        return jsonify(access_token=access_token), 200
     else:
-        # Incorrect email or password
-        return jsonify({"msg": "Bad username or password"}), 401
+        return jsonify({"msg": "Bad email or password"}), 401
+    
 
+@api.route('/token', methods=['POST'])
+def create_token():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+  
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id }) ,200
 
-
-
-@api.route('/private', methods=['GET'])
+#private route
+@api.route("/private", methods=["GET"])
 @jwt_required()
-def handle_private():
-    current_user_email = get_jwt_identity()
-    user = User.query.filter_by(email=current_user_email).first()
+def protected():
+    current_user_id = get_jwt_identity()    
+    user = User.query.get(current_user_id)
 
-    if user:
-        return jsonify(user=user.serialize()), 200
-
-    return jsonify({"msg": "User not found"}), 404
+    if user == None:
+        response_body = {
+            "msg": "Please login to continue"
+        }
+        return jsonify(response_body)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -1,51 +1,49 @@
 import React, { useState, useEffect } from "react";
 import getState from "./flux.js";
 
-// Initializing our context, by default it's going to be null.
+// Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
-// This function injects the global store to any view/component.
-// You can see the usage example in your layout component.
-const injectContext = (PassedComponent) => {
-    const StoreWrapper = (props) => {
-        // Initialize the state using the getState function
-        const [state, setState] = useState(
-            getState({
-                getStore: () => state.store,
-                getActions: () => state.actions,
-                setStore: (updatedStore) => {
-                    // We use a functional update to correctly merge the new store updates
-                    // with the current state to avoid closures on stale state.
-                    setState((currentState) => ({
-                        ...currentState,
-                        store: {
-                            ...currentState.store,
-                            ...updatedStore
-                        },
-                    }));
-                }
-            })
-        );
+// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
+// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
+const injectContext = PassedComponent => {
+	const StoreWrapper = props => {
+		//this will be passed as the contenxt value
+		const [state, setState] = useState(
+			getState({
+				getStore: () => state.store,
+				getActions: () => state.actions,
+				setStore: updatedStore =>
+					setState({
+						store: Object.assign(state.store, updatedStore),
+						actions: { ...state.actions }
+					})
+			})
+		);
 
-        useEffect(() => {
-            // This function is the equivalent to "window.onLoad", it only runs once.
-            // You should perform your API requests here.
-            // IMPORTANT: Use actions from the state to manipulate the store, do not use setState directly.
-            
-            // Example:
-            // state.actions.loadInitialData();
+		useEffect(() => {
+			/**
+			 * EDIT THIS!
+			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
+			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
+			 * store, instead use actions, like this:
+			 **/
+			
+			state.actions.tokenFromStore();
 
-        }, []); // Note: empty dependency array means this effect runs once, similar to componentDidMount
+			
+		}, []);
 
-        // Provide the state as the context value. This includes `store`, `actions`, and the `setStore` function.
-        return (
-            <Context.Provider value={state}>
-                <PassedComponent {...props} />
-            </Context.Provider>
-        );
-    };
-
-    return StoreWrapper;
+		// The initial value for the context is not null anymore, but the current state of this component,
+		// the context will now have a getStore, getActions and setStore functions available, because they were declared
+		// on the state of this component
+		return (
+			<Context.Provider value={state}>
+				<PassedComponent {...props} />
+			</Context.Provider>
+		);
+	};
+	return StoreWrapper;
 };
 
 export default injectContext;
