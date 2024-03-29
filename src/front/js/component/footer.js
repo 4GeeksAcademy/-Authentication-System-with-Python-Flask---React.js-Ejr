4532b12bcddc0d2d91043ad0f9365c70e09071c6
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-03-29 18:26:43 +0000

    prueba

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => (
 	<footer className="footer mt-5 py-3 text-center">
 		<p>
-			Footer: Realizado por Ernesto, Javier y Denis.
+			Footer: Realizado por Ernesto, Javier y Denis...
 		</p>
 	</footer>
 );
