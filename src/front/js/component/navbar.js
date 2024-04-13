5505e66807f8ae9dda01c9b9cc12bdd0f059cacf
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-04-13 10:47:55 +0000

    Dandole color y forma a la landing page, falta el foother y algunas funcionalidades

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,15 +4,15 @@ import logo from "../../img/klipartz.com.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="d-flex justify-content-between m-3 w-100">
+		<nav className="navbar navbar-light bg-light p-0">
+			<div className="d-flex justify-content-between w-100 back-navbar align-items-center">
 				<div>
 					<Link to="/">
-						<img className="logo" src={logo}  />
+						<img className="logo m-3" src={logo}  />
 					</Link>
 				</div>
 				<div className="ml-auto">
-					<i class="fa-solid fa-circle-user fa-2x"></i>
+					<i class="fa-solid fa-circle-user fa-2x m-3 "></i>
 				</div>
 			</div>
 		</nav>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,37 +5,60 @@ import "../../styles/home.css";
 import portada from "../../img/pngwing.com.png"
 import imgMundi from "../../img/cropped-footer-foto1.png"
 import imgLugares2 from "../../img/pngwing.com (2).png"
- 
+import imgRanking from "../../img/klipartz.com (2).png"
+import { Link } from "react-router-dom";
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5 ">
-			<h1 className="titulo">Adivina la Nacionalidad</h1>
-			<p>
-				<img src={portada} className="w-100" />
-			</p>
-			<div className="row m-5 align-items-center">
-				<div className="col-6 p-5">
-					<h1 className="cambria negrita">¿Que es Spot-IT?</h1>
-					<p className="cambria negrita texto">"Spot-IT" es un juego de trivia geográfica en el que los jugadores deben adivinar la ubicación de lugares icónicos del mundo. El juego presenta a los jugadores una serie de imágenes de lugares famosos, como monumentos, edificios históricos, paisajes naturales y sitios turísticos de renombre mundial.</p>
-				</div>
-				<div className="col-6">
-					<img src={imgMundi} className="w-100" />
+		<div className="text-center ">
+			<div className="back-texto2">
+				<h1 className="titulo pt-1 ">GuessNation</h1>
+				<div className="imagen-boton ">
+					<img src={portada} className="w-100" />
+					<div>
+						<Link to="/demo">
+							<button class="boton">JUGAR</button>
+						</Link>
+					</div>
 				</div>
 			</div>
-			<div className="row m-5 align-items-center">
-				<div className="col-6 p-5">
-					<img src={imgLugares2} className="w-100" />
+			<div className=" back-texto3">
+				<div className="row align-items-center p-5">
+					<div className="col-6 p-5">
+						<h1 className="cambria negrita">¿Que es GuessNation?</h1>
+						<p className="cambria negrita texto">"GuessNation" es un juego de trivia geográfica en el que los jugadores deben adivinar la ubicación de lugares icónicos del mundo. El juego presenta a los jugadores una serie de imágenes de lugares famosos, como monumentos, edificios históricos, paisajes naturales y sitios turísticos de renombre mundial.</p>
+					</div>
+					<div className="col-6">
+						<img src={imgMundi} className="w-100" />
+					</div>
 				</div>
-				<div className="col-6">
-					<h1 className="cambria negrita">¿Como Jugar?</h1>
-					<p className="cambria negrita texto">El juego comienza con la presentación de una imagen de un lugar icónico del mundo. Esta imagen puede ser de un monumento famoso, una ciudad reconocible, una maravilla natural, etc.<br/>
-					Junto con la imagen, se proporcionan varias opciones de respuesta, que consisten en diferentes países o regiones del mundo. Los jugadores deben seleccionar la opción que creen que corresponde a la ubicación del lugar mostrado en la imagen.<br/>
-					Una vez que los jugadores hayan revisado la imagen y las opciones de respuesta, deben seleccionar la respuesta que consideren correcta. Pueden elegir entre las diferentes opciones proporcionadas.<br/>
-					Después de que los jugadores hayan seleccionado su respuesta, el juego verifica si la respuesta es correcta o incorrecta. Si la respuesta es correcta, los jugadores obtienen puntos y pasan a la siguiente imagen. Si la respuesta es incorrecta, se muestra la ubicación correcta y se pasa a la siguiente imagen.<br/>
-					El juego continúa presentando nuevas imágenes y opciones de respuesta hasta que los jugadores decidan terminar la partida. En ese momento, se muestra la puntuación final de los jugadores y pueden compararla con la de otros jugadores para ver quién ha obtenido la puntuación más alta.<br/>
-					</p>
+			</div>
+			<div className=" back-texto1">
+				<div className="row p-5 align-items-center">
+					<div className="col-6 p-5">
+						<img src={imgLugares2} className="w-100" />
+					</div>
+					<div className="col-6">
+						<h1 className="cambria negrita">¿Como Jugar?</h1>
+						<p className="cambria negrita texto">
+							En "GuessNation" verás fotos de lugares interesantes de todo el mundo, como la Torre Eiffel o el Monumento a la Libertad. Tu trabajo es seleccionar en qué país esta ubicada la imagen. Si adivinas correctamente, ¡ganarás puntos y te convertirás en un experto en geografía!
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className=" back-texto3">
+				<div className="row p-5 align-items-center">
+					<div className="col-6 p-5">
+						<h1 className="cambria negrita">Ranking</h1>
+						<p className="cambria negrita texto">
+						En "GuessNations", cada vez que adivinas correctamente la ubicación de un lugar famoso, ganas un punto. ¡Podrás mantener una racha de victorias para mostrar tu habilidad y conocimiento geográfico! Compite contra jugadores de todo el mundo y sube en el ranking global. ¿Estás listo para aceptar el desafío? ¡Únete a GuessNations y comienza tu viaje por el mundo hoy mismo!
+						</p>
+					</div>
+					<div className="col-6">
+						<img src={imgRanking} className="w-100" />
+					</div>
 				</div>
 			</div>
 			<div className="alert alert-info">
