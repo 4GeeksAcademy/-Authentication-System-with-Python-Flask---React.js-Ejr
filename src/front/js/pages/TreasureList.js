eef34ac8e40d5022bd619a95652d1612c4d2a41c
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-04-23 14:30:10 +0000

    treasurelist responsive

--- src/front/js/pages/TreasureList.js
+++ src/front/js/pages/TreasureList.js
@@ -31,9 +31,9 @@ const TreasureList = () => {
                 const response = await fetch(process.env.BACKEND_URL + "/api/treasures", {
                     method: "GET"
                 });
-    
+
                 if (!response.ok) throw new Error("Failed to update treasure list");
-    
+
                 const data = await response.json();
                 setTreasures(data);
             } catch (error) {
@@ -55,44 +55,46 @@ const TreasureList = () => {
                     style={{ marginBottom: '20px', width: '25%', height: '45px' }}
                 />
             </div>
-            <table className="table-list">
-                <thead>
-                    <tr className="cabecero">
-                        <th className="user-title">User</th>
-                        <th className="image-title">Image</th>
-                        <th className="name-title">Name</th>
-                        <th className="city-title">City</th>
-                        <th className="find-it-title">Find It</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {currentTreasures.map((treasure, index) => (
-                        <tr className="elementos" key={index}>
-                            <td className="user-elements ps-3">
-                                <img
-                                    src={treasure.userPhoto ? treasure.userPhoto : "https://st.depositphotos.com/1537427/3571/v/450/depositphotos_35717211-stock-illustration-vector-user-icon.jpg"}
-                                    alt="User"
-                                    className="rounded-circle user-photo"
-                                    style={{ width: '30px', height: '30px', marginRight: '10px', verticalAlign: 'middle' }}
-                                />
-                                {treasure.username}
-                            </td>
-                            <td className="image-elements">
-                                <img src={treasure.image} alt="Treasure" className="image-treasure-list" />
-                            </td>
-                            <td className="name-elements ps-2">{treasure.name}</td>
-                            <td className="city-elements ps-2">{treasure.city_name}</td>
-                            <td className="find-it-elements">
-                                <Link to={`/treasure/${treasure.id}`}>
-                                    <button className="button-details btn btn-warning">Details</button>
-                                </Link>
-                            </td>
+            <div>
+                <table className="table-list">
+                    <thead>
+                        <tr className="cabecero">
+                            <th className="user-title">User</th>
+                            <th className="image-title">Image</th>
+                            <th className="name-title">Name</th>
+                            <th className="city-title">City</th>
+                            <th className="find-it-title">Find It</th>
                         </tr>
-                    ))}
-                </tbody>
-
-
-            </table>
+                    </thead>
+                    <tbody>
+                        {currentTreasures.map((treasure, index) => (
+                            <tr className="elementos" key={index}>
+                                <td className="user-elements ps-3">
+                                    <img
+                                        src={treasure.userPhoto ? treasure.userPhoto : "https://st.depositphotos.com/1537427/3571/v/450/depositphotos_35717211-stock-illustration-vector-user-icon.jpg"}
+                                        alt="User"
+                                        className="rounded-circle user-photo"
+                                        style={{ width: '30px', height: '30px', marginRight: '10px', verticalAlign: 'middle' }}
+                                    />
+                                    {treasure.username}
+                                </td>
+                                <td className="image-elements">
+                                    <div className="image-container">
+                                        <img src={treasure.image} alt="Treasure" className="image-treasure-list" />
+                                    </div>
+                                </td>
+                                <td className="name-elements ps-2">{treasure.name}</td>
+                                <td className="city-elements ps-2">{treasure.city_name}</td>
+                                <td className="find-it-elements">
+                                    <Link to={`/treasure/${treasure.id}`}>
+                                        <button className="button-details btn btn-warning">Details</button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <div className="pt-2">
                 <button className="btn btn-prev me-1" onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>🡸 Prev</button>
                 <button className="btn btn-next" onClick={() => paginate(currentPage + 1)} disabled={currentPage * treasuresPerPage >= treasuresFilter.length}>Next 🡺</button>
