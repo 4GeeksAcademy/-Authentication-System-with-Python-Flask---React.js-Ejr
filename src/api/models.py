1d26929fc7674fd77f9cfea0b537f165eb26e284
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-04-28 20:42:05 +0000

    cambios finales

--- src/api/models.py
+++ src/api/models.py
@@ -46,7 +46,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(500), unique=False, nullable=False)
     username = db.Column(db.String(30), unique=True, nullable=False)
     user_type = db.Column(db.Enum(MyEnum), nullable=False)
     photo = db.Column(db.String, unique=False, nullable=True)
