de3bff2cb810bf03287a5d6189da2d0655957226
KevinGaBo <153501399+KevinGaBo@users.noreply.github.com>
2024-05-06 19:11:22 +0000

    añadido el input de confirmar contraseña

--- src/front/js/component/SignUp.js
+++ src/front/js/component/SignUp.js
@@ -1,51 +1,42 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; 
 import "../../styles/signUp.css";
 import { Navigate, useNavigate } from "react-router-dom";
 
-export const Home = () => {
+export const SingUp = () => {
 	const [showModal, setShowModal] = useState(false);
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
+    const [password, setPassword] = useState("");
 	const navigate = useNavigate()
 	const handleClose = () => setShowModal(false);
 	const handleShow = () => setShowModal(true);
 
 	const handleEmailChange = (event) => {
 		setEmail(event.target.value);
-	};
-
-	const handlePasswordChange = (event) => {
+	  };
+	
+	  const handlePasswordChange = (event) => {
 		setPassword(event.target.value);
-	};
-	const handleConfirmPassword = (event) => {
-		setConfirmPassword(event.target.value);
-	}
+	  };
 
-	const handleSubmit = (event) => {
+	  const handleSubmit = (event) => {
 		event.preventDefault();
-		if (password !== confirmPassword) {
-			alert("La contraseña no coincide");
-			return;
-		}
-		fetch(process.env.BACKEND_URL + "/signup", {
+		 fetch(process.env.BACKEND_URL + "/signup", {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({ email, password })
 		})
-			.then(res => res.json())
-			.then(data => {
-				if (data && data.access_token) {
-					sessionStorage.setItem("token", data.access_token)
-					handleClose();
-					/* navigate("/user"); */
-				}
-			})
-			.catch(error => {
-				alert.error('Ha ocurrido un error', error);
-			});
+		.then(res => res.json())
+		.then(data => {
+			if (data && data.access_token) {
+				handleClose(); 
+				navigate(/* Ruta para el perfil */);
+			}
+		})
+		.catch(error => {
+			alert.error('Ha ocurrido un error', error);
+		});
 	};
 
 	return (
@@ -73,23 +64,14 @@ export const Home = () => {
 									onChange={handlePasswordChange}
 									placeholder="Contraseña"
 								/>
-								<input
-									type="password"
-									name="ConfirPassword"
-									value={confirmPassword}
-									onChange={handleConfirmPassword}
-									placeholder="Confirmar Contraseña"
-								/>
-								<div>
-									<button className="button-input close-button" onClick={handleClose}>Cerrar</button>
-									<button
-										className="button-input submit"
-										type="submit">
-										Enviar
-									</button>
-								</div>
+								<button className="button-input close-button" onClick={handleClose}>Cerrar</button>
+								<button 
+									className="button-input submit" 
+									type="submit">
+									Enviar
+								</button>
 							</form>
-
+							
 						</div>
 					</div>
 				)}
