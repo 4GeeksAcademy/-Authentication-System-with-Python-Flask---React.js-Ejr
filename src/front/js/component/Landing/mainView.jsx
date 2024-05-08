0b9b781e7c4cd764ea1d7d04e8ad106d696630e9
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-05-08 19:34:32 +0000

    adding styles to the landing page

--- src/front/js/component/Landing/mainView.jsx
+++ src/front/js/component/Landing/mainView.jsx
@@ -3,7 +3,20 @@ import React from "react";
 import "./../../../styles/Landing-styles/mainView.css";
 
 const MainView = () => {
-  return <div className="background-slider"></div>;
+  return (
+    <div className="text-container">
+      <div classname="left-text-container">
+        <h2>
+          <span className="green-text">FREE</span> YOUR BODY
+        </h2>
+      </div>
+      <div classname="right-text-container">
+        <h2>
+          GET FIT WITH <span className="green-text">MKJ</span>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default MainView;
--- src/front/js/pages/Landing/landing.js
+++ src/front/js/pages/Landing/landing.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../../store/appContext";
+import MainView from "../../component/Landing/mainView.jsx";
+
+import "../../../styles/Landing-styles/landing.css"
 
 export const Landing = () => {
     const { store, actions } = useContext(Context);
 
     return (
-        <div>
+        <div className="main-view-container ">
+            <div className="background-slider">
 
+            </div>
+            <MainView />
         </div>
     );
 };
