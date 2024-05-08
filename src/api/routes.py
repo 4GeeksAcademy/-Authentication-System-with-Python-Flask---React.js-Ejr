9d5ea9b0d2b71e2e76f22e1fb2ace5126a0de92d
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-08 15:06:25 +0000

    Signup Terminado

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,29 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Vehicle, FavoriteVehicle, MyVehicleInRent
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager 
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user_exist = User.query.filter_by(email=email).first()
+    if user_exist is None: 
+        new_user = User(
+            email=email, 
+            password=password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "User has already exist"}), 400
