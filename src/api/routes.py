24da383d221efc76acbd7b97d1347c753c437828
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-09 10:01:42 +0000

    Endpoint eliminar un vehiculo de favoritos DELETE favorite/vehicle/<int:vehicle_id>

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,3 +28,129 @@ def signup():
         return jsonify(access_token=access_token), 200
     else:
         return jsonify({"msg": "User has already exist"}), 400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@api.route('/favorite/vehicle/<int:vehicle_id>', methods=['DELETE'])
+@jwt_required()
+def delete_favorite_vehicle(vehicle_id): 
+    email = get_jwt_identity()
+    user_exist = User.query.filter_by(email=email).first()
+    user_id = user_exist.id
+    vehicle_exist = Vehicle.query.filter_by(id=vehicle_id).first()
+    if vehicle_exist is None:
+        return jsonify({"msg": "This vehicle doesn't exist"}), 400
+    else:
+        favorite_vehicle_to_delete = FavoriteVehicle.query.filter_by(vehicle_id=vehicle_id, user_id=user_id).first()
+        if favorite_vehicle_to_delete:
+            db.session.delete(favorite_vehicle_to_delete)
+            db.session.commit()
+            return jsonify({"msg": "Vehicle deleted to favorites"}), 200
+        else:  
+            return ({"msg": "This vehicle doesn't exist in favorites"}), 400
