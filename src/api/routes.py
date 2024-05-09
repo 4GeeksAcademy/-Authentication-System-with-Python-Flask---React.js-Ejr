7f8b501425bcbc8a76b4fc1ec393bae4978be487
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-05-09 09:10:42 +0000

    creado endopoint post de vehiculos

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,3 +28,59 @@ def signup():
         return jsonify(access_token=access_token), 200
     else:
         return jsonify({"msg": "User has already exist"}), 400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------------------------
+@api.route('/vehicle', methods=['POST'])
+def add_vehicle():
+    marca_modelo = request.json.get("marca_modelo")
+    matricula = request.json.get("matricula")
+    motor = request.json.get("motor")
+    tipo_cambio = request.json.get("tipo_cambio")
+    asientos = request.json.get("asientos")
+    precio = request.json.get("precio")
+
+    if not (marca_modelo and matricula and motor and tipo_cambio and asientos and precio):
+        return jsonify({"msg": "Todos los campos son obligatorios."}), 400
+
+    existing_vehicle = Vehicle.query.filter_by(matricula=matricula).first()
+    if existing_vehicle:
+        return jsonify({"msg": "El vehículo con esta matrícula ya existe"}), 400
+
+    new_vehicle = Vehicle(
+        marca_modelo=marca_modelo,
+        matricula=matricula,
+        motor=motor,
+        tipo_cambio=tipo_cambio,
+        asientos=asientos,
+        precio=precio
+    )
+    
+    db.session.add(new_vehicle)
+    db.session.commit()
+
+    return jsonify({"msg": "El vehículo ha sido creado correctamente"}), 200
+
