9b6bee100874e519e34b804b2027fbe7e595e711
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-05-10 00:59:57 +0000

    fixed material ui imports

--- src/front/js/component/RegionCard.js
+++ src/front/js/component/RegionCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 let coffee = {
   'Central America': [
