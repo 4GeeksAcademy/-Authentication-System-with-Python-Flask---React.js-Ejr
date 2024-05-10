e7ad28e68705e835fe255780196b466a48e55285
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-05-10 09:09:32 +0000

    elimnado las relaciones en user y vehiculos

--- src/api/models.py
+++ src/api/models.py
@@ -2,27 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-""" class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-     """
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
-    my_vehicle_in_rent = db.relationship('MyVehicleInRent', backref='user', lazy=True)
     favorites_vehicles = db.relationship('FavoriteVehicle', backref='user', lazy=True)
 
     def __repr__(self):
@@ -42,7 +25,6 @@ class Vehicle(db.Model):
     tipo_cambio = db.Column(db.String(50), nullable=False)
     asientos = db.Column(db.String(50), nullable=False)
     precio = db.Column(db.String(50), nullable=False)
-    my_vehicle_for_rent = db.relationship('MyVehicleInRent', backref='vehicle', lazy=True)
     favorites_vehicles = db.relationship('FavoriteVehicle', backref='vehicle', lazy=True)
 
     def __repr__(self):
