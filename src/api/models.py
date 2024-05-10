b5f5bb9d750917ca2f66623ea1bd5b6892e15992
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-05-10 21:41:04 +0000

    Backend - Models - Se añadieron campos a la clase User

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,22 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String, unique=False, nullable=False)
+    age = db.Column(db.Integer)
+    region = db.Column(db.String)
+    timezone = db.Column(db.String)
+    languages = db.Column(db.String)
+    image = db.Column(db.LargeBinary)  # Cambia <type> por el tipo de datos adecuado
+    xbox = db.Column(db.String)
+    psn = db.Column(db.String)
+    steam = db.Column(db.String)
+    google_play = db.Column(db.String)
+    nintendo = db.Column(db.String)
+    epic_id = db.Column(db.String)
+    bio = db.Column(db.String)
+    gender = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +31,19 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "age": self.age,
+            "region": self.region,
+            "timezone": self.timezone,
+            "languages": self.languages,
+            # Omitir la serialización de la contraseña y la imagen por motivos de seguridad
+            "xbox": self.xbox,
+            "psn": self.psn,
+            "steam": self.steam,
+            "google_play": self.google_play,
+            "nintendo": self.nintendo,
+            "epic_id": self.epic_id,
+            "bio": self.bio,
+            "gender": self.gender,
+            "admin": self.admin
         }
