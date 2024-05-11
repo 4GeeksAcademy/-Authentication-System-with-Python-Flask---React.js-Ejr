5ee2b949e762c24b683793fbec0e74d6ea1269b3
Roberto <51332297+Robertolor@users.noreply.github.com>
2024-05-11 21:01:57 +0000

    Backend - Modelado - Se creo la tabla Room y se corrigio error en codigo source donde 'or not' se mostraba repetido dentro de routes en la ln53. FalEl siguiente paso es relationships.

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -11,7 +12,7 @@ class User(db.Model):
     region = db.Column(db.String)
     timezone = db.Column(db.String)
     languages = db.Column(db.String)
-    image = db.Column(db.LargeBinary)  # Cambia <type> por el tipo de datos adecuado
+    image = db.Column(db.LargeBinary)
     xbox = db.Column(db.String)
     psn = db.Column(db.String)
     steam = db.Column(db.String)
@@ -21,7 +22,6 @@ class User(db.Model):
     bio = db.Column(db.String)
     gender = db.Column(db.String)
     admin = db.Column(db.Boolean, unique=False, nullable=True)
-
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -45,3 +45,35 @@ class User(db.Model):
             "gender": self.gender,
             "admin": self.admin
         }
+    
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+    room_name = db.Column(db.String(80), nullable=False)
+    game_name = db.Column(db.Integer, nullable=False)
+    platform = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    mood = db.Column(db.String, nullable=False)
+    reviews = db.Column(db.Integer)
+    in_room = db.Column(db.Integer, nullable=False)
+    
+
+    def __repr__(self):
+        return f'<Room {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "date": self.date,
+            "time": self.time,
+            "room_name": self.room_name,
+            "platform": self.platform,
+            "description": self.description,
+            "mood": self.mood,
+            "members": self.in_room
+        }
+    
