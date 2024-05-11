11832cd557ee8d68f916d9b8b3bc306b5a48e321
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-05-11 22:38:53 +0000

    fix/ información descriptiva en los alt de las img del caruosel

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -10,13 +10,13 @@ export const Home = () => {
         <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src="https://content.cuerpomente.com/medio/2023/10/17/atlas_20b57cee_231017144606_1200x630.jpg" className="d-block w-100" alt="..."/>
+                    <img src="https://content.cuerpomente.com/medio/2023/10/17/atlas_20b57cee_231017144606_1200x630.jpg" className="d-block w-100" alt="Atlas"/>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://miro.medium.com/v2/resize:fit:1400/0*tzFGcWiy64pwb7NU.png" className="d-block w-100" alt="..."/>
+                    <img src="https://miro.medium.com/v2/resize:fit:1400/0*tzFGcWiy64pwb7NU.png" className="d-block w-100" alt="Lenguajes de programación"/>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://firebase.google.com/static/images/products/realtime-database/database-3.png?hl=es-419" className="d-block w-100" alt="..."/>
+                    <img src="https://firebase.google.com/static/images/products/realtime-database/database-3.png?hl=es-419" className="d-block w-100" alt="Bases de datos"/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
