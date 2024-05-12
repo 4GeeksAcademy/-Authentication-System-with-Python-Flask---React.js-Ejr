4f7240ec7b24a2316ee48f7d4ef8fc5986b3ab8a
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-12 18:45:41 +0000

    Modal de opcion de dias de alquiler agregado

---
+++ src/front/js/component/modalalquilar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const ModalAlquilar = () => {
+    return (
+        <>
+            <button type="button" class="boton Alquilar btn btn-lg btn-light mb-5 ms-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Alquiler
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">¿Cuantos días desea alquilar?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault1"/>
+                                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                    7 días
+                                </label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault2"/>
+                                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                    15 días
+                                </label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault3"/>
+                                <label className="form-check-label" htmlFor="flexRadioDefault3">
+                                    30 días
+                                </label>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </>
+    );
+};
