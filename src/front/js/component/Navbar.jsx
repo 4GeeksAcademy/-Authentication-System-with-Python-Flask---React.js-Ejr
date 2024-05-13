864aba4bd4552a9c433191f0c8b9fc5b9571aca4
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-05-13 03:22:56 +0000

    fix/layout y navbar-Navbar.jsx

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
@@ -8,7 +9,9 @@ export const Navbar = () => {
                     <a className="navbar-brand">Atlas learning</a>
                     <p>More about us</p>
                     <form className="d-flex">
+                        <Link to='/FormUser'>
                             <button className='btn btn-outline-success m-1' type='submit'>signup</button>
+                        </Link>
                             <button className="btn btn-outline-success m-1" type="submit">login</button>
                             <button className='btn btn-outline-success m-1' type='submit'><i className="fa-solid fa-cart-shopping fa-fade" style={{color: "#13ec49;"}}></i></button>
                     </form>
