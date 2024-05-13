5d1e567ac002a93636c3dc9c4cd72d90887199f5
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-13 10:05:59 +0000

    Modal Agregado a Singup

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate} from "react-router-dom";
+import swal from 'sweetalert';
 import "../../styles/index.css";
 
 export const Signup = () => {
@@ -15,32 +16,28 @@ export const Signup = () => {
         let isSignup = await actions.signup(inputEmail, inputPassword) 
         if (isSignup){
             console.log('Signup successful');
-            alert("Usuario creado correctamente");
-            navigate("/");
+            swal ( "Registro con éxito" ,  "Gracias por registrarse en nuestar web!" ,  "success" )
+             navigate("/");
         } else {
             setSignupError(true);
             console.log('Signup failed');
         }
     };
     return (
-        <div className="contactForm">
-            <h1 className="title">Signup</h1>
-            <form onSubmit={handleSubmit}>
-                 <div className="mb-3">
-                     <label htmlFor="exampleInputEmail1" className="form-label" style={{ color: 'brown' }}>Email</label>
-                     <input type="email" className="form-control" id="exampleInputEmail1" placeholder="enter your email" onChange={(e) => setInputEmail(e.target.value)} />
-                 </div>
-                 {/* <div className="mb-3">
-                     <label htmlFor="exampleInputName" className="form-label" style={{ color: 'orange' }}>Name</label>
-                     <input type="text" className="form-control" id="exampleInputName" placeholder="enter your name" name='name' value={inputName} onChange={(e) => setInputName(e.target.value)} />
-                 </div> */}
-                 <div className="mb-3">
-                     <label htmlFor="exampleInputPassword" className="form-label" style={{ color: 'brown' }}>Password</label>
-                     <input type="password" className="form-control" id="exampleInputPassword" placeholder="xxxxxxx" onChange={(e) => setInputPassword(e.target.value)} />
-                 </div>
-                 {signupError ? <div className="text-danger mb-3">Todos los campos son obligatorios</div> : null}
-                 <button type="submit" className="login btn btn-dark">Signup</button>
-            </form>
-        </div>
+            <div className="contactForm">
+                <h1 className="title">Signup</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label" style={{ color: 'brown' }}>Email</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="enter your email" onChange={(e) => setInputEmail(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword" className="form-label" style={{ color: 'brown' }}>Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword" placeholder="xxxxxxx" onChange={(e) => setInputPassword(e.target.value)} />
+                    </div>
+                    {signupError ? <div className="text-danger mb-3">Todos los campos son obligatorios</div> : null}
+                    <button type="submit" className="login btn btn-dark"data-bs-toggle="modal" data-bs-target="#exampleModal">Signup</button>
+                </form>
+            </div>
     )
 };
