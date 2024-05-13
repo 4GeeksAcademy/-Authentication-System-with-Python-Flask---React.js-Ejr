67ac309fcc44fd504d7031070788854d3f75481b
Marinamb19 <150665821+Marinamb19@users.noreply.github.com>
2024-05-13 18:41:12 +0000

    falta fetch

--- src/front/js/pages/User/userForm.js
+++ src/front/js/pages/User/userForm.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import "./../../../styles/User/userForm.css";
 import { Context } from "../../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 const userForm = () => {
     const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         name: '',
         surnames: '',
         age: '',
         height: '',
         weight: '',
-        objetives: '',
-        illness: ''
+        illness: '',
+        objetives: ''
     });
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+    const handleChange = (e) => {
+        const { name, value } = e.target;
         setFormData({
             ...formData,
             [name]: value
@@ -27,7 +28,6 @@ const userForm = () => {
 
             if (
                 formData.name === '' ||
-                formData.surnames === '' ||
                 formData.age === '' ||
                 formData.height === '' ||
                 formData.weight === '' ||
@@ -47,7 +47,7 @@ const userForm = () => {
             });
 
             if (response.ok) {
-                history.push('/user');
+                navigate('/user');
             } else {
                 console.error('Error al enviar los datos');
             }
@@ -56,87 +56,85 @@ const userForm = () => {
         }
     }
 
-
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <label>
-                Surnames:
-                <input
-                    type="text"
-                    name="surnames"
-                    value={formData.surnames}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <label>
-                Age:
-                <input
-                    type="number"
-                    name="age"
-                    value={formData.age}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <label>
-                Height (cm):
-                <input
-                    type="number"
-                    name="height"
-                    value={formData.height}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <label>
-                Weight (kg):
-                <input
-                    type="number"
-                    name="weight"
-                    value={formData.weight}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <label>
-                Objetives:
-                <input
-                    type="text"
-                    name="objetives"
-                    value={formData.objetives}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label>
-                Illness:
-                <input
-                    type="text"
-                    name="illness"
-                    value={formData.illness}
-                    onChange={handleChange}
-                    required
-                />
-            </label>
-            <br />
-            <button type="submit">Send</button>
-        </form>
+        <div className="container-form">
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(e);
+        }}>
+                <label>
+                    Name and Surnames:
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Age 
+                    <input
+                        type="number"
+                        name="age"
+                        value={formData.age}
+                        onChange={handleChange}
+                        min="16"
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Height (cm):
+                    <input
+                        type="number"
+                        name="height"
+                        value={formData.height}
+                        onChange={handleChange}
+                        placeholder="000"
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Weight (kg):
+                    <input
+                        type="text"
+                        name="weight"
+                        value={formData.weight}
+                        onChange={handleChange}
+                        pattern="\d+(\,\d{1,2})?"
+                        placeholder="00.00"
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Illness:
+                    <input
+                        type="text"
+                        name="illness"
+                        value={formData.illness}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Objetives:
+                    <input
+                        type="text"
+                        name="objetives"
+                        value={formData.objetives}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Send</button>
+            </form>
+
+        </div>
     );
 }
 export default userForm;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,4 +1,5 @@
 import { jwtDecode } from "jwt-decode";
+import userForm from "../pages/User/userForm";
 
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
@@ -32,33 +33,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 
+getMessage: async () => {
+	try {
+		// fetching data from the backend
+		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+		const data = await resp.json()
+		setStore({ message: data.message })
+		// don't forget to return something, that is how the async resolves
+		return data;
+	} catch (error) {
+		console.log("Error loading message from backend", error)
+	}
+},
+	changeColor: (index, color) => {
+		//get the store
+		const store = getStore();
 
-			getMessage: async () => {
-				try {
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				} catch (error) {
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+		//we have to loop the entire demo array to look for the respective index
+		//and change its color
+		const demo = store.demo.map((elm, i) => {
+			if (i === index) elm.background = color;
+			return elm;
+		});
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+		//reset the global store
+		setStore({ demo: demo });
+	}
 		}
 	};
 };
