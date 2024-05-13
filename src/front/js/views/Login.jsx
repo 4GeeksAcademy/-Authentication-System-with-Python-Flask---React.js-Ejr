2fb4ae60ca562e31f289a46f80946dca9a4b13ec
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-05-13 18:37:42 +0000

    login+action POST

--- src/front/js/views/Login.jsx
+++ src/front/js/views/Login.jsx
@@ -1,40 +1,50 @@
-import React, {useState, useContext} from 'react'
-import {Context} from "../store/appContext"
+import React, { useState, useContext } from 'react'
+import { Context } from "../store/appContext"
 
 
 const Login = () => {
-    const {store,actions}=useContext(Context)
-    const [data, setData] = useState({})
-    const handlerInput = (e) =>{
+  const { store, actions } = useContext(Context)
+  const [mail, setMail] = useState("")
+  const [password, setPassword] = useState("")
 
-      let value = e.target.value
-      setData({...data, [e.target.name]:value}) 
-    }
-    const handlerSubmit = async() =>{
-      try { 
-        await actions.login(data)
+  // const [data, setData] = useState({})
+  // const handlerInput = (e) =>{
+
+  //   let value = e.target.value
+  //   setData({...data, [e.target.name]:value}) 
+  // }
+  const handlerSubmit = async (e) => {
+    e.preventDefault()
+    if (mail != "" && password != "") {
+
+      try {
+        await actions.login(mail,password)
       } catch (error) {
         console.error(error)
       }
     }
+    else {
+      console.log("faltan datos")
+    }
+  }
 
   return (
     <form>
-    <div className="mb-3">
-      <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-      <input type="email" onChange={handlerInput} name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div className="mb-3">
-      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-      <input type="password" onChange={handlerInput} name="password" className="form-control" id="exampleInputPassword1"/>
-    </div>
-    <div className="mb-3 form-check">
-      <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-      <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" onClick={handlerSubmit} className="btn btn-primary">Submit</button>
-  </form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" value={mail} onChange={(e) => setMail(e.target.value)} name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" className="form-control" id="exampleInputPassword1" />
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" onClick={(e) => handlerSubmit(e)} className="btn btn-primary">Submit</button>
+    </form>
   )
 }
 
