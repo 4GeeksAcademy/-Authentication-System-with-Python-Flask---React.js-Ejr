5b42fc0a8f06a7685f81cfe0fc4df8a8eee26eb9
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-05-13 00:44:50 +0000

    made it so that africa and asia-pacific views pull the coffees from the global store

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,40 +1,48 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			order: {
-				total: null,
-				items: []
-			}
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			fetchCoffee: async () => {
-				const response = await fetch(
-				  `https://fake-coffee-api.vercel.app/api`
-				)
-				  .then((response) => {
-					if (!response.ok) {
-					  throw new Error("Network response was not okay");
-					}
-					return response.json();
-				  })
-				  .catch((err) => {
-					console.error(err);
-				  });
-				  
-				  const coffeePerCategory = response.reduce((acc, curr) => {
-					if (!acc[curr.region]) {
-					  acc[curr.region] = [];
-					}
-				  
-					acc[curr.region] = [...acc[curr.region], curr];
-					return acc;
-				  }, {});
-				  const actualStore = getStore();
-				  setStore({...actualStore, ...coffeePerCategory});
-			  },
+	  store: {
+		order: {
+		  total: null,
+		  items: []
 		}
+	  },
+	  actions: {
+		// Use getActions to call a function within a function
+		fetchCoffee: async () => {
+		  const response = await fetch(`https://fake-coffee-api.vercel.app/api`)
+			.then((response) => {
+			  if (!response.ok) {
+				throw new Error("Network response was not okay");
+			  }
+			  return response.json();
+			})
+			.catch((err) => {
+			  console.error(err);
+			});
+  
+		  const coffeePerCategory = response.reduce((acc, curr) => {
+			if (!acc[curr.region]) {
+			  acc[curr.region] = [];
+			}
+  
+			acc[curr.region] = [...acc[curr.region], curr];
+			return acc;
+		  }, {});
+		  const actualStore = getStore();
+		  setStore({ ...actualStore, ...coffeePerCategory });
+		},
+		addCoffeeToOrder: (coffee) => {
+		  const actualStore = getStore();
+		  const { order } = actualStore;
+		  const updatedOrder = {
+			total: order.total + coffee.price,
+			items: [...order.items, coffee]
+		  };
+		  setStore({ ...actualStore, order: updatedOrder });
+		}
+	  }
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
+  
