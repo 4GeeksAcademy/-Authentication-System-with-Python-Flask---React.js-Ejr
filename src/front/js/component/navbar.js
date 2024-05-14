dc4594ddcfae4b2f7416b977ab91725853065707
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-05-14 02:10:01 +0000

    css botones de navbar y details, .ePdiente hacer 100 responsiviva y revisar funcionamiento del carrusel

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import logoImageUrl from "../../img/logoHW.png";
@@ -9,7 +9,7 @@ export const Navbar = () => {
 	const navigate = useNavigate();
 	const token = localStorage.getItem("token")
 
-	const handleLogOut= () => {
+	const handleLogOut = () => {
 		actions.logOut();
 		navigate('/')
 	};
@@ -18,80 +18,84 @@ export const Navbar = () => {
 		if (store.vehicles.length !== 0) {
 			console.log(actions);
 			actions.favorites();
-		} 
-	},[store.vehicles])
+		}
+	}, [store.vehicles])
 
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light">
 			<div className="container-fluid">
-				<div className="d-flex">
-					<Link to="/"> 
-						<img className="logo" src={logoImageUrl}/> 
+				<div className="d-flex align-items-center">
+					<Link to="/">
+						<img className="logo" src={logoImageUrl} />
 					</Link>
-					<div className="align-self-center">
-						<h2 className="navbar-text ms-3 pt-4 display-4 text-center"><strong>Friendly Wheels</strong></h2>
+					<div className="ms-3">
+						<h2 className="navbar-text ms-3 pt-4 text-center"><strong>Friendly Wheels</strong></h2>
 					</div>
 				</div>
 				{token ?
 					<>
-						<div className="d-flex">
-							<div className="btn-group me-5">
-								<button className="btn-lg btn-light">Añadir vehículo</button>
-							</div>
-							<div className="btn-group me-5">
-								<button className="btn btn-light btn-lg dropdown-toggle text-dark align-items-center mx-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Favoritos
-									<span className="px-2 ms-1 text-success" style={{borderRadius:"30px"}}>{store.favorites.length}</span>
-								</button>
-								<ul className="dropdown-menu">
-									{store.favorites.length === 0 
-										? <li className="text-center">(empty)</li>
-										: (store.favorites.map((item, index) => (
-											<li key={index} className="d-flex justify-content-between text-primary">
-												{item.matricula}
-												<button onClick={() => actions.removeFav(item.id)} className="btn p-0 px-1">
-													<i className="fas fa-trash"></i>
-												</button>
-											</li>
-										)))
-									}
-								</ul>
-							</div>
-							<div className="btn-group me-5">
-								<button className="btn btn-light btn-lg dropdown-toggle text-dark align-items-center mx-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Tú
-									<span className="px-2 ms-1 text-success" style={{borderRadius:"30px"}}>{store.myVehicles.length}</span>
-								</button>
-								<ul className="dropdown-menu">
-									{store.myVehicles.length === 0 
-										? <li className="text-center">(empty)</li>
-										: <p>Tengo algo</p>  /* En esta linea iria el map */
-									}
-								</ul>
-							</div>
-							<div>
-								<button className="btn-lg btn-light"onClick={handleLogOut}>Log Out</button>
-							</div>
+					<div className="d-flex align-items-center">
+						<div className="nav-favorito text-dark align-items-center mx-0 fs-4" type="button">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item active me-2 me-n2">
+									<a class="nav-link" href="#">Añadir Vehículo<span class="sr-only">(current)</span></a>
+								</li>
+							</ul>
+						</div>
+						</div>
+						<div className="nav-item me-2 me-n2">
+							<a className="nav-favorito dropdown-toggle text-dark align-items-center mx-0 fs-4" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Favoritos
+								<span className="px-1 text-dark fs-4" style={{ borderRadius: "30px" }}>{store.favorites.length}</span>
+							</a>
+							<ul className="dropdown-menu">
+								{store.favorites.length === 0
+									? <li className="text-center">(empty)</li>
+									: (store.favorites.map((item, index) => (
+										<li key={index} className="d-flex justify-content-between text-primary m-2">
+											{item.matricula}
+											<button onClick={() => actions.removeFav(item.id)} className="btn p-0 px-1">
+												<i className="fas fa-trash"></i>
+											</button>
+										</li>
+									)))
+								}
+							</ul>
+						</div>
+						<div className="btn-group me-1 text-center">
+							<a className="nav-favorito dropdown-toggle text-dark align-items-center mx-0 fs-4" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Tú
+								<span className="px-1 text-dark fs-4" style={{ borderRadius: "30px" }}>{store.myVehicles.length}</span>
+							</a>
+							<ul className="dropdown-menu">
+								{store.myVehicles.length === 0
+									? <li className="text-center">(empty)</li>
+									: <p>Tengo algo</p>  /* En esta linea iria el map */
+								}
+							</ul>
+						</div>
+						<div>
+							<button className="btn-lg btn-light" onClick={handleLogOut}>Log Out</button>
 						</div>
 					</>
 					: (
 						<>
 							<div className="d-flex">
-								<Link to="/login"> 
-									<div className="btn-group me-5">	
+								<Link to="/login">
+									<div className="btn-group me-3">
 										<button className="btn-lg btn-light">Login</button>
 									</div>
-								</Link> 
-								<Link to="/signup"> 
-									<div className="btn-group me-5">	
+								</Link>
+								<Link to="/signup">
+									<div className="btn-group me-5">
 										<button className="btn-lg btn-light">Signup</button>
 									</div>
-								</Link> 
+								</Link>
 							</div>
 						</>
 					)
-				} 			
-      		</div>
-   		 </nav>
+				}
+			</div>
+		</nav >
 	);
 };
