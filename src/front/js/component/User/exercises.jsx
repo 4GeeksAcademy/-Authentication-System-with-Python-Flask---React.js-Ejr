853b0aa2f344c024b676ef3f9797e4171754b5ea
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-05-14 19:27:33 +0000

    pagination to exercises added, missing styling

--- src/front/js/component/User/exercises.jsx
+++ src/front/js/component/User/exercises.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Context } from "../../store/appContext";
+import ReactPaginate from "react-paginate";
+
 import ExerciseCard from "./exerciseCard.jsx";
 
 import "../../../styles/User-styles/exercises.css";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const { store, actions } = useContext(Context);
+  const [pageNumber, setPageNumber] = useState(0);
+  const exercisesPerPage = 9;
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -13,12 +17,12 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
       if (bodyPart === "all") {
         exercisesData = await actions.fetchData(
-          "https://exercisedb.p.rapidapi.com/exercises",
+          "https://exercisedb.p.rapidapi.com/exercises?limit=1300",
           store.exerciseOptions
         );
       } else {
         exercisesData = await actions.fetchData(
-          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=1300`,
           store.exerciseOptions
         );
       }
@@ -29,14 +33,49 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     fetchExercisesData();
   }, [bodyPart]);
 
+  const indexOfLastExercise = pageNumber * exercisesPerPage + exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const handlePageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <div id="exercises" className="exercises-container">
       <h3 className="exercises-title">Showing Results</h3>
       <div className="exercises-result-container">
-        {exercises.map((exercise, index) => (
-          <ExerciseCard key={index} exercise={exercise} />
-        ))}
+        {currentExercises.length > 0 ? (
+          currentExercises.map((exercise, index) => (
+            <ExerciseCard key={index} exercise={exercise} />
+          ))
+        ) : (
+          <p>No exercises to display.</p>
+        )}
       </div>
+      <ReactPaginate
+        pageCount={Math.ceil(exercises.length / exercisesPerPage)}
+        nextLabel="next >"
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={1}
+        onPageChange={handlePageChange}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 };
