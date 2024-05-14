f3ee3d31c57fcfd1d780017dcd9933f1ececb22a
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-05-14 00:02:00 +0000

    updated code to match roberts

--- src/front/js/pages/regions.js
+++ src/front/js/pages/regions.js
@@ -357,19 +357,37 @@ let coffee = {
   export const Regions = () => {
     return (
         <>
-            <ul>
+            
                 {Object.keys(coffee).map((region) => {
                     return ( 
-                      <li key={region}>
-                        <RegionCard>
+                     
+                        <RegionCard image={getImageForRegion(region)}>
                         <Link to={`/regions/${region.toLowerCase().replace(/\s+/g, '-')}`}>
                           {region}
                         </Link>         
                         </RegionCard>
-                      </li>
+                      
                     );
                 })}
-            </ul>
+            
         </>
     );
 };
+const getImageForRegion = (region) => {
+  switch (region) {
+    case 'Central America':
+      return 'https://www.familysearch.org/en/wiki/img_auth.php/thumb/0/07/Central_America.png/900px-Central_America.png';
+    case 'Africa':
+      return 'https://www.nourhan.com/wp-content/uploads/2018/11/africa-map-with-capitals-large-political-map-of-africa-with-capitals-1999-africa-map-with-capitals-1.jpeg';
+    // Add cases for other regions...
+    case 'South America':
+      return 'https://d1sz1zrtudycb.cloudfront.net/wp-content/uploads/2019/11/633986_south-america.jpg';
+    case 'Asia Pacific':
+      return 'https://www.english-blogs.com/wp-content/uploads/2014/05/Asia-Pacific.gif';
+    case 'Middle East':
+      return 'https://www.gifex.com/images/0X0/2009-11-17-11127/Middle-East-Political-Map.jpg';
+    default:
+      return ''; // Default empty image URL
+  }
+};
+
