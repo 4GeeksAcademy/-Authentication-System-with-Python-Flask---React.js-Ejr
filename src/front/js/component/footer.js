eef1d552635d6b05cd1cead6567b048e46bf3549
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-14 15:15:14 +0000

    Botones del Navbar responsivos

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,9 +4,7 @@ import "../../styles/index.css";
 export const Footer = () => (
 	<footer className="footer fs-4  mt-auto py-3" style={{ height: "100px", backgroundColor: "#a9ce68" }}>
 		<div className="container justify-content-center text-center">
-			<p><i className="fa fa-heart text-danger" /> Made by Carmelo, Diana, Marta e Inés  <i className="fa fa-heart text-danger" />
-				<a href="http://www.4geeksacademy.com"></a>
-			</p>
+			<p>Made by Carmelo, Diana, Marta e Inés</p>
 		</div>
 	</footer>
 );
