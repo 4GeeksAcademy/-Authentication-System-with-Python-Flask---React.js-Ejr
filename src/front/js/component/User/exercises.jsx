cea4dc6280036f7a7263fcf4dc96123d195bed18
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-05-14 14:25:43 +0000

    nearly completed exercices, TODO pagination & style

--- src/front/js/component/User/exercises.jsx
+++ src/front/js/component/User/exercises.jsx
@@ -7,6 +7,28 @@ import "../../../styles/User-styles/exercises.css";
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const { store, actions } = useContext(Context);
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await actions.fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          store.exerciseOptions
+        );
+      } else {
+        exercisesData = await actions.fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          store.exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
     <div id="exercises" className="exercises-container">
       <h3 className="exercises-title">Showing Results</h3>
