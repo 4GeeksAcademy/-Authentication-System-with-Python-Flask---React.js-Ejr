b92f126585ac1b0383feafea854f72764e0b7fc4
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-05-15 14:56:54 +0000

    cambios

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Context } from '../store/appContext';
+import Private from '../component/private';
 
 const Private = () => {
     const [email, setEmail] = useState('');
@@ -16,19 +17,7 @@ const Private = () => {
 
     return (
         <div>
-            <h1>VISTA PRIVADA, HAS INICIADO SESIÓN</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            <h1>VISTA PRIVADA, HAS INICIADO SESIÓN</h1>            
         </div>
     );
 };
