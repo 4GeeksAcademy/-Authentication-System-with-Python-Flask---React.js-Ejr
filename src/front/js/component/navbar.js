c841cff594b4534ae4320eb3060af855c3ffdce3
finalfantasylover02 <109549032+finalfantasylover02@users.noreply.github.com>
2024-05-15 23:59:01 +0000

    new branch with new code

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import CoffeeLogo from "./CoffeeLogo";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-backgroundColor: '#2db734'">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1"></span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/signIn">
-						<button className=""></button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		<AppBar position="fixed" sx={{ top: 0, backgroundColor: '#2db734' }}>
+            <Toolbar style={{ justifyContent: 'space-between' }}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit', display: 'flex', alignItems: 'center' }}>
+                    <CoffeeLogo width="50px" height="50px" /> {/* Logo on the far left */}
+                </Link>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+                    CODEFUSION CAFE {/* Centered text */}
+                </Typography>
+                <div className="ml-auto">
+                    <Link to="/signIn">
+                        <button className="btn btn-primary">EMPLOYEE ID</button>
+                    </Link>
+                </div>
+            </Toolbar>
+        </AppBar>
 	);
 };
