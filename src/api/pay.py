567223e12bcec7a5c246275b7ecb23602e8f5eaf
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-05-15 08:09:40 +0000

    comienzo con el metodo de pago

---
+++ src/api/pay.py
@@ -0,0 +1,38 @@
+# pipenv install stripe
+#! /usr/bin/env python3.6
+
+import os
+from flask import Flask, redirect, request
+
+import stripe
+
+stripe.api_key = 'sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y'
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='public')
+
+YOUR_DOMAIN = 'http://localhost:4242'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': '{{PRICE_ID}}',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '?success=true',
+            cancel_url=YOUR_DOMAIN + '?canceled=true',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
+
+if __name__ == '__main__':
+    app.run(port=4242)
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -223,6 +223,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			}
 		}
 	};
-};
+;
 
 export default getState;
