14cf7748c390d8781562859c93d31665a9337fad
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-05-15 20:49:38 +0000

    fix/modificacion de archivos para conexion del flux y views

--- src/front/js/component/LogIn.jsx
+++ src/front/js/component/LogIn.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
+import { FaCircleArrowLeft } from "react-icons/fa6";
+
 export const LogIn = () => {
     const { store, actions } = useContext(Context)
     const navigate = useNavigate()
-    const [selectedRole, setSelectedRole] = useState(null)
+    const [selectedRole, setSelectedRole] = useState('')
     const [active, setActive] = useState(false)
-    const [typeRol, setTypeRol ] = useState(null)
+    const [counter, setCounter] = useState(7)
+    const [redirectPath, setRedirectPath] = useState('')
     const [login, setLogin] = useState({
         "email": '',
         "password": ''
@@ -18,7 +21,6 @@ export const LogIn = () => {
         const { name, value } = eve.target;
         if (name === 'isPeople') {
             setSelectedRole(value)
-            setTypeRol(value)
             setActive(true)
         } else {
             setLogin(prev => ({
@@ -28,10 +30,15 @@ export const LogIn = () => {
         }
     }
 
-    function handlerLogin(eve) {
-        eve.preventDefault();
+    async function handlerLogin(e) {
+        e.preventDefault();
         if (login.email !== '' && login.password !== '') {
-            actions.loginIn(login, typeRol)
+            await actions.loginIn(login, selectedRole);
+            if (localStorage.getItem('jwt-token')) {
+                setCounter(0)
+            }
+        } else {
+            alert('Ingrese todo los campos')
         }
     }
 
@@ -43,75 +50,123 @@ export const LogIn = () => {
         setActive(false)
     }
 
+    function handlerHome(){
+        navigate('/')
+    }
+
+    console.log(selectedRole)
+
+    useEffect(() => {
+        if (redirectPath !== '') {
+            navigate(redirectPath)
+        }
+    }, [navigate, redirectPath])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prevCounter => {
+                if (prevCounter + 1 === 4) {
+                    setRedirectPath(`/${selectedRole}View`)
+                    clearInterval(interval)
+                }
+                return prevCounter + 1;
+            });
+        }, 500);
+
+        return () => clearInterval(interval)
+    }, [setRedirectPath, selectedRole])
+
     return (
-        <div className='row d-flex flex-row'>
-            <div className='col-md-12 col-lg-5 d-flex justify-content-center align-items-start'>
-                <div className='border border-black rounded-3 mx-auto my-5 p-3 w-75'>
-                    <h1 className='text-center my-3'>Log In</h1>
-                    <form onSubmit={handlerLogin}>
+        <div className=' position-relative'>
+            <div className=' d-flex justify-content-center position-absolute top-00 start-50 translate-middle-x'>
+                <div className={`text-center w-100 ${(counter >= 1 && counter <= 3) ? "alert alert-success" : "d-none"}`} >
+                    {"Log In Successfully"}
+                </div>
+
+            </div>
+            {/* <div className={`d-flex justify-content-center position-absolute top-00 start-50 translate-middle-x ${ (store.error == '') ? 'd-none' : 'd-block'}`}>
+                    <div className="text-center w-100 alert alert-danger">
+                        {"Se ha presentado un error al Iniciar Session"}
+                    </div>
+
+                </div> */}
+            <div className='row d-flex flex-row'>
+                <div className='col-md-12 col-lg-5 d-flex justify-content-center align-items-start'>
+                    <div className='border border-black rounded-3 mx-auto my-5 p-3 w-75'>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div className='d-flex justify-content-center align-items-center mx-2 fs-4' onClick={handlerHome} style={{ cursor: "pointer" }}>
+                                <FaCircleArrowLeft />
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <h1>Log In</h1>
+                            </div>
+                        </div>
+                        <form onSubmit={handlerLogin}>
 
 
-                        {
-                            (active)
-                                ? <div>
-                                    <div className='col-md my-3'>
-                                        <label className='my-2'>Email</label>
-                                        <input
-                                            name='email'
-                                            value={login.email}
-                                            onChange={handlerChangeLogin}
-                                            type="text"
-                                            placeholder='Ingrese email'
-                                            className="form-control"
-                                        />
-                                    </div>
+                            {
+                                (active)
+                                    ? <div>
+                                        <div className='col-md my-3'>
+                                            <label className='my-2'>Email {store.error}</label>
+                                            <input
+                                                name='email'
+                                                value={login.email}
+                                                onChange={handlerChangeLogin}
+                                                type="text"
+                                                placeholder='Ingrese email'
+                                                className="form-control"
+                                            />
+                                        </div>
 
-                                    <div className='col-md my-3'>
-                                        <label className='my-2'>Password</label>
-                                        <input
-                                            name='password'
-                                            value={login.password}
-                                            onChange={handlerChangeLogin}
-                                            type='password'
-                                            placeholder='Ingrese password'
-                                            className="form-control"
-                                        />
-                                    </div>
+                                        <div className='col-md my-3'>
+                                            <label className='my-2'>Password</label>
+                                            <input
+                                                name='password'
+                                                value={login.password}
+                                                onChange={handlerChangeLogin}
+                                                type='password'
+                                                placeholder='Ingrese password'
+                                                className="form-control"
+                                            />
+                                        </div>
 
-                                    <div className='col-md' style={{ marginTop: '80px' }}>
-                                        <button className='btn btn-primary w-100' onClick={handlerLogin}>Login In</button>
+                                        <div className='col-md' style={{ marginTop: '80px' }}>
+                                            <button className='btn btn-primary w-100' onClick={handlerLogin}>Login In</button>
+                                        </div>
+                                        <div className='col-md my-3 text-center'>
+                                            <p className='text-decoration-underline' onClick={handlerGoToRegister} style={{ cursor: "pointer" }}>Don't have an account yet? click here to register.</p>
+                                        </div>
+                                        <div className='col-md my-3 text-center'>
+                                            <p className='text-decoration-underline' onClick={handlerChangeActive} style={{ cursor: "pointer" }}>Do you want to change roles?</p>
+                                        </div>
                                     </div>
-                                    <div className='col-md my-3 text-center'>
-                                        <p className='text-decoration-underline' onClick={handlerGoToRegister} style={{ cursor: "pointer" }}>Don't have an account yet? click here to register.</p>
+                                    : <div className='col-md-3'>
+                                        <label className="form-label">Role</label>
+                                        <div className="input-group has-validation">
+                                            <select className="form-select" name='isPeople' onChange={handlerChangeLogin} value={selectedRole} required>
+                                                <option disabled value="Choose">Choose</option>
+                                                <option value='teacher'>Teacher</option>
+                                                <option value='user'>Student</option>
+                                                <option value='manager'>Manager</option>
+                                            </select>
+                                        </div>
                                     </div>
-                                    <div className='col-md my-3 text-center'>
-                                        <p className='text-decoration-underline' onClick={handlerChangeActive} style={{ cursor: "pointer" }}>Do you want to change roles?</p>
-                                    </div>
-                                </div>
-                                : <div className='col-md-3'>
-                                    <label className="form-label">Role</label>
-                                    <div className="input-group has-validation">
-                                        <select className="form-select" name='isPeople' onChange={handlerChangeLogin} value={selectedRole} required>
-                                            <option selected disabled value="Choose">Choose</option>
-                                            <option value='teacher'>Teacher</option>
-                                            <option value='user'>Student</option>
-                                            <option value='manager'>Manager</option>
-                                        </select>
-                                    </div>
-                                </div>
-                        }
+                            }
 
-                    </form>
+                        </form>
+                    </div>
+                </div>
+                <div className='col-lg-7 d-md-none d-lg-block d-flex justify-content-center align-items-center'>
+                    <img
+                        src="https://www.ceac.es/sites/default/files/2020-08/estudiar-online-ceac.jpg.webp"
+                        alt="imgLogInEducation"
+                        className='img-fluid'
+                        style={{ height: "100vh", width: 'auto' }}
+                    />
                 </div>
             </div>
-            <div className='col-lg-7 d-md-none d-lg-block d-flex justify-content-center align-items-center'>
-                <img
-                    src="https://www.ceac.es/sites/default/files/2020-08/estudiar-online-ceac.jpg.webp"
-                    alt="imgLogInEducation"
-                    className='img-fluid'
-                    style={{ height: "100vh", width: 'auto' }}
-                />
-            </div>
         </div>
+
     );
 };
--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -4,41 +4,21 @@ import { Context } from '../store/appContext';
 
 export const Navbar = () => {
 
-    const navigate = useNavigate()
-    function handleUserView() {
-        navigate('/UserView')
-    }
 
-    const navigateManagerView = useNavigate()
-    function handleManagerView(){
-        navigateManagerView('/ManagerView')
-    }
-    const navigateTeacherView = useNavigate()
-    function handleTeacherView(){
-        navigateTeacherView('/TeacherView')
-    }
     return (
         <div>
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand">Atlas learning</a>
-                    <p>More about us</p>
-                    <button onClick={handleUserView}>UserView</button>
-                    <button onClick={handleManagerView}>ManagerView</button>
-                    <button onClick={handleTeacherView}>TeacherView</button>
                     <div className="d-flex">
                         <Link to='/FormUser'>
-                            <button className='btn btn-outline-success m-1' type='submit'>signup</button>
+                            <button className='btn btn-outline-success m-1' type='submit'>Sign Up</button>
                         </Link>
                         <Link to='/logIn'>
-                            <button className="btn btn-outline-success m-1" type="submit">login</button>
+                            <button className="btn btn-outline-success m-1" type="submit">Log In</button>
                         </Link>
 
-                        <Link to='/TeacherView'>
-                            <button className="btn btn-outline-success m-1" type="submit"> Private User</button>
-                        </Link>
-
-                        <button className='btn btn-outline-success m-1' type='submit'><i className="fa-solid fa-cart-shopping fa-fade" style={{ color: "#13ec49;" }}></i></button>
+                        <button className='btn btn-outline-success m-1' type='submit'><i className="fa-solid fa-cart-shopping fa-fade" style={{ color: "#13ec49" }}></i></button>
                     </div>
                 </div>
             </nav>
