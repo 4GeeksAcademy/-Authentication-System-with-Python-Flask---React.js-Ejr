c6ce7ca05411e0e2052e31dde617d9d6a94a5609
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-05-15 00:49:29 +0000

    feat/ Components UserCourses.jsx, UserPayment.jsx and UserProfile.jsx added. Dynamic conection in UserDashboard to the components mentioned above

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,6 +14,7 @@ import { FormUser } from "./pages/FormUser.jsx";
 import { LogIn } from "./component/LogIn.jsx";
 import { UserView } from "./pages/UserView.jsx";
 
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
