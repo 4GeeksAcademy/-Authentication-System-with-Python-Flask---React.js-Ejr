7c5c65d3509b8520167504e1d87913ea6fcc6e79
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-05-15 14:28:23 +0000

    cambios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
-import Signup from "./component/registroUsuario";
-import Login from "./component/iniciarSesion";
 import "../../styles/home.css";
+import Signup from "../component/registroUsuario";
+import Login from "../component/iniciarSesion";
 
 export const Home = () => {
     const { actions } = useContext(Context);
