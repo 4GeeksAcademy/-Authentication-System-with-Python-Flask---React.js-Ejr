7d8845d3563285190e0018ee9cff9507de05ef17
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-05-16 08:57:48 +0000

    cambios

--- src/front/js/component/private.js
+++ src/front/js/component/private.js
@@ -6,20 +6,20 @@ const Private = () => {
     const history = useHistory();
     const { actions, store } = useContext(Context);
 
-    useEffect(() => {
-        // Verificar si el usuario está autenticado al cargar el componente
-        if (!store.isLoggedIn) {
-            // Si el usuario no está autenticado, redirigir al componente de inicio de sesión
-            history.push('/login');
-        }
-    }, [history, store.isLoggedIn]);
+    // useEffect(() => {
+    //     // Verificar si el usuario está autenticado al cargar el componente
+    //     if (!store.isLoggedIn) {
+    //         // Si el usuario no está autenticado, redirigir al componente de inicio de sesión
+    //         history.push('/login');
+    //     }
+    // }, [history, store.isLoggedIn]);
 
-    const handleLogout = () => {
-        // Lógica para cerrar sesión (ejemplo)
-        actions.logout();
-        // Redirigir al componente de inicio de sesión después de cerrar sesión
-        history.push('/private');
-    };
+    // const handleLogout = () => {
+    //     // Lógica para cerrar sesión (ejemplo)
+    //     actions.logout();
+    //     // Redirigir al componente de inicio de sesión después de cerrar sesión
+    //     history.push('/private');
+    // };
 
     return (
         <div>
