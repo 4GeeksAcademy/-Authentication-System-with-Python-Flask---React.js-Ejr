2e96a407cae045005148958aed95b99c35d9a2d7
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-05-18 02:18:33 +0000

    fix/actualizacion de signout

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
 export const Navbar = () => {
     const { store, actions } = useContext(Context)
+    const navigate = useNavigate()
+
+    function handleHomeView() {
+        localStorage.removeItem('jwt-token')
+        navigate('/SignOut')
+    }
 
     return (
         <div>
@@ -11,21 +17,30 @@ export const Navbar = () => {
                 <div className="container-fluid">
                     <a className="navbar-brand">Atlas learning</a>
                     <div className="d-flex">
-                       
-                            {
-                              localStorage.getItem('jwt-token')
-                                ? <p>estoy aqui</p>
+
+                        {
+                            (localStorage.getItem('jwt-token'))
+                                ? <div>
+                                <Link to={`/${store.currentRole}View`}>
+                                    <button className='btn btn-outline-success m-1' >Panel</button>
+                                </Link>
+
+                                <button className="btn btn-outline-danger m-1" onClick={handleHomeView}>
+                                    Sign Out
+                                </button>
+
+                            </div>
                                 : <div>
                                     <Link to='/FormUser'>
-                                <button className='btn btn-outline-success m-1' >Sign Up</button>
-                            </Link>
-                            <Link to='/logIn'>
-                                <button className="btn btn-outline-success m-1" >Log In</button>
-                            </Link>
+                                        <button className='btn btn-outline-success m-1' >Sign Up</button>
+                                    </Link>
+                                    <Link to='/logIn'>
+                                        <button className="btn btn-outline-success m-1" >Log In</button>
+                                    </Link>
                                 </div>
-                            }
-                            
-                    
+                        }
+
+
 
                         <button className='btn btn-outline-success m-1'><i className="fa-solid fa-cart-shopping fa-fade" style={{ color: "#13ec49" }}></i></button>
                     </div>
