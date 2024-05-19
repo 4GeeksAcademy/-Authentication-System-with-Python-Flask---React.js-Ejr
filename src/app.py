a3b1cdfe26bf3910e69be77ccb00f29ef5cc43c7
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-05-19 20:33:08 +0000

    APP- Se importa bcrypt y jwt en el archivo principal app

--- src/app.py
+++ src/app.py
@@ -11,6 +11,8 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -19,6 +21,7 @@ static_file_dir = os.path.join(os.path.dirname(
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+jwt = JWTManager(app)
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
