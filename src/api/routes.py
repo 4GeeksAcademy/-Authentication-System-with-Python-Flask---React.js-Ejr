b40933550e854be670887d02d489b35e20dd4250
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-05-20 01:37:38 +0000

    imported get_jwt_identity

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Transactions
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
