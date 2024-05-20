a848b0ad13ea4fcaee15368d317d8b5c25d00b89
Osián <149536063+OsianJL@users.noreply.github.com>
2024-05-20 09:14:29 +0000

    models, admin and script ok

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,17 +1,27 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Electric, Acoustic, Classical, Favorites
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
+    admin = Admin(app, name='Guitar Blog Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+   #Special MODELview for favorite table
+    class FavoritesView(ModelView):
+        column_list = ('electric_id', 'acoustic_id', 'classical_id', 'user_id')
+        form_columns = ('electric_id', 'acoustic_id', 'classical_id', 'user_id')  
 
+
+    # Add your models here, for example this is how we add a the User model to the admin
+        
+    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Electric, db.session))
+    admin.add_view(ModelView(Acoustic, db.session))
+    admin.add_view(ModelView(Classical, db.session))
+    admin.add_view(FavoritesView(Favorites, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,125 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+# Here we declare our tables (models) with their atributes: 
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    electric_id = db.Column(db.Integer, db.ForeignKey('electric.id'))
+    acoustic_id = db.Column(db.Integer, db.ForeignKey('acoustic.id'))
+    classical_id = db.Column(db.Integer, db.ForeignKey('classical.id'))
+    
+   
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Favorites {self.id}>'  
 
     def serialize(self):
         return {
             "id": self.id,
+            "electric_id": self.electric_id,
+            "acoustic_id": self.acoustic_id,
+            "classical_id": self.classical_id,
+        
+        }
+    
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=True)
+    is_active = db.Column(db.Boolean(), nullable=False)
+    user_favorites = db.relationship(Favorites)
+
+    def __repr__(self):
+        return f'<User {self.first_name}>' 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+    
+class Electric(db.Model):
+    __tablename__ = 'electric'
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(250), nullable=False)
+    scale = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    pickups = db.Column(db.String(250), nullable=False)
+    image = db.Column(db.String(500), nullable=True)
+    electric_favorites = db.relationship(Favorites)
+
+    def __repr__(self):
+        return f'<Electric {self.model}>'  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "model": self.model,
+            "scale": self.scale,
+            "price": self.price,
+            "color": self.color,
+            "manufacturer": self.manufacturer,
+            "pickups": self.pickups,
+            "image": self.image,
+        }
+
+class Acoustic(db.Model):
+    __tablename__ = 'acoustic'
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(250), nullable=False)
+    scale = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    image = db.Column(db.String(500), nullable=True)
+    acoustic_favorites = db.relationship(Favorites)
+
+    def __repr__(self):
+        return f'<Acoustic {self.model}>' 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "model": self.model,
+            "scale": self.scale,
+            "price": self.price,
+            "color": self.color,
+            "manufacturer": self.manufacturer,
+            "image": self.image,
+        }
+    
+class Classical(db.Model):
+    __tablename__ = 'classical'
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(250), nullable=False)
+    scale = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    image = db.Column(db.String(500), nullable=True)
+    classical_favorites = db.relationship(Favorites)
+
+    def __repr__(self):
+        return f'<Classical {self.model}>' 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "model": self.model,
+            "scale": self.scale,
+            "price": self.price,
+            "color": self.color,
+            "manufacturer": self.manufacturer,
+            "image": self.image,
         }
