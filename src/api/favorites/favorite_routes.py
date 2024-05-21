92b9d0e3b576b1df49502d94386afc4dda69a384
Osián <149536063+OsianJL@users.noreply.github.com>
2024-05-21 18:59:24 +0000

    favorites ok

--- src/api/favorites/favorite_routes.py
+++ src/api/favorites/favorite_routes.py
@@ -2,8 +2,9 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Electric, Favorites
+from api.models import db, User, Electric, Acoustic, Classical, Favorites
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_cors import CORS
 
 favorite_bp = Blueprint('favorite', __name__)
@@ -12,16 +13,16 @@ favorite_bp = Blueprint('favorite', __name__)
 CORS(favorite_bp)
 
 
-####### OBTENER TODOS LOS FAVORITOS DE UN USUARIO ######
+# Retrieve all user favorites
 @favorite_bp.route('/user/favorites', methods=['GET'])
-# @jwt_required()
+@jwt_required()
 def get_all_favorites_of_user():
-    # email = get_jwt_identity()
+    email = get_jwt_identity()
 
-    # user_exists = User.query.filter_by(email=email).first()
+    user_exists = User.query.filter_by(email=email).first()
 
-    # if user_exists is None: 
-    #        return jsonify({"msg": "This user does not exist"}), 401
+    if user_exists is None: 
+           return jsonify({"msg": "This user does not exist"}), 401
 
     # user_id = user_exists.id
 
@@ -37,3 +38,152 @@ def get_all_favorites_of_user():
     
     else: 
         return jsonify({"msg": "this user has no favorites yet"}), 404
+    
+
+# Add favorite electric
+@favorite_bp.route('/favorites/electric/<int:electric_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_electric(electric_id):
+
+    email = get_jwt_identity()
+  
+
+    user_exists = User.query.filter_by(email=email).first()
+
+    if user_exists is None: 
+           return jsonify({"msg": "This user does not exist"}), 401
+
+    user_id = user_exists.id
+
+    electric_exists = Electric.query.filter_by(id=electric_id).first()
+    
+    if electric_exists is None: 
+           return jsonify({"msg": "This electric guitar does not exist"}), 401
+    
+    
+    query_results = Favorites.query.filter_by(electric_id=electric_id, user_id=user_id).first()
+    print(query_results)
+    if query_results is None: 
+
+            new_favorite = Favorites(electric_id=electric_id, user_id=user_id)
+            new_electric = Electric.query.filter_by(id=electric_id).first()
+            db.session.add(new_favorite)
+            db.session.commit()
+
+            response_body = {
+                 "msg": "ok", 
+                 "results": new_electric.serialize()
+            }
+            return jsonify(response_body), 200 
+
+    else:
+            return ({"msg": "this user already has this electric guitar as a favorite"}), 409
+
+
+# Add favorite acoustic
+@favorite_bp.route('/favorites/acoustic/<int:acoustic_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_acoustic(acoustic_id):
+
+    email = get_jwt_identity()
+  
+
+    user_exists = User.query.filter_by(email=email).first()
+
+    if user_exists is None: 
+           return jsonify({"msg": "This user does not exist"}), 401
+
+    user_id = user_exists.id
+
+    acoustic_exists = Electric.query.filter_by(id=acoustic_id).first()
+    
+    if acoustic_exists is None: 
+           return jsonify({"msg": "This acoustic guitar does not exist"}), 401
+    
+    
+    query_results = Favorites.query.filter_by(acoustic_id=acoustic_id, user_id=user_id).first()
+    print(query_results)
+    if query_results is None: 
+
+            new_favorite = Favorites(acoustic_id=acoustic_id, user_id=user_id)
+            new_acoustic = Acoustic.query.filter_by(id=acoustic_id).first()
+            db.session.add(new_favorite)
+            db.session.commit()
+
+            response_body = {
+                 "msg": "ok", 
+                 "results": new_acoustic.serialize()
+            }
+            return jsonify(response_body), 200 
+
+    else:
+            return ({"msg": "this user already has this acoustic guitar as a favorite"}), 409
+    
+
+# Add favorite classical
+@favorite_bp.route('/favorites/classical/<int:classical_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_classical(classical_id):
+
+    email = get_jwt_identity()
+  
+
+    user_exists = User.query.filter_by(email=email).first()
+
+    if user_exists is None: 
+           return jsonify({"msg": "This user does not exist"}), 401
+
+    user_id = user_exists.id
+
+    classical_exists = Classical.query.filter_by(id=classical_id).first()
+    
+    if classical_exists is None: 
+           return jsonify({"msg": "This classical guitar does not exist"}), 401
+    
+    
+    query_results = Favorites.query.filter_by(classical_id=classical_id, user_id=user_id).first()
+    print(query_results)
+    if query_results is None: 
+
+            new_favorite = Favorites(classical_id=classical_id, user_id=user_id)
+            new_classical = Classical.query.filter_by(id=classical_id).first()
+            db.session.add(new_favorite)
+            db.session.commit()
+
+            response_body = {
+                 "msg": "ok", 
+                 "results": new_classical.serialize()
+            }
+            return jsonify(response_body), 200 
+
+    else:
+            return ({"msg": "this user already has this classical guitar as a favorite"}), 409
+    
+
+@favorite_bp.route('/favorites/<int:favorite_id>', methods=['DELETE'])
+@jwt_required()
+def delete_favorite(favorite_id):
+    email = get_jwt_identity()
+
+    user_exists = User.query.filter_by(email=email).first()
+    if user_exists is None: 
+            return jsonify({"msg": "this user does not exist"})
+    
+    user_id = user_exists.id
+
+    favorite_exists = Favorites.query.filter_by(id=favorite_id).first()
+    if favorite_exists is None: 
+            return jsonify({"msg": "this favorite does not exist"})
+    
+
+    query_results = Favorites.query.filter_by(id=favorite_id, user_id=user_id).first()
+
+    if query_results: 
+         
+            db.session.delete(query_results)
+            db.session.commit()
+            return ({"msg": "ok, its deleted"}), 200
+
+    else: 
+
+           return ({"msg": "there is nothing to delete"}), 200
--- src/api/guitars/guitar_routes.py
+++ src/api/guitars/guitar_routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Electric
+from api.models import db, User, Electric, Acoustic, Classical
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -11,16 +11,7 @@ guitar_bp = Blueprint('guitar', __name__)
 # Allow CORS requests to this API
 CORS(guitar_bp)
 
-
-# @api.route('/hello', methods=['POST', 'GET'])
-# def handle_hello():
-
-#     response_body = {
-#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-#     }
-
-#     return jsonify(response_body), 200
-
+# Retrieve all electric guitars
 @guitar_bp.route('/electric-guitars', methods=['GET'])
 def get_electric_guitars():
     query_results = Electric.query.all()
@@ -35,3 +26,107 @@ def get_electric_guitars():
     }
     
     return jsonify(response_body), 200
+
+# Retrieve all acoustic guitars
+@guitar_bp.route('/acoustic-guitars', methods=['GET'])
+def get_acoustic_guitars():
+    query_results = Acoustic.query.all()
+    results = list(map(lambda item: item.serialize(), query_results))
+
+    if results == []:
+        return jsonify("no acoustic guitars in the database"), 404
+    
+    response_body = {
+        "msg": "ok",
+        "results": results
+    }
+    
+    return jsonify(response_body), 200
+
+# Retrieve all classical guitars
+@guitar_bp.route('/classical-guitars', methods=['GET'])
+def get_classical_guitars():
+    query_results = Classical.query.all()
+    results = list(map(lambda item: item.serialize(), query_results))
+
+    if results == []:
+        return jsonify("no classical guitars in the database"), 404
+    
+    response_body = {
+        "msg": "ok",
+        "results": results
+    }
+    
+    return jsonify(response_body), 200
+
+# Retrieve a specific electric guitar model with dynamic URL
+@guitar_bp.route('/electric/<int:electric_id>', methods=['GET'])
+def get_one_electric(electric_id):
+
+    query_result = Electric.query.filter_by(id=electric_id).first()
+    if query_result is None:
+        return jsonify({"msg": "there is no electric guitar matching the ID provided"}), 404
+    
+    response_body = {
+        "msg": "ok",
+        "id": query_result.id,
+        "results": query_result.serialize()
+    }
+    return jsonify(response_body), 200
+
+# Retrieve a specific acoustic guitar model with dynamic URL
+@guitar_bp.route('/acoustic/<int:acoustic_id>', methods=['GET'])
+def get_one_acoustic(acoustic_id):
+
+    query_result = Electric.query.filter_by(id=acoustic_id).first()
+    if query_result is None:
+        return jsonify({"msg": "there is no acoustic guitar matching the ID provided"}), 404
+    
+    response_body = {
+        "msg": "ok",
+        "id": query_result.id,
+        "results": query_result.serialize()
+    }
+    return jsonify(response_body), 200
+
+# Retrieve a specific classical guitar model with dynamic URL
+@guitar_bp.route('/classical/<int:classical_id>', methods=['GET'])
+def get_one_classical(classical_id):
+
+    query_result = Electric.query.filter_by(id=classical_id).first()
+    if query_result is None:
+        return jsonify({"msg": "there is no classical guitar matching the ID provided"}), 404
+    
+    response_body = {
+        "msg": "ok",
+        "id": query_result.id,
+        "results": query_result.serialize()
+    }
+    return jsonify(response_body), 200
+
+# Create new guitar
+@guitar_bp.route('/new-electric-guitar', methods=['POST'])
+def add_new_electric_guitar():
+    data = request.json
+
+    electric_exists = Electric.query.filter_by(model=data["model"]).first()
+    
+    if electric_exists is None: 
+
+            new_electric_guitar = Electric(
+                model=data["model"], 
+                color=data["color"], 
+                image=data["image"], 
+                manufacturer=data["manufacturer"], 
+                pickups=data["pickups"], 
+                price=data["price"], 
+                scale=data["scale"], 
+                )
+            db.session.add(new_electric_guitar)
+            db.session.commit()
+            return ({"msg": "ok, a new electric guitar has been added to the database"}), 200
+
+       
+
+    else:
+            return ({"msg": "this electric guitar is already included in the database"}), 409
