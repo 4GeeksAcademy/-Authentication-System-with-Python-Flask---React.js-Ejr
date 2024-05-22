8607a06274cf62e505329a8e3328252a41835279
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-22 10:57:46 +0000

    Modal de recuperar contraseña

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -50,7 +50,7 @@ export const Navbar = () => {
 						<div className="usericon"><i className="fas fa-user-circle"></i></div>
 					</button>
 					<div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-						<div className="offcanvas-header">
+						<div className="offcanvas-header my-0">
 							<h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
 							<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 						</div>
