7165a6ff3eb8d24b4ac53d84769f111ebb055d09
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-05-22 15:24:31 +0000

    Agregado Texto cuando no hya favoritos}

--- src/front/js/pages/favoritos.js
+++ src/front/js/pages/favoritos.js
@@ -13,35 +13,42 @@ export const Favoritos = () => {
         <>
             <div className="footer-view vehicles mb-5 mt-2 justify-content-center bg-light">
 				<div className="container">
-                    <h1 className="h1 text-center pt-2"><strong>MIS FAVORITOS</strong></h1>
+                    <h1 className="h1 pt-2"><strong>Favoritos</strong></h1>
 					<div className="row Map Cards text-dark d-flex justify-content-center">
-                        {store.favorites.map((vehicle) => {
-                            return (
+                        {store.favorites.length === 0 
+                            ? (
                                 <>
-                                    <div className="card col-md-4 mb-2 me-5 mt-4" style={{ width: "22rem", height: "27rem" }}>
-                                        <Link to={`/details/${vehicle.id}`} className="text-decoration-none text-black">
-                                            <a className="cardvehicles" href="#">
-                                                <img className="Vehiculo mt-3" src="https://somoselectricos.com/wp-content/uploads/modelos/Audi-e_tron.png" />
-                                            </a>
-                                            <div className="card-body p-1">
-                                                <div>
-                                                    <h5 className="card-title mt-2 mb-4"><strong></strong> {vehicle.marca_modelo.toUpperCase()}</h5>
-                                                    <p className="card-text mb-1"><strong>Tipo de motor:</strong> {vehicle.motor}</p>
-                                                    <p className="card-text mb-1"><strong>Tipo de cambio:</strong> {vehicle.tipo_cambio}</p>
-                                                    <p className="card-text mb-1"><strong>Asientos:</strong> {vehicle.asientos}</p>
-                                                    <p className="card-text mb-1"><strong>Precio:</strong> {vehicle.precio} €</p>
-                                                </div>
-                                                </div>
-                                        </Link>
-                                        <div className="d-flex justify-content-end">
-                                            <button onClick={() => actions.removeFav(vehicle.id)} className="btn-dark rounded">
-                                                Eliminar
-                                            </button>
-                                        </div>
-                                    </div>
+                                    <div className="fs-4 mt-4 mb-3"><strong>Crea tu primera lista de favoritos</strong></div>
+                                    <div className="texto-favoritos">Pulsa en el icono del corazón para guardar los coches y las experiencias <br></br> que más te gusten en Favoritos mientras buscas.</div>
                                 </>
                             )
-                        })}
+                            : (store.favorites.map((vehicle) => (
+                                    <>
+                                        <div className="card col-md-4 mb-2 me-5 mt-4" style={{ width: "22rem", height: "27rem" }}>
+                                            <Link to={`/details/${vehicle.id}`} className="text-decoration-none text-black">
+                                                <a className="cardvehicles" href="#">
+                                                    <img className="Vehiculo mt-3" src="https://somoselectricos.com/wp-content/uploads/modelos/Audi-e_tron.png" />
+                                                </a>
+                                                <div className="card-body p-1">
+                                                    <div>
+                                                        <h5 className="card-title mt-2 mb-4"><strong></strong> {vehicle.marca_modelo.toUpperCase()}</h5>
+                                                        <p className="card-text mb-1"><strong>Tipo de motor:</strong> {vehicle.motor}</p>
+                                                        <p className="card-text mb-1"><strong>Tipo de cambio:</strong> {vehicle.tipo_cambio}</p>
+                                                        <p className="card-text mb-1"><strong>Asientos:</strong> {vehicle.asientos}</p>
+                                                        <p className="card-text mb-1"><strong>Precio:</strong> {vehicle.precio} €</p>
+                                                    </div>
+                                                    </div>
+                                            </Link>
+                                            <div className="d-flex justify-content-end">
+                                                <button onClick={() => actions.removeFav(vehicle.id)} className="btn-dark rounded">
+                                                    Eliminar
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            ))
+                        }
                     </div>
                 </div>
             </div>
