8d49a91142ea248eebaf891680744b9a842483f3
Marinamb19 <150665821+Marinamb19@users.noreply.github.com>
2024-05-22 17:01:38 +0000

    para hacer un pull

--- src/front/js/pages/Trainer/trainer.js
+++ src/front/js/pages/Trainer/trainer.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import "../../../styles/Trainer-styles/trainer.css";
 
 import { Context } from "../../store/appContext";
-import TrainerExercise from "../../component/Trainer/trainerExercise.jsx";
 import TrainerCalendar from "../../component/Trainer/trainerCalendar.jsx";
+import TrainerView from "../../component/Trainer/trainerView.jsx";
+import TrainerExercise from "../../component/Trainer/trainerExercise.jsx";
 
 export const Trainer = () => {
     const { store, actions } = useContext(Context);
-
+// el trainer exercise tiene que ir en la vista individual del usuario dentro de la card 
     return (
         <div className="trainer-container">
             <TrainerCalendar />
+            <TrainerView />
             <TrainerExercise />
         </div>
     );
