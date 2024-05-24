c8a1cd4e7a2f6538a6b8741e21c5af7b97a91ccd
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-05-24 12:12:09 +0000

    Validación signupfrorm

--- src/front/js/component/singupform.js
+++ src/front/js/component/singupform.js
@@ -1,43 +1,81 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import swal from 'sweetalert';
 import "../../styles/index.css";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export const SignupForm = () => {
-    const [inputEmail, setInputEmail]=useState("")
-    const [inputPassword, setInputPassword]=useState("")
-	const navigate = useNavigate();
-    const {actions } = useContext(Context);
+    const { actions } = useContext(Context);
+    // const [inputEmail, setInputEmail]=useState("")
+    // const [inputPassword, setInputPassword]=useState("")
+    const navigate = useNavigate();
+    const formik = useFormik({
+        initialValues: {
+            inputEmail: '',
+            inputPassword: ''
+        },
+        validationSchema: Yup.object({
+            inputEmail: Yup.string().email('Invalid email address').required('Required'),
+            inputPassword: Yup.string().max(8, 'Debe tener 8 caracteres máximo').min(6, 'Debe tener 6 caracteres mínimo')
+                .required('Required'),
+        }),
+        onSubmit: values => {
+            async function handleSubmit() {
+                let isSignup = await actions.signup(values.inputEmail, values.inputPassword)
+                if (isSignup === "success") {
+                    swal("Registro con éxito", "Gracias por registrarse en nuestra web!", "success")
+                    navigate("/");
+                } else if (isSignup === "email_exist") {
+                    swal("Este correo ya se encuentra registrado", "Por favor intentelo con otro correo", "error")
+                } else {
+                    swal("Todos los campos son obligatorios", "Por favor intentelo de nuevo", "error")
+                }
+            };
+            handleSubmit()
+        },
+    });
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        let isSignup = await actions.signup(inputEmail, inputPassword) 
-        if (isSignup === "success"){
-            swal ( "Registro con éxito" ,  "Gracias por registrarse en nuestra web!" ,  "success" )
-            navigate("/");
-        } else if (isSignup === "email_exist") {
-            swal ( "Este correo ya se encuentra registrado", "Por favor intentelo con otro correo" ,  "error" )
-        } else {
-            swal ( "Todos los campos son obligatorios" ,  "Por favor intentelo de nuevo" ,  "error" )
-        }
-    };
+
+    // async function handleSubmit(e) {
+    //     e.preventDefault()
+    //     let isSignup = await actions.signup(inputEmail, inputPassword) 
+    //     if (isSignup === "success"){
+    //         swal ( "Registro con éxito" ,  "Gracias por registrarse en nuestra web!" ,  "success" )
+    //         navigate("/");
+    //     } else if (isSignup === "email_exist") {
+    //         swal ( "Este correo ya se encuentra registrado", "Por favor intentelo con otro correo" ,  "error" )
+    //     } else {
+    //         swal ( "Todos los campos son obligatorios" ,  "Por favor intentelo de nuevo" ,  "error" )
+    //     }
+    // };
     return (
-            <div className="contactForm">
-                <h1 className="title text-center pb-4">Crear cuenta</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label" style={{ color: 'brown' }}>Correo electrónico</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Ingrese su correo electrónico" onChange={(e) => setInputEmail(e.target.value)} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword" className="form-label" style={{ color: 'brown' }}>Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword" placeholder="xxxxxxx" onChange={(e) => setInputPassword(e.target.value)} />
-                    </div>
-                    <div className="d-flex justify-content-center mt-5">
-                        <button type="submit" className="login btn-lg btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">Regístrate</button>
-                    </div>
-                </form>
-            </div>
+        <div className="contactForm">
+            <h1 className="title text-center pb-4">Crear cuenta</h1>
+            <form onSubmit={formik.handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="InputEmail" className="form-label" style={{ color: 'brown' }}>Correo electrónico</label>
+                    <input type="email" className="form-control" name="inputEmail" id="inputEmail" placeholder="Ingrese su correo electrónico" onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.inputEmail} />
+                    {formik.touched.inputEmail && formik.errors.inputEmail ? (
+                        <div>{formik.errors.inputEmail}</div>
+                    ) : null}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="InputPassword" className="form-label" style={{ color: 'brown' }}>Contraseña</label>
+                    <input type="password" className="form-control" name="inputPassword" id="inputPassword" placeholder="xxxxxxx" onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.inputPassword} />
+                    {formik.touched.inputPassword && formik.errors.inputPassword ? (
+                        <div>{formik.errors.inputPassword}</div>
+                    ) : null}
+                </div>
+                <div className="d-flex justify-content-center mt-5">
+                    <button type="submit" className="login btn-lg btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">Regístrate</button>
+                </div>
+            </form>
+        </div>
     )
 };
