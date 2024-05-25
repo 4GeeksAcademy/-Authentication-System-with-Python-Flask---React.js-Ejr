1baa4ac7617ac7779115e2c106aed7364a7cdf28
sopze92 <60981650+Sopze92@users.noreply.github.com>
2024-05-25 02:14:08 +0200

    404 finished, fixed svg again

---
+++ src/frontend/js/app/tailwindFooler.jsx
@@ -0,0 +1,18 @@
+// UNUSED, just used to force npm run tailwind to compile some classes, feel free to add whatever
+const TailwindFooler=()=>{
+	return (
+    <div className={`
+    w-10scr   min-w-10scr   max-w-10scr   h-10scr   min-h-10scr   max-h-10scr    
+    w-20scr   min-w-20scr   max-w-20scr   h-20scr   min-h-20scr   max-h-20scr    
+    w-30scr   min-w-30scr   max-w-30scr   h-30scr   min-h-30scr   max-h-30scr    
+    w-40scr   min-w-40scr   max-w-40scr   h-40scr   min-h-40scr   max-h-40scr    
+    w-50scr   min-w-50scr   max-w-50scr   h-50scr   min-h-50scr   max-h-50scr    
+    w-60scr   min-w-60scr   max-w-60scr   h-60scr   min-h-60scr   max-h-60scr    
+    w-70scr   min-w-70scr   max-w-70scr   h-70scr   min-h-70scr   max-h-70scr    
+    w-80scr   min-w-80scr   max-w-80scr   h-80scr   min-h-80scr   max-h-80scr    
+    w-90scr   min-w-90scr   max-w-90scr   h-90scr   min-h-90scr   max-h-90scr    
+    w-100scr  min-w-100scr  max-w-100scr  h-100scr  min-h-100scr  max-h-100scr   
+    `}></div>
+  )
+}
+export default TailwindFooler
