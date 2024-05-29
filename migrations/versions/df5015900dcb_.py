c730fd2ce458bba0a74762ed8d45890792568dd4
Andrés Salamanca <141695644+asalam3312@users.noreply.github.com>
2024-05-29 01:00:29 +0000

    fix/ now the Student can see the payments made

--- migrations/versions/df5015900dcb_.py
+++ migrations/versions/df5015900dcb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7f3dbcd9b05c
+Revision ID: df5015900dcb
 Revises: 
-Create Date: 2024-05-28 03:04:52.191763
+Create Date: 2024-05-28 22:53:50.163993
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7f3dbcd9b05c'
+revision = 'df5015900dcb'
 down_revision = None
 branch_labels = None
 depends_on = None
