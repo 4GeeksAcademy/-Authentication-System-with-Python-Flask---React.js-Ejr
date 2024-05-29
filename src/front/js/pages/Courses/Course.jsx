cd6b813d76b3e37a230ef9d35662fd49e18ad885
Andrés Salamanca <141695644+asalam3312@users.noreply.github.com>
2024-05-29 04:47:46 +0000

    fix/ view course button works now, Course view renders the information for each course

--- src/front/js/pages/Courses/Course.jsx
+++ src/front/js/pages/Courses/Course.jsx
@@ -1,14 +1,96 @@
-import React from 'react'
+// import React, {useContext, useEffect, useState} from 'react'
+// import { useParams } from 'react-router-dom'
+// import { Context } from '../../store/appContext'
+// import { Navbar } from '../../component/Navbar.jsx'
+
+// const Course = () => {
+//     const { id } = useParams();
+//     const { store } = useContext(Context);
+//     const [course, setCourse] = useState(null);
+//     const [media, setMedia] = useState(null);
+
+//     useEffect(()=>{
+//       if(store.course && store.course.access_to_courses){
+//         const foundCourse = store.course.access_to_courses.find(course => course.id === parseInt(id))
+//         setCourse(foundCourse)
+//       }
+//     }, [store.course, id]);
+
+
+//     useEffect(()=>{
+//       if(store.media && store.media.access_to_media){
+//         const foundMedia = store.media.access_to_media.find(media => media.id === parseInt(id))
+//         setMedia(foundMedia)
+//       }
+//     }, [store.media, id])
+
+//     console.log(store.media)
+//   return (
+//     <div>
+//       <Navbar />
+//       {course ? (
+//         <div>
+//           <h1>{course.title}</h1>
+//           <img src={store.media} alt="Course Thumbnail" /> 
+//           <p>{course.description}</p>
+//           <div>{course.assessment}</div>
+//           <div>{course.titleTeacher}</div>
+//           <div>{course.dateExpiration}</div>
+//         </div>
+//       ): (
+//         <div>No course Data</div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default Course
+
+import React, {useContext, useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
+import { Context } from '../../store/appContext'
+import { Navbar } from '../../component/Navbar.jsx'
 
 const Course = () => {
-    const params = useParams()
+    const { id } = useParams();
+    const { store } = useContext(Context);
+    const [course, setCourse] = useState(null);
+    const [media, setMedia] = useState(null);
 
+    useEffect(()=>{
+      if(store.course && store.course.access_to_courses){
+        const foundCourse = store.course.access_to_courses.find(course => course.id === parseInt(id))
+        setCourse(foundCourse)
+      }
+    }, [store.course, id]);
+    console.log(course)
+
+    useEffect(()=>{
+      if(store.media && store.media.access_to_media){
+        const foundMedia = store.media.access_to_media.find(media => media.id === parseInt(id))
+        setMedia(foundMedia)
+      }
+    }, [store.media, id])
+
+    console.log(store.media)
   return (
     <div>
-      <div>Course {params.id}</div>
+      <Navbar />
+      {course ? (
+        <div>
+          <h1>{course.title}</h1>
+          <img src={store.media} alt="Course Thumbnail" />
+          <p>{course.description}</p>
+          <div>{course.assessment}</div>
+          <div>{course.titleTeacher}</div>
+          <div>{course.dateExpiration}</div>
+        </div>
+      ): (
+        <div>No course Data</div>
+      )}
     </div>
   )
 }
 
 export default Course
+
