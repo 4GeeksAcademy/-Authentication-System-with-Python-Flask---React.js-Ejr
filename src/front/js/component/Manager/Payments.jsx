97ae0b2b82eee52b0d7973bae91480592a2dc49d
Andrés Salamanca <141695644+asalam3312@users.noreply.github.com>
2024-05-29 23:25:18 +0000

    feat/ payments in manager

--- src/front/js/component/Manager/Payments.jsx
+++ src/front/js/component/Manager/Payments.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../../store/appContext';
 
 export const Payments = () => {
+  const { store } = useContext(Context);
+  const urlPayments = process.env.BACKEND_URL + '/api/payment/courses';
+
+  const [payments, setPayments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const paymentInfo = async () => {
+    try {
+      const response = await fetch(urlPayments);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("Data from backend:", data);
+
+      // Establecer todos los pagos recibidos del backend
+      setPayments(data.payments);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    paymentInfo();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
-    <div className="card mb-3">
-      <img src="https://revistadeconsultoria.com/wp-content/uploads/2023/12/1469101133738_1-1.jpg" className="card-img-top" alt="Pagos" />
-      <div className="card-body">
-        <h5 className="card-title">Payments</h5>
-        <p className="card-text">This is how your financial statements go</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  )
-}
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Payment #</th>
+            <th scope="col">Date</th>
+            <th scope="col">Title course</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Type of payment</th>
+            <th scope="col">User ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {payments.length > 0 ? (
+            payments.map((pay, index) => (
+              <tr key={index}>
+                <th scope="row">{pay.id}</th>
+                <td>{pay.date}</td>
+                <td>{pay.titleCourse}</td>
+                <td>{pay.padAmount}</td>
+                <td>{pay.typePayment}</td>
+                <td>{pay.userId}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="6">No payments found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
 
