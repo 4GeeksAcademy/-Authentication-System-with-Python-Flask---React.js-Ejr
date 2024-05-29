9c2788613d01c6f903edaa1d305e50704dabcaae
sopze92 <60981650+Sopze92@users.noreply.github.com>
2024-05-29 17:19:55 +0200

    update

---
+++ src/frontend/js/component/cards/workspaceCard.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
+const WorkspaceCard= ({id, title, thumbnail})=>{
+  const 
+    nav= useNavigate()
+
+  return (
+    <div className="flex flex-col gap-3">
+    <p className="ml-1 text-sm">{title}</p>
+    <button className="w-60 h-36 bg-gray-600 rounded-xl flex items-center justify-center overflow-hidden" onClick={()=>nav(`/workspace/${id}`)}>
+      <img className="transition duration-300 ease-in-out hover:scale-105" src={thumbnail} alt="workspace thumbnail" /> 
+    </button>
+  </div>
+  ) 
+}
+
+export default WorkspaceCard
