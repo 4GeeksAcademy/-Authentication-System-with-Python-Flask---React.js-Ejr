7d0a1abd63075cbcecfc27708ca5a4865e9b3234
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-05-31 20:55:32 +0000

    FRONT-Profile. Renderizado para mostrar el profile y mostrar el profile en edit profile. Dentro de esto se encuentra la informacion de inputs y de algunos botones. Seguidamente vamos con las funcionalidades de la foto de perfil.

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Context } from '../store/appContext';
 import { useNavigate } from 'react-router-dom';
+import switchIcon from '../../img/switch.png';
+import playstationIcon from '../../img/playstation.png';
+import xboxIcon from '../../img/xbox.png';
+import steamIcon from '../../img/steam.png';
+import discordIcon from '../../img/discord.png';
+import epicIcon from '../../img/epic.png';
 import '../../styles/profile.css';
 
 export const Profile = () => {
     const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
     const [profileData, setProfileData] = useState(store.user || {
         email: '',
         username: '',
@@ -24,11 +32,22 @@ export const Profile = () => {
         gender: '',
         url_image: '' // Añadir url_image al estado inicial
     });
-    const [imageFile, setImageFile] = useState(null);
+    
     const [isEditing, setIsEditing] = useState(false);
     const [error, setError] = useState(null);
-    const navigate = useNavigate();
+    const platforms = [
+        { name: 'Xbox', icon: xboxIcon },
+        { name: 'PSN', icon: playstationIcon },
+        { name: 'Steam', icon: steamIcon },
+        { name: 'Discord', icon: discordIcon },
+        { name: 'Switch', icon: switchIcon },
+        { name: 'Epic ID', icon: epicIcon }
+    ];
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [imageFile, setImageFile] = useState(null);
 
+
+    
     useEffect(() => {
         const fetchProfile = async () => {
             const userId = localStorage.getItem('userId');
@@ -43,7 +62,6 @@ export const Profile = () => {
                 navigate('/login'); // Redirige si no se pudieron obtener los datos del usuario
             }
         };
-
         fetchProfile(); // Ejecuta la función para obtener los datos del perfil al montar el componente
     }, []);
 
@@ -54,8 +72,36 @@ export const Profile = () => {
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
-        setImageFile(file);
+        if (file) {
+            setImageFile(file);
+        }
     };
+
+
+    const handleImageUpload = async () => {
+        if (!imageFile) {
+            setError('No file selected.');
+            return;
+        }
+        setError(null);
+        try {
+            const imageUrl = await actions.uploadImageToCloudinary(imageFile);
+            if (!imageUrl) {
+                throw new Error('Failed to upload image to Cloudinary');
+            }
+            const updatedProfileData = { ...profileData, url_image: imageUrl };
+            const success = await actions.updateProfile(updatedProfileData);
+            if (success) {
+                setProfileData(updatedProfileData);
+                setImageFile(null); // Clear the selected file
+            } else {
+                throw new Error('Failed to update profile with new image.');
+            }
+        } catch (error) {
+            setError(error.message || 'An unexpected error occurred.');
+        }
+    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -85,159 +131,209 @@ export const Profile = () => {
         }
     };
 
-    const handleDelete = async () => {
+    //------DELETING PROFILE HANDLERS-------------------------------------------------------------------------------------------------
+
+    // Abre el modal de confirmación
+    const handleDeleteClick = () => {
+        setShowConfirmModal(true);
+    };
+
+    // Maneja la confirmación de la eliminación
+    const handleConfirmDelete = async () => {
         try {
             const success = await actions.deleteProfile();
             if (success) {
-                navigate('/signup');
+                navigate('/signup');  // Navega al signup tras eliminar el perfil
+                setShowConfirmModal(false);  // Cierra el modal
             } else {
                 setError('Failed to delete profile. Please try again.');
+                setShowConfirmModal(false);  // Asegura cerrar el modal incluso si hay error
             }
         } catch (error) {
             setError('An unexpected error occurred. Please try again.');
+            setShowConfirmModal(false);  // Cierra el modal en caso de error
         }
     };
 
+    // Cierra el modal sin realizar acciones
+    const handleCloseModal = () => {
+        setShowConfirmModal(false);
+    };
+
+    //-------------------------------------------------------------------------------------------------------------------------------------
+
     return (
-        <div>
-            <h1>Profile</h1>
-            {!isEditing ? (
-                <div>
-                    {profileData.url_image && (
-                        <img src={profileData.url_image} alt="Profile"  className="profile-image"/>
+        <div className="container mt-3 profile-container">
+            <h6>Profile Settings</h6>
+            <div className="mb-3 row">
+                {profileData.url_image && (
+                    <img src={profileData.url_image} alt="Profile" className="profile-image mb-3" />
+                )}
+                <div className="col">
+                    {!editingImage && (
+                        <button className="btn btn-primary" onClick={handleEditPictureClick}>
+                            Edit Profile Picture
+                        </button>
                     )}
-                    <p>Email: {profileData.email}</p>
-                    <p>Username: {profileData.username}</p>
-                    <p>First Name: {profileData.first_name}</p>
-                    <p>Last Name: {profileData.last_name}</p>
-                    <p>Age: {profileData.age}</p>
-                    <p>Region: {profileData.region}</p>
-                    <p>Timezone: {profileData.timezone}</p>
-                    <p>Languages: {profileData.languages}</p>
-                    <p>Xbox: {profileData.xbox}</p>
-                    <p>PSN: {profileData.psn}</p>
-                    <p>Steam: {profileData.steam}</p>
-                    <p>Discord: {profileData.discord}</p>
-                    <p>Nintendo: {profileData.nintendo}</p>
-                    <p>Epic ID: {profileData.epic_id}</p>
-                    <p>Bio: {profileData.bio}</p>
-                    <p>Gender: {profileData.gender}</p>
-                    <button onClick={() => setIsEditing(true)}>Edit Profile</button>
+                    {editingImage && !imageFile && (
+                        <>
+                            <input
+                                type="file"
+                                id="fileInput"
+                                name="image"
+                                onChange={handleFileChange}
+                                style={{ display: 'none' }}
+                                className="form-control"
+                            />
+                            <button className="btn btn-secondary" onClick={() => document.getElementById('fileInput').click()}>
+                                Choose New Profile Picture
+                            </button>
+                            <button className="btn btn-secondary" onClick={handleCancelEditing}>
+                                Cancel Editing Profile Picture
+                            </button>
+                        </>
+                    )}
+                    {imageFile && (
+                        <>
+                            <span className="ms-2">{imageFile.name}</span>
+                            <button className="btn btn-primary" onClick={handleImageUpload}>
+                                Save Profile Picture
+                            </button>
+                            <button className="btn btn-secondary" onClick={handleCancelEditing}>
+                                Cancel Editing Profile Picture
+                            </button>
+                        </>
+                    )}
                 </div>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={profileData.username}
-                        onChange={handleInputChange}
-                    />
-                    <label>First Name:</label>
-                    <input
-                        type="text"
-                        name="first_name"
-                        value={profileData.first_name}
-                        onChange={handleInputChange}
-                    />
-                    <label>Last Name:</label>
-                    <input
-                        type="text"
-                        name="last_name"
-                        value={profileData.last_name}
-                        onChange={handleInputChange}
-                    />
-                    <label>Age:</label>
-                    <input
-                        type="number"
-                        name="age"
-                        value={profileData.age}
-                        onChange={handleInputChange}
-                    />
-                    <label>Region:</label>
-                    <input
-                        type="text"
-                        name="region"
-                        value={profileData.region}
-                        onChange={handleInputChange}
-                    />
-                    <label>Timezone:</label>
-                    <input
-                        type="text"
-                        name="timezone"
-                        value={profileData.timezone}
-                        onChange={handleInputChange}
-                    />
-                    <label>Languages:</label>
-                    <input
-                        type="text"
-                        name="languages"
-                        value={profileData.languages}
-                        onChange={handleInputChange}
-                    />
-                    <label>Xbox:</label>
-                    <input
-                        type="text"
-                        name="xbox"
-                        value={profileData.xbox}
-                        onChange={handleInputChange}
-                    />
-                    <label>PSN:</label>
-                    <input
-                        type="text"
-                        name="psn"
-                        value={profileData.psn}
-                        onChange={handleInputChange}
-                    />
-                    <label>Steam:</label>
-                    <input
-                        type="text"
-                        name="steam"
-                        value={profileData.steam}
-                        onChange={handleInputChange}
-                    />
-                    <label>Discord:</label>
-                    <input
-                        type="text"
-                        name="discord"
-                        value={profileData.discord}
-                        onChange={handleInputChange}
-                    />
-                    <label>Nintendo:</label>
-                    <input
-                        type="text"
-                        name="nintendo"
-                        value={profileData.nintendo}
-                        onChange={handleInputChange}
-                    />
-                    <label>Epic ID:</label>
-                    <input
-                        type="text"
-                        name="epic_id"
-                        value={profileData.epic_id}
-                        onChange={handleInputChange}
-                    />
-                    <label>Bio:</label>
-                    <textarea
-                        name="bio"
-                        value={profileData.bio}
-                        onChange={handleInputChange}
-                    ></textarea>
-                    <label>Gender:</label>
-                    <input
-                        type="text"
-                        name="gender"
-                        value={profileData.gender}
-                        onChange={handleInputChange}
-                    />
-                    <label>Profile Image:</label>
-                    <input type="file" name="image" onChange={handleFileChange} />
-                    <button type="submit">Update Profile</button>
-                    <button type="button" onClick={() => setIsEditing(false)}>Cancel</button>
-                </form>
-            )}
-            <button onClick={handleDelete}>Delete Profile</button>
-            {error && <p>{error}</p>}
+            </div>
+            <div>
+                <div className="row">
+                    {/** Username and Email */}
+                    <div className="mb-3 col-6">
+                        <label>Username:</label>
+                        <input type="text" value={profileData.username} readOnly={!isEditing} className="form-control" />
+                    </div>
+                    <div className="mb-3 col-6">
+                        <label>
+                            {isEditing ? "You cannot change your email address" : "Email:"}
+                        </label>
+                        <input
+                            type="text"
+                            value={profileData.email}
+                            readOnly // Este campo siempre es de solo lectura
+                            className="form-control"
+                        />
+                    </div>
+                </div>
+                {/** First Name and Last Name */}
+                <div className="row">
+                    <div className="mb-3 col-6">
+                        <label>First Name:</label>
+                        <input type="text" value={profileData.first_name} readOnly={!isEditing} className="form-control" />
+                    </div>
+                    <div className="mb-3 col-6">
+                        <label>Last Name:</label>
+                        <input type="text" value={profileData.last_name} readOnly={!isEditing} className="form-control" />
+                    </div>
+                </div>
+                {/** Password and Age */}
+                <div className="row">
+                    <div className="mb-3 col-6">
+                        <label>Password:</label>
+                        <input type="text" value={profileData.password} readOnly={!isEditing} className="form-control" />
+                    </div>
+                    <div className="mb-3 col-6">
+                        <label>Age:</label>
+                        <input type="text" value={profileData.age} readOnly={!isEditing} className="form-control" />
+                    </div>
+                </div>
+                {/** Gender and Bio */}
+                <div className="row">
+                    <div className="mb-3 col-6">
+                        <label>Gender:</label>
+                        <input type="text" value={profileData.gender} readOnly={!isEditing} className="form-control" />
+                    </div>
+                    <div className="mb-3 col-6">
+                        <label>Bio:</label>
+                        <textarea value={profileData.bio} readOnly={!isEditing} className="form-control"></textarea>
+                    </div>
+                </div>
+                {/** Gaming Profiles */}
+                <div className="row">
+                    {platforms.map((platform, index) => (
+                        <div className="mb-3 col-4" key={index}>
+                            <label>
+                                <img src={platform.icon} alt={platform.name} style={{ width: '24px', marginRight: '10px' }} />
+                            </label>
+                            <input
+                                type="text"
+                                value={profileData[platform.name.replace(/\s/g, '').toLowerCase()]}
+                                readOnly={!isEditing}
+                                className="form-control mb-3"
+                            />
+                        </div>
+                    ))}
+                </div>
+                {/** Other Details */}
+                <div className="row">
+                    {["Region", "Timezone", "Languages"].map((detail, index) => (
+                        <div className="mb-3 col-4" key={index}>
+                            <label className="me-2">{detail}:</label>
+                            <input type="text" value={profileData[detail.toLowerCase()]} readOnly={!isEditing} className="form-control mb-3" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="d-flex justify-content-end">
+                {!isEditing && (
+                    <button onClick={() => setIsEditing(true)} className="btn btn-primary">
+                        Edit Profile
+                    </button>
+                )}
+                {isEditing && (
+                    <div className="d-flex justify-content-between">
+                        <div>
+                            <button onClick={handleDeleteClick} className="btn btn-danger">
+                                Delete Profile
+                            </button>
+                            {showConfirmModal && (
+                                <div className="modal show" style={{ display: 'block' }}>
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title">Confirm Deletion</h5>
+                                                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleCloseModal}>
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div className="modal-body">
+                                                <p>Are you sure you want to delete your profile? This action cannot be undone.</p>
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary" onClick={handleCloseModal}>Cancel</button>
+                                                <button type="button" className="btn btn-danger" onClick={handleConfirmDelete}>Delete Profile</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        <div>
+                            <button onClick={() => {
+                                // Aquí debe ir el código para guardar los cambios
+                                // setIsEditing(false) puede llamarse después de actualizar los datos correctamente
+                            }} className="btn btn-primary me-2">
+                                Update Profile
+                            </button>
+                            <button type="button" onClick={() => setIsEditing(false)} className="btn btn-danger">
+                                Cancel Changes
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+            {error && <p className="text-danger">{error}</p>}
         </div>
     );
+
 };
