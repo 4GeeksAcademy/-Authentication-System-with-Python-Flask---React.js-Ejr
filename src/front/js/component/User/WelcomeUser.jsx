b69add505c10062a4b1fc7cf92ad1bfed2b10eb1
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-06-01 08:25:10 +0000

    actualizacion

--- src/front/js/component/User/WelcomeUser.jsx
+++ src/front/js/component/User/WelcomeUser.jsx
@@ -12,16 +12,19 @@ export const WelcomeUser = () => {
         setActive(!active);
     };
 
+    const payments = store.payment && store.payment.payments ? store.payment.payments : [];
+    const courses = store.course && store.course.access_to_courses ? store.course.access_to_courses : [];
+    
     return (
         <div className="container-fluid" style={{ position: "relative", textAlign: "center", height: "100vh", padding: 0, margin: 0 }}>
-
+    
             <div>
                 <img src="https://media.licdn.com/dms/image/D4D12AQFAm-bJ8YZRNQ/article-cover_image-shrink_720_1280/0/1658653581725?e=2147483647&v=beta&t=suMPUJnAI7EG2IgsJ4F7fcisAMbvVRXeJlKXJhDbF7Y" class="img-fluid opacity-50" alt="Manager" style={{ height: "50vh", width: "100%", objectFit: "cover" }} />
             </div>
             <div className='text-black fw-bolder rounded-pill' style={{ position: "absolute", top: "20%", width: "100%", color: "white" }}>
-
+    
                 <h1 className='fs-1 fw-bolder'>Welcome, Student!</h1>
-
+    
                 <h5>There is nothing better than learning and improving every day...</h5>
             </div>
             <div className="container-fluid">
@@ -36,7 +39,7 @@ export const WelcomeUser = () => {
                                 <h3>Payment Information</h3>
                             </div>
                         </div>
-
+    
                         <div className="row w-100">
                             <div className="col-12">
                                 <div
@@ -67,15 +70,15 @@ export const WelcomeUser = () => {
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
                                             Operator
                                         </a>
-
+    
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-                        { store.payment && store.payment.payments && store.payment.payments.length === 0
+    
+                        {payments.length === 0
                             ? "No hay payment cargados"
-                            : store.payment.payments.map((item) => (
+                            : payments.map((item) => (
                                 <div className="row w-100" key={item.id}>
                                     <div className="col-12">
                                         <div
@@ -112,19 +115,17 @@ export const WelcomeUser = () => {
                                 </div>
                             ))}
                     </div>
-
-                    <div className="col-6 border rounded-4 my-3 mx-3 px-2 py-2 d-flex flex-column align-items-center">
-
-                        {/* Title*/}
+                    <div className="col-5 border rounded-4 my-3 mx-3 px-2 py-2 d-flex flex-column align-items-center">
+                        {/* Title */}
                         <div className='d-flex justify-content-center align-items-center'>
                             <div className='p-3 mx-3 border rounded-circle d-flex justify-content-center align-items-center'>
-                                <FaUserGraduate />
+                                <FaUniversity />
                             </div>
                             <div>
-                                <h3>Courses Information</h3>
+                                <h3>Course Selected</h3>
                             </div>
                         </div>
-
+    
                         <div className="row w-100">
                             <div className="col-12">
                                 <div
@@ -138,43 +139,32 @@ export const WelcomeUser = () => {
                                             #
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Title
+                                            Date
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Category
+                                            Title Course
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Modules Length
+                                            Pad Amount
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Certificate To Get
+                                            Type Payment
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Price
+                                            Course Selected
                                         </a>
                                         <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Description
+                                            Operator
                                         </a>
-                                        <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Assessment
-                                        </a>
-                                        <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Create Date
-                                        </a>
-                                        <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Title Teacher
-                                        </a>
-                                        <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                            Date Expiration
-                                        </a>
+    
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-                        {store.course && store.course.access_to_courses && store.course.access_to_courses.length === 0
-                            ? "No hay course cargados"
-                            : store.course.access_to_courses.map((item) => (
+    
+                        {courses.length === 0
+                            ? "No hay courses cargados"
+                            : courses.map((item) => (
                                 <div className="row w-100" key={item.id}>
                                     <div className="col-12">
                                         <div
@@ -183,39 +173,27 @@ export const WelcomeUser = () => {
                                                 backgroundColor: item.id % 2 === 0 ? '#F0F0F0' : '#C8C8C8'
                                             }}
                                         >
-                                            <div className="rounded-2 d-flex justify-content-between w-100" style={{ overflow: 'auto' }} >
+                                            <div className="rounded-2 d-flex justify-content-between w-100" style={{ overflow: 'auto' }}>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
                                                     {item.id}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.title}
+                                                    {item.date}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.category}
+                                                    {item.titleCourse}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '50%' }}>
-                                                    {item.modulesLength}
+                                                    {item.padAmount}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.titlCertificateToGet}
+                                                    {item.typePayment}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.price}
+                                                    {item.courseId}
                                                 </a>
                                                 <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.description}
-                                                </a>
-                                                <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.assessment}
-                                                </a>
-                                                <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.createDate}
-                                                </a>
-                                                <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.titleTeacher}
-                                                </a>
-                                                <a className="text-decoration-none text-dark m-3" style={{ cursor: 'pointer', width: '10%', maxHeight: "450px" }}>
-                                                    {item.dateExpiration}
+                                                    {item.userId}
                                                 </a>
                                             </div>
                                         </div>
@@ -223,11 +201,11 @@ export const WelcomeUser = () => {
                                 </div>
                             ))}
                     </div>
-
                 </div>
             </div>
         </div>
     );
+    
 };
 
 
