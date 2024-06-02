2c89718efcd66a11153406cb32dd2d20633c92db
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-06-02 19:18:17 +0000

    Se agrga mensaje de apoyo y se hace responsive

--- src/front/js/pages/BuyMeACoffee.jsx
+++ src/front/js/pages/BuyMeACoffee.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PayPalButton from '../component/PayPalButton.jsx';
-import '../../styles/BuyMeACoffee.css'; // Asegúrate de crear este archivo
+import '../../styles/BuyMeACoffee.css'; // Asegúrate de que este archivo de CSS esté bien configurado
 
 export const BuyMeACoffee = () => {
     const [successMessage, setSuccessMessage] = useState('');
@@ -12,6 +12,9 @@ export const BuyMeACoffee = () => {
     return (
         <div className="coffee-container">
             <h1>Buy Us a Coffee</h1>
+            <p className="support-message">
+            "We play, just like you — whether it's saving princesses, building empires, or blasting through aliens. Think of this as dropping a coin into your game of choice, helping to keep our gaming universe ad-free and awesome. Chip in a coffee's worth to keep us in the game! Game on, and may the force be with you."
+            </p>
             <div className="message-container">
                 {successMessage && <p className="success-message">{successMessage}</p>}
             </div>
