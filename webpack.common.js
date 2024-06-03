cab4bd1838a594e7a25c0ef5069c3df56811170b
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-06-03 10:30:35 -0400

    se agrego logo en pestaña

--- webpack.common.js
+++ webpack.common.js
@@ -38,11 +38,10 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js']
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //       favicon: '4geeks.ico',
-  //       template: 'template.html'
-  //   }),
-  //   new Dotenv({ safe: true, systemvars: true })
-  // ]
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'template.html'
+    }),
+    new Dotenv({ safe: true, systemvars: true })
+  ]
 };
