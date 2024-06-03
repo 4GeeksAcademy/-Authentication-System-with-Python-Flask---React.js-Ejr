0d5c301832a99bfa1cf232947b2f0c7ea168e7c0
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-06-03 10:18:20 -0400

    se agrego logo en pestaña

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: 'logow.png',
+        favicon: '4geeks.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
