0a62edc01e89583fab69e3d2495669f684733e01
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-06-03 22:25:32 +0000

    fix/update card

--- src/front/js/component/Courses/CoursesContainer.jsx
+++ src/front/js/component/Courses/CoursesContainer.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useContext, useRef } from "react";
 import { Context } from '../../store/appContext';
-import '../../../styles/components.css';
-import { useNavigate } from "react-router-dom";
+import '../../../styles/components.css'
+import { useNavigate } from "react-router-dom"
 import { LuHeart } from "react-icons/lu";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa"; // Iconos para las flechas de navegación
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 
 export const CoursesContainer = () => {
     const { store, actions } = useContext(Context);
@@ -44,7 +44,7 @@ export const CoursesContainer = () => {
                 ) : (
                     store.course && store.course.access_to_courses && store.course.access_to_courses.map((item, index) => {
                         return (
-                            <div key={index} className='card border-0 cardEdit shadow rounded-5 text-white bg-dark col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-12 me-3' style={{width: "20rem"}}>
+                            <div key={index} className='card border-0 cardEdit shadow rounded-5 text-white bg-dark col-xxl-3 col-xl-3 col-lg-4 col-md-5 col-sm-12 me-3' style={{ width: "20rem" }}>
                                 <div className="card-img-top">
                                     <div className="course-thumbnail">
                                         <img
@@ -56,7 +56,7 @@ export const CoursesContainer = () => {
                                         />
                                     </div>
                                 </div>
-                                <div className="card-body rounded-bottom-4 p-3 position-relative" style={{
+                                <div className="card-body rounded-bottom-4 p-3" style={{
                                     backgroundColor: (item.id % 5 === 0) ? "#18BEBE" :
                                         (item.id % 4 === 0) ? "#3A6F99" :
                                             (item.id % 3 === 0) ? "#F6CA1F" :
@@ -67,14 +67,19 @@ export const CoursesContainer = () => {
                                         <div>
                                             <h3 className="card-title fw-bolder fs-5 text-white">{item.title}</h3>
                                         </div>
-                                        <div className="py-2 px-2 border fs-6 rounded-pill d-inline-flex justify-content-center align-items-center btnFav" style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                                        <div className="py-2 px-2 border fs-6 rounded-pill d-inline-flex justify-content-center align-items-center btnFav" >
                                             <strong>subscribe</strong>
                                         </div>
                                     </div>
-                                    <p className="border rounded-pill py-1 px-1 me-3 d-inline-flex letter text-truncate" style={{ maxWidth: '70%', backgroundColor: "rgba(255, 255, 255, 0.5)" }}>{item.categoryTitle}</p>
-                                    <p className="border rounded-pill py-1 px-1 d-inline-flex z-3 position-absolute letter text-truncate" style={{ right: 0, maxWidth: '70%', backgroundColor: "rgba(255, 255, 255, 0.5)" }}>Modulos: {item.modulesLength}</p>
+
+                                    <div className="mt-3">
+                                        <p className="border rounded-pill fs-6 py-1 px-1 me-3 d-inline-flex letter text-truncate" style={{ maxWidth: '40%', backgroundColor: "rgba(255, 255, 255, 0.5)" }}>{item.categoryTitle}</p>
+
+                                        <p className="border rounded-pill fs-6 py-1 px-1 d-inline-flex letter text-truncate" style={{ right: 0, maxWidth: '40%', backgroundColor: "rgba(255, 255, 255, 0.5)" }}>Modulos: {item.modulesLength}</p>
+                                    </div>
+
                                     <div className="d-flex justify-content-end">
-                                        <button className='py-2 px-2 border fs-6 rounded-circle d-flex justify-content-center align-items-center btnFav' onClick={() => handleAddTrolley(item.title, item.id, item.price)} style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+                                        <button className='py-2 px-2 border fs-6 rounded-circle d-flex justify-content-center align-items-center btnFav' onClick={() => handleAddTrolley(item.title, item.id, item.price)} >
                                             <LuHeart size={20} />
                                         </button>
                                     </div>
@@ -84,7 +89,7 @@ export const CoursesContainer = () => {
                     })
                 )}
             </div>
-            <button className="d-flex justify-content-center align-items-center btnFav" onClick={handleNext}>
+            <button className="btnFav d-flex justify-content-center align-items-center" onClick={handleNext}>
                 <FaArrowRight />
             </button>
         </div>
