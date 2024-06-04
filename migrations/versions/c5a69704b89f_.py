f6652c88d916cdfa0c8a0446ac346c6083fbf16d
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-06-04 11:11:15 +0000

    cambio nombre en la version de migracion

--- migrations/versions/c5a69704b89f_.py
+++ migrations/versions/c5a69704b89f_.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'eb56497f11e3'
+revision = 'c5a69704b89f'
 down_revision = None
 branch_labels = None
 depends_on = None
