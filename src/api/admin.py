6fa6de494a81425cf1a8d48aec1d3ef852c9f8da
Frank Kenneth Spacey Helder <franksh14@gmail.com>
2024-08-01 09:11:03 +0000

    tablas en SQlalchemy hechas

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,6 @@
-  
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Role, Car, Appointment, Service, Comment
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +11,11 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Role, db.session))
+    admin.add_view(ModelView(Car, db.session))
+    admin.add_view(ModelView(Appointment, db.session))
+    admin.add_view(ModelView(Service, db.session))
+    admin.add_view(ModelView(Comment, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Role(db.Model):
+    __tablename__= 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role_name = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Roles {self.role_name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "role_name": self.role_name,
+        }
+
+class User(db.Model):
+    __tablename__= 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False, unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    appointments = db.relationship('Appointment', backref='user', lazy=True)
+    cars = db.relationship('Car', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +37,93 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "phone_number": self.phone_number,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "role_id": self.role_id,
+        }
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+    id = db.Column(db.Integer, primary_key=True)
+    car_model = db.Column(db.String(120), nullable=False)
+    license_plate = db.Column(db.String(100), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Car {self.car_model}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "car_model": self.car_model,
+            "license_plate": self.license_plate,
+            "user_id": self.user_id,
+        }
+
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('cars.id'), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('services.id'), nullable=False)
+    status = db.Column(db.Boolean(), nullable=False)
+    comments = db.relationship('Comment', backref='appointment', lazy=True)
+
+    def __repr__(self):
+        return f'<Appointment {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "time": self.time,
+            "user_id": self.user_id,
+            "car_id": self.car_id,
+            "job_id": self.job_id,
+        }
+
+class Service(db.Model):
+    __tablename__ = 'services'
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(300))
+    duration = db.Column(db.Integer, nullable=False)
+    max_appointments = db.Column(db.Integer, nullable=False, default=3)
+    appointments = db.relationship('Appointment', backref='service', lazy=True)
+
+    def __repr__(self):
+        return f'<Service {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "service_name": self.service_name,
+            "description": self.description,
+            "duration": self.duration,
+            "max_appointments": self.max_appointments,
+        }
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(300), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    appointment_id = db.Column(db.Integer, db.ForeignKey('appointments.id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)   
+
+    def __repr__(self):
+        return f'<Comment {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
+            "user_id": self.user_id,
+            "appointment_id": self.appointment_id,
+            "role_id": self.role_id,
+            "timestamp": self.timestamp,
         }
