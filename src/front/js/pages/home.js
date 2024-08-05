460255723ca3b2193937ac9da958671549f6a8c2
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-08-05 07:34:11 +0000

    Home Finished

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,41 +1,83 @@
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import heroImg from "../../img/hero-img.png";
+import worldDottedMap from "../../img/world-dotted-map.png";
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+
+// const worldDottedMap = require("../../img/world-dotted-map.png");
+// const heroImg = require("../../img/hero-img.svg");
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container text-center my-5">
-      <div className="row">
-        <div className="col">
-          <div id="carouselExample" className="carousel slide">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img src="https://picsum.photos/600/400" className="d-block w-100 rounded" alt="..." />
-              </div>
-              <div className="carousel-item">
-                <img src="https://picsum.photos/600/400" className="d-block w-100 rounded" alt="..." />
-              </div>
-              <div className="carousel-item">
-                <img src="https://picsum.photos/600/400" className="d-block w-100 rounded" alt="..." />
+    <>
+      <section id="hero" className="hero section dark-background snipcss-OOBoC">
+        <img
+          src={worldDottedMap}
+          alt=""
+          className="hero-bg aos-init aos-animate"
+          data-aos="fade-in"
+        />
+        <div className="container">
+          <div className="row gy-4 d-flex justify-content-between">
+            <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+              <h2 data-aos="fade-up" className="aos-init aos-animate">
+                Your Trusted Auto Repair Workshop
+              </h2>
+              <p
+                data-aos="fade-up"
+                data-aos-delay="100"
+                className="aos-init aos-animate"
+              >
+                Keep your vehicle in top shape. Book appointments easily and
+                manage your car maintenance effortlessly with us. We're your
+                reliable partner for all auto repair needs.
+              </p>
+              <button className="btn btn-primary w-100 p-3 fw-bold">
+                Book Your Appointment at Our Trusted Workshop
+              </button>
+              <div
+                className="row gy-4 aos-init aos-animate"
+                data-aos="fade-up"
+                data-aos-delay="300"
+              >
+                <div className="col-lg-3 col-6">
+                  <div className="stats-item text-center w-100 h-100">
+                    <span>1232</span>
+                    <p>Happy Clients</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-6">
+                  <div className="stats-item text-center w-100 h-100">
+                    <span>521</span>
+                    <p>Scheduled Services</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-6">
+                  <div className="stats-item text-center w-100 h-100">
+                    <span>980</span>
+                    <p>Support Requests</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-6">
+                  <div className="stats-item text-center w-100 h-100">
+                    <span>32</span>
+                    <p>Years of Experience</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+            <div
+              className="col-lg-5 order-1 order-lg-2 hero-img aos-init aos-animate"
+              data-aos="zoom-out"
+            >
+              <img src={heroImg} className="img-fluid mb-3 mb-lg-0" alt="" />
+            </div>
           </div>
         </div>
-        <div className="col order-5">
-          Second in DOM, with a larger order
-        </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
