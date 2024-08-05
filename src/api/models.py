f4b13d75817be77b02344d2edb3c6828bca1018f
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2024-08-05 17:11:31 +0000

    fix: models relationships

--- src/api/models.py
+++ src/api/models.py
@@ -15,9 +15,7 @@ class User(db.Model):
     itineraries = db.relationship('Itinerary', back_populates='author')
     comments = db.relationship('Comments', back_populates='author')
     reports = db.relationship('Reports', back_populates='author', foreign_keys='Reports.author_id')
-    reported_user = db.relationship('Reports', back_populates='reported_user', foreign_keys='Reports.reported_user_id')
-
-
+    reported_user_reports = db.relationship('Reports', back_populates='reported_user', foreign_keys='Reports.reported_user_id')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -39,15 +37,11 @@ class Itinerary(db.Model):
     duration = db.Column(db.Integer, unique=False, nullable=False)
     images = db.Column(db.JSON, unique=False, nullable=True)
     itinerary = db.Column(db.JSON, unique=False, nullable=False)
-    creation_date = db.Column(db.Date, default=func.current_date(), unique=False, nullable=False,)
+    creation_date = db.Column(db.Date, default=func.current_date(), unique=False, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=True)
-    # score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
 
     author = db.relationship('User', back_populates='itineraries')
-    # score = db.relationship('Score', back_populates='itinerary')
     comments = db.relationship('Comments', back_populates='itinerary')
-    # tags = db.relationship('Tags', secondary='Itinerary_Tags_Rel', back_populates='itineraries')
 
     def __repr__(self):
         return f'<Itinerary {self.title}>'
@@ -68,22 +62,20 @@ class Itinerary(db.Model):
             'itinerary': self.itinerary,
             'creation_date': self.creation_date,
             'author': self.author.username,
-            'comments': [comment.serialize() for comment in self.comments],
-            'tags': [tag.serialize() for tag in self.tags]
+            'comments': [comment.serialize() for comment in self.comments]
         }
     
-
 class Comments(db.Model):
     __tablename__= 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     text = db.Column(db.String(250), unique=False, nullable=False)
-    reports_id = db.Column(db.Integer,  db.ForeignKey('reports.id'))
+    itinerary_id = db.Column(db.Integer, db.ForeignKey('itinerary.id'), nullable=False)
 
-    reports = db.relationship('Reports', back_populates='comment', foreign_keys=[reports_id])
     author = db.relationship('User', back_populates='comments')
     itinerary = db.relationship('Itinerary', back_populates='comments')
+    reports = db.relationship('Reports', back_populates='comment')  # One-to-many report
 
     def __repr__(self):
         return f'<Comments {self.id}>'
@@ -93,21 +85,21 @@ class Comments(db.Model):
             "id": self.id,
             "author_id": self.author_id,
             "text": self.text,
-            "reports_id": self.reports_id,
-            
+            "itinerary_id": self.itinerary_id,
+            "reports": [report.serialize() for report in self.reports]
         }
 
 class Reports(db.Model):
     __tablename__= 'reports'
 
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
-    reported_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
+    reported_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    comment = db.relationship('Comments', back_populates='reports', foreign_keys=[comment_id])
+    comment = db.relationship('Comments', back_populates='reports')  # One-to-many comment
     author = db.relationship('User', back_populates='reports', foreign_keys=[author_id])
-    reported_user = db.relationship('User', back_populates='reported_user', foreign_keys=[reported_user_id])
+    reported_user = db.relationship('User', back_populates='reported_user_reports', foreign_keys=[reported_user_id])
 
     def __repr__(self):
         return f'<Report {self.id}>'
@@ -118,5 +110,4 @@ class Reports(db.Model):
             "author_id": self.author_id,
             "reported_user_id": self.reported_user_id,
             "comment_id": self.comment_id,
-            
         }
