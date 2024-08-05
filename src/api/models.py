a6a675befd2ddb3225a35acc71a9da92b62c023a
ivanzr17 <139049435+ivanzr17@users.noreply.github.com>
2024-08-05 11:59:32 +0000

    EEERROOOOR

--- src/api/models.py
+++ src/api/models.py
@@ -13,7 +13,9 @@ class User(db.Model):
     creation_date = db.Column(db.Date, default=func.current_date(), unique=False, nullable=False)
 
     itineraries = db.relationship('Itinerary', back_populates='author')
-    # comments = db.relationship('Comments', back_populates='author')
+    comments = db.relationship('Comments', back_populates='author')
+    reports = db.relationship('Reports', back_populates='author', foreign_keys='Reports.author_id')
+    reported_user = db.relationship('Reports', back_populates='reported_user', foreign_keys='Reports.reported_user_id')
 
 
 
@@ -39,17 +41,18 @@ class Itinerary(db.Model):
     itinerary = db.Column(db.JSON, unique=False, nullable=False)
     creation_date = db.Column(db.Date, default=func.current_date(), unique=False, nullable=False,)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=True)
     # score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
 
     author = db.relationship('User', back_populates='itineraries')
     # score = db.relationship('Score', back_populates='itinerary')
-    # comments = db.relationship('Comments', back_populates='itinerary')
+    comments = db.relationship('Comments', back_populates='itinerary')
     # tags = db.relationship('Tags', secondary='Itinerary_Tags_Rel', back_populates='itineraries')
 
     def __repr__(self):
         return f'<Itinerary {self.title}>'
     
-    def serialize_siemple(self):
+    def serialize_simple(self):
         return {
             'id': self.id,
             'title': self.title
@@ -67,4 +70,53 @@ class Itinerary(db.Model):
             'author': self.author.username,
             'comments': [comment.serialize() for comment in self.comments],
             'tags': [tag.serialize() for tag in self.tags]
+        }
+    
+
+class Comments(db.Model):
+    __tablename__= 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
+    text = db.Column(db.String(250), unique=False, nullable=False)
+    reports_id = db.Column(db.Integer,  db.ForeignKey('reports.id'))
+
+    reports = db.relationship('Reports', back_populates='comment', foreign_keys=[reports_id])
+    author = db.relationship('User', back_populates='comments')
+    itinerary = db.relationship('Itinerary', back_populates='comments')
+
+    def __repr__(self):
+        return f'<Comments {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "author_id": self.author_id,
+            "text": self.text,
+            "reports_id": self.reports_id,
+            
+        }
+
+class Reports(db.Model):
+    __tablename__= 'reports'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    reported_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    comment = db.relationship('Comments', back_populates='reports', foreign_keys=[comment_id])
+    author = db.relationship('User', back_populates='reports', foreign_keys=[author_id])
+    reported_user = db.relationship('User', back_populates='reported_user', foreign_keys=[reported_user_id])
+
+    def __repr__(self):
+        return f'<Report {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "author_id": self.author_id,
+            "reported_user_id": self.reported_user_id,
+            "comment_id": self.comment_id,
+            
         }
