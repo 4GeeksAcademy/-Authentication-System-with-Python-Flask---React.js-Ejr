cd7ea8e2811e2a87033e9128aa14f96fe4ced27a
Lautaro <107657857+tatidee@users.noreply.github.com>
2024-08-06 15:30:41 +0000

    create models

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,73 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime,timezone
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre_usuario = db.Column(db.String(50), nullable=False)
+    apellido = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.Text, nullable=True)
+    fecha_de_nacimiento = db.Column(db.Date, nullable=False)
+    codigo_de_area = db.Column(db.String(10), nullable=False)
+    telefono = db.Column(db.String(20), unique=True, nullable=False)
+    correo = db.Column(db.String(40), unique=True, nullable=False)
+    clave = db.Column(db.String(80), nullable=False)
+    is_psicologo = db.Column(db.Boolean(), nullable=False)
+    foto = db.Column(db.String(255), nullable=True)
+    is_active = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.correo}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "nombre_usuario": self.nombre_usuario,
+            "apellido": self.apellido,
+            "descripcion": self.descripcion,
+            "fecha_de_nacimiento": self.fecha_de_nacimiento,
+            "codigo_de_area": self.codigo_de_area,
+            "telefono": self.telefono,
+            "correo": self.correo,
+            # No serialicemos la clave por motivos de seguridad
+            "is_psicologo": self.is_psicologo,
+            "foto": self.foto,
+            "is_active": self.is_active,
+        }
+    
+class Especialidades(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    especialidad = db.Column(db.String(100), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Especialidad {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "especialidad": self.especialidad,
+        }
+class Comentarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id_profesional = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comentario = db.Column(db.Text, nullable=False)
+    puntaje = db.Column(db.Integer, nullable=False)
+    fecha_de_publicacion = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    
+    usuario = db.relationship('User', foreign_keys=[id_usuario])
+    profesional = db.relationship('User', foreign_keys=[id_profesional])
+
+    def __repr__(self):
+        return f'<Comentario {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_usuario": self.id_usuario,
+            "id_profesional": self.id_profesional,
+            "comentario": self.comentario,
+            "puntaje": self.puntaje,
+            "fecha_de_publicacion": self.fecha_de_publicacion.isoformat(),
         }
