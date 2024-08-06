ae1b7434905bf5b5a2792ad78565871dae533240
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2024-08-06 14:12:04 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand ms-5" href="#">HablemosUY</a>
+    <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link active mx-4" aria-current="page" href="#">Preguntas frecuentes</a>
+        <a class="nav-link mx-4" href="#">Profesionales</a>
+		<a class="nav-link mx-4 me-5 pe-5" href="#">Precios</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
 	);
 };
