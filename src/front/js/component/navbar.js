24fe8cd02a5355de86e6915257d36ea5bfaaed7e
Kevin Padilla <kevpadilla01@gmail.com>
2024-08-07 19:33:02 +0000

    sideMenu added to navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,34 +1,21 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { SideMenu } from "./sideMenu";
 
 export const Navbar = () => {
     const { store, actions } = useContext(Context);
 
     return (
-        <nav className="navbar navbar-light bg-light">
-            <div className="container">
-                <Link to="/">
-                    <span className="navbar-brand mb-0 h1">React Boilerplate</span>
-                </Link>
-                <div className="ml-auto d-flex gap-2">
-                    {store.currentUser ? (
-                        <>
-                            <Link to="/private">
-                                <button className="btn btn-primary">Profile</button>
-                            </Link>
-                            <button className="btn btn-secondary" onClick={() => actions.logout()}>Logout</button>
-                        </>
-                    ) : (
-                        <>
-                            <Link to="/signup">
-                                <button className="btn btn-primary">Sign up</button>
-                            </Link>
-                            <Link to="/login">
-                                <button className="btn btn-primary">Login</button>
-                            </Link>
-                        </>
-                    )}
+        <nav className="p-2 border-b border-neutral-600">
+            <div className="flex justify-between items-center w-full">
+                <SideMenu />
+                <div className="flex items-center gap-1">
+                    <svg className="size-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  fill="#e8eaed"><path d="m536-84-56-56 142-142-340-340-142 142-56-56 56-58-56-56 84-84-56-58 56-56 58 56 84-84 56 56 58-56 56 56-142 142 340 340 142-142 56 56-56 58 56 56-84 84 56 58-56 56-58-56-84 84-56-56-58 56Z" />
+                    </svg>
+                    <span className="font-bold text-xl text-neutral-50 mr-2">
+                        GymTrack
+                    </span>
                 </div>
             </div>
         </nav>
