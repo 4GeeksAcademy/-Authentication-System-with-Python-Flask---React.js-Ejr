93129779cd9138cbd80ee0bb5dc45a6b852e277b
MenUrux <jorgepino.utu@gmail.com>
2024-08-07 14:27:36 +0000

    Modified dashboard, added carousel, remake homeview

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -2,17 +2,17 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
 
+import { CarouselHome } from "../component/carouselHome";
 
-export const Home = () => {
+export const Dashboard = () => {
 	const { store, actions } = useContext(Context);
-
 
 	return (
 		<>
-			<h1>Soy el home</h1>
-			<Link className="text-red-600 p-4 border-2 border-solid border-red-500" to={'/dashboard'}>Dashboard</Link>
+			<CarouselHome />
 		</>
 	);
+
+
 };
