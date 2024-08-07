d399e612da3f89ae20ecc78b9ccdd1f51c233281
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2024-08-07 15:45:21 +0000

    sección profesionales destacados

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			footer
 		</p>
 	</footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../img/logo.png"
+import logo from "../../img/logo.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary navbar-transparent">
-  <div className="container-fluid">
-	<div className="ms-5">
-	<img className="logo" src={logo} />
-    <a className="navbar-brand text-light ms-3" href="#">HablemosUY</a>
-	</div>
-	
-    <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav ms-auto">
-        <a className="nav-link active mx-4 text-light" aria-current="page" href="#">Preguntas frecuentes</a>
-        <a className="nav-link mx-4 text-light" href="#">Profesionales</a>
-		<a className="nav-link mx-4 me-5 pe-5 text-light" href="#">Precios</a>
-      </div>
-    </div>
-  </div>
-</nav>
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-transparent">
+      <div className="container-fluid">
+        <div className="ms-5">
+          <img className="logo" src={logo} alt="Logo" />
+          <Link className="navbar-brand text-light ms-3" to="/">HablemosUY</Link>
+        </div>
 
-	);
+        <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link active mx-4 text-light" aria-current="page" to="/">Preguntas frecuentes</Link>
+            <Link className="nav-link mx-4 text-light" to="/">Profesionales</Link>
+            <Link className="nav-link mx-4 text-light" to="/">Precios</Link>
+            <Link to="/vista-login">
+              <button type="button" className="btn btn-outline-light me-5">Iniciar sesión</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
