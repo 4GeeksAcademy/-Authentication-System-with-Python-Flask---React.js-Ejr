f27a7f6b7ed07cdd65b54794a84d05bef7a275b7
MenUrux <jorgepino.utu@gmail.com>
2024-08-07 21:02:19 +0000

    added profile

---
+++ src/front/js/component/profileIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+// Acá habría que pasar el usuario x el store y despues mostrarle la imagen, si es que le vamos a poner
+const ProfileIcon = () => (
+    <img className='size-40 rounded-full' src='https://avatar.iran.liara.run/public/girl' />
+);
+
+export default ProfileIcon;
