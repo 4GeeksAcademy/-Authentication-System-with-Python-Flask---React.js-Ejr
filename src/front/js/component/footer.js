8366706f0a69dea322712ae635bdbb2709d89800
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-08-08 15:49:45 +0000

    v2

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,30 +1,29 @@
 import React, { Component } from "react";
 import "../../styles/footer.css";
 import { FaFacebook, FaInstagram, FaTwitter  } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 export const Footer = () => (
-	<footer className="footer flex-sm-row flex-column text-center ">
-			<div className="text-footer ">
-			<ul className="d-flex list-inline m-0">
-				<li className="list-inline-item me-4"><a href="#">Sobre nosotros</a></li>
-				<li className="list-inline-item me-4"><a href="#">Contáctanos</a></li>
-				<li className="list-inline-item me-4"><a href="#">Política de privacidad</a></li>
-				<li className="list-inline-item me-4"><a href="#">Política de cookies</a></li>
-				<li className="list-inline-item me-4"><a href="#">Política de Copyright</a></li>
-				<li className="list-inline-item me-4"><a href="#">Condiciones legales</a></li>
+	<div className="container">
+	<footer className="footer d-flex  justify-content-around align-items-center m-0">
+			<div className="text-footer">
+				<a className="me-3" href="#">Sobre nosotros</a>
+				<a className="me-3" href="#">Contáctanos</a>
+				<a className="me-3" href="#">Política de privacidad</a>
+				<a className="me-3" href="#">Política de cookies</a>
+				<a className="me-3" href="#">Política de Copyright</a>
+				<a className="me-3" href="#">Condiciones legales</a>
 			
-			</ul>
+			
 			</div>
-			<div className="iconos-footer ">
-			<ul className="d-flex list-inline m-0">
-				<li className="list-inline-item me-4">¡Síguenos!</li>
-				<li className="list-inline-item me-4"><a href="https://www.facebook.com/groups/fitiniespana/?locale=es_LA" target="_blank"><FaFacebook /></a></li>
-				<li className="list-inline-item me-4"><a href="https://www.instagram.com/empleos_espana.es" target="_blank"><FaInstagram /></a></li>
-				<li className="list-inline-item me-4"><a href="https://x.com/empleocero" target="_blank"><FaTwitter /></a></li>
+			<div className="iconos-footer">
+				<span className="list-inline-item me-4">¡Síguenos!</span>
+				<a className="me-4" href="https://www.facebook.com/groups/fitiniespana/?locale=es_LA" target="_blank"><FaFacebook /></a>
+				<a className="me-4" href="https://www.instagram.com/empleos_espana.es" target="_blank"><FaInstagram /></a>
+				<a className="me-4" href="https://x.com/empleocero" target="_blank"><FaTwitter /></a>
 				
-			</ul>
 			</div>
-
+			
 	</footer>
+	</div>
+	
 );
