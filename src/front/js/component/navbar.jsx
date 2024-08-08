b29cd1cd9389588c4894ee2f8ace548b722d9697
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-08-08 18:05:02 +0000

    Nav-Bar luis

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -5,12 +5,12 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/home" className="custom-link">
+					<span className="navbar-brand mb-0 h1 custom-font">ShareTrips</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn custom-button rounded-pill "> <i class="bi bi-person-circle"></i> Iniciar Sesion</button>
 					</Link>
 				</div>
 			</div>
