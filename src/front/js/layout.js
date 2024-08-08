d023995e1ddf8fdd9b61e2fddf2b426b31059630
mateo1627 <140858704+mateo1627@users.noreply.github.com>
2024-08-08 12:44:42 +0000

    Login near completion; fetchs left

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -20,7 +20,7 @@ const Layout = () => {
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
-        <div>
+        <>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
@@ -35,7 +35,7 @@ const Layout = () => {
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
-        </div>
+        </>
     );
 };
 
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,52 +1,21 @@
+import axios from "axios";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+		users:[]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			loginFetch: async () => {
+				try {
+				  let response = await axios.get(`${process.env.BACKEND_URL}/api/login`);
+				 console.log(response)
+				  return response.data;
+				} catch (error) {
+				  console.error('Error:', error);
+				  throw error; 
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			  },
 		}
 	};
 };
