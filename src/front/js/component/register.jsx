20c2606971f814d7078ddb6cb560c88b93769374
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-08-08 17:16:20 +0000

    login component changes

--- src/front/js/component/register.jsx
+++ src/front/js/component/register.jsx
@@ -1,30 +1,118 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 
 export const Register = () => {
+
+	const formik = useFormik({
+		initialValues: {
+			nombre: '',
+			apellido: '',
+			fechaDeNacimiento: '',
+			codigoPais: '',
+			telefono: '',
+			email: '',
+			password: '',
+			checkTerminos: ''
+		},
+		validationSchema: Yup.object({
+			nombre: Yup.string().min(4, '4 characters').required('Nombre requerido'),
+			apellido: Yup.string().required('Apellido requerido'),
+			fechaDeNacimiento: Yup.string().required('Fecha de nacimiento requerida'),
+			codigoPais: Yup.string().max(4, '4 characters').required('Código de área requerido'),
+			telefono: Yup.string().required('Teléfono requerido'),
+			email: Yup.string().required('Email requerido'),
+			password: Yup.string().min(7, '7 characters').required('Contraseña requerida'),
+			checkTerminos: Yup.string().required('Debe aceptar Términos y Condiciones')
+		}),
+		onSubmit: values => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+
+
 	return (
 		<div className="form-container text-center mb-3 border border-3 rounded">
-			<p className="title fw-semibold text-opacity-75">Registrarse</p>
-			<form className="form">
-				<input type="text" className="input" placeholder="Nombre:" />
-				<input type="text" className="input" placeholder="Apelldo:" />
-				<input type="date" className="input" placeholder="Fecha de nacimiento:" />
-				<label htmlFor="codigo-pais" className="d-inline-flex p-2">Código de Área:</label>
-				<input list="codigos-pais" id="codigo-pais" name="codigo-pais" placeholder="+589" />
-				<datalist id="codigos-pais">
-					<option value="+1">EE.UU</option>
-					<option value="+34">España</option>
+			<p className="title fw-semibold text-secondary">Registrarse</p>
+			<form className="form" onSubmit={formik.handleSubmit}>
+				<input type="text" className="input" name="nombre" placeholder="Nombre:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.nombre} />
+				{formik.touched.nombre && formik.errors.nombre ? (
+					<div className="text-danger">{formik.errors.nombre}</div>
+				) : null}
+
+				<input type="text" className="input" name="apellido" placeholder="Apellido:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.apellido} />
+				{formik.touched.apellido && formik.errors.apellido ? (
+					<div className="text-danger">{formik.errors.apellido}</div>
+				) : null}
+
+				<label htmlFor="fecha-de-nacimiento" className="d-inline-flex p-2 text-secondary">Fecha de nacimiento:</label>
+				<input type="date" className="input text-secondary" name="fechaDeNacimiento" placeholder="Fecha de nacimiento:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.fechaDeNacimiento} />
+				{formik.touched.fechaDeNacimiento && formik.errors.fechaDeNacimiento ? (
+					<div className="text-danger">{formik.errors.fechaDeNacimiento}</div>
+				) : null}
+
+				<label htmlFor="codigo-pais" className="d-inline-flex p-2 text-secondary">Código de Área:</label>
+				<input list="codigos-pais" className="input text-secondary" id="codigo-pais" name="codigoPais" placeholder="+589"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.codigoPais} />
+				{formik.touched.codigoPais && formik.errors.codigoPais ? (
+					<div className="text-danger">{formik.errors.codigoPais}</div>
+				) : null}
+
+				<datalist className="bg-waring" id="codigos-pais">
+					<option value="+54">Argentina</option>
+					<option value="+56">Chile</option>
+					<option value="+57">Colombia</option>
 					<option value="+52">México</option>
-					<option value="+44">Reino Unido</option>
+					<option value="+595">Paraguay</option>
+					<option value="+51">Perú</option>
 					<option value="+589">Uruguay</option>
+					<option value="+58">Venezuela</option>
 				</datalist>
-				<input type="text" className="input" placeholder="Teléfono:" />
-				<input type="email" className="input" placeholder="Email" />
-				<input type="password" className="input" placeholder="Password" />
+
+				<input type="text" className="input" name="telefono" placeholder="Teléfono:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.telefono} />
+				{formik.touched.telefono && formik.errors.telefono ? (
+					<div className="text-danger">{formik.errors.telefono}</div>
+				) : null}
+
+				<input type="email" className="input" name="email" placeholder="Email:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.email} />
+				{formik.touched.email && formik.errors.email ? (
+					<div className="text-danger">{formik.errors.email}</div>
+				) : null}
+				<input type="password" className="input" name="password" placeholder="Password:"
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.password} />
+				{formik.touched.password && formik.errors.password ? (
+					<div className="text-danger">{formik.errors.password}</div>
+				) : null}
 				<div className="">
-					<input className="form-check-input me-2" type="checkbox" id="checkboxNoLabel" value="" aria-label="Términos y condiciones" />
+					<input className="form-check-input me-2" type="checkbox" id="checkboxNoLabel" name="checkTerminos" aria-label="Términos y condiciones" 
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.checkTerminos} />
+				{formik.touched.checkTerminos && formik.errors.checkTerminos ? (
+					<div className="text-danger">{formik.errors.checkTerminos}</div>
+				) : null}
 					<span>
 						He leído y acepto los términos y condiciones de uso.
 					</span>
@@ -40,7 +128,7 @@ export const Register = () => {
 				<span class="linea"></span>
 			</div>
 			<div className="buttons-container">
-				<button className="button mt-4 justify-content-center">
+				<button className="button justify-content-center">
 					<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262">
 						<path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
 						<path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
