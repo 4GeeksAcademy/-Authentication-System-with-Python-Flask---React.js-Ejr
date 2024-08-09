e3e209b08f770e819f6e798e27a7fc13d195d698
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-08-09 08:55:30 +0000

    actualización con layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
+
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
@@ -12,8 +13,18 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 
-const Layout = () => {
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import EditarPerfil from "./pages/editarPerfil";
+import Favoritos from "./pages/favoritos";
+import Cursos from "./pages/cursos";
+import Logout from "./pages/logout";
 
+
+//create your first component
+const Layout = () => {
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
@@ -25,10 +36,14 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                        <Route element={<Perfil />} path="/perfil" />
+                        <Route element={<Single />} path="/single/:theid"/>
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<SignUp />} path="/signup"/>
+                        <Route element={<EditarPerfil />} path="/editarPerfil"/>
+                        <Route element={<Favoritos />} path="/favoritos" />
+                        <Route element={<Cursos />} path="/cursos" />
+                        <Route element={<Logout />} path="/logout" />
+                        <Route element={<h1>Not found!</h1>} path="*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
