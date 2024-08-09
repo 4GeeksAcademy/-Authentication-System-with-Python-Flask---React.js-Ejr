ddf53a655a2af5b29de168492df1f925d57038c4
Stephaniemtoyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-08-09 09:13:05 +0000

    cambios en el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-
+import "../../styles/footer.css";
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<footer>
+        <div class="footer-container">
+            <ul class="footer-links">
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#about">Sobre Nosotros</a></li>
+                <li><a href="#services">Servicios</a></li>
+                <li><a href="#contact">Contacto</a></li>
+            </ul>
+			<p>&copy; 2024 LEARNING NETWORK. Todos los derechos reservados.</p>
+        </div>
+    </footer>
 );
