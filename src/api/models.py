bd068c7bd565db9dbde95e664f49e51ea9463b77
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-08-09 17:34:49 +0000

    tablas

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column (db.String (20), unique=True, nullable=False)
+    username = db.Column (db.String(50), unique=True, nullable=False)
+    photo = db.Column (db.String(200), unique=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    country = db.Column (db.String (20), unique=False, nullable=False)
+    #Profile_programador i profile_empleador pendiente
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +19,12 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "username": self.username,
+            "photo": self.photo,
+            "country": self.country
+
             # do not serialize the password, its a security breach
         }
+    
+    
