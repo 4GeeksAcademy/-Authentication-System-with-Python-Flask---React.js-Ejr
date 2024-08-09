14a392046388215dbb9c3f5b2f390a8f747a5389
Juan Diego Elissalde Gudefín <118232153+Juan-Diego-E@users.noreply.github.com>
2024-08-09 12:45:39 +0000

    Creating home view

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import BannerImg from "../../../../public/images/banner-bg.png";
+import BannerImg from "../../../../public/images/banner-2-bg.png";
+import GirlImg from "../../../../public/images/girl-ph.jpg";
 import NutriImg from "../../../../public/images/chicken-plate.jpg";
 import SportsImg from "../../../../public/images/chicken-plate.jpg";
 import ScrollBanner from "../../../../public/images/scroll-banner-bg.png";
@@ -11,16 +12,22 @@ export const Home = () => {
 
 	return (
 		<div className="home-container mx-auto w-100 row">
-			<div className="banner mx-auto w-100 col">
-				<img src={BannerImg} className="w-100" />
-				<h1>Bienvenid@</h1>
+			{/* **********___BANNER___********** */}
+			<div className="banner w-100 p-0">
+				<div className="title-container text-center">
+					<h1>Bienvenid@</h1>
+				</div>
+				<img src={GirlImg} />
 			</div>
-			<section className="d-flex mx-auto col-10">
-				<article className="d-flex flex-column align-center">
+			
+
+			{/* **********___PRIMER___SECCION___********** */}
+			<section className="first-section mx-auto col-10">
+				<article className="d-flex flex-column align-center justify-content-between">
 					<h1 className="border-bottom">
 						Nutrición
 					</h1>
-					<div className="text">
+					<div className="text-container">
 						<p className="fs-5">
 							Lorem ipsum dolor sit amet consectetur,
 							adipisicing elit. Pariatur quos voluptate,
@@ -33,21 +40,80 @@ export const Home = () => {
 							Aut veritatis qui, officia enim illo culpa.
 						</p>
 					</div>
-					<div>
-						<a></a>
-					</div>
+					<a>
+						<h5>
+							Más...
+						</h5>
+					</a>
 				</article>
-				<div className="img-container my-auto">
+				<div className="img-container">
 					<img src={NutriImg} className="float-end" />
 				</div>
 			</section>
-			{/* <img className="rounded-5 object-fit-cover" src="https://i.pinimg.com/736x/32/dc/77/32dc7737da3054d0933bb59254bebb35.jpg" /> */}
-			{/* Deportes */}
 			<div className="parallax parallax-container-1 w-100"></div>
 
-			<div className="parallax parallax-container-2 w-100"></div>
-			<section>
 
+
+			{/* **********___SEGUNDA___SECCION___********** */}
+			<section className="second-section mx-auto col-10">
+				<article className="d-flex flex-column align-center">
+					<h1 className="border-bottom">
+						Deportes
+					</h1>
+					<div className="text-container">
+						<p className="fs-5">
+							Lorem ipsum dolor sit amet consectetur,
+							adipisicing elit. Pariatur quos voluptate,
+							modi libero, asperiores distinctio voluptatem
+							architecto vitae, quae nulla optio molestias
+							aliquam. Error perspiciatis praesentium omnis
+							quaerat temporibus quo cumque illum labore nostrum
+							dignissimos a nisi molestiae, fugiat nemo ex
+							accusamus saepe quidem reprehenderit repellendus!
+							Aut veritatis qui, officia enim illo culpa.
+						</p>
+					</div>
+					<a>
+						<h5>
+							Más...
+						</h5>
+					</a>
+				</article>
+				<div className="img-container">
+					<img className="rounded-5 object-fit-cover" src="https://i.pinimg.com/736x/32/dc/77/32dc7737da3054d0933bb59254bebb35.jpg" />
+				</div>
+			</section>
+			<div className="parallax parallax-container-2 w-100"></div>
+
+
+
+			{/* **********___SECCION___FINAL___********** */}
+			<section className="mx-auto col-10 d-flex justify-space-between">
+				<article className="d-flex flex-column align-center w-50">
+					<h1 className="border-bottom">
+						Tips
+					</h1>
+					<div className="text-container">
+						<ul className="fs-4">
+							<li>Tip 1</li>
+							<li>Tip 2</li>
+							<li>Tip 3</li>
+							<li>Tip 4</li>
+							<li>Tip 5</li>
+						</ul>
+					</div>
+				</article>
+				<article className="d-flex flex-column align-center w-50-lg text-center">
+					<h1 className="border-bottom">
+						Nuestros productos
+					</h1>
+					<div className="products-container container row row-cols-2 w-100 mx-auto">
+						<div className="col bg-warning p-5">Prod 1</div>
+						<div className="col bg-success p-5">Prod 2</div>
+						<div className="col bg-success p-5">Prod 2</div>
+						<div className="col bg-warning p-5">Prod 3</div>
+					</div>
+				</article>
 			</section>
 		</div>
 	);
