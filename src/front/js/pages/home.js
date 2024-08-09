511d0aa238cec83cb9b6e52265d90d0e639ce6cb
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-08-09 17:34:49 +0000

    tablas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Navbar } from "../component/navbar";
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 return (
-		<div>
-		  <Navbar />
-		</div>
+	<div>
+		
+	</div>
 	  );
 };
