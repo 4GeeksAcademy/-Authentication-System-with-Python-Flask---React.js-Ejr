1afd0d1cb7ccf80e523d299469ca9a042e58bce3
darioduarte1 <darioduarte1991@gmail.com>
2024-08-09 16:49:18 +0000

    alteracion backend, alteracion nombre jumbotron, vista contactus

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../component/tickeate.png";
 import { useNavigate } from "react-router-dom";
 import "../../styles/navbar.css";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
   const navigate = useNavigate()
