d6dfd18e4e0d4afa5ba7c83da3b84fce9d056481
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-08-09 10:19:43 +0000

    Contruccion del jumbotron

---
+++ src/front/js/component/jumbotron.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <div>
+            <header className="bg-light py-5">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-8">
+                            <h1 className="display-4 fw-bold">Welcome to My Website</h1>
+                            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                        </div>
+                        <div className="col-lg-4 text-center">
+                            <img src="https://cdn.midjourney.com/b0e208dd-bb10-4583-8528-132745647d7d/0_2.png" alt="Example" className="img-fluid rounded" />
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default jumbotron;
