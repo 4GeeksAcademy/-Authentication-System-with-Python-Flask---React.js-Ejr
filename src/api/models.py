4587b653054c329cd0e67dec9187e736dcfa5394
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-08-09 18:06:11 +0000

    tablas modificadas

--- src/api/models.py
+++ src/api/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     country = db.Column (db.String (20), unique=False, nullable=False)
+   
     #Profile_programador i profile_empleador pendiente
 
     def __repr__(self):
@@ -26,5 +27,28 @@ class User(db.Model):
 
             # do not serialize the password, its a security breach
         }
+
+class Programador(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #proyectos 
+    precio_hora = db.Column (db.Integer, unique=False)
+    tecnologias = db.Column (db.String(200), unique=False)
+    experiencia = db.Column(db.String(200), unique=False)
+    descripcion = db.Column(db.String(300), unique=False)
+    rating = db.Column (db.Float(2), unique=False)
     
-    
+
+    def __repr__(self):
+        return f'<Programador {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "precio_hora": self.precio_hora,
+            "tecnologias": self.tecnologias,
+            "experiencia": self.experiencia,
+            "descripcion": self.descripcion,
+            "rating": self.rating
+
+            # do not serialize the password, its a security breach
+        }
