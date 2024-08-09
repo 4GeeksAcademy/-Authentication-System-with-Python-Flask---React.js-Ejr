d0bac1e5d2865ec6be774de1601c9aada427cc41
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-08-09 11:26:09 +0000

    4º push

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/signUp.css";
-
 import { Context } from "../store/appContext";
+
+//PENDIENTE CAMBIAR FUNCIONES Y CONECTAR CON API
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
@@ -28,22 +29,11 @@ export const Demo = () => {
         console.log(dataForm)
     }
 
-    function toggleCheckbox(alumnoCheck, profesorCheck) {
-        const alumnoCheckbox = document.getElementById(alumnoCheck);
-        const profesorCheckbox = document.getElementById(profesorCheck);
-
-        if (alumnoCheckbox.checked) {
-            profesorCheckbox.checked = false;
-        }
-    }
-
-    //toggleCheckbox(alumnoCheck, profesorCheck)
-
     return(
         <div>
-            <form className="container d-flex flex-column align-items-center mt-5 p-5" id="formularioRegistro">
+            <form className="container d-flex flex-column align-items-center mt-5 p-5" id="formularioRegistro" onSubmit={handleSubmit}>
 				<label>Email
-                    <input className="form-control" name="email" value={dataForm.email} placeholder="" onChange={handleChange} type="text"></input>
+                    <input className="form-control" name="email" value={dataForm.email} placeholder="Introduce tu email aquí" onChange={handleChange} type="text"></input>
                 </label>
 			    <label>Password
                     <input className="form-control" name="phone" value={dataForm.password} placeholder="" onChange={handleChange} type="text"></input>
@@ -52,17 +42,19 @@ export const Demo = () => {
                     <input className="form-control" name="address" value={dataForm.confirmPassword} placeholder="" onChange={handleChange} type="text"></input>
                 </label>
 				<div className="p-3">Registrarse como:</div>
-  				<div>
-					<div class="mb-3 form-check">
-    					<input type="checkbox" class="form-check-input" id="alumnoCheck"></input>
-    						<label class="form-check-label" for="alumnoCheck">Alumno</label>
-  					</div>
-					<div class="mb-3 form-check">
-    					<input type="checkbox" class="form-check-input" id="profesorCheck"></input>
-    						<label class="form-check-label" for="profesorCheck">Profesor</label>
-  					</div>
-				</div>
-				<input className="btn btn-primary" value="Regístrate" type="submit"/>
+  				<div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+                    <label className="form-check-label" for="flexRadioDefault1">
+                        Alumno
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+                    <label className="form-check-label" for="flexRadioDefault2">
+                        Profesor
+                    </label>
+                </div>
+				<input className="btn btn-primary mt-3" value="Regístrate" type="submit"/>
 			</form>
         </div>
     )
