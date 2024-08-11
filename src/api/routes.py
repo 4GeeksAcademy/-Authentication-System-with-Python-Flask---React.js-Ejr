42c6027cde56796b0033cfda336a00daa1e0bb40
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-08-11 15:56:14 +0000

    finish login/register whith JWT and BCRYPT

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,11 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 from api.models import db, User, Programador
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import generate_password_hash , check_password_hash
+
 
 api = Blueprint('api', __name__)
+
 
 # Allow CORS requests to this API
 CORS(api)
@@ -29,14 +33,32 @@ def register():
     password = request.json.get("password", None)
     country = request.json.get("country", None)
     if not name or not username or not email or not password or not country:
-        return jsonify({'success':False, 'msg':'Todos los campos son necesarios'})
+        return jsonify({'register':False, 'msg':'Todos los campos son necesarios'})
     email_exist = User.query.filter_by(email=email).first()
     if email_exist:
-        return jsonify({'success': False, 'msg':'Ya existe una cuenta registrada con el email '+ email}),400
+        return jsonify({'register': False, 'msg':'Ya existe una cuenta registrada con el email '+ email}),400
 
-    new_user = User(name=name, username=username, email=email, password=password, country=country )
+    hashed_password = generate_password_hash(password).decode('utf-8')
+    
+    new_user = User(name=name, username=username, email=email, password=hashed_password, country=country )
+    access_token = create_access_token(identity=new_user.id)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'success': True, 'msg':'Usuario registrado correctamente', 'user':new_user.serialize()}),200
+    return jsonify({'success': True, 'msg':'Usuario registrado correctamente', 'user':new_user.serialize(), 'token':access_token}),200
 
 
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user= User.query.filter_by(email=email).first()
+    if user:
+        if (check_password_hash(user.password, password)):
+            access_token = create_access_token(identity=user.id)
+            return jsonify({'login': True, 'msg': 'Has iniciado sesión', 'user':user.serialize(), 'token': access_token}),200
+        return jsonify({'login': False, 'msg': 'La contraseña es incorrecta'}),400
+    return jsonify({'login': False, 'msg': 'No hay ningún usuario registrado con los datos introducidos'}),404
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=3245, debug=True)
+        
