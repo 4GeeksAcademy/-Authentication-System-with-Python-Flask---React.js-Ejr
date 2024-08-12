6d4dd41aef2ca549d0559daa698f30638cb9be46
Giordi29 <157144092+Giordi29@users.noreply.github.com>
2024-08-12 10:40:51 +0000

    trabajando en signup

--- src/front/js/component/signup.js
+++ src/front/js/component/signup.js
@@ -1,47 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/singup.css";
 
-const Singup = () => {
-    return (
-        <div className="container" id="container">
-            <div className="form-container sign-up">
-                <form>
-                    <h1>Create Account</h1>
-                    <div className="social-icons">
-                        <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                    </div>
-                    <span>or use your email for registration</span>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button type="submit">Sign Up</button>
-                </form>
-            </div>
-            <div className="form-container sign-in">
-                <form>
-                    <h1>Sign In</h1>
+const Signup = () => {
+    const [isActive, setIsActive] = useState(false);
 
-                    <span>Use your email and password</span>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <a href="#">Forget Your Password?</a>
-                    <button type="submit">Sign In</button>
-                </form>
-            </div>
-            <div className="toggle-container">
-                <div className="toggle">
-                    <div className="toggle-panel toggle-left">
-                        <h1>Welcome Back!</h1>
-                        <p>Enter your personal details to use all of site features</p>
-                        <button className="hidden" id="login">Sign In</button>
-                    </div>
-                    <div className="toggle-panel toggle-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Register with your personal details to use all of site features</p>
-                        <button className="hidden" id="register">Sign Up</button>
+    const handleRegisterClick = () => {
+        setIsActive(true);
+    };
+
+    const handleLoginClick = () => {
+        setIsActive(false);
+    };
+
+    return (
+        <div className="singup-body">
+            <div className={`singup-container ${isActive ? "singup-active" : ""}`} id="container">
+                <div className="singup-form-container singup-sign-up">
+                    <form>
+                        <h1>Create Account</h1>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+                <div className="singup-form-container singup-sign-in">
+                    <form>
+                        <h1>Sign In</h1>
+                        <span>Use your email and password</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forget Your Password?</a>
+                        <button type="submit">Sign In</button>
+                    </form>
+                </div>
+                <div className="singup-toggle-container">
+                    <div className="singup-toggle">
+                        <div className="singup-toggle-panel singup-toggle-left">
+                            <h1>Welcome Back!</h1>
+                            <p>Enter your personal details to use all of site features</p>
+                            <button className="singup-hidden" id="login" onClick={handleLoginClick}>
+                                Sign In
+                            </button>
+                        </div>
+                        <div className="singup-toggle-panel singup-toggle-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Register with your personal details to use all of site features</p>
+                            <button className="singup-hidden" id="register" onClick={handleRegisterClick}>
+                                Sign Up
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -49,4 +58,4 @@ const Singup = () => {
     );
 };
 
-export default Singup;
+export default Signup;
