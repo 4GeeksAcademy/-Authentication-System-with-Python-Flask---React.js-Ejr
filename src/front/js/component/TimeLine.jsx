783fd98b3d55ccd8954144a6f000b92767b10a21
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-12 19:36:12 +0000

    Modificacion de CardOffer y timeLine

--- src/front/js/component/TimeLine.jsx
+++ src/front/js/component/TimeLine.jsx
@@ -4,12 +4,21 @@ export const TimeLine = () =>{
 
 
 
-    return(
-        <>
-            <h2>Time Line</h2>
-        </>
-
-
-
-    )
+    return (
+        <div className="container mt-5">
+          <div className="row">
+            {jobOffers.map((offer, index) => (
+              <div className="col-md-4" key={index}>
+                <JobOfferCard 
+                  title={offer.title}
+                  company={offer.company}
+                  location={offer.location}
+                  salary={offer.salary}
+                  description={offer.description}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      );
 }
