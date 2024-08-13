bf25d4dbc968ce842c3d932b122aa91e92fb732d
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2024-08-13 18:36:34 +0000

    First Commit on Navbar

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,31 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Beer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+   # brewery_id = db.Column(db.Integer, db.ForeignKey('brewery.id'), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    bjcp_style = db.Column(db.String(120), nullable=False)
+    IBUs = db.Column(db.String(120), nullable=False)
+    volALC = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    picture_of_beer_url = db.Column(db.String(250), nullable=True) 
+
+
+    def __repr__(self):
+        return f'Beer {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+    #        "brewery_id": self.brewery_id,
+            "name": self.name,
+            "bjcp_style": self.bjcp_style,
+            "IBUs": self.IBUs,
+            "volALC": self.volALC,
+            "description": self.description,
+            "picture_of_beer_url": self.picture_of_beer_url   
+        }
