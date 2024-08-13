a0817a5aa5f92427fb59b2df761765087ce100bc
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-08-13 00:12:26 +0000

    started routes this is crashing

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,48 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from models import User, Game
 
-api = Blueprint('api', __name__)
+app = Blueprint('app', __name__)
 
 # Allow CORS requests to this API
-CORS(api)
+CORS(app)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
+@app.route('/users', methods=['GET'])
+def all_users():
+    users = User.query.all()
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
-    return jsonify(response_body), 200
+    return jsonify(users), 200
+
+@app.route('/user/<int:id>', methods=['POST', 'GET', 'DELETE', 'PATCH'])
+def one_user(req, id):     
+    if req.method =='POST':
+        #add a user into database
+        if req.body.user_name and req.body.password:
+            #IF USER NAME ALREADY EXISTS THEN RETURN "USERNAME ALREADY EXISTS", 400 SOMETHING RESEARCH @TODO
+            #IF PASSWORD IS INCORRECT THEN RETURN "PASSWORD IS INCORRECT", 400 SOMETHING @TODO
+            return jsonify("User Has Been Created."), 200
+        else:
+            return jsonify("Please use valid Email/Password"), 405
+    elif req.method == 'GET':
+            get_user = User.query.filter_by(id = id).first() #.first returns the first encounter of that id
+            return jsonify(get_user), 200
+    elif req.method == 'DELETE':
+            delete_user= User.query.filter_by(id = id).delete()
+            return jsonify(delete_user), 200
+    #elif req.method == 'PATCH':
+        #edits individual user
+            #return  
+
+# @app.route('/games', methods=['GET'])        
+# def all_games():
+
+#     req.body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
+
+#     return jsonify(req.body), 200
