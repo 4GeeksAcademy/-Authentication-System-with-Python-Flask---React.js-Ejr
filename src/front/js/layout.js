630af2d20c6a9a867112a9fc1f7dc77a584b4284
mateo1627 <140858704+mateo1627@users.noreply.github.com>
2024-08-13 15:00:02 +0000

    Improvements to /products

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -32,7 +32,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<LogIn />} path="/logIn" />
                         <Route element={<Register />} path="/register" />
-                        <Route element={<ListOfProducts />} path="/listofproducts" />
+                        <Route element={<ListOfProducts />} path="/products" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
