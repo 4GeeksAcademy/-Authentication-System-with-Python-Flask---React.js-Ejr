2847526baec73a2cf95855a45fe423699f0309d4
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-08-13 17:38:47 +0000

    jumbotron ajustes

--- src/front/js/component/jumbotron.jsx
+++ src/front/js/component/jumbotron.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Searchbar from "./searchbar.jsx"
 import "../../styles/jumbotron.css";
+import { Link } from 'react-router-dom';
 
 export const Jumbotron = () => {
     return (
     <div class="p-5 mb-4 bg-body-tertiary jumbotron">
-      <div class="container-fluid py-5 ms-5">
-        <h5 class="fw-bold ms-5 mb-3 text-white">Busca qué hacer</h5>
-        <div className="d-flex h-100 ms-4">
+      <div class="container-fluid py-5 buscar">
+        <h5 class="fw-bold mb-3 text-white">Busca qué hacer</h5>
+        <div className="d-flex h-100">
           <div className="search d-flex">
-            <input className="search_input w-50" type="text" name="search" placeholder="Destino/@username"/>
-            <input className="search_input w-25 me-auto" type="number" min={1} name="days" placeholder="Duración"/>
-            <a href="#" className="search_icon"><i className="fa fa-search"></i></a>
+            <input className="search_input w-50 mt-0" type="text" name="search" placeholder="Destino/@username"/>
+            <input className="search_input w-25 mt-0 me-auto" type="number" min={1} name="days" placeholder="Duración"/>
+            <Link to={'/search'}>
+            {/* <a href="#" className="search_icon"><i className="fa fa-search"></i></a> */}
+            <i className="fa fa-search"></i>
+            </Link>
           </div>
         </div>
       </div>
