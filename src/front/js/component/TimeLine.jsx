da392be1cadd4c15bcf7424327341626639997d5
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-14 10:11:58 +0000

    TimeLine, CardOffer, aingleOffer   modificado

--- src/front/js/component/TimeLine.jsx
+++ src/front/js/component/TimeLine.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { CardOffer } from "./CardOffer.jsx";
+import { Context } from "../store/appContext.js";
 
 export const TimeLine = () =>{
+  const {store, actions} = useContext(Context);
+  const {jobOffers} = store;
 
+  useEffect(() =>{
+    if(jobOffers === 0){
+      actions.loadJobOffers();
+    }
+  }, [jobOffers])
 
 
     return (
-        <div className="container mt-5">
-          <div className="row">
+        <div className="container d-flex justify-content-center mt-5">
+          <div className="row d-flex flex-column">
             {jobOffers.map((offer, index) => (
               <div className="col-md-4" key={index}>
-                <JobOfferCard 
+                <CardOffer 
                   title={offer.title}
                   company={offer.company}
                   location={offer.location}
                   salary={offer.salary}
                   description={offer.description}
+                  id ={offer.id}
                 />
               </div>
             ))}
