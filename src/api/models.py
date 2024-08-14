506ce258dfadc8aad30c117a98f371db7cdef49c
Ruperth <97520685+iRuperth@users.noreply.github.com>
2024-08-14 09:25:59 +0000

    actualizacion

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,17 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role = db.Column(db.String(80), nullable=False)
 
+
+class Profesor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role = db.Column(db.String(80), nullable=False)
+
+    
     def __repr__(self):
         return f'<User {self.email}>'
 
