4095192b2d7a1efed0154bd380b892fe451fbde6
ivanzr17 <139049435+ivanzr17@users.noreply.github.com>
2024-08-14 17:44:29 +0000

    Rutas destacadas done

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -4,6 +4,7 @@ import "../../styles/home.css";
 import { Navbar } from "../component/navbar.jsx";
 import { LoginRegister } from "../component/registerModal.jsx";
 import { Jumbotron } from "../component/jumbotron.jsx";
+import RutaDestacada  from "../component/rutaDestacada.jsx"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -13,6 +14,16 @@ export const Home = () => {
 		<Navbar />
 		<LoginRegister />
 		<Jumbotron />
+		<h2>Rutas destacadas 🔥</h2>
+		<div className="d-flex justify-content-around justify-content-xxl-evenly mx-5">
+		<RutaDestacada />
+		<RutaDestacada />
+		<RutaDestacada />
+		</div>
+		
+	
+		
+		
 		{/* // Aquí irá el componente Jumbotron+searchbar
 		// Aquí irá el componente RutasDestacadas
 		//El footer se renderiza en layout de manera predefinida */}
