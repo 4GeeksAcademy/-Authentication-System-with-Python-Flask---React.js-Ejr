43832316e3fe2454a2a1c5ff25da5a602be5b851
darioduarte1 <darioduarte1991@gmail.com>
2024-08-14 11:15:39 +0000

    Alteracion Contact Us parte3

--- src/front/js/pages/ContactUs.js
+++ src/front/js/pages/ContactUs.js
@@ -4,46 +4,24 @@ import "../../styles/ContactUs.css";
 const ContactUs = () => {
     return (
         <div className="contact-body">
-            <div className="singup-container" id="container">
-                <div className="singup-form-container singup-sign-up">
-                    <form>
-                        <h1>Create Account</h1>
-                        <div className="singup-social-icons">
-                            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit">Sign Up</button>
-                    </form>
-                </div>
-                <div className="singup-form-container singup-sign-in">
-                    <form>
-                        <h1>Sign In</h1>
-                        <span>Use your email and password</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <a href="#">Forget Your Password?</a>
-                        <button type="submit">Sign In</button>
-                    </form>
-                </div>
-                <div className="singup-toggle-container">
-                    <div className="singup-toggle">
-                        <div className="singup-toggle-panel singup-toggle-left">
-                            <h1>Welcome Back!</h1>
-                            <p>Enter your personal details to use all of site features</p>
-                            <button className="singup-hidden" id="login">Sign In</button>
-                        </div>
-                        <div className="singup-toggle-panel singup-toggle-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Register with your personal details to use all of site features</p>
-                            <button className="singup-hidden" id="register">Sign Up</button>
-                        </div>
+            <div className="contactUs-container">
+                <div className="contactUs-container-left">
+                    <div className="p-3 text-center">
+                        <h1>Contact Us</h1>
+                        <p>Register with your personal details to use all of site features</p>
+                        <button className="signup-hidden" id="register">Sign Up</button>
                     </div>
+                </div>
+                <div className="contactUs-container-right">
+                    <form>
+                        <h2>Level up your brand</h2>
+                        <span>You can reach us anytime via tickeate@gmail.com</span>
+                        <input type="name" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="phone" placeholder="Phone" />
+                        <input type="help" placeholder="How can we help?" />
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
