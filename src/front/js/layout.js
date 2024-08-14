6a9b5da688caa0eec1071a3db2cebaf37ffd93b9
carmenflorsl <164540909+carmenflorsl@users.noreply.github.com>
2024-08-14 18:24:26 +0000

    sin errores

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./component/Login";
-import { Demo } from "./component/Register";
+import { Home } from "./pages/home";
+import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Gestor_bebe } from "./pages/gestor_bebe";
+import { Login } from "./component/Login";
+import { Register } from "./component/Register";
 
 //create your first component
 const Layout = () => {
@@ -24,9 +26,11 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
+                <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<Login />} path="/Login" />
                         <Route element={<Gestor_bebe />} path="/gestor_bebe" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
