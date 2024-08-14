4873d5007760044c5f61db1336a260c1ab8e04e9
darioduarte1 <darioduarte1991@gmail.com>
2024-08-14 08:15:25 +0000

    Alteracion de la vista ContactUs Part2

--- src/front/js/pages/ContactUs.js
+++ src/front/js/pages/ContactUs.js
@@ -1,24 +1,53 @@
 import React from "react";
+import "../../styles/ContactUs.css";
 
 const ContactUs = () => {
-    return <>
-        <form>
-            <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    return (
+        <div className="contact-body">
+            <div className="singup-container" id="container">
+                <div className="singup-form-container singup-sign-up">
+                    <form>
+                        <h1>Create Account</h1>
+                        <div className="singup-social-icons">
+                            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+                            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+                            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+                            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+                <div className="singup-form-container singup-sign-in">
+                    <form>
+                        <h1>Sign In</h1>
+                        <span>Use your email and password</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forget Your Password?</a>
+                        <button type="submit">Sign In</button>
+                    </form>
+                </div>
+                <div className="singup-toggle-container">
+                    <div className="singup-toggle">
+                        <div className="singup-toggle-panel singup-toggle-left">
+                            <h1>Welcome Back!</h1>
+                            <p>Enter your personal details to use all of site features</p>
+                            <button className="singup-hidden" id="login">Sign In</button>
+                        </div>
+                        <div className="singup-toggle-panel singup-toggle-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Register with your personal details to use all of site features</p>
+                            <button className="singup-hidden" id="register">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
-            </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    </>
-}
+        </div>
+    );
+};
 
 export default ContactUs;
