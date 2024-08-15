181bd42f8a069ea0b28cd5854cb090f6b66e0de7
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-15 13:41:20 +0000

    modificaciones timeline Branch

--- src/front/js/pages/SingleOffer.jsx
+++ src/front/js/pages/SingleOffer.jsx
@@ -1,13 +1,11 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext.js";
-import "../../styles/SingleOffer.css"
-
+import "../../styles/SingleOffer.css";
 
 export const SingleOffer = () => {
-    const { id } = useParams();  
-    const {store} = useContext(Context);
-
+    const { id } = useParams();
+    const { store } = useContext(Context);
 
     const offer = store.jobOffers.find(offer => offer.id === parseInt(id));
 
@@ -17,28 +15,69 @@ export const SingleOffer = () => {
 
     return (
         <div className="container my-5">
-            <div className="row ">
-                <div className="col-6">
-                    <div className="card single-card">
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="card single-card single-offer-box mb-4">
                         <div className="card-body">
-                            <div className="d-flex">
-                                <h2 className="">{offer.title}</h2>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex flex-column">
+                                    <h2 className="mb-0">{offer.title}</h2>
+                                    <span className="text-muted">{offer.company}<span className="ms-3">{offer.location}</span></span>
+                                </div>
+                                <button className="btn btn-single-offer btn-lg">Inscribirse a la oferta</button>
                             </div>
-                            <p>{offer.company}<span className="ms-3">{offer.location}</span></p>
-                            <p><u>{offer.salary}</u></p>
-                            <p className="fw-bold">Description</p>
-                            <p>{offer.description}</p>
+                            <div className="salary-box">
+                                <p className="mb-1 text-success">
+                                    <i className="bi bi-currency-euro me-2"></i>
+                                    <span>{offer.salary}</span>
+                                </p>
+                            </div>
+
+                            <hr />
+                            <div className="data-single-offer-container">
+                                <h5 className="fw-bold my-2">Requisitos</h5>
+                                <div className="requisit-list d-flex">
+                                    <ul className="me-4">
+                                        <li>Estudios mínimos</li>
+                                        <li>Años de experiencia</li>
+                                        <li>{offer.location}</li>
+                                    </ul>
+                                    <ul>
+                                        <li>Tipo de contrato</li>
+                                        <li>Tecnologías</li>
+                                        <li>Otros</li>
+                                    </ul>
+                                </div>
+
+                                <div className="offer-description mt-3">
+                                    <h5 className="fw-bold">Descripción</h5>
+                                    <p className="description">{offer.description}</p>
+                                </div>
+                                <button className="btn btn-single-offer btn-lg mt-3">Inscribirse a la oferta</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-6">
-                    <h2></h2>
+
+                <div className="col-md-4">
+                    <div className="card mb-3 card-single-offer">
+                        <h5 className="card-header">Featured</h5>
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <button href="#" className="btn btn-primary">Go somewhere</button>
+                        </div>
+                    </div>
+                    <div className="card card-single-offer">
+                        <h5 className="card-header">Featured</h5>
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <button href="#" className="btn btn-primary">Go somewhere</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
-
-
-            
         </div>
     );
-}
+};
