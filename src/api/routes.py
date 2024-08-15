011a5dd885a87c93f1c06cdbc4bdf70a98c9b69b
darioduarte1 <darioduarte1991@gmail.com>
2024-08-15 09:58:40 +0000

    models.py - introducion en User de password y is active (line 15 y 16), routes.py - creacion de las rutas (todo el fichero), app.py - introducion de jwt token (line 23 y 24)

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,12 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +17,48 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    request_body_user = request.get_json()
+    print (request_body_user)
+    user = User.query.filter_by(email=request_body_user["email"], password=request_body_user["password"]).first()
+    
+    if user:
+        return jsonify({"message": "Email and password already exists"})
+    new_user = User(email=request_body_user["email"], password=request_body_user["password"])
+    print(new_user)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "New user added"}), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password =data.get ('password')
+
+    if not email or not password:
+        return jsonify({"message": "Email and password are required"})
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if not user:
+        return jsonify({"message": "Email and password incorrect"})
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify({"token": access_token, "user_id": user.id})
+
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def validate_token():
+    
+    current_user_id = get_jwt_identity()
+    print(current_user_id)
+
+    user = User.query.filter_by(id=current_user_id).first()
+    if user is None:
+        raise APIException("User not found", status_code=404)
+
+    return jsonify("User authenticated"), 200
