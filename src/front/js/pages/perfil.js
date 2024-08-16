2ce3f6a9a49788f4a72c7a9ddd27c92ea08b7a10
Sergio Añez <160168084+SergioGygax@users.noreply.github.com>
2024-08-16 18:14:54 +0000

    perfil, falta reset

---
+++ src/front/js/pages/perfil.js
@@ -0,0 +1,9 @@
+import React from "react";
+import UploadImage from "../component/perfilInfo";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+    return (
+        <UploadImage />
+    )}
