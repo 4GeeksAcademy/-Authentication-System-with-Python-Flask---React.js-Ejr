9b5896f5a2b70fd04410609237ea7a35d1083019
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-08-16 15:48:47 +0000

    Modificaciones dentro de la page gallery y construccion de modal (por modificar)

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -2,6 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
