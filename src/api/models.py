bc68599fdebf248a703d7e32676101bd2f97bfe0
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-08-18 08:33:27 +0000

    form registro terminado a falta de enlazar

--- src/api/models.py
+++ src/api/models.py
@@ -61,7 +61,7 @@ class User(db.Model):
     __tablename__="user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column (db.String (20), nullable=False)
-    username = db.Column (db.String(50), unique=True, nullable=False)
+    username = db.Column (db.String(50), nullable=False)
     photo = db.Column (db.String(200))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,58 +1,97 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import "../../styles/register.css";
 import { useForm  } from "react-hook-form";
 import { Context } from "../store/appContext";
+import AlertSuccess from "../component/alertSuccess";
+import { Navigate, useNavigate } from "react-router-dom";
  
 // INTRODUCIR BLUR IN DE MAGIC UI EN TEXTO H1
 export const Register = () => {
-    const [isDev, setIsDev] = useState(true)
+    const [isDev, setIsDev] = useState(false)
     const { store, actions } = useContext(Context);
-    const { register, formState: { errors },handleSubmit } = useForm();
-      // Submit your data into Redux store
-      const onSubmit = formData => actions.register(formData);
-
-    return(
-        <div className="container-fluid ">
+    const { register, formState: { errors },handleSubmit,reset  } = useForm();
+    const navigate=useNavigate()
+      
+    const onSubmit = formData => actions.register(formData)
+    
+    // Pendiente recibir datos si es Dev o Empleador y al enviar los datos reenviar a los perfiles
+    useEffect(()=>{
+        if(store.success){
+            const timer = setTimeout(()=>{
+                navigate("/")
+            },3000)
+            return () => clearTimeout(timer);
+        } else{const timer = setTimeout(()=>{
+            actions.resetStore()
+            reset()
+        },3000)
+            return () => clearTimeout(timer);
             
-            <form onSubmit={handleSubmit(onSubmit)} className="w-50 mx-auto my-5">
+        }  
+            
+    },[store.success])
+
+      
+    return(
+        <div className="container-fluid p-0">
+            
+            <form onSubmit={handleSubmit(onSubmit)} className="w-50 mx-auto my-5 m-0 ">
                 
+            
             <h1 className="border-bottom w-100 text-center">Registro</h1>
-            <div className="w-100 px-4">
+           
+                {store?.msg && ( <AlertSuccess/>)}
+                
+                
+            
+            
+            <div className="w-100 px-4 ">
+            
             <div className="form-group mb-2">
-                <label htmlFor="name" className="form-label">Nombre:</label>
+                <label htmlFor="name" className="form-label m-0 fw-semibold">Nombre:</label>
                 <input type="text" className="form-control" id="name" placeholder="Nombre.."
-                {...register("name", { required: true })} aria-invalid={errors.name ? "true" : "false"} />
-                {errors.name?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El nombre está vacío</button>}
+                {...register("name", { required: true, pattern:{value: /^[A-Za-z]+$/i, message:'Solo se permiten letras'} })} aria-invalid={errors.name ? "true" : "false"} />
+                {errors.name?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El campo nombre está vacío</button>}
+                {errors.name?.message && <button className="btn btn-warning w-100 mt-2"  role="alert">{errors.name.message}</button>}
             </div>
+           
             <div className="form-group mb-2">
-                <label htmlFor="username" className="form-label">Apellidos:</label>
+                <label htmlFor="username" className="form-label m-0 fw-semibold">Apellidos:</label>
                 <input type="text" className="form-control" id="username" placeholder="Apellidos.."
                 {...register("username", { required: true })} aria-invalid={errors.username ? "true" : "false"} />
-                {errors.username?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">Los apellidos está vacío</button>}
+                {errors.username?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El campo apellidos está vacío</button>}
             </div>
             <div className="form-group mb-2">
-                <label htmlFor="email" className="form-label">Email:</label>
+                <label htmlFor="email" className="form-label m-0 fw-semibold">Email:</label>
                 <input type="email" className="form-control" id="email" placeholder="Email.."
                 {...register("email", { required: true })} aria-invalid={errors.email ? "true" : "false"} />
-                {errors.email?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El email está vacío</button>}
+                {errors.email?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El campo email está vacío</button>}
 
             </div>
             <div className="form-group mb-2">
-                <label htmlFor="password" className="form-label">Contraseña:</label>
-                <input type="password" className="form-control" id="password" placeholder="Contraseña.."
-                {...register("password", { required: true })} aria-invalid={errors.password ? "true" : "false"} />
-                {errors.password?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">La contraseña está vacía</button>}
+                <label htmlFor="country" className="form-label m-0 fw-semibold">Pais:</label>
+                <input type="text" className="form-control" id="country" placeholder="Pais.."
+                {...register("country", { required: true })} aria-invalid={errors.country ? "true" : "false"} />
+                {errors.country?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El campo pais está vacío</button>}
 
+            </div>
+            <div className="form-group mb-2">
+                <label htmlFor="password" className="form-label m-0 fw-semibold">Contraseña:</label>
+                <input type="password" className="form-control" id="password" placeholder="Contraseña.."
+                {...register("password", { required: true, minLength:{value:6, message:'Debe tener mínimo 6 caracteres'} })} aria-invalid={errors.password ? "true" : "false"} />
+                {errors.password?.type === 'required' && <button className="btn btn-warning w-100 mt-2"  role="alert">El campo contraseña está vacío</button>}
+                {errors.password?.message && <button className="btn btn-warning w-100 mt-2"  role="alert">{errors.password?.message}</button>}
               
             </div>
             {!isDev &&(
                 <div className="form-group">
-                <label for="cif" className="form-label">CIF</label>
+                <label htmlFor="cif" className="form-label m-0 fw-semibold">CIF:</label>
                 <input type="text" className="form-control" id="cif" {...register("cif", {required:true})} placeholder="Escriba el CIF.."/>
             </div>
             )}
-            <button type="submit" className="btn w-100 mt-2 stylebutton">Submit</button>
+            <button type="submit" className="btn w-100 mt-2 stylebutton">Registrarse</button>
             </div>
+            
             </form>
 
            
