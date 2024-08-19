cb113dd663074aa137ce40c44b8c9aaeecfb88a3
Kaylabree <111409668+Kaylabree@users.noreply.github.com>
2024-08-19 23:03:47 +0000

    first commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+
+    <div class="main-div">
+      <h1>ThunderCats</h1>
+      <div class="left-div">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="dropdown" data-bs-theme="dark">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
+                Categories
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
+                <li><a class="dropdown-item" id="actionGames" href="#">Action</a></li>
+                <li><a class="dropdown-item" id="roleplayingGames" href="#">RPG</a></li>
+                <li><a class="dropdown-item" id="strategy" href="#">Strategy</a></li>
+              </ul>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+            </div>
+
+
+          </div>
+        </nav>
+      </div>
+      <div class="right-div">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+
+  );
 };
+//<div class="main_div">
+//<div class="left-div">
+//<button>Home</button>
+// <button>Categories</button>
+////<button>Favorites</button>
+//</div>
+//<div class="right-div">
+//  <button>Login</button>
+// <button>signup</button>
+//</div>
+//</div>
