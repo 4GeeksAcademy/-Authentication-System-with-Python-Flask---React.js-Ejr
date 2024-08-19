7289b175664c81ccab0d5bea035f8d29f526b2c4
Tingting Ji Zhou <168580008+TingtingJiZ@users.noreply.github.com>
2024-08-19 19:10:48 +0000

    asd

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,15 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Genres(db.Model):
+    __tablename__ = "genres"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    def __repr__(self):
+        return f'<Genre {self.id} - {self.name}>'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
