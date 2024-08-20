a0b3808eaa8d1225a3755d36b3b7f022e4f15dbd
mateo1627 <140858704+mateo1627@users.noreply.github.com>
2024-08-20 17:54:30 +0000

    list of products functionalities

--- src/front/js/pages/listofproducts.jsx
+++ src/front/js/pages/listofproducts.jsx
@@ -8,18 +8,24 @@ import Product from "../component/product.jsx";
 const ListOfProducts = () => {
     const { store, actions } = useContext(Context);
     const { products } = store;
-    const [visibleProducts, setVisibleProducts] = useState(6);
+    const [showMoreProducts, setShowMoreProducts] = useState(false);
 
     useEffect(() => {
         actions.getProducts();
     }, []);
 
     const loadMoreProducts = () => {
-        setVisibleProducts(prevVisible => prevVisible + 3); 
+        setShowMoreProducts(prevState => !prevState);
     };
 
+  
+    const firstGroup = products.slice(0, 3);  
+    const secondGroup = products.slice(3, 6); 
+
+   
+    const thirdGroup = products.slice(6);
+
     return (
-        
         <div className="w-100 m-0 p-0 list-of-products-container">
             <div className="invisible-header-box"></div>
             <div className="banner-container w-100 d-flex">
@@ -28,10 +34,10 @@ const ListOfProducts = () => {
                         Nuestros productos
                     </h1>
                 </div>
-                <img src={BannerProducts} className="banner-img mb-5" alt="banner-img" />
+                <img src={BannerProducts} className="banner-img" alt="banner-img" />
             </div>
 
-            <div className="container-background align-items-center justify-content-center d-flex mt-5">
+            <div className="container-background align-items-center justify-content-center d-flex h-50">
                 <div className="container background">
                     <div className="row d-flex container-top-products col-xs-12">
                         <div className="col-md-4 col-xs-12 container-top-product">
@@ -47,30 +53,63 @@ const ListOfProducts = () => {
                 </div>
             </div>
 
+         
             <div className="container background mt-5">
                 <div className="row d-flex container-top-products dynamic-products">
-                    {products && products.length > 0 && 
-                        products.slice(0, visibleProducts).map((product, index) => (
-                            <Product 
-                                className="col-md-4 col-xs-12"
-                                key={index}
-                                id={product.id}
-                                name={product.name}
-                                cost={product.cost}
-                            />
-                        ))}
+                    {firstGroup.map((product) => (
+                        <Product
+                            className="col-md-4 col-xs-12"
+                            key={product.id}
+                            id={product.id}
+                            name={product.name}
+                            cost={product.cost}
+                            image_url={product.image_url}
+                        />
+                    ))}
                 </div>
             </div>
 
-            
-                <div className="btn-container w-100 d-flex justify-content-center mt-4">
-                    <button className="btn-see-more px-5 py-3 rounded" onClick={loadMoreProducts}>
-                        Ver más
-                    </button>
-                </div>
            
+            <div className="container background mt-5">
+                <div className="row d-flex container-top-products dynamic-products">
+                    {secondGroup.map((product) => (
+                        <Product
+                            className="col-md-4 col-xs-12"
+                            key={product.id}
+                            id={product.id}
+                            name={product.name}
+                            cost={product.cost}
+                            image_url={product.image_url}
+                        />
+                    ))}
+                </div>
+            </div>
+
+  
+            {showMoreProducts && (
+                <div className="container background mt-5">
+                    <div className="row d-flex flex-wrap container-top-products dynamic-products">
+                        {thirdGroup.map((product) => (
+                            <Product
+                                className="col-md-4 col-xs-12"
+                                key={product.id}
+                                id={product.id}
+                                name={product.name}
+                                cost={product.cost}
+                                image_url={product.image_url}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )}
+
+            <div className="btn-container w-100 d-flex justify-content-center mt-5">
+                <button className="btn-see-more px-5 py-3 rounded" onClick={loadMoreProducts}>
+                    {showMoreProducts ? 'Ver menos' : 'Ver más'}
+                </button>
+            </div>
         </div>
     );
-}
+};
 
 export default ListOfProducts;
