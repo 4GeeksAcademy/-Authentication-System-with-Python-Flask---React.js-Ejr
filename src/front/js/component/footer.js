01ff1c5c5ba8f9752225a77a7c6ae5cf1329ce4c
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-08-20 11:32:16 +0000

    links en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "../../styles/footer.css";
+import { Link } from "react-router-dom";
+
 export const Footer = () => (
 	<footer>
-        <div class="footer-container">
-            <ul class="footer-links">
-                <li><a href="#home">Inicio</a></li>
-                <li><a href="#about">Sobre Nosotros</a></li>
+        <div className="footer-container">
+            <ul className="footer-links">
+                <Link to="/">
+                    <li><a href="#home">Inicio</a></li>
+                </Link>
+                <Link to="/about">
+                    <li><a href="#about">Sobre Nosotros</a></li>
+                </Link>
                 <li><a href="#services">Servicios</a></li>
                 <li><a href="#contact">Contacto</a></li>
             </ul>
