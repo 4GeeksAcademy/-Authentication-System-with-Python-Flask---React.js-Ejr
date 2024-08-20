ffad6b9bef4c7b150c16431c6062dcb96a22d442
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-08-20 09:00:23 +0000

    v2

--- src/front/js/component/CardAyuda.js
+++ src/front/js/component/CardAyuda.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../styles/cardAyuda.css";
+import { AiFillShop } from "react-icons/ai";
+import { BsPersonBoundingBox } from "react-icons/bs";
 
-const CardAyuda = ({text}) => {
-  
+const CardAyuda = ({ text }) => {
   return (
-    <div className='col-4 bg-white text-danger w-40 mx-2 rounded'>
-      <div>Iconos</div>
-      <h3>{text}</h3>
-      <hr/>
-      <p>Descripcion</p>
+    <div className="col-4 bg-white colorGeneral w-40 mx-2 rounded">
+      <div>
+        <div className="d-flex justify-content-center m-2 align-items-center">
+          {text == "EMPLEADOR" ? (
+            <AiFillShop size={40} />
+          ) : (
+            <BsPersonBoundingBox size={40} />
+          )}
+          <h3 className="mx-4">{text}</h3>
+        </div>
+      </div>
+      <hr />
+      <p className="text-center">Pulsa <button>Aquí</button> para ver las preguntas más frecuentes</p>
     </div>
-  )
-}
+  );
+};
 
-export default CardAyuda
+export default CardAyuda;
--- src/front/js/pages/preguntasFrecuentes.js
+++ src/front/js/pages/preguntasFrecuentes.js
@@ -2,19 +2,24 @@ import React from "react";
 import "../../styles/preguntasFrecuentes.css";
 import CardAyuda from "../component/CardAyuda";
 import AcordeonPreguntas from "../component/AcordeonPreguntas";
+import 'animate.css';
+import { RiCodeBoxLine } from "react-icons/ri";
+import { MdWork } from "react-icons/md";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { GrMoney } from "react-icons/gr";
 
 const preguntasFrecuentes = () => {
   return (
     <>
     <div className="container">
       <div>
-        <div className="header-preguntas">
-          <h1 className="text-center py-3 fw-bolder fuente">
+        <div className="header-preguntas colorFondoGeneral">
+          <h1 className="text-center py-3 fw-bolder fuente animate__animated animate__bounceInLeft ">
             ¿Cómo podemos ayudarte?
           </h1>
           <div className="row justify-content-center ">
-            <CardAyuda text="empleador" />
-            <CardAyuda text="programador" />
+            <CardAyuda text="EMPLEADOR" />
+            <CardAyuda text="PROGRAMADOR" />
           </div>
         </div>
         <div>
@@ -24,26 +29,34 @@ const preguntasFrecuentes = () => {
       </div>
     </div>
    
-        <div className="row banner-preguntas text-center align-items-center">
-          <div className="col-6">
+        <div className="row banner-preguntas colorFondoGeneral text-center align-items-center">
+          <div className="col-7 borde">
             <h1>Registrate en floppy</h1>
             <p>Encuentra lo que estás buscando</p>
-            <button className="btn btn-success">Registrarse</button>
+            <button className="btn btn-light colorGeneral fs-5">Registrarse</button>
+            
           </div>
-          <div className="col-6">
-            <div className="col border border-white">
-              <h4>Es hora de encontrar empleo </h4>
-              <p>Miles de empresas te están buscando!!!</p>
+          <div className="col-5">
+            <div className="col">
+              <h4 >Es hora de encontrar empleo </h4>
+              <p className="mt-0">Miles de empresas te están buscando!!!</p>
             </div>
             <div className="col">
               <h4>Es hora de encontrar programadores </h4>
-              <p>Miles de programadores están esperando ser contratados</p>
+              <p className="mt-0">Miles de programadores están esperando ser contratados</p>
             </div>
           </div>
         </div>
-        <div className="row">
-            <div>El mejor servicio de Floopy</div>
-            <div>Imagenes e iconos</div>
+        <div className="row justify-content-center my-5 align-items-center">
+            <div className="col-4 text-center rounded text-style colorFondoGeneral text-white py-3 mx-5 ">
+              <h3 >El mejor servicio en Floppy</h3>
+            </div>
+            <div className="col-4 icons-style colorGeneral mx-5">
+            <RiCodeBoxLine />
+            <MdWork />
+            <BsPersonWorkspace />
+            <GrMoney /> 
+            </div>
         </div>
   
     </>
