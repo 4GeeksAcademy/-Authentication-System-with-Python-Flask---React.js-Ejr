4181d28025a78f19c466796c0a54f88ef9e3ab3a
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-21 11:58:56 +0000

    TimeLine modified: searchbar

--- src/front/js/component/ListOffers.jsx
+++ src/front/js/component/ListOffers.jsx
@@ -2,33 +2,39 @@ import React, { useContext, useEffect } from "react";
 import { CardOffer } from "./CardOffer.jsx";
 import { Context } from "../store/appContext.js";
 
-export const ListOffers = () =>{
-  const {store, actions} = useContext(Context);
-  const {jobOffers} = store;
+export const ListOffers = ({ searchTerm }) => {
+  const { store, actions } = useContext(Context);
+  const { jobOffers } = store;
 
-  useEffect(() =>{
-    if(jobOffers === 0){
+  useEffect(() => {
+    if (jobOffers.length === 0) {
       actions.loadJobOffers();
     }
-  }, [jobOffers])
+  }, [jobOffers, actions]);
 
-    return (
-        <div className="container d-flex justify-content-center my-5">
-          <div className="row d-flex flex-column">
-            {jobOffers.map((offer, index) => (
-              <div className="col" key={index}>
-                <CardOffer 
-                  title={offer.title}
-                  company={offer.company}
-                  modality={offer.modality}
-                  location={offer.location}
-                  salary={offer.salary}
-                  description={offer.description}
-                  id ={offer.id}
-                />
-              </div>
-            ))}
+  const filteredOffers = jobOffers.filter((offer) =>
+    offer.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    offer.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    offer.location.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="container d-flex justify-content-center my-5">
+      <div className="row d-flex flex-column">
+        {filteredOffers.map((offer, index) => (
+          <div className="col" key={index}>
+            <CardOffer
+              title={offer.title}
+              company={offer.company}
+              modality={offer.modality}
+              location={offer.location}
+              salary={offer.salary}
+              description={offer.description}
+              id={offer.id}
+            />
           </div>
-        </div>
-      );
-}
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/front/js/pages/TimeLine.jsx
+++ src/front/js/pages/TimeLine.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-import {ListOffers} from "../component/ListOffers.jsx"
+import React, { useState } from "react";
+import { ListOffers } from "../component/ListOffers.jsx";
+import "../../styles/TimeLine.css"
 
+export const TimeLine = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filterTerm, setFilterTerm] = useState("");
 
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
+    const handleSearchClick = () => {
+        setFilterTerm(searchTerm);
+    };
 
-export const TimeLine = () =>{
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearchClick();
+        }
+    };
 
-
-
-    return(
+    return (
         <>
-            <ListOffers />
+            <div className="search-bar container my-3 d-flex justify-content-center">
+                <input
+                    type="text"
+                    className=" form-control me-2"
+                    placeholder="Buscar ofertas..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    onKeyDown={handleKeyDown}
+                />
+                <button
+                    className="btn btn-search"
+                    onClick={handleSearchClick}
+                >
+                    Buscar
+                </button>
+            </div>
+            <ListOffers searchTerm={filterTerm} />
         </>
-
-
-
-    )
-}
+    );
+};
