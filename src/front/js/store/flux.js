f88c76305ba6721ded7bdaf3d5de01e78ab705d1
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-21 19:44:29 +0000

    Form modified

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -29,7 +29,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						modality: "On-site",
 						location: "Spain",
 						salary: "$50,000 - $70,000/year",
-						description: "We are looking for a skilled Backend Developer to..."
+						description: "Nuestra esencia nos ha llevado a la vanguardia de la tecnología, a romper paradigmas y a brindar las soluciones que realmente se corresponden a las necesidades de cada cliente. Nuestro talento nos ha hecho poder decir con orgullo que somos una de las 6 empresas TOP de tecnologías en el mundo. Buscamos a grandes profesionales que tengan experiencia en proyectos desarrollando con tecnologías Front End: React, Angular. Trabajaras siempre acompañado de un gran equipo y con metodologías ágiles como Scrum, usando herramientas como Jira y Confluence para estar unido a tu equipo y a los avances del proyecto"
 					},
 					{
 						id: 3,
@@ -40,8 +40,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						modality: "On-site",
 						location: "Spain",
 						salary: "$90,000 - $100,000/year",
-						description: "We are looking for a skilled Project Manager to..."
-					},
+						description: "Nuestra esencia nos ha llevado a la vanguardia de la tecnología, a romper paradigmas y a brindar las soluciones que realmente se corresponden a las necesidades de cada cliente. Nuestro talento nos ha hecho poder decir con orgullo que somos una de las 6 empresas TOP de tecnologías en el mundo. Buscamos a grandes profesionales que tengan experiencia en proyectos desarrollando con tecnologías Front End: React, Angular. Trabajaras siempre acompañado de un gran equipo y con metodologías ágiles como Scrum, usando herramientas como Jira y Confluence para estar unido a tu equipo y a los avances del proyecto"				},
 					{
 						id: 4,
 						title: "UI/UX",
@@ -51,7 +50,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						tecnologies: "HTML, CSS, Javascript, Python",
 						location: "Spain",
 						salary: "$40,000 - $60,000/year",
-						description: "We are looking for a skilled Backend Developer to..."
+						description: "Nuestra esencia nos ha llevado a la vanguardia de la tecnología, a romper paradigmas y a brindar las soluciones que realmente se corresponden a las necesidades de cada cliente. Nuestro talento nos ha hecho poder decir con orgullo que somos una de las 6 empresas TOP de tecnologías en el mundo. Buscamos a grandes profesionales que tengan experiencia en proyectos desarrollando con tecnologías Front End: React, Angular. Trabajaras siempre acompañado de un gran equipo y con metodologías ágiles como Scrum, usando herramientas como Jira y Confluence para estar unido a tu equipo y a los avances del proyecto"
 					}
 					
 				];
