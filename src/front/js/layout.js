03dd18961ad7e4a1a0030b98ef408c3c27d6d9c8
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-08-21 10:24:18 +0000

    posicion footer terminada

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -27,7 +27,7 @@ const Layout = () => {
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
-        <div>
+        <>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
@@ -44,7 +44,7 @@ const Layout = () => {
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
-        </div>
+        </>
     );
 };
 
