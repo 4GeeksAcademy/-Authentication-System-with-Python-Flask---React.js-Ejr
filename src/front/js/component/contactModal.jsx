4586180d0f4f2888cceb3287ee486158fc1725da
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-08-22 19:03:21 +0000

    contact a bdd y email

--- src/front/js/component/contactModal.jsx
+++ src/front/js/component/contactModal.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/contact_modal.css"
 
 export const ContactModal = () => {
+    const [formData, setFormData] = useState({
+        contactEmail: '',
+        asunto: '',
+        descripcion: ''
+    })
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value})
+        console.log(formData)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const resp = await fetch(process.env.BACKEND_URL + '/api/contact', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    accept: 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = resp.json()
+            if(!resp.ok){
+                const errorMsg = data.msg
+                throw new Error(errorMsg)
+            }
+            return { success : true }
+        } catch (error) {
+            console.error('Error enviando el formulario', error.message)
+            return { success: false, msg: error.message}
+        }
+    }
 
 	return(
 
@@ -16,20 +49,20 @@ export const ContactModal = () => {
                         <div className=" text-center">
                             <h1 className="action fs-5 text-black">Contacto</h1>
                         </div>
-                        <form action="submit" className="text-start">
+                        <form onSubmit={handleSubmit} className="text-start">
                             <div className="mb-3 w-75 mx-auto">
                                 <label htmlFor="contactEmail" className="form-label text-black">Email</label>
-                                <input type="email" className="form-control rounded-pill" name="contactEmail" id="contactEmail" required/>
+                                <input onChange={handleChange} type="email" className="form-control rounded-pill" name="contactEmail" id="contactEmail" required/>
                             </div>
 
                             <div className="mb-3 w-75 mx-auto">
                                 <label htmlFor="asunto" className="form-label text-black">Asunto</label>
-                                <input type="text" className="form-control rounded-pill" name="asunto" id="asunto" required/>
+                                <input onChange={handleChange} type="text" className="form-control rounded-pill" name="asunto" id="asunto" required/>
                             </div>
 
                             <div className="mb-3 w-75 mx-auto">
-                                <label htmlFor="description" className="form-label text-black">Descripción</label>
-                                <textarea className="form-control" id="description" name="description" rows="4" required></textarea>
+                                <label htmlFor="descripcion" className="form-label text-black">Descripción</label>
+                                <textarea onChange={handleChange} className="form-control" id="descripcion" name="descripcion" rows="4" required></textarea>
                             </div>
                             <div className="text-center">
                             <button
