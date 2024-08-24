0d39fde29e9071392deeb0274b522b30f871aa2b
FloresKev99 <165217315+FloresKev99@users.noreply.github.com>
2024-08-24 00:40:46 +0000

    updated home and created card.js and card.css

---
+++ src/front/js/component/card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../styles/card.css";
+
+export const Card = () => {
+    return(
+        <div className="card bg-dark text-white" style={{ width: "18rem" }}>
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzU2YTY2OTgtZGZjZi00MTAyLThlYjUtMWM5ZmYzOGEyOWJhXkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_FMjpg_UX1000_.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+    );
+};
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Card } from "../component/card";
 
 import "../../styles/home.css";
 
@@ -10,46 +11,8 @@ export const Home = () => {
     <div className="text-center mt-5" id="home">
       <h1 id="heading">Game Library</h1>
       <div className="container d-flex gap-5 ">
-        <div className="card " style={{ width: "18rem" }}>
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzU2YTY2OTgtZGZjZi00MTAyLThlYjUtMWM5ZmYzOGEyOWJhXkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_FMjpg_UX1000_.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-        <div className="card " style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-        <div className="card " style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-        <div className="card " style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
+        <Card />
+        
       </div>
     </div>
   );
