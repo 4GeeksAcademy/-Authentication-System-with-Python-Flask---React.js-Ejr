9be1d4010fbbf4160e68b386e60097d69162a1e9
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-08-25 18:40:55 +0000

    Login-Register Responsive, Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,13 +1,11 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../styles/navbar.css";
-
 import logo_bebe from "../../img/logo_bebe.png";
 import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-
-  const { actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -15,9 +13,15 @@ export const Navbar = () => {
     navigate('/login'); // Redirige a la página de login
   };
 
+  if (!store.token) {
+    // Si no hay token, no renderiza la Navbar
+    return null;
+  }
+
   return (
     <nav className="navbar navbar-dark fixed-top" style={{ backgroundColor: '#075E81' }}>
       <div className="container-fluid d-flex justify-content-between align-items-center">
+        {/* Botón del menú hamburguesa */}
         <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <div className="custom-toggler-icon">
             <span className="dot dot1"></span>
@@ -25,34 +29,38 @@ export const Navbar = () => {
             <span className="dot dot3"></span>
           </div>
         </button>
-        <div className="d-flex justify-content-end">
+        {/* Icono de perfil visible solo en pantallas grandes */}
+        <div className="d-none d-md-flex justify-content-end">
           <Link to="/login">
-            <i className="fa-solid fa-circle-user" style={{ fontSize: '30px', color: 'white' }}></i>
+            <i className="fa-solid fa-circle-user profile-icon" style={{ fontSize: '30px', color: 'white' }}></i>
           </Link>
         </div>
         <div className="offcanvas offcanvas-start text-bg-dark offcanvas-custom" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li className="nav-item">
-                <h1 className="titulo-navItem" style={{ color: '#FBEE84' }}><Link to="/blog" style={{ color:'inherit', textDecoration: 'none' }}>Blog</Link></h1>
+                <h1 className="titulo-navItem" style={{ color: '#FBEE84' }}>
+                  <Link to="/blog" style={{ color: 'inherit', textDecoration: 'none' }}>Blog</Link>
+                </h1>
                 <ul className="no-bullets">
-                  <li><Link to="/recipes" style={{ color:'inherit', textDecoration: 'none' }}>Recipes</Link></li>
-                  <li><Link to="/news" style={{ color:'inherit', textDecoration: 'none' }}>News</Link></li>
+                  <li><Link to="/recipes" style={{ color: 'inherit', textDecoration: 'none' }}>Recipes</Link></li>
+                  <li><Link to="/news" style={{ color: 'inherit', textDecoration: 'none' }}>News</Link></li>
                 </ul>
               </li>
               <li className="nav-item">
-                <h1 className="titulo-navItem"><Link to="/dashboard" style={{ color: '#63DDFC', textDecoration: 'none' }}>Baby</Link></h1>
+                <h1 className="titulo-navItem">
+                  <Link to="/dashboard" style={{ color: '#63DDFC', textDecoration: 'none' }}>Baby</Link>
+                </h1>
                 <ul className="no-bullets">
-                  <li><Link to="/dashboard" style={{ color:'inherit', textDecoration: 'none' }}>Baby's Diary</Link></li>
-                  <li><Link to="/gestor_bebes" style={{ color:'inherit', textDecoration: 'none' }}>Edit Baby's Profile</Link></li>
-                  <li><Link to="/gestor_bebes" style={{ color:'inherit', textDecoration: 'none' }}>Manage Babies</Link></li>
+                  <li><Link to="/dashboard" style={{ color: 'inherit', textDecoration: 'none' }}>Baby's Diary</Link></li>
+                  <li><Link to="/gestor_bebes" style={{ color: 'inherit', textDecoration: 'none' }}>Manage Babies</Link></li>
                 </ul>
               </li>
               <li className="nav-item">
-                <h1 className="titulo-navItem" style={{ color: '#B4E49D' }}>**Profile</h1>
+                <h1 className="titulo-navItem" style={{ color: '#B4E49D' }}>Profile</h1>
                 <ul className="no-bullets">
-                  <li> <Link to="/gestor_perfil" style={{ color: 'inherit', textDecoration: 'none' }}>**See profile</Link></li>
-                  <li>**Edit profile</li>
+                  <li><Link to="/gestor_perfil" style={{ color: 'inherit', textDecoration: 'none' }}>See profile</Link></li>
+                  <li>Edit profile</li>
                   <li><button className="btn btn-link" onClick={handleLogout} style={{ color: 'inherit', textDecoration: 'none' }}>Log Out</button></li>
                 </ul>
               </li>
