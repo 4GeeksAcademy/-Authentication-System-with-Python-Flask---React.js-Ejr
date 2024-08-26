2470b0ba948c79759da57d81effa66f967b27e57
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-08-26 09:46:43 +0000

    arreglando models

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     is_teacher = db.Column(db.Boolean, default=False)
@@ -24,14 +24,14 @@ class User(db.Model):
     
 class Profesor(db.Model):
     __tablename__ = "profesor"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), nullable=True)
     lastname = db.Column(db.String(120),nullable=True)
-    telefono = db.Column(db.Integer(120), nullable=True)
+    telefono = db.Column(db.Integer(), nullable=True)
     address = db.Column(db.String(120),nullable=True)
-    #añadir linea de codigo donde va la foto del usuario
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
-    relationship_user = db.relationship('User',back_populates ='profesor', lazy=True)
+    avatar=db.Column(db.String(250))
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id')) 
+    relationship_user = db.relationship('User',backref ='profesor', lazy=True)
 
     def serialize(self):
         return {
@@ -46,14 +46,14 @@ class Profesor(db.Model):
 
 class Alumno(db.Model):
     __tablename__ = "alumno"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(120), nullable=True)
     lastname = db.Column(db.String(120),nullable=True)
-    telefono = db.Column(db.Integer(120), nullable=True)
+    telefono = db.Column(db.Integer(), nullable=True)
     address = db.Column(db.String(120),nullable=True)
     #añadir linea de codigo donde va la foto del usuario
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
-    relationship_user = db.relationship('User',back_populates ='alumno', lazy=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id')) 
+    relationship_user = db.relationship('User',backref ='alumno', lazy=True)
 
     def serialize(self):
         return {
@@ -67,19 +67,19 @@ class Alumno(db.Model):
 
 class Curso(db.Model):
     __tablename__ = "curso"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    #añadir linea de codigo donde va la imagen del curso
+    portada=db.Column(db.String(120), nullable=False)
     resumen = db.Column(db.String(120), nullable=True)
     categoria = db.Column(db.String(120), nullable=True)
-    valoraciones = db.Column(db.Integer, nullable=True)
+    valoraciones = db.Column(db.Integer(), nullable=True)
     niveles = db.Column(db.String(120), nullable=True)
-    precio = db.Column(db.Integer, nullable=True)
+    precio = db.Column(db.Integer(), nullable=True)
     fecha_inicio = db.Column(db.String(120), nullable=True)#no se si es String u otro tipo
     idioma = db.Column(db.String(120), nullable=True)
     modulos = db.Column(db.String(120),nullable=True)
-    profesor_id = db.Column(db.Integer, db.ForeignKey('profesor.id')) 
-    relationship_profesor = db.relationship('Profesor',back_populates ='curso', lazy=True)
+    profesor_id = db.Column(db.Integer(), db.ForeignKey('profesor.id')) 
+    relationship_profesor = db.relationship('Profesor',backref ='curso', lazy=True)
 
     def serialize(self):
         return {
@@ -96,14 +96,30 @@ class Curso(db.Model):
             "profesor_id": self.profesor_id,
             #añadir linea de codigo donde va la imagen del curso
         }
+    
+class Videos(db.Model) :
+    __tablename__ = "videos"
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(120), nullable=True)
+    url = db.Column(db.String(120), nullable=True)
+    text = db.Column(db.String(250), nullable=False)
+    curso_id = db.Column(db.Integer(), db.ForeignKey('curso.id'))
+    relationship_contenido = db.relationship('Curso',backref ='contenido', lazy=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "resumen": self.resumen,
+        }
 
 class Matricula(db.Model):
     __tablename__ = "matricula"
-    id = db.Column(db.Integer, primary_key=True)
-    curso_id = db.Column(db.Integer, db.ForeignKey('curso.id'))
-    relationship_user = db.relationship('Curso',back_populates ='matricula', lazy=True) 
-    alumno_id = db.Column(db.Integer, db.ForeignKey('alumno.id'))
-    relationship_user = db.relationship('Alumno',back_populates ='matricula', lazy=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    curso_id = db.Column(db.Integer(), db.ForeignKey('curso.id'))
+    relationship_curso = db.relationship('Curso',backref ='matricula', lazy=True) 
+    alumno_id = db.Column(db.Integer(), db.ForeignKey('alumno.id'))
+    relationship_alumno = db.relationship('Alumno',backref ='matricula', lazy=True)
 
     def serialize(self):
         return {
@@ -115,11 +131,11 @@ class Matricula(db.Model):
 class Pagos(db.Model):
     __tablename__ = "pagos"
     id = db.Column(db.Integer, primary_key=True)
-    matricula_id = db.Column(db.Integer, db.ForeignKey('matricula.id'))
-    relationship_user = db.relationship('Curso',back_populates ='matricula', lazy=True) 
-    alumno_id = db.Column(db.Integer, db.ForeignKey('alumno.id'))
-    relationship_user = db.relationship('Alumno',back_populates ='matricula', lazy=True)
-    profesor_id = db.Column(db.Integer, db.ForeignKey('profesor.id')) 
+    matricula_id = db.Column(db.Integer(), db.ForeignKey('matricula.id'))
+    relationship_curso = db.relationship('Curso',back_populates ='matricula', lazy=True) 
+    alumno_id = db.Column(db.Integer(), db.ForeignKey('alumno.id'))
+    relationship_alumno = db.relationship('Alumno',back_populates ='matricula', lazy=True)
+    profesor_id = db.Column(db.Integer(), db.ForeignKey('profesor.id')) 
     relationship_profesor = db.relationship('Profesor',back_populates ='matricula', lazy=True)
     fecha_pago = db.Column(db.String(120), nullable=True)#no se si es String u otro tipo
 
