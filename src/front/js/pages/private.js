080bf08281310f9c6a47c0dd42840a0c8c37cee8
Dário Duarte <darioduarte1991@gmail.com>
2024-08-27 15:15:18 +0000

    Part 4 - Alteracion visual del css del componente

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import "../../styles/private.css";
 
 export const Private = () => {
     const navigate = useNavigate();
 
     const goToHomeAndReload = () => {
-        navigate('/');  // Navega a Home
-        window.location.reload(); // Fuerza la recarga de la página
+        navigate('/');
+        window.location.reload();
     };
 
     return (
-        <div>
-            <h1>Ya estás Logueado</h1>
-            <button onClick={goToHomeAndReload}>Ir a Home</button>
+        <div className="private-body">
+            <div className="private-container">
+                <div className="private-container-left">
+                    <div className="image-container">
+                        <img src="https://cdn.prod.website-files.com/654506076ba62ddb88e187e2/65cf7e29b660bdc3aea4ba26_Untitled-design-14.jpeg" alt="Profile" className="profile-image"/>
+                    </div>
+                    <h1>Ya estás Logueado</h1>
+                </div>
+                <div className="private-container-right">
+                    <form>
+                        <button onClick={goToHomeAndReload}>Ir a Home</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
