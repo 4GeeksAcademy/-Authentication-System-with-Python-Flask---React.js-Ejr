ed0b7f7b3668692146b8d68f3a9b8a61b2d4c125
Stephaniemtoyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-08-28 16:12:07 +0000

    cambios1

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -18,17 +18,18 @@ return (
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <a href="https://example.com/page1" target="_blank" rel="noopener noreferrer">
-                        <img src="https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="First Slide" />
+                        <img src="https://i.blogs.es/78408e/programador/1366_2000.jpeg" className="d-block w-100" alt="First Slide" />
                     </a>
+                    <div className="text-overlay">Aprende, Conecta y Crece</div>
                 </div>
                 <div className="carousel-item">
                     <a href="https://example.com/page2" target="_blank" rel="noopener noreferrer">
-                        <img src="https://via.placeholder.com/800x400.png?text=Second+Slide" className="d-block w-100" alt="Second Slide" />
+                        <img src="https://images.pexels.com/photos/4050315/pexels-photo-4050315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="Second Slide" />
                     </a>
                 </div>
                 <div className="carousel-item">
                     <a href="https://example.com/page3" target="_blank" rel="noopener noreferrer">
-                        <img src="https://via.placeholder.com/800x400.png?text=Third+Slide" className="d-block w-100" alt="Third Slide" />
+                        <img src="https://esden.es/wp-content/uploads/2024/06/lenguajesdeprogramacion-qbit-01.webp" className="d-block w-100" alt="Third Slide" />
                     </a>
                 </div>
             </div>
