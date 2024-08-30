2de221341ac33503b820d3a5cf7d236d1a254b44
Ruperth <97520685+iRuperth@users.noreply.github.com>
2024-08-30 10:56:26 +0000

    cambio verVideo

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,11 +14,12 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 
-import {Login} from "./pages/login";
-import {SignUp} from "./pages/signUp";
+import { Login } from "./pages/login";
+import { SignUp } from "./pages/signUp";
 import Cursos from "./pages/cursos";
+import VerVideo from "./pages/verVideo";
 import VistaProfe from "./pages/vistaProfe"; // importar VistaProfe
-import PrivateRoute from "./component/privadaProfe"; 
+import PrivateRoute from "./component/privadaProfe";
 import PrivateRouteAlumno from "./component/privadaAlumno";
 import VistaAlumno from "./pages/vistaAlumno";
 import VistaPago from "./pages/vistaPago";
@@ -30,7 +31,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <>
@@ -40,21 +41,22 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Login />} path="/login" />
-                        <Route element={<SignUp />} path="/signup"/>
-                        <Route element={<Perfil />} path="/perfil"/>
+                        <Route element={<SignUp />} path="/signup" />
+                        <Route element={<Perfil />} path="/perfil" />
                         <Route element={<Cursos />} path="/cursos" />
+                        <Route element={<VerVideo />} path="/verVideo" />
                         <Route element={<About />} path="/about" />
                         <Route element={<Contacto />} path="/contacto" />
                         <Route element={<VistaPago />} path="/vistaPago" />
                         {/* Protege la ruta VistaAlumno con PrivateRouteAlumno, si el store.autentificacion es true,  y store.usuarioPr?.is_teacher es true, puede accerder */}
                         {/*DESCOMENTAR<Route element={<PrivateRouteAlumno vista={VistaAlumno} />} path="/vistaAlumno" />*/}
-                         {/* Protege la ruta VistaProfe con PrivateRoute, si el store.autentificacion es true,  y store.usuarioPr?.is_teacher es true, puede acceder */}
+                        {/* Protege la ruta VistaProfe con PrivateRoute, si el store.autentificacion es true,  y store.usuarioPr?.is_teacher es true, puede acceder */}
                         {/*DESCOMENTAR<Route element={<PrivateRoute vista={VistaProfe} />} path="/vistaProfe" />*/}
                         {/*ELIMINAR LINEA 54 Y 55 AL TERMINAR ESTA RAMA*/}
                         <Route element={<VistaAlumno />} path="/vistaAlumno" />
                         <Route element={<VistaProfe />} path="/vistaProfe" />
                         <Route element={<Vistacurso />} path="/vistacurso" />
-                        <Route element={<h1>Not found!</h1>} path="*" /> 
+                        <Route element={<h1>Not found!</h1>} path="*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
