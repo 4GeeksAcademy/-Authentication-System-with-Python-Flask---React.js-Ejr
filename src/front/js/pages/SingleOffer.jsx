2bb0419e6da50c87f999c553cc61fd1513c4ebc1
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2024-08-30 19:21:40 +0000

    modifies

--- src/front/js/pages/SingleOffer.jsx
+++ src/front/js/pages/SingleOffer.jsx
@@ -83,88 +83,86 @@ export const SingleOffer = () => {
     return (
         <>
             <div className="container my-5">
-                <div className="row">
-                    <div className="col-md-8">
-                        <div className="card single-card single-offer-box mb-4">
-                            <div className="card-body">
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <div className="card-offer-logo">
-                                        <img
-                                            src="https://img.freepik.com/vector-premium/concepto-pequena-empresa-fachada-cafeteria-tiendas-ventas_654623-1161.jpg"
-                                            alt="Company Logo"
-                                        />
-                                    </div>
-                                    <div className="d-flex flex-column offer-header">
-                                        <h2 className="mb-0">{offer.name}</h2>
-                                        <span className="text-muted">
-                                            {offer.fecha_publicacion}
-                                            <span className="ms-3">{offer.localidad}</span>
-                                        </span>
-                                        <div className="salary-box">
-                                            <span className="text-success">{offer.salario}</span>
-                                        </div>
-                                        <span className="text-muted">Número de inscritos: {numeroInscritos}</span>
-                                    </div>
-                                    {isProgramador && (
-                                        <button
-                                            className={`btn ${isSubscribed ? 'btn-desinscribirse' : 'btn-inscribirse'} btn-lg`}
-                                            onClick={handleApplyClick}
-                                        >
-                                            {isSubscribed ? 'Desinscribirse' : 'Inscribirse'}
-                                        </button>
-                                    )}
+                <div className="card single-card single-offer-box mb-4">
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="card-offer-logo">
+                                <img
+                                    src="https://img.freepik.com/vector-premium/concepto-pequena-empresa-fachada-cafeteria-tiendas-ventas_654623-1161.jpg"
+                                    alt="Company Logo"
+                                />
+                            </div>
+                            <div className="d-flex flex-column offer-header">
+                                <h2 className="mb-0">{offer.name}</h2>
+                                <span className="text-muted">
+                                    {offer.fecha_publicacion}
+                                    <span className="ms-3">{offer.localidad}</span>
+                                </span>
+                                <div className="salary-box">
+                                    <span className="text-success">{offer.salario}</span>
                                 </div>
-                                <div className="requisit-list d-flex mt-4">
-                                    <ul className="text-muted">
-                                        <li>{offer.modalidad}</li>
-                                        <li>{offer.experiencia_minima}</li>
-                                        <li>{offer.salario}</li>
-                                        <li>{offer.plazo}</li>
-                                    </ul>
-                                    <ul className="text-muted">
-                                        <li>{offer.idiomas}</li>
-                                        <li>{offer.tipo_contrato}</li>
-                                        <li>{offer.horario}</li>
-                                        <li>{offer.estudios_minimos}</li>
-                                    </ul>
-                                </div>
-                                <hr />
-                                <div className="requisitos-minimos-single-offer">
-                                    <div className="text-secondary fw-bold fs-2 text-decoration-underline">
-                                        <h3>Requisitos Mínimos</h3>
-                                    </div>
-                                    <p className="text-muted text-start">
-                                        {offer.requisitos_minimos}
-                                    </p>
-                                </div>
-                                <hr />
-                                <div className="data-single-offer-container">
-                                    <div className="offer-description mt-3">
-                                        <h5 className="fw-bold">Descripción</h5>
-                                        <p className="single-offer-description">{offer.descripcion}</p>
-                                    </div>
-                                    <div className="sigle-buttons">
-                                    {isProgramador && (
-                                        <button
-                                            className={`btn-inscribirse-up ${isSubscribed ? 'btn-desinscribirse-single' : 'btn-inscribirse-sigle'} btn-lg mt-3`}
-                                            onClick={handleApplyClick}
-                                        >
-                                            {isSubscribed ? 'Desinscribirse' : 'Inscribirse'}
-                                        </button>
-                                    )}
-                                    </div>
-                                </div>
+                                <span className="text-muted">Número de inscritos: {numeroInscritos}</span>
+                            </div>
+                            {isProgramador && (
+                                <button
+                                    className={`btn ${isSubscribed ? 'btn-desinscribirse' : 'btn-inscribirse'} btn-lg`}
+                                    onClick={handleApplyClick}
+                                >
+                                    {isSubscribed ? 'Desinscribirse' : 'Inscribirse'}
+                                </button>
+                            )}
+                        </div>
+                        <div className="requisit-list d-flex mt-4">
+                            <ul className="text-muted">
+                                <li>{offer.modalidad}</li>
+                                <li>{offer.experiencia_minima}</li>
+                                <li>{offer.salario}</li>
+                                <li>{offer.plazo}</li>
+                            </ul>
+                            <ul className="text-muted">
+                                <li>{offer.idiomas}</li>
+                                <li>{offer.tipo_contrato}</li>
+                                <li>{offer.horario}</li>
+                                <li>{offer.estudios_minimos}</li>
+                            </ul>
+                        </div>
+                        <hr />
+                        <div className="requisitos-minimos-single-offer">
+                            <div className="text-secondary fw-bold fs-2 text-decoration-underline">
+                                <h3>Requisitos Mínimos</h3>
+                            </div>
+                            <p className="text-muted text-start">
+                                {offer.requisitos_minimos}
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="data-single-offer-container">
+                            <div className="offer-description mt-3">
+                                <h5 className="fw-bold">Descripción</h5>
+                                <p className="single-offer-description">{offer.descripcion}</p>
+                            </div>
+                            <div className="sigle-buttons">
+                                {isProgramador && (
+                                    <button
+                                        className={`btn-inscribirse-up ${isSubscribed ? 'btn-desinscribirse-single' : 'btn-inscribirse-sigle'} btn-lg mt-3`}
+                                        onClick={handleApplyClick}
+                                    >
+                                        {isSubscribed ? 'Desinscribirse' : 'Inscribirse'}
+                                    </button>
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
+
+
             </div>
             {isModalOpen && (
                 <ModalJobApply
                     message={modalMessage}
                     type={modalType}
                     onClose={handleCloseModal}
-                    showLoginButton={showLoginButton} 
+                    showLoginButton={showLoginButton}
                 />
             )}
         </>
