848165db6e17eccc8aa09b66624ef4d3ed779ac0
MenUrux <jorge.pino@estudiantes.utec.edu.uy>
2024-08-30 18:03:10 +0000

    fixes home and multistep

--- src/front/js/component/homeExercises.js
+++ src/front/js/component/homeExercises.js
@@ -9,7 +9,7 @@ export const HomeExercises = () => {
     return (
         <>
             <h2 className="text-md text-center text-neutral-200 md:text-2xl">Escoge entre una variedad de ejercicios</h2>
-            <div className="overflow-hidden w-11/12 mx-auto">
+            <div className="overflow-hidden w-11/12 mx-auto max-w-[1920px]">
                 <InfiniteSlider />
             </div>
         </>
