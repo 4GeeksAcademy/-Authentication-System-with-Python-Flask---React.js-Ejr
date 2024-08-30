ff52247ffc5cfa735c2c1b0fc18ef7fbf5ffff42
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-08-30 17:26:58 +0000

    algo de limpieza

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,19 +28,6 @@ def get_users():
     else:
         return jsonify({"error": "User not found"}), 404
 
-# @api.route('/users', methods=['POST'])
-# def create_user():
-#     data = request.get_json()
-#     if not data or 'id' not in data or 'name' not in data or 'telefono' not in data:
-#         return jsonify({"error": "Invalid data"}), 400
-#     user_id = data['id']
-#     if User.query.get(user_id):
-#         return jsonify({"error": "User already exists"}), 400
-#     new_user = User(id=user_id, name=data['name'], telefono=data['telefono'])
-#     db.session.add(new_user)
-#     db.session.commit()
-#     return jsonify({"status": "User created", "user": new_user.serialize()}), 201
-
 @api.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     data = request.get_json()
@@ -103,19 +90,6 @@ def get_cursos():
     else:
         return jsonify({"error": "Cursos not found"}), 404
 
-# @api.route('/login', methods=['POST'])
-# def login():
-#     print("Request JSON:", request.json)
-#     email = request.json.get('email')
-#     password = request.json.get('password')
-#     user = User.query.filter_by(email=email).first()
-#     if user:
-#         if user.password == password:
-#             access_token = create_access_token(identity=user.id)
-#             return jsonify({'success': True, 'user': user.serialize(), 'token': access_token}), 200
-#         return jsonify({'success': False, 'msg': 'Combinación usuario/contraseña no es válida'}), 400
-#     return jsonify({'success': False, 'msg': 'El correo electrónico no tiene una cuenta asociada'}), 404
-
 @api.route('/login', methods=['POST'])
 def login():
     data = request.json
@@ -137,30 +111,6 @@ def login():
         }), 200
     
     return jsonify({'success': False, 'msg': 'Combinación usuario/contraseña no es válida'}), 401
-
-# @api.route('/signup', methods=['POST'])
-# def signup():
-
-#     email = request.json.get('email', None)
-#     password = request.json.get('password', None)
-#     type = request.json.get('type', None)
-#     if not email or not password or not type or type is None:
-#         return jsonify({'success': False, 'msg': 'Todos los campos son necesarios'}), 400
-#     user = User.query.filter_by(email=email).first()
-#     if user:
-#         return jsonify({'success': False, 'msg': 'El correo electrónico ya tiene una cuenta, intenta iniciar sesión'}), 400
-#     new_user = User(email=email, password=password, is_active=True)
-#     db.session.add(new_user)
-#     db.session.commit()
-#     access_token = create_access_token(identity=new_user.id)
-#     if type == 'alumno':
-#         #crear registro en tabla de alumno con este id como user
-#         print(type)
-#         return jsonify({'success': True, 'user': new_user.serialize(), 'token': access_token}), 200
-#     if type== 'profesor':
-#         #crear registro en tabla de profesor con este id como user
-#         print(type)
-#         return jsonify({'success': True, 'user': new_user.serialize(), 'token': access_token}), 200
 
 @api.route('/signup', methods=['POST'])
 def create_user2():
