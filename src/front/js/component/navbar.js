417800e6d845c7916aa1892e7bafb70624960e90
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-08-31 01:20:50 +0000

    added more actions in flux and integrated 3rd party apim updated cards and design of page. removed categories from navbar and added to page to gilter through games

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-
-    <div class="main-div">
+    <div className="main-div">
       <h1>ThunderCats</h1>
+<<<<<<< HEAD
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="left-div">
           <div class="container-fluid">
@@ -15,13 +15,42 @@ export const Navbar = () => {
             </button>
             <div class="dropdown" data-bs-theme="dark">
               <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
+=======
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> 
+        <div className="container-fluid">
+          
+          <div
+            className="left-div"
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
+            <a
+              className="navbar-brand"
+              href="#"
+              style={{ marginRight: '15px' }}
+            >
+              Home
+            </a>
+            <div
+              className="dropdown"
+              data-bs-theme="dark"
+              style={{ marginRight: '15px' }}
+            >
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButtonDark"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+>>>>>>> 550832f ("second commit")
                 Categories
               </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
-                <li><a class="dropdown-item" id="actionGames" href="#">Action</a></li>
-                <li><a class="dropdown-item" id="roleplayingGames" href="#">RPG</a></li>
-                <li><a class="dropdown-item" id="strategy" href="#">Strategy</a></li>
+              <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButtonDark">
+                <li><a className="dropdown-item" id="actionGames" href="#">Action</a></li>
+                <li><a className="dropdown-item" id="roleplayingGames" href="#">RPG</a></li>
+                <li><a className="dropdown-item" id="strategy" href="#">Strategy</a></li>
               </ul>
+<<<<<<< HEAD
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
@@ -34,19 +63,32 @@ export const Navbar = () => {
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
           </ul>
+=======
+            </div>
+            <a
+              className="nav-link"
+              href="#"
+              style={{ marginRight: '15px' }}
+            >
+              Favorites
+            </a>
+          </div>
+          
+          
+          <div className="right-div" style={{ display: 'flex', alignItems: 'center' }}>
+            <a
+              href="#"
+              style={{ marginRight: '15px' }}
+            >
+              <span className="glyphicon glyphicon-user"></span> Sign Up
+            </a>
+            <a href="#">
+              <span className="glyphicon glyphicon-log-in"></span> Login
+            </a>
+          </div>
+>>>>>>> 550832f ("second commit")
         </div>
       </nav>
     </div>
   );
 };
-//<div class="main_div">
-//<div class="left-div">
-//<button>Home</button>
-// <button>Categories</button>
-////<button>Favorites</button>
-//</div>
-//<div class="right-div">
-//  <button>Login</button>
-// <button>signup</button>
-//</div>
-//</div>
