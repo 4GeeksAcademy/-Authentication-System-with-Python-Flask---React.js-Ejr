6dac09bc89aa1005f1e6b9de2867cc0451851684
Sergio Arribas <arribastellezsergio@gmail.com>
2024-09-02 19:09:44 +0000

    [initial] fix style code according best practices

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
