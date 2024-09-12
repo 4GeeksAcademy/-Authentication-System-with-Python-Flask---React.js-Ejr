cdea69a03997b66ec28a0b55d591f3a53a83a480
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-09-12 23:06:32 +0000

    cards complete

---
+++ src/front/js/pages/genre_selection.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../../styles/genre_selection.css"
+
+import logo from "../../img/logo-marca.png"
+import sport from "../../img/material-symbols_sports-score.png"
+import strategy from "../../img/ph_horse-fill.png"
+import rpg from "../../img/solar_shield-bold-duotone.png"
+import action from "../../img/ph_sword-bold.png"
+import adventure from "../../img/material-symbols-light_map.png"
+export const GenreSelection = () => {
+    return (
+    <div className="d-flex flex-column min-vh-100" style={{backgroundColor: "#16171C", color: "#fff"}}>
+        <img src={logo} alt="Logo" style={{width: '20%', height: '80px', margin: '10px'}} />
+    <div className="container text-center my-auto">
+        <div className="row justify-content-center align-items-center">
+            <h2 className="mb-5 fw-bold" >Favorite Game Genres</h2>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={action} alt="Acción" style={{width: '24%', height: '70px', objectFit: 'cover'}} />
+                    <p>Acción</p>
+                </div>
+            </div>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={adventure} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <p>Adventure</p>
+                </div>
+            </div>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={rpg} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <p>RPG</p>
+                </div>
+            </div>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={strategy} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <p>Strategy</p>
+                </div>
+            </div>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={sport} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <p>Sports</p>
+                </div>
+            </div>
+            <div className="col-md-4 mb-3">
+                <div className="genre-card">
+                    <img src={sport} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <p>Shooter</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer className="text-center py-3 my-4">
+        <button className="btn-prev"><i class="fa-solid fa-arrow-left me-2"></i>Back</button>
+        <button className="btn-next">Continue</button>
+    </footer>
+    </div>
+);
+}
