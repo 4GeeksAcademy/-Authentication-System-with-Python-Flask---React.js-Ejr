3027d67abdda503bf21a5920be7349947d87e8a6
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-09-13 00:34:12 +0000

    genre cards

--- src/front/js/pages/genre_selection.jsx
+++ src/front/js/pages/genre_selection.jsx
@@ -7,10 +7,11 @@ import strategy from "../../img/ph_horse-fill.png"
 import rpg from "../../img/solar_shield-bold-duotone.png"
 import action from "../../img/ph_sword-bold.png"
 import adventure from "../../img/material-symbols-light_map.png"
+import shooter from "../../img/Vector.png"
 export const GenreSelection = () => {
     return (
     <div className="d-flex flex-column min-vh-100" style={{backgroundColor: "#16171C", color: "#fff"}}>
-        <img src={logo} alt="Logo" style={{width: '20%', height: '80px', margin: '10px'}} />
+        <img src={logo} alt="Logo" style={{width: '40%', height: '80px', margin: '10px', objectFit: "contain"}} />
     <div className="container text-center my-auto">
         <div className="row justify-content-center align-items-center">
             <h2 className="mb-5 fw-bold" >Favorite Game Genres</h2>
@@ -28,13 +29,13 @@ export const GenreSelection = () => {
             </div>
             <div className="col-md-4 mb-3">
                 <div className="genre-card">
-                    <img src={rpg} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <img src={rpg} alt="Acción" style={{width: '20%', height: '70px', objectFit: 'cover'}} />
                     <p>RPG</p>
                 </div>
             </div>
             <div className="col-md-4 mb-3">
                 <div className="genre-card">
-                    <img src={strategy} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                    <img src={strategy} alt="Acción" style={{width: '20%', height: '70px', objectFit: 'cover'}} />
                     <p>Strategy</p>
                 </div>
             </div>
@@ -45,17 +46,19 @@ export const GenreSelection = () => {
                 </div>
             </div>
             <div className="col-md-4 mb-3">
-                <div className="genre-card">
-                    <img src={sport} alt="Acción" style={{width: '30%', height: '70px', objectFit: 'cover'}} />
+                <div className="genre-card selected">
+                    <img src={shooter} alt="Acción" style={{width: '23%', height: '70px', objectFit: 'cover'}} />
                     <p>Shooter</p>
                 </div>
             </div>
         </div>
     </div>
     <footer className="text-center py-3 my-4">
-        <button className="btn-prev"><i class="fa-solid fa-arrow-left me-2"></i>Back</button>
-        <button className="btn-next">Continue</button>
-    </footer>
+    <div className="d-flex justify-content-center gap-2 flex-wrap">
+        <button className="btn btn-prev"><i className="fa-solid fa-arrow-left me-2"></i>Back</button>
+        <button className="btn btn-next">Continue</button>
+    </div>
+</footer>
     </div>
 );
 }
