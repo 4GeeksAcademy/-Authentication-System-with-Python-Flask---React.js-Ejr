369fdae1d268901394d260d3ad99965c008e0824
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-09-13 21:16:02 +0000

    age complete

--- src/front/js/pages/age_verification.jsx
+++ src/front/js/pages/age_verification.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import logo from "../../img/logo/logo-marca.png"
 
 export const AgeVerification = () => {
     return(
-        <div>
-            AgeVerification
+        <div className="d-flex flex-column min-vh-100 pb-3" style={{backgroundColor: "#16171C", color: "#fff"}}>
+            <img  src={logo} alt="Logo" style={{width: '40%', height: '80px', margin: '10px', objectFit: "contain"}} />
+        <div className="container text-center my-auto">
+            <div className="row justify-content-center align-items-center">
+                <h2 className="mb-5 fw-bold" style={{color:"#8C67F6"}}>GDPR</h2>
+                <div className="col-md-6 mb-3">
+                    <p className="fs-5">
+                    I confirm that I am <b>at least 13 years old</b> and consent to the processing of my data in accordance with the GDPR for the purpose of finding and matching with other players to enjoy gaming together.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <footer className="text-center py-3 my-4">
+        <div className="d-flex justify-content-center gap-2 flex-wrap">
+            <button className="btn btn-prev"><i className="fa-solid fa-arrow-left me-2"></i>Back</button>
+            <button className="btn btn-next">Continue</button>
+        </div>
+    </footer>
         </div>
     )
 }
