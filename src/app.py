7bd1e04ef6215358e2359a5e7724cf5352c11787
zz-ederfs <161877224+zz-ederfs@users.noreply.github.com>
2024-09-17 20:37:13 +0000

    user register endonpoint done

--- src/app.py
+++ src/app.py
@@ -10,6 +10,9 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 # from models import Person
 
@@ -17,7 +20,11 @@ ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.url_map.strict_slashes = False
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
